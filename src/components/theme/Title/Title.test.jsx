74cdd1910c7333c70c92fe2aaa81aeb3d56a31be
Rob Gietema <rob.gietema@gmail.com>
2017-03-29 22:12:20 +0200

    Added jest testrunner.

---
+++ src/components/theme/Title/Title.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Title from './Title';
+
+test('Renders', () => {
+  const component = renderer.create(
+    <Title title="test" />
+  );
+  const json = component.toJSON();
+  expect(json).toMatchSnapshot();
+});
