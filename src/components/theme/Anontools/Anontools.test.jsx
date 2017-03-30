9d0d51d97f331f72628514080d0f491debd11b1f
Rob Gietema <rob.gietema@gmail.com>
2017-03-30 18:59:51 +0200

    Fix jest test runner.

---
+++ src/components/theme/Anontools/Anontools.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Anontools from './Anontools';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+const mockStore = configureStore();
+
+describe('Anontools', () => {
+  it('renders an anontools component when no token is specified', () => {
+    const store = mockStore({ userSession: { token: null } });
+    const component = renderer.create(
+      <Provider store={store}>
+        <Anontools />
+      </Provider>
+    );
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+
+  it('should not render an anontools component when a token is specified', () => {
+    const store = mockStore({ userSession: { token: '1234' } });
+    const component = renderer.create(
+      <Provider store={store}>
+        <Anontools />
+      </Provider>
+    );
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+})
