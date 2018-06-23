d6aabc0e4c1b865d4e678690b271162fc5f064f9
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-06-23 13:19:25 +0200

    Add import and JSDocs

--- src/components/theme/Icon/Icon.jsx
+++ src/components/theme/Icon/Icon.jsx
@@ -1,8 +1,23 @@
+/**
+ * Icon component.
+ * @module components/theme/Icon/Icon
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const defaultSize = '100%';
 
+/**
+ * Component to display an Icon.
+ * @function Field
+ * @param {Object} props Component properties.
+ * @param {string} props.name Name source object.
+ * @param {string} props.size Size of the Icon (in px).
+ * @param {string} props.color Color of the Icon.
+ * @param {string} props.className className to add to the component.
+ * @param {string} props.title Title (a11y).
+ * @returns {string} Markup of the component.
+ */
 const Icon = ({ name, size, color, className, title }) => (
   <svg
     xmlns={name.attributes.xmlns}
@@ -15,6 +30,11 @@ const Icon = ({ name, size, color, className, title }) => (
   />
 );
 
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
 Icon.propTypes = {
   name: PropTypes.shape({
     xmlns: PropTypes.string,
@@ -27,6 +47,11 @@ Icon.propTypes = {
   title: PropTypes.string,
 };
 
+/**
+ * Default properties.
+ * @property {Object} defaultProps Default properties.
+ * @static
+ */
 Icon.defaultProps = {
   size: defaultSize,
   color: null,
