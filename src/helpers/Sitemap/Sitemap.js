2188edc7ef637c9dcf6fc1b098ee0f82a81e6754
Rob Gietema <rob.gietema@gmail.com>
2018-06-24 14:43:48 +0200

    Fix linting error.

--- src/helpers/Sitemap/Sitemap.js
+++ src/helpers/Sitemap/Sitemap.js
@@ -39,7 +39,7 @@ export const generateSitemap = () =>
         const result = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n${items.join(
           '\n',
         )}\n</urlset>`;
-        zlib.gzip(new Buffer(result, 'utf8'), (err, buffer) => {
+        zlib.gzip(Buffer.from(result, 'utf8'), (err, buffer) => {
           resolve(buffer);
         });
       }
