2c0e793c5ff9b676aaa3b375aea298627cad2953
Rob Gietema <rob.gietema@gmail.com>
2018-09-19 22:35:40 +0200

    Set plone as default.

--- src/config/index.js
+++ src/config/index.js
@@ -43,7 +43,7 @@ export default defaults(
   {
     host: 'localhost',
     port: '4300',
-    // apiPath: 'http://localhost:8080/Plone', // for Plone
-    apiPath: 'http://localhost:8081/db/web', // for guillotina
+    apiPath: 'http://localhost:8080/Plone', // for Plone
+    // apiPath: 'http://localhost:8081/db/web', // for guillotina
   },
 );
