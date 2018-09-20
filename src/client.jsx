e06eeb8cf8fc23ceca151ef6bb307ea2c21d16fa
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-09-20 15:18:35 +0200

    Transfer client and server to Plone-React

--- src/client.jsx
+++ src/client.jsx
@@ -1,42 +1,41 @@
-import 'babel-polyfill';
-
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-intl-redux';
+import configureStore from './store';
+import { ConnectedRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import { Api, persistAuthToken } from './helpers';
 import { ReduxAsyncConnect } from 'redux-connect';
-import { browserHistory, Router } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
 import nlLocaleData from 'react-intl/locale-data/nl';
 import deLocaleData from 'react-intl/locale-data/de';
 import enLocaleData from 'react-intl/locale-data/en';
 import { addLocaleData } from 'react-intl';
+
+import routes from '~/routes';
+
 import 'semantic-ui-less/semantic.less';
 
-import configureStore from './store';
-import getRoutes from './routes';
-import { Api, persistAuthToken } from './helpers';
-import config from './config';
+export default function client() {
+  const history = createBrowserHistory();
+  const api = new Api();
 
-const api = new Api();
-const initialState = window.__data; // eslint-disable-line no-underscore-dangle
-const store = configureStore(initialState, undefined, false, api);
-const history = syncHistoryWithStore(browserHistory, store);
-addLocaleData([...nlLocaleData, ...deLocaleData, ...enLocaleData]);
-persistAuthToken(store);
+  const store = configureStore(window.__PRELOADED_STATE__, history, api);
+  addLocaleData([...nlLocaleData, ...deLocaleData, ...enLocaleData]);
+  persistAuthToken(store);
 
-hydrate(
-  <Provider store={store} key="provider">
-    <Router
-      basename={config.publicUrl}
-      render={props => <ReduxAsyncConnect helpers={{ api }} {...props} />}
-      history={history}
-    >
-      {getRoutes(store)}
-    </Router>
-  </Provider>,
-  document.getElementById('main'),
-);
+  hydrate(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <BrowserRouter>
+          <ReduxAsyncConnect routes={routes} helpers={api} />
+        </BrowserRouter>
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('main'),
+  );
 
-if (module.hot) {
-  module.hot.accept();
+  if (module.hot) {
+    module.hot.accept();
+  }
 }
