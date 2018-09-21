2503ff675c3defb57cb7dc50bf59740e574d08bb
nileshgulia1 <nileshgulia@gmail.com>
2018-09-21 12:30:31 +0530

    Fix tests wrapped in router1

--- src/components/manage/Actions/Actions.test.jsx
+++ src/components/manage/Actions/Actions.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
-import Router from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Actions from './Actions';
 
