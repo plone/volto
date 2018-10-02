f8aae3a00a6482ed96615b685aacf91d5d0e0489
Rob Gietema <rob.gietema@gmail.com>
2018-10-02 18:30:34 +0200

    Reformat package.json.

--- babel.js
+++ babel.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "presets": [
-    "razzle/babel",
-    "stage-0"
+  presets: ['razzle/babel', 'stage-0'],
+  plugins: [
+    'transform-decorators-legacy',
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: 'src',
+      },
+    ],
   ],
-  "plugins": [
-    "transform-decorators-legacy",
-    ["babel-plugin-root-import", {
-      "rootPathSuffix": "src"
-    }]
-  ]
 };
