762c1aaad048afcccfb53a2ce3179a2fe55fbec7
ajayns <ajay.ns@outlook.com>
2018-11-11 15:16:49 +0900

    Add HTML tile Edit component

--- src/components/manage/Tiles/HTML/Edit.jsx
+++ src/components/manage/Tiles/HTML/Edit.jsx
@@ -3,12 +3,82 @@
  * @module components/manage/Tiles/HTML/Edit
  */
 
- import React from 'react';
+import React, { Component } from 'react';
 
- const Edit = () => {
-   return (
-     <div>HTML</div>
-   )
- }
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-markup';
+import { Button } from 'semantic-ui-react';
 
- export default Edit;
+import './styles.css';
+
+import { Icon } from '../../../../components';
+import trashSVG from '../../../../icons/delete.svg';
+
+/**
+ * Edit html tile class.
+ * @class Edit
+ * @extends Component
+ */
+export default class Edit extends Component {
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '',
+    }
+  }
+
+  /**
+   * Change html handler
+   * @method onChangeUrl
+   * @param {string} code New value html
+   * @returns {undefined}
+   */
+  onChange(code) {
+    console.log('onChange', code);
+    this.setState({ code });
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      <div
+        onClick={() => this.props.onSelectTile(this.props.tile)}
+        className={[
+          'tile',
+          this.props.selected && 'selected',
+        ]
+          .filter(e => !!e)
+          .join(' ')}
+      >
+        <Editor
+          value={this.state.code}
+          placeholder={`<p>Add some HTML here</p>`}
+          onValueChange={code => this.onChange(code)}
+          highlight={code => highlight(code, languages.html)}
+          padding={8}
+          className="html-editor"
+        />
+        {this.props.selected && (
+          <Button
+            icon
+            basic
+            onClick={() => this.props.onDeleteTile(this.props.tile)}
+            className="tile-delete-button"
+          >
+            <Icon name={trashSVG} size="18px" />
+          </Button>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/manage/Tiles/HTML/View.jsx
+++ src/components/manage/Tiles/HTML/View.jsx
@@ -1,24 +1,19 @@
 /**
- * View image tile.
+ * View html tile.
  * @module components/manage/Tiles/Image/View
  */
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image } from 'semantic-ui-react';
 
 /**
- * View image tile class.
+ * View html tile class.
  * @class View
  * @extends Component
  */
 const View = ({ data }) => (
-  <p
-    className={['tile', 'image', 'align', data.align]
-      .filter(e => !!e)
-      .join(' ')}
-  >
-    <Image src={`${data.url}/@@images/image`} alt="" />
+  <p>
+    <div dangerouslySetInnerHTML={{ __html: data.html }} />
   </p>
 );
 
