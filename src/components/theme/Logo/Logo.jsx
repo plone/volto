7767b02b0ac0a10d3f0bc0a6196cca2267266377
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-29 01:36:36 +0530

    Update Logo.jsx

--- src/components/theme/Logo/Logo.jsx
+++ src/components/theme/Logo/Logo.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import { Image } from 'semantic-ui-react';
 
-import LogoImage from '@plone/volto/components/theme/Logo/Logo.svg';
+import LogoImage from '@plone/volto/src/components/theme/Logo/Logo.svg';
 
 const messages = defineMessages({
   site: {
