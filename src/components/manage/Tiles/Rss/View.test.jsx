e95e9258e93ede586011e57dfc47131a1316384d
nileshgulia1 <nileshgulia@gmail.com>
2019-02-13 17:45:33 +0530

    updated tests

--- src/components/manage/Tiles/Rss/View.test.jsx
+++ src/components/manage/Tiles/Rss/View.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import View from './View';
 
-test('renders a view video component', () => {
+test('renders a view rss component', () => {
   const component = renderer.create(
     <View data={{ url: 'https://www.reddit.com/.rss' }} />,
   );
