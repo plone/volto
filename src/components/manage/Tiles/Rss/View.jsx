cd028afefdd16a09f14f8e013b7c7eaa3232ca5b
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-02-14 18:45:27 +0100

    Fix the request

--- src/components/manage/Tiles/Rss/View.jsx
+++ src/components/manage/Tiles/Rss/View.jsx
@@ -5,8 +5,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Feed, Image } from 'semantic-ui-react';
-import { settings } from '~/config';
+import { Card, Container, Feed, Image } from 'semantic-ui-react';
 import Parser from 'rss-parser';
 
 /**
@@ -15,86 +14,90 @@ import Parser from 'rss-parser';
  * @extends Component
  */
 class View extends Component {
-constructor(props){
-  super(props);
-  this.state = {
-feed: {},
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    data: PropTypes.objectOf(PropTypes.any).isRequired,
   };
-}
 
-componentDidMount() {
-  (async () => {
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   * @constructs WysiwygEditor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: {},
+    };
+  }
+
+  /**
+   * Component did mount
+   * @method componentDidMount
+   * @returns {undefined}
+   */
+  componentDidMount() {
     const parser = new Parser();
-    const myfeed = await parser.parseURL(this.props.data.url);
-    this.setState({
-      feed: myfeed,
-    });
-  })()
-}
+    if (this.props.data.url) {
+      parser.parseURL(this.props.data.url, (err, feed) => {
+        this.setState({
+          feed,
+        });
+      });
+    }
+  }
 
-render(){
-return (
-  <p
-    className={['tile', 'image', 'align', this.props.data.align]
-      .filter(e => !!e)
-      .join(' ')}
-  >
-    {this.props.data.url.match('.rss') ? (
-      <Card>
-        <Feed>
-          {this.state.feed.image && (
-            <Feed.Event>
-              <Image
-                src={
-                  this.state.feed.image.url
-                }
-                alt=""
-              />
-            </Feed.Event>
-          )}
-          <Feed.Event>
-            <Feed.Date>
-              {this.state.feed.lastBuildDate}
-            </Feed.Date>
-          </Feed.Event>
-          <Feed.Event>
-            {this.state.feed.feedUrl}
-          </Feed.Event>
-          {this.state.feed.items && (
-            <Feed.Event>
-              {this.state.feed.items.forEach(item => (
-                <div>
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      Boolean(this.state.feed) && (
+        <p
+          className={['tile', 'image', 'align', this.props.data.align]
+            .filter(e => !!e)
+            .join(' ')}
+        >
+          {this.props.data.url.match('.rss') ? (
+            <Card>
+              <Feed>
+                {this.state.feed.image && (
                   <Feed.Event>
-                    {item.title}
+                    <Image src={this.state.feed.image.url} alt="" />
                   </Feed.Event>
+                )}
+                <Feed.Event>
+                  <Feed.Date>{this.state.feed.lastBuildDate}</Feed.Date>
+                </Feed.Event>
+                <Feed.Event>{this.state.feed.feedUrl}</Feed.Event>
+                {this.state.feed.items && (
                   <Feed.Event>
-                    {item.author}
+                    {this.state.feed.items.forEach(item => (
+                      <div>
+                        <Feed.Event>{item.title}</Feed.Event>
+                        <Feed.Event>{item.author}</Feed.Event>
+                      </div>
+                    ))}
                   </Feed.Event>
-                </div>
-              ))}
-            </Feed.Event>
+                )}
+              </Feed>
+            </Card>
+          ) : (
+            <Container>
+              <center>Please Enter Correct Rss Url</center>
+            </Container>
           )}
-        </Feed>
-      </Card>
-    ) : (
-        <Container>
-          <center>
-            Please Enter Correct Rss Url
-      </center>
-        </Container>
-      )}
-  </p>
-)
+        </p>
+      )
+    );
+  }
 }
-}
-
-/**
- * Property types.
- * @property {Object} propTypes Property types.
- * @static
- */
-View.propTypes = {
-  data: PropTypes.objectOf(PropTypes.any).isRequired,
-};
 
 export default View;
