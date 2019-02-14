9d45495534b7fcd30f51bd8c3907d0048c9f16e6
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-02-14 19:30:05 +0100

    Fix tests

--- src/components/manage/Tiles/Maps/View.test.jsx
+++ src/components/manage/Tiles/Maps/View.test.jsx
@@ -4,7 +4,12 @@ import View from './View';
 
 test('renders a view map component', () => {
   const component = renderer.create(
-    <View data={{ url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.1588325439866!2d14.375751314853806!3d40.62637905078705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133b9ead3c50b6e3%3A0x8269b49eeba4177d!2sHotel+Sorrento+City!5e0!3m2!1sen!2sin!4v1549980037453' }} />,
+    <View
+      data={{
+        url:
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.7835278268726!2d14.38842915203974!3d40.634655679238854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133b994881d943cb%3A0x6ab93db57d3272f0!2sHotel+Mediterraneo+Sorrento!5e0!3m2!1sen!2ses!4v1550168740166" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>',
+      }}
+    />,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
