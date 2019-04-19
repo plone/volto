be8ed66d483227fc65d7eebf3bd032733906b7d1
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-04-19 10:56:43 +0200

    Add automatic setup/teardown for every cypress test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,43 @@
+import xmlrpc from 'xmlrpc';
+
 beforeEach(function() {
-  cy.log('I run before every test in every spec file!!!!!!');
+  cy.log('Setup API fixture');
+
+  // create a client
+  const client = xmlrpc.createClient({
+    host: 'localhost',
+    port: 55001,
+    path: '/plone/RobotRemote',
+  });
+
+  // Setup site
+  client.methodCall(
+    'run_keyword',
+    [
+      'remote_zodb_setup',
+      ['plone.app.robotframework.testing.PLONE_ROBOT_TESTING'],
+    ],
+    () => {},
+  );
+});
+
+afterEach(function() {
+  cy.log('Tearing down API fixture');
+
+  // create a client
+  const client = xmlrpc.createClient({
+    host: 'localhost',
+    port: 55001,
+    path: '/plone/RobotRemote',
+  });
+
+  // Tearing down
+  client.methodCall(
+    'run_keyword',
+    [
+      'remote_zodb_teardown',
+      ['plone.app.robotframework.testing.PLONE_ROBOT_TESTING'],
+    ],
+    () => {},
+  );
 });
