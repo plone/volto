32ea6a505816ec1e2b33e4bb5813b61a41764590
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-06-20 10:47:46 +0200

    Undo import reordering (keep it simple)

--- src/config/Tiles.jsx
+++ src/config/Tiles.jsx
@@ -1,27 +1,27 @@
 import { defineMessages } from 'react-intl';
 
-import ViewDescriptionTile from '@plone/volto/components/manage/Tiles/Description/View';
-import ViewHeroImageLeftTile from '@plone/volto/components/manage/Tiles/HeroImageLeft/View';
-import ViewHTMLTile from '@plone/volto/components/manage/Tiles/HTML/View';
-import ViewImageTile from '@plone/volto/components/manage/Tiles/Image/View';
-import ViewListingTile from '@plone/volto/components/manage/Tiles/Listing/View';
-import ViewMapTile from '@plone/volto/components/manage/Tiles/Maps/View';
-import ViewTextTile from '@plone/volto/components/manage/Tiles/Text/View';
 import ViewTitleTile from '@plone/volto/components/manage/Tiles/Title/View';
+import ViewDescriptionTile from '@plone/volto/components/manage/Tiles/Description/View';
+import ViewTextTile from '@plone/volto/components/manage/Tiles/Text/View';
+import ViewListingTile from '@plone/volto/components/manage/Tiles/Listing/View';
+import ViewImageTile from '@plone/volto/components/manage/Tiles/Image/View';
 import ViewVideoTile from '@plone/volto/components/manage/Tiles/Video/View';
+import ViewHeroImageLeftTile from '@plone/volto/components/manage/Tiles/HeroImageLeft/View';
+import ViewMapTile from '@plone/volto/components/manage/Tiles/Maps/View';
+import ViewHTMLTile from '@plone/volto/components/manage/Tiles/HTML/View';
 
+import EditTitleTile from '@plone/volto/components/manage/Tiles/Title/Edit';
 import EditDescriptionTile from '@plone/volto/components/manage/Tiles/Description/Edit';
-import EditHeroImageLeftTile from '@plone/volto/components/manage/Tiles/HeroImageLeft/Edit';
-import EditHTMLTile from '@plone/volto/components/manage/Tiles/HTML/Edit';
+import EditTextTile from '@plone/volto/components/manage/Tiles/Text/Edit';
 import EditImageTile from '@plone/volto/components/manage/Tiles/Image/Edit';
 import EditListingTile from '@plone/volto/components/manage/Tiles/Listing/Edit';
-import EditMapTile from '@plone/volto/components/manage/Tiles/Maps/Edit';
-import EditTextTile from '@plone/volto/components/manage/Tiles/Text/Edit';
-import EditTitleTile from '@plone/volto/components/manage/Tiles/Title/Edit';
 import EditVideoTile from '@plone/volto/components/manage/Tiles/Video/Edit';
+import EditHeroImageLeftTile from '@plone/volto/components/manage/Tiles/HeroImageLeft/Edit';
+import EditMapTile from '@plone/volto/components/manage/Tiles/Maps/Edit';
+import EditHTMLTile from '@plone/volto/components/manage/Tiles/HTML/Edit';
 
-import codeSVG from '@plone/volto/icons/code.svg';
 import globeSVG from '@plone/volto/icons/globe.svg';
+import codeSVG from '@plone/volto/icons/code.svg';
 import heroSVG from '@plone/volto/icons/hero.svg';
 import listBulletSVG from '@plone/volto/icons/list-bullet.svg';
 
