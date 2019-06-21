beec12538b69b75f3c00fb4d2f8263b90e9b38f5
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-06-21 11:39:50 +0200

    Return to the master version of the Edit view

--- src/components/manage/Tiles/Image/View.jsx
+++ src/components/manage/Tiles/Image/View.jsx
@@ -5,65 +5,42 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import cx from 'classnames';
 import { settings } from '~/config';
 import ImageZoom from 'react-medium-image-zoom';
 
-import { flattenToAppURL } from '@plone/volto/helpers';
+import { flattenToAppURL } from '../../../../helpers';
 
 /**
  * View image tile class.
  * @class View
  * @extends Component
  */
-const View = ({ data, detached }) => (
+const View = ({ data }) => (
   <p
     className={cx(
       'tile image align',
       {
         center: !Boolean(data.align),
-        detached,
       },
       data.align,
     )}
   >
-    {data.url && (
-      <>
-        {(() => {
-          const image = (
-            <img
-              src={
-                data.url.includes(settings.apiPath)
-                  ? `${flattenToAppURL(data.url)}/@@images/image`
-                  : data.url
-              }
-              alt={data.alt || ''}
-            />
-          );
-          if (data.external) {
-            const isReallyExternal =
-              (data.external.startsWith('http') ||
-                data.external.startsWith('https')) &&
-              !data.external.includes(settings.apiPath);
-
-            if (isReallyExternal) {
-              return <a href={data.external}>{image}</a>;
-            } else {
-              return (
-                <Link to={data.external.replace(settings.apiPath, '')}>
-                  {image}
-                </Link>
-              );
-            }
-          } else if (data.href) {
-            return <Link to={data.href}>{image}</Link>;
-          } else {
-            return image;
-          }
-        })()}
-      </>
-    )}
+    <ImageZoom
+      image={{
+        src: data.url.startsWith(settings.apiPath)
+          ? `${flattenToAppURL(data.url)}/@@images/image`
+          : data.url,
+        alt: '',
+        className: 'ui image',
+      }}
+      zoomImage={{
+        src: data.url.startsWith(settings.apiPath)
+          ? `${flattenToAppURL(data.url)}/@@images/image`
+          : data.url,
+        alt: '',
+      }}
+    />
   </p>
 );
 
