f3095e037a84123d59feb9f637e4b4e381e69389
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-09-22 19:40:28 +0200

    Add missing BrowserRouter import.

--- src/start-client.jsx
+++ src/start-client.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-intl-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
