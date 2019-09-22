732664dd3fa2ba5ccf5ad2b44ada4d4db7056e19
nileshgulia1 <nileshgulia@gmail.com>
2019-09-22 20:28:37 +0530

    added more conditionals for weekdays(reccursive view)

--- src/components/theme/View/Recurrence.jsx
+++ src/components/theme/View/Recurrence.jsx
@@ -4,15 +4,15 @@ import { List } from 'semantic-ui-react';
 import moment from 'moment';
 import { datesForDisplay } from './EventWhen';
 
-const Recurrence = props => {
+const findRecurrence = props => {
   const reccDates = [];
   const arr = props.recurrence.split(';');
+  const days = arr[1].substring(arr[1].indexOf('=') + 1, arr[1].length);
   const num = arr[arr.length - 1];
   const count = num.match(/[0-9]/g);
   const newDates = datesForDisplay(props.start, props.end);
   if (arr[0].includes('DAILY')) {
     let firstDate = moment(newDates.startDate);
-
     for (let i = 1; i <= parseInt(count); i++) {
       reccDates.push(
         firstDate
@@ -22,6 +22,24 @@ const Recurrence = props => {
           .concat(` from ${newDates.startTime} to ${newDates.endTime}`),
       );
     }
+  } else if (arr[0].includes('WEEKLY')) {
+    let firstDate = moment(newDates.startDate);
+    days.split(',').map(item => {
+      reccDates.push(
+        firstDate
+          .day(item)
+          ._d.toString()
+          .substring(0, 15)
+          .concat(` from ${newDates.startTime} to ${newDates.endTime}`),
+      );
+    });
+  }
+  return reccDates;
+};
+
+const Recurrence = props => {
+  const reccDates = findRecurrence(props);
+  if (reccDates.length > 0) {
     return (
       <div>
         <List items={reccDates} />
