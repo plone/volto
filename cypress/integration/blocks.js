9d36ca4ea9781825ac6cc42414af272ed28fa083
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-04 17:35:19 +0200

    Trying to fix the flaky test

--- cypress/integration/blocks.js
+++ cypress/integration/blocks.js
@@ -11,6 +11,14 @@ if (Cypress.env('API') !== 'guillotina') {
         .clear()
         .type('My title');
 
+      // Trying to get rid of the flaky test
+      cy.get('.tile.inner.text .public-DraftEditor-content')
+        .click()
+        .type('My text')
+        .get('span[data-text]')
+        .contains('My text')
+        .clear();
+
       cy.get('#toolbar-save').click();
 
       cy.get('#page-document').should('have.text', 'My title');
