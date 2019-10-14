cb78bc0cdbfa63a05bec4dbecc9e1dd40f3a5bd9
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-10-14 13:33:55 +0200

    Add WaitForResourceToLoad command to Cypress.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -104,3 +104,27 @@ Cypress.Commands.add(
     }
   },
 );
+
+Cypress.Commands.add('waitForResourceToLoad', (fileName, type) => {
+  const resourceCheckInterval = 40;
+
+  return new Cypress.Promise(resolve => {
+    const checkIfResourceHasBeenLoaded = () => {
+      const resource = cy
+        .state('window')
+        .performance.getEntriesByType('resource')
+        .filter(entry => !type || entry.initiatorType === type)
+        .find(entry => entry.name.includes(fileName));
+
+      if (resource) {
+        resolve();
+
+        return;
+      }
+
+      setTimeout(checkIfResourceHasBeenLoaded, resourceCheckInterval);
+    };
+
+    checkIfResourceHasBeenLoaded();
+  });
+});
