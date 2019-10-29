8e2fcd8aa76707c8bcf4924793221423b1014923
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-29 19:50:53 +0100

    Fix G tests for the renaming

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,21 @@
 // --- AUTOLOGIN -------------------------------------------------------------
 Cypress.Commands.add('autologin', () => {
-  let api_url;
+  let api_url, user, password;
   if (Cypress.env('API') === 'guillotina') {
     api_url = 'http://localhost:8081/db/container';
+    user = 'root';
+    password = 'root';
   } else {
     api_url = 'http://localhost:55001/plone';
+    user = 'admin';
+    password = 'secret';
   }
 
   cy.request({
     method: 'POST',
     url: `${api_url}/@login`,
     headers: { Accept: 'application/json' },
-    body: { login: 'admin', password: 'secret' },
+    body: { login: user, password: password },
   }).then(response => cy.setCookie('auth_token', response.body.token));
 });
 
