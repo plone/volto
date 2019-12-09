aa339403fa495d1017433c3d5fb9cc330a09e168
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-12-09 19:00:21 +0100

    Added CTRL+ENTER feature in text blocks by default. It creates a newline inside the same text chunk (`<p>`)

--- src/components/manage/Blocks/Text/Edit.jsx
+++ src/components/manage/Blocks/Text/Edit.jsx
@@ -8,8 +8,9 @@ import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 import { doesNodeContainClick } from 'semantic-ui-react/dist/commonjs/lib';
 import Editor from 'draft-js-plugins-editor';
-import { convertFromRaw, convertToRaw, EditorState } from 'draft-js';
+import { convertFromRaw, convertToRaw, EditorState, RichUtils } from 'draft-js';
 import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
+import isSoftNewlineEvent from 'draft-js/lib/isSoftNewlineEvent';
 import { defineMessages, injectIntl } from 'react-intl';
 import { includes, isEqual } from 'lodash';
 
@@ -191,7 +192,13 @@ class Edit extends Component {
           blockRenderMap={settings.extendedBlockRenderMap}
           blockStyleFn={settings.blockStyleFn}
           placeholder={this.props.intl.formatMessage(messages.text)}
-          handleReturn={() => {
+          handleReturn={e => {
+            if (isSoftNewlineEvent(e)) {
+              this.onChange(
+                RichUtils.insertSoftNewline(this.state.editorState),
+              );
+              return 'handled';
+            }
             if (!this.props.detached) {
               const selectionState = this.state.editorState.getSelection();
               const anchorKey = selectionState.getAnchorKey();
