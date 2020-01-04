0751fe7aaf5d2e48e7f20b9b54ce6da842fb9df3
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-04 22:28:02 +0200

    Use item['@id'] as key instead of iteration index

--- src/components/manage/AnchorPlugin/components/LinkButton/AddLinkForm.jsx
+++ src/components/manage/AnchorPlugin/components/LinkButton/AddLinkForm.jsx
@@ -233,8 +233,8 @@ class AddLinkForm extends Component {
           />
         </div>
         <ul style={{ margin: 0, paddingLeft: '35px' }}>
-          {map(this.props.search, (item, ix) => (
-            <li style={{ padding: '5px' }} key={ix}>
+          {map(this.props.search, item => (
+            <li style={{ padding: '5px' }} key={item['@id']}>
               <button
                 style={{ cursor: 'pointer' }}
                 onClick={e => this.onSelectItem(e, addAppURL(item['@id']))}
