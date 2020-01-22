42ad6424d251c99f39456e58a475677ca6d1e290
Víctor Fernández de Alba <sneridagh@gmail.com>
2020-01-22 10:41:05 +0100

    Fixcollections (#1123)
    * Initial work
    
    * Do not break if the MultipleSelectionWidget field applied is not included in the site vocabulary. This applies to Subject field when the tag value is not there any more.
    
    * Get this change back to the original
    
    * Improve cx class conditional key
    
    * Fix collections on creation when query is empty, fix edit as well.

--- src/components/manage/Toolbar/Toolbar.jsx
+++ src/components/manage/Toolbar/Toolbar.jsx
@@ -418,8 +418,8 @@ class Toolbar extends Component {
                   messages.shrinkToolbar,
                 )}
                 className={cx({
-                  [this.props.content.review_state]:
-                    this.props.content && this.props.content.review_state,
+                  [this.props.content?.review_state]: this.props.content
+                    ?.review_state,
                 })}
                 onClick={this.handleShrink}
               />
