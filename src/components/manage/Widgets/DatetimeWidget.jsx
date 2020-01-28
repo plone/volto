f544082976b6029460f5d21f5a93b2654a0d8780
Nicola Zambello <nzambello@protonmail.com>
2020-01-28 15:44:19 +0100

    DatetimeWidget: worked on pickers and default value

--- src/components/manage/Widgets/DatetimeWidget.jsx
+++ src/components/manage/Widgets/DatetimeWidget.jsx
@@ -3,108 +3,146 @@
  * @module components/manage/Widgets/DatetimeWidget
  */
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Grid, Label } from 'semantic-ui-react';
 import { map } from 'lodash';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 import TimePicker from 'rc-time-picker';
+
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 import 'rc-time-picker/assets/index.css';
 
 /**
- * DatetimeWidget component class.
- * @function DatetimeWidget
- * @returns {string} Markup of the component.
+ * DatetimeWidget component class
+ * @class DatetimeWidget
+ * @extends Component
  */
-const DatetimeWidget = ({
-  id,
-  title,
-  required,
-  description,
-  error,
-  value,
-  onChange,
-  fieldSet,
-}) => {
-  const [focused, setFocused] = useState(false);
-  const [date, setDate] = useState(value ? moment(value) : moment());
-  const [time, setTime] = useState(value ? moment(value) : moment());
+class DatetimeWidget extends Component {
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   * @constructs DatetimeWidget
+   */
+  constructor(props) {
+    super(props);
 
-  const onDateChange = date => {
-    setDate(date);
-    onDateTimeChange(date);
+    this.state = {
+      focused: false,
+      datetime: this.props.value ? moment(this.props.value) : moment(),
+    };
+  }
+
+  /**
+   * Update date storage
+   * @method onDateChange
+   * @param {Object} date updated momentjs Object for date
+   * @returns {undefined}
+   */
+  onDateChange = date => {
+    this.setState(
+      prevState => ({
+        datetime: prevState.datetime.set({
+          year: date.year(),
+          month: date.month(),
+          date: date.date(),
+        }),
+      }),
+      () => this.onDateTimeChange(),
+    );
   };
 
-  const onTimeChange = time => {
-    setTime(time);
-    onDateTimeChange();
+  /**
+   * Update date storage
+   * @method onTimeChange
+   * @param {Object} time updated momentjs Object for time
+   * @returns {undefined}
+   */
+  onTimeChange = time => {
+    this.setState(
+      prevState => ({
+        datetime: prevState.datetime.set({
+          hours: time.hours(),
+          minutes: time.minutes(),
+          seconds: 0,
+        }),
+      }),
+      () => this.onDateTimeChange(),
+    );
   };
 
-  const onDateTimeChange = () => {
-    let newDate = date;
-    newDate.hours(time.hours());
-    newDate.minutes(time.minutes());
-    newDate.seconds(0);
-    console.log(date.toISOString());
-    onChange(id, newDate.toISOString());
+  /**
+   * Update date storage
+   * @method onDateTimeChange
+   * @returns {undefined}
+   */
+  onDateTimeChange = () => {
+    this.props.onChange(this.props.id, this.state.datetime.toISOString());
   };
 
-  return (
-    <Form.Field
-      inline
-      required={required}
-      error={error.length > 0}
-      className={description ? 'help' : ''}
-      id={`${fieldSet || 'field'}-${id}`}
-    >
-      <Grid>
-        <Grid.Row stretched>
-          <Grid.Column width="4">
-            <div className="wrapper">
-              <label htmlFor={`field-${id}`}>{title}</label>
-            </div>
-          </Grid.Column>
-          <Grid.Column width="8">
-            {/* <Input
-              id={`field-${id}`}
-              name={id}
-              type="datetime-local"
-              value={value || ''}
-              onChange={({ target }) =>
-                onChange(id, target.value === '' ? undefined : target.value)
-              }
-            /> */}
-            <SingleDatePicker
-              date={date}
-              onDateChange={date => onDateChange(date)}
-              focused={focused}
-              onFocusChange={({ focused }) => setFocused(focused)}
-              id={id}
-            />
-            <TimePicker
-              defaultValue={time}
-              onChange={time => onTimeChange(time)}
-              showSecond={false}
-            />
-            {map(error, message => (
-              <Label key={message} basic color="red" pointing>
-                {message}
-              </Label>
-            ))}
-          </Grid.Column>
-        </Grid.Row>
-        {description && (
+  /**
+   * Handle SingleDatePicker focus
+   * @method onFocusChange
+   * @param {boolean} focused component focus state.
+   * @returns {undefined}
+   */
+  onFocusChange = ({ focused }) => this.setState({ focused });
+
+  render() {
+    const { id, title, required, description, error, fieldSet } = this.props;
+    const { datetime, focused } = this.state;
+
+    return (
+      <Form.Field
+        inline
+        required={required}
+        error={error.length > 0}
+        className={description ? 'help' : ''}
+        id={`${fieldSet || 'field'}-${id}`}
+      >
+        <Grid>
           <Grid.Row stretched>
-            <Grid.Column stretched width="12">
-              <p className="help">{description}</p>
+            <Grid.Column width="4">
+              <div className="wrapper">
+                <label htmlFor={`field-${id}`}>{title}</label>
+              </div>
+            </Grid.Column>
+            <Grid.Column width="8">
+              <SingleDatePicker
+                date={datetime}
+                onDateChange={this.onDateChange}
+                focused={focused}
+                onFocusChange={this.onFocusChange}
+                id={id}
+              />
+              <TimePicker
+                defaultValue={datetime}
+                onChange={this.onTimeChange}
+                allowEmpty={false}
+                showSecond={false}
+              />
+              {map(error, message => (
+                <Label key={message} basic color="red" pointing>
+                  {message}
+                </Label>
+              ))}
             </Grid.Column>
           </Grid.Row>
-        )}
-      </Grid>
-    </Form.Field>
-  );
-};
+          {description && (
+            <Grid.Row stretched>
+              <Grid.Column stretched width="12">
+                <p className="help">{description}</p>
+              </Grid.Column>
+            </Grid.Row>
+          )}
+        </Grid>
+      </Form.Field>
+    );
+  }
+}
 
 /**
  * Property types.
--- src/components/manage/Widgets/DatetimeWidget.test.jsx
+++ src/components/manage/Widgets/DatetimeWidget.test.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import DatetimeWidget from './DatetimeWidget';
+import moment from 'moment';
 
 test('renders a datetime widget component', () => {
   const component = renderer.create(
-    <DatetimeWidget id="my-field" title="My field" onChange={() => {}} />,
+    <DatetimeWidget
+      id="my-field"
+      title="My field"
+      onChange={() => {}}
+      value={moment('2019-10-21').toISOString()}
+    />,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
