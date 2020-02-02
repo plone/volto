cdfe3322bfb02cccf4e357716b8a1b70ec708d37
Victor Fernandez de Alba <sneridagh@gmail.com>
2020-02-02 10:51:25 +0100

    Add i18n messages

--- src/components/theme/OutdatedBrowser/OutdatedBrowser.jsx
+++ src/components/theme/OutdatedBrowser/OutdatedBrowser.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Message, Container } from 'semantic-ui-react';
+import { FormattedMessage } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { settings } from '~/config';
 
@@ -9,13 +10,21 @@ const OutdatedBrowser = () => {
     settings.notSupportedBrowsers.includes(browserdetect.name) && (
       <Container style={{ marginBottom: '20px' }}>
         <Message negative>
-          <Message.Header>You are using an outdated browser</Message.Header>
+          <Message.Header>
+            <FormattedMessage
+              id="You are using an outdated browser"
+              defaultMessage="You are using an outdated browser"
+            />
+          </Message.Header>
           <p>
-            You are using {`${browserdetect.name} ${browserdetect.version}`}{' '}
-            which is deprecated by its vendor. That means that it does not get
-            security updates and it is not ready for current modern web
-            features, which deteriorates the user experience. Please upgrade to
-            a modern browser.
+            <FormattedMessage
+              id="deprecated_browser_notice_message"
+              defaultMessage="You are using {browsername} {browserversion} which is deprecated by its vendor. That means that it does not get security updates and it is not ready for current modern web features, which deteriorates the user experience. Please upgrade to a modern browser."
+              values={{
+                browsername: browserdetect.name,
+                browserversion: browserdetect.version,
+              }}
+            />
           </p>
         </Message>
       </Container>
