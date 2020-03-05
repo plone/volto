baf2bfe8509496e9994fa5200a5d4f51579adf9f
Alin Voinea <contact@avoinea.com>
2020-03-05 12:20:51 +0200

    Fix: Dynamically get blocksFieldname and blocksLayoutFieldname

--- src/components/manage/Add/Add.jsx
+++ src/components/manage/Add/Add.jsx
@@ -180,10 +180,17 @@ class Add extends Component {
   render() {
     if (this.props.schemaRequest.loaded) {
       const visual = hasBlocksData(this.props.schema.properties);
+      const blocksFieldname = getBlocksFieldname(this.props.schema.properties);
+      const blocksLayoutFieldname = getBlocksLayoutFieldname(
+        this.props.schema.properties,
+      );
+
       const initialBlocks =
-        this.props.schema.properties.blocks?.default || this.initialBlocks;
-      const initialBlocksLayout = this.props.schema.properties.blocks_layout
-        ?.default || { items: this.initialBlocksLayout };
+        this.props.schema.properties[blocksFieldname]?.default ||
+        this.initialBlocks;
+      const initialBlocksLayout = this.props.schema.properties[
+        blocksLayoutFieldname
+      ]?.default || { items: this.initialBlocksLayout };
 
       return (
         <div id="page-add">
