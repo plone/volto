fd1cfa5e5e2070d92d8783f417d40ddf94d64a7b
Víctor Fernández de Alba <sneridagh@gmail.com>
2020-03-28 10:17:59 +0100

    Reenable video blocks cypress tests based on the rework on lazy load branch (#1308)

--- cypress/integration/blocks-video.js
+++ cypress/integration/blocks-video.js
@@ -2,7 +2,8 @@ if (Cypress.env('API') !== 'guillotina') {
   describe('Blocks Tests', () => {
     beforeEach(() => {
       // Wait a bit to previous teardown to complete correctly because Heisenbug in this point
-      // cy.wait(2000);
+      cy.wait(2000);
+      cy.visit('/');
       cy.autologin();
       cy.createContent('Document', 'my-page', 'My Page');
       cy.visit('/my-page/edit');
@@ -13,31 +14,63 @@ if (Cypress.env('API') !== 'guillotina') {
       cy.waitForResourceToLoad('?fullobjects');
     });
 
-    // it('Add Video Block with MP4 Video', () => {
-    //   cy.get(`.block.title [data-contents]`)
-    //     .clear()
-    //     .type('My title');
-    //   cy.get('.block.inner.text .public-DraftEditor-content').click();
-    //   cy.get('.ui.basic.icon.button.block-add-button').click();
-    //   cy.get('.title')
-    //     .contains('Media')
-    //     .click();
-    //   cy.get('.ui.basic.icon.button.video')
-    //     .contains('Video')
-    //     .click();
-    //   cy.get('.toolbar-inner > .ui > input')
-    //     .click()
-    //     .type('https://1.videolyser.de/videos/1714848/11745228_hd.mp4')
-    //     .type('{enter}');
-    //   cy.get('#toolbar-save').click();
+    it('Add Video Block with YouTube Video', () => {
+      cy.get(`.block.title [data-contents]`)
+        .clear()
+        .type('My title');
+      cy.get('.block.inner.text .public-DraftEditor-content').click();
+      cy.get('.ui.basic.icon.button.block-add-button').click();
+      cy.get('.ui.basic.icon.button.video')
+        .contains('Video')
+        .click();
+      cy.get('.toolbar-inner > .ui > input')
+        .click()
+        .type('https://youtu.be/T6J3d35oIAY')
+        .type('{enter}');
+      cy.get('#toolbar-save').click();
+      cy.get('.block.video');
+    });
 
-    //   cy.get('.block.video video').should(
-    //     'have.attr',
-    //     'src',
-    //     'https://1.videolyser.de/videos/1714848/11745228_hd.mp4',
-    //   );
+    it('Add Video Block with Vimeo Video', () => {
+      cy.get(`.block.title [data-contents]`)
+        .clear()
+        .type('My title');
+      cy.get('.block.inner.text .public-DraftEditor-content').click();
+      cy.get('.ui.basic.icon.button.block-add-button').click();
+      cy.get('.ui.basic.icon.button.video')
+        .contains('Video')
+        .click();
+      cy.get('.toolbar-inner > .ui > input')
+        .click()
+        .type('https://vimeo.com/85804536')
+        .type('{enter}');
+      cy.get('#toolbar-save').click();
+      cy.get('.block.video');
+    });
 
-    //   cy.visit('/my-page/edit');
-    // });
+    it('Add Video Block with MP4 Video', () => {
+      cy.get(`.block.title [data-contents]`)
+        .clear()
+        .type('My title');
+      cy.get('.block.inner.text .public-DraftEditor-content').click();
+      cy.get('.ui.basic.icon.button.block-add-button').click();
+
+      cy.get('.ui.basic.icon.button.video')
+        .contains('Video')
+        .click();
+      cy.get('.toolbar-inner > .ui > input')
+        .click()
+        .type('https://1.videolyser.de/videos/1714848/11745228_hd.mp4')
+        .type('{enter}');
+      cy.get('#toolbar-save').click();
+
+      cy.get('.block.video video').should(
+        'have.attr',
+        'src',
+        'https://1.videolyser.de/videos/1714848/11745228_hd.mp4',
+      );
+
+      cy.visit('/my-page/edit');
+    });
   });
 }
