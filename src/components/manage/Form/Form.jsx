1793615c1ce0d03d8eb902bb8529c3f83545b7d5
iFlameing <ialokkumarsingh0@gmail.com>
2020-04-28 17:21:21 +0530

    added word limit widget

--- src/components/manage/Form/Form.jsx
+++ src/components/manage/Form/Form.jsx
@@ -214,24 +214,6 @@ class Form extends Component {
    * @returns {undefined}
    */
   onChangeField(id, value) {
-    let remlength = 140 - value.length;
-    if (remlength < 0) {
-      this.setState({
-        errors: {
-          ...this.state.errors,
-          [id]: `you have exceed word limit by ${remlength}`,
-        },
-      });
-    }
-    if (remlength > 0) {
-      let newSetErrors = this.state.errors;
-      delete newSetErrors[id];
-      this.setState({
-        errors: {
-          ...newSetErrors,
-        },
-      });
-    }
     this.setState({
       formData: {
         ...this.state.formData,
