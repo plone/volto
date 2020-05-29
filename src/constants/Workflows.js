ee63ceff01496d4f0270b92c043b070d53d231a2
iFlameing <ialokkumarsingh0@gmail.com>
2020-05-29 20:55:58 +0530

    translating workflow

--- src/constants/Workflows.js
+++ src/constants/Workflows.js
@@ -4,16 +4,58 @@
  */
 
 import { last, split } from 'lodash';
+import { createIntl, createIntlCache } from 'react-intl';
+
+const locale = Locale.setLocale(); //I don't know how to get the locale message
+const messages = Locale.setMessages(); //getting messages from the json file.
+const cache = createIntlCache();
+const intl = createIntl({ locale, messages }, cache);
+// reference https://stackoverflow.com/questions/47785313/react-intl-for-non-components
 
 export default function getWorkflowMapping(url, current) {
   const mapping = {
-    published: { value: 'public', label: 'Public', color: '#007bc1', url },
-    publish: { value: 'public', label: 'Public', color: '#007bc1', url },
-    private: { value: 'private', label: 'Private', color: '#ed4033', url },
-    pending: { value: 'pending', label: 'Pending', color: '#f6a808', url },
-    send_back: { value: 'private', label: 'Private', color: '#ed4033', url },
-    retract: { value: 'private', label: 'Private', color: '#ed4033', url },
-    submit: { value: 'review', label: 'Review', color: '#f4e037', url },
+    published: {
+      value: intl.formatMessage({ id: 'public' }),
+      label: intl.formatMessage({ id: 'Public' }),
+      color: '#007bc1',
+      url,
+    },
+    publish: {
+      value: intl.formatMessage({ id: 'public' }),
+      label: intl.formatMessage({ id: 'Public' }),
+      color: '#007bc1',
+      url,
+    },
+    private: {
+      value: intl.formatMessage({ id: 'private' }),
+      label: intl.formatMessage({ id: 'Private' }),
+      color: '#ed4033',
+      url,
+    },
+    pending: {
+      value: intl.formatMessage({ id: 'pending' }),
+      label: intl.formatMessage({ id: 'Pending' }),
+      color: '#f6a808',
+      url,
+    },
+    send_back: {
+      value: intl.formatMessage({ id: 'private' }),
+      label: intl.formatMessage({ id: 'Private' }),
+      color: '#ed4033',
+      url,
+    },
+    retract: {
+      value: intl.formatMessage({ id: 'private' }),
+      label: intl.formatMessage({ id: 'Private' }),
+      color: '#ed4033',
+      url,
+    },
+    submit: {
+      value: intl.formatMessage({ id: 'review' }),
+      label: intl.formatMessage({ id: 'Review' }),
+      color: '#f4e037',
+      url,
+    },
   };
 
   if (url) {
