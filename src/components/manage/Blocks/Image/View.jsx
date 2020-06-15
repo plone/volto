3015cd25ada5eb4df25164eb5a2f1f47f3259087
iFlameing <ialokkumarsingh0@gmail.com>
2020-06-15 19:49:55 +0530

    minor fix

--- src/components/manage/Blocks/Image/View.jsx
+++ src/components/manage/Blocks/Image/View.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
+import { settings } from '~/config';
 
 import { flattenToAppURL, isInternalURL } from '@plone/volto/helpers';
 
