3c8a31fbf899fe58b3a53cc4b9d0ade67562be5f
Silviu Bogan <silviubogan@gmail.com>
2020-07-07 17:43:49 +0300

    Removed a console.warn call

--- src/helpers/Blocks/Blocks.js
+++ src/helpers/Blocks/Blocks.js
@@ -61,9 +61,6 @@ export function blockHasValue(data) {
   const blockType = data['@type'];
   const check = blocks.blocksConfig[blockType]?.blockHasValue;
   if (!check) {
-    console.warn(
-      `No valid blockHasValue implementation for block type '${blockType}'!`,
-    );
     return true;
   }
   return check(data);
