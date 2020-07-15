43642f21a525081552d749702720e0133130c1bd
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-07-15 16:44:30 +0200

    reset to use pastanaga theme

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,6 @@
  * Then, in your `theme.config` file, change the following variable:
  * `@container   : 'pastanaga-cms-ui'`
  */
-import '../theme/themes/pastanaga-cms-ui/extras/cms-ui.semantic.less';
-//import 'semantic-ui-less/semantic.less';
+// import '../theme/themes/pastanaga-cms-ui/extras/cms-ui.semantic.less';
+import 'semantic-ui-less/semantic.less';
 import '../theme/themes/pastanaga/extras/extras.less';
