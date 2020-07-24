75bbaae937f91a3d05bf51d69a98ab50e14084a9
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-24 15:28:34 +0300

    Fix use of object values

--- razzle.config.js
+++ razzle.config.js
@@ -92,7 +92,7 @@ const defaultModify = (config, { target, dev }, webpack) => {
       /node_modules\/@plone\/volto\/theme/,
       /plone\.volto\/theme/,
       /node_modules\/semantic-ui-less/,
-      ...registry.getResolveAliases().values(),
+      ...Object.values(registry.getResolveAliases()),
     ],
     use: dev
       ? [
