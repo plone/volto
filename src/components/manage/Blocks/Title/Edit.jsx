af9b007a638197a68343b3c7c90949f99ee12878
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-07-25 20:55:05 +0300

    Cleanup Title/Edit.jsx

--- src/components/manage/Blocks/Title/Edit.jsx
+++ src/components/manage/Blocks/Title/Edit.jsx
@@ -9,7 +9,6 @@ import PropTypes from 'prop-types';
 import { stateFromHTML } from 'draft-js-import-html';
 import { Editor, DefaultDraftBlockRenderMap, EditorState } from 'draft-js';
 import { defineMessages, injectIntl } from 'react-intl';
-import { settings } from '~/config';
 import { FormStateContext } from '@plone/volto/components/manage/Form/FormContext';
 
 const messages = defineMessages({
@@ -82,7 +81,6 @@ class Edit extends Component {
    * @returns {undefined}
    */
   componentDidMount() {
-    this.setState({ mounted: true });
     if (this.node) {
       this.node.focus();
       this.node._onBlur = () => this.setState({ focus: false });
@@ -147,10 +145,7 @@ class Edit extends Component {
         blockRenderMap={extendedBlockRenderMap}
         handleReturn={() => {
           this.props.onSelectBlock(
-            this.props.onAddBlock(
-              settings.defaultBlockType,
-              this.props.index + 1,
-            ),
+            this.props.onAddBlock('text', this.props.index + 1),
           );
           return 'handled';
         }}
