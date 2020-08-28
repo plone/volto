9063e86f93e7e9c9112ae90710d63d03f50ce934
Alin Voinea <contact@avoinea.com>
2020-08-28 21:51:56 +0300

    Cleanup re-order

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,20 +3,23 @@
  * @module reducers/root
  */
 
+import { reducer as reduxAsyncConnect } from 'redux-connect';
+import { intlReducer } from 'react-intl-redux';
+
 import actions from '@plone/volto/reducers/actions/actions';
 import addons from '@plone/volto/reducers/addons/addons';
 import apierror from '@plone/volto/reducers/apierror/apierror';
 import breadcrumbs from '@plone/volto/reducers/breadcrumbs/breadcrumbs';
 import browserdetect from '@plone/volto/reducers/browserdetect/browserdetect';
-import clipboard from '@plone/volto/reducers/clipboard/clipboard';
 import comments from '@plone/volto/reducers/comments/comments';
 import content from '@plone/volto/reducers/content/content';
 import controlpanels from '@plone/volto/reducers/controlpanels/controlpanels';
+import clipboard from '@plone/volto/reducers/clipboard/clipboard';
 import diff from '@plone/volto/reducers/diff/diff';
 import emailNotification from '@plone/volto/reducers/emailNotification/emailNotification';
 import form from '@plone/volto/reducers/form/form';
-import groups from '@plone/volto/reducers/groups/groups';
 import history from '@plone/volto/reducers/history/history';
+import groups from '@plone/volto/reducers/groups/groups';
 import messages from '@plone/volto/reducers/messages/messages';
 import navigation from '@plone/volto/reducers/navigation/navigation';
 import querystring from '@plone/volto/reducers/querystring/querystring';
@@ -26,14 +29,12 @@ import schema from '@plone/volto/reducers/schema/schema';
 import search from '@plone/volto/reducers/search/search';
 import sharing from '@plone/volto/reducers/sharing/sharing';
 import sidebar from '@plone/volto/reducers/sidebar/sidebar';
-import toolbar from '@plone/volto/reducers/toolbar/toolbar';
 import types from '@plone/volto/reducers/types/types';
 import users from '@plone/volto/reducers/users/users';
 import userSession from '@plone/volto/reducers/userSession/userSession';
 import vocabularies from '@plone/volto/reducers/vocabularies/vocabularies';
 import workflow from '@plone/volto/reducers/workflow/workflow';
-import { intlReducer } from 'react-intl-redux';
-import { reducer as reduxAsyncConnect } from 'redux-connect';
+import toolbar from '@plone/volto/reducers/toolbar/toolbar';
 import { addonReducers } from '~/config';
 
 /**
