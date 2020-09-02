d8ef3b2b57133f8115fadac1656f1f27f8daeb31
Alin Voinea <contact@avoinea.com>
2020-09-03 00:49:10 +0300

    Update Email, Password, URL Widgets tests

--- src/components/manage/Widgets/EmailWidget.test.jsx
+++ src/components/manage/Widgets/EmailWidget.test.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-intl-redux';
+
 import EmailWidget from './EmailWidget';
 
+const mockStore = configureStore();
+
 test('renders an email widget component', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+  });
   const component = renderer.create(
-    <EmailWidget
-      id="test-email"
-      title="My Email"
-      onChange={() => {}}
-      onBlur={() => {}}
-      onClick={() => {}}
-    />,
+    <Provider store={store}>
+      <EmailWidget
+        id="test-email"
+        title="My Email"
+        onChange={() => {}}
+        onBlur={() => {}}
+        onClick={() => {}}
+      />
+    </Provider>,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
--- src/components/manage/Widgets/UrlWidget.test.jsx
+++ src/components/manage/Widgets/UrlWidget.test.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-intl-redux';
+
 import UrlWidget from './UrlWidget';
 
+const mockStore = configureStore();
+
 test('renders an url widget component', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+  });
+
   const component = renderer.create(
-    <UrlWidget
-      id="test-url"
-      title="My Url"
-      onChange={() => {}}
-      onBlur={() => {}}
-      onClick={() => {}}
-    />,
+    <Provider store={store}>
+      <UrlWidget
+        id="test-url"
+        title="My Url"
+        onChange={() => {}}
+        onBlur={() => {}}
+        onClick={() => {}}
+      />
+    </Provider>,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
