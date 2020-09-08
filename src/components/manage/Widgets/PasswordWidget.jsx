84b03ea450a87ea616e599c932f4f259f1296e80
Alin Voinea <contact@avoinea.com>
2020-09-08 17:04:18 +0300

    Cleanup password widget

--- src/components/manage/Widgets/PasswordWidget.jsx
+++ src/components/manage/Widgets/PasswordWidget.jsx
@@ -13,49 +13,30 @@ import { Input } from 'semantic-ui-react';
  * @function PasswordWidget
  * @returns {string} Markup of the component.
  */
-const PasswordWidget = ({
-  id,
-  title,
-  required,
-  description,
-  error,
-  value,
-  onChange,
-  onBlur,
-  onClick,
-  fieldSet,
-  wrapped,
-  minLength,
-  maxLength,
-}) => (
-  <FormFieldWrapper
-    id={id}
-    title={title}
-    description={description}
-    required={required}
-    error={error}
-    fieldSet={fieldSet}
-    wrapped={wrapped}
-  >
-    <Input
-      id={`field-${id}`}
-      name={id}
-      type="password"
-      value={value || ''}
-      onChange={({ target }) =>
-        onChange(id, target.value === '' ? undefined : target.value)
-      }
-      onBlur={({ target }) =>
-        onBlur(id, target.value === '' ? undefined : target.value)
-      }
-      onClick={() => onClick()}
-      minLength={minLength || null}
-      maxLength={maxLength || null}
-      autoComplete="off"
-    />
-  </FormFieldWrapper>
-);
+const PasswordWidget = (props) => {
+  const { id, value, onChange, onBlur, onClick, minLength, maxLength } = props;
 
+  return (
+    <FormFieldWrapper {...props}>
+      <Input
+        id={`field-${id}`}
+        name={id}
+        type="password"
+        value={value || ''}
+        onChange={({ target }) =>
+          onChange(id, target.value === '' ? undefined : target.value)
+        }
+        onBlur={({ target }) =>
+          onBlur(id, target.value === '' ? undefined : target.value)
+        }
+        onClick={() => onClick()}
+        minLength={minLength || null}
+        maxLength={maxLength || null}
+        autoComplete="off"
+      />
+    </FormFieldWrapper>
+  );
+};
 /**
  * Property types.
  * @property {Object} propTypes Property types.
