a19cd6617b93bd7689fccd7daa4f73385fb54015
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-15 23:23:10 +0300

    Simplify AlignWidget, get rid of code already in FormFieldWrapper

--- src/components/manage/Widgets/AlignWidget.jsx
+++ src/components/manage/Widgets/AlignWidget.jsx
@@ -5,41 +5,10 @@
  */
 
 import React from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 
-import { FormFieldWrapper, Icon } from '@plone/volto/components';
+import { FormFieldWrapper } from '@plone/volto/components';
 import { AlignBlock } from '@plone/volto/helpers';
-
-const messages = defineMessages({
-  default: {
-    id: 'Default',
-    defaultMessage: 'Default',
-  },
-  idTitle: {
-    id: 'Short Name',
-    defaultMessage: 'Short Name',
-  },
-  idDescription: {
-    id: 'Used for programmatic access to the fieldset.',
-    defaultMessage: 'Used for programmatic access to the fieldset.',
-  },
-  title: {
-    id: 'Title',
-    defaultMessage: 'Title',
-  },
-  description: {
-    id: 'Description',
-    defaultMessage: 'Description',
-  },
-  required: {
-    id: 'Required',
-    defaultMessage: 'Required',
-  },
-  delete: {
-    id: 'Delete',
-    defaultMessage: 'Delete',
-  },
-});
 
 const AlignWidget = ({
   description,
@@ -57,37 +26,6 @@ const AlignWidget = ({
   value,
   wrapped,
 }) => {
-  const schema = {
-    fieldsets: [
-      {
-        id: 'default',
-        title: intl.formatMessage(messages.default),
-        fields: ['title', 'id', 'description', 'required'],
-      },
-    ],
-    properties: {
-      id: {
-        type: 'string',
-        title: intl.formatMessage(messages.idTitle),
-        description: intl.formatMessage(messages.idDescription),
-      },
-      title: {
-        type: 'string',
-        title: intl.formatMessage(messages.title),
-      },
-      description: {
-        type: 'string',
-        widget: 'textarea',
-        title: intl.formatMessage(messages.description),
-      },
-      required: {
-        type: 'boolean',
-        title: intl.formatMessage(messages.required),
-      },
-    },
-    required: ['id', 'title'],
-  };
-
   return (
     <FormFieldWrapper
       id={id}
@@ -101,23 +39,6 @@ const AlignWidget = ({
       draggable={true}
       className="align-widget"
     >
-      {onEdit && (
-        <div className="toolbar">
-          <button
-            className="item ui noborder button"
-            onClick={() => onEdit(id, schema)}
-          >
-            <Icon name="write square" size="large" color="blue" />
-          </button>
-          <button
-            aria-label={intl.formatMessage(messages.delete)}
-            className="item ui noborder button"
-            onClick={() => onDelete(id)}
-          >
-            <Icon name="close" size="large" color="red" />
-          </button>
-        </div>
-      )}
       <AlignBlock
         align={value}
         onChangeBlock={(block, { align }) => onChange(id, align)}
