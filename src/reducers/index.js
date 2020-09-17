bdc0b797e0e1536dab3a6c3aec09e3e696d07c23
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-17 15:30:57 +0300

    Plug the formSelection as reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -35,6 +35,7 @@ import userSession from '@plone/volto/reducers/userSession/userSession';
 import vocabularies from '@plone/volto/reducers/vocabularies/vocabularies';
 import workflow from '@plone/volto/reducers/workflow/workflow';
 import toolbar from '@plone/volto/reducers/toolbar/toolbar';
+import formSelection from '@plone/volto/reducers/formSelection/formSelection';
 import { addonReducers } from '~/config';
 
 /**
@@ -76,6 +77,7 @@ const reducers = {
   vocabularies,
   workflow,
   toolbar,
+  formSelection,
   ...addonReducers,
 };
 
