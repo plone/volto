459ad46c4df2584a6a602cee02a84021b95f0c6e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-17 14:49:47 +0300

    Also export InlineForm and BlocksForm from @plone/volto/components

--- src/components/index.js
+++ src/components/index.js
@@ -110,6 +110,8 @@ export UsersControlpanelUser from '@plone/volto/components/manage/Controlpanels/
 export UsersControlpanelGroups from '@plone/volto/components/manage/Controlpanels/UsersControlpanelGroups';
 export DiffField from '@plone/volto/components/manage/Diff/DiffField';
 export DragDropList from '@plone/volto/components/manage/DragDropList/DragDropList';
+export InlineForm from '@plone/volto/components/manage/Form/InlineForm';
+export BlocksForm from '@plone/volto/components/manage/Blocks/Block/BlocksForm';
 
 export FormFieldWrapper from '@plone/volto/components/manage/Widgets/FormFieldWrapper';
 export ArrayWidget from '@plone/volto/components/manage/Widgets/ArrayWidget';
