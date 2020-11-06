fab3ede2336e3c7f2cd31f9e6a8cc553ca9dc3d2
Víctor Fernández de Alba <sneridagh@gmail.com>
2020-11-06 19:39:03 +0100

    Revert type-in detection in draftjs link widget, as that leads to a regression (#1976)

--- src/components/manage/AnchorPlugin/utils/URLUtils.js
+++ src/components/manage/AnchorPlugin/utils/URLUtils.js
@@ -1,7 +1,6 @@
 import prependHttp from 'prepend-http';
 import urlRegex from './urlRegex';
 import mailRegex from './mailRegex';
-import { parse as parseUrl } from 'url';
 
 export default {
   isUrl(text) {
@@ -20,16 +19,6 @@ export default {
   },
 
   normalizeUrl(url) {
-    if (__CLIENT__) {
-      const windowURL = parseUrl(window.location.href);
-      const serverURL = `${windowURL.protocol}//${windowURL.host}`;
-
-      // If the url is internal (pasted from the browser directly) treat it as internal
-      // Remove the server part (as if the user has typed '/the-path/content')
-      if (url.startsWith(serverURL)) {
-        return url.replace(serverURL, '');
-      }
-    }
     return prependHttp(url);
   },
 };
