217583c31b1b769bbbe431438e96f807e1ee1c14
Silviu Bogan <silviubogan@gmail.com>
2020-11-20 15:36:50 +0200

    Attempt to mock loadable.lib for unit tests

--- src/components/manage/Blocks/HTML/Edit.test.jsx
+++ src/components/manage/Blocks/HTML/Edit.test.jsx
@@ -7,6 +7,36 @@ import Edit from './Edit';
 
 const mockStore = configureStore();
 
+// Loadable components is tied to webpack, seems most people use webpack in their tests.
+// Rather than that, we mock the loadable function to load the module eagarly and expose a load() function to be able to await the load
+function loadable(load) {
+  let Component;
+  // Capture the component from the module load function
+  const loadPromise = load().then((val) => {
+    console.log('val', val);
+    return (Component = val.default);
+  });
+  // Create a react component which renders the loaded component
+  const Loadable = (props) => {
+    if (!Component) {
+      throw new Error(
+        'Bundle split module not loaded yet, ensure you beforeAll(() => MyLazyComponent.load()) in your test, import statement: ' +
+          load.toString(),
+      );
+    }
+    return <Component {...props} />;
+  };
+  Loadable.load = () => loadPromise;
+  return Loadable;
+}
+
+jest.mock('@plone/volto/helpers', () => {
+  return {
+    withLoadable: jest.fn((libStr) => loadable(() => import(libStr))),
+  };
+});
+// import { withLoadable } from '@plone/volto/helpers';
+
 test('renders an edit html block component', () => {
   const store = mockStore({
     content: {
