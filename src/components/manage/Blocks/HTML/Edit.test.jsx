c0fa80c56fc92a0e699e6aa9afe32689c8db7997
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-11-25 23:12:32 +0200

    Fix tests

--- src/components/manage/Blocks/HTML/Edit.test.jsx
+++ src/components/manage/Blocks/HTML/Edit.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
 import { render, waitFor, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 
 import Edit from './Edit';
 
