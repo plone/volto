6654b42b57ed7fc8f1ba09df29823a654ef78a0c
Silviu Bogan <silviubogan@gmail.com>
2021-01-18 12:58:55 +0200

    Introduce cache, more doc comments

--- src/critical-css.js
+++ src/critical-css.js
@@ -1,20 +1,54 @@
 import { existsSync, lstatSync, readFileSync } from 'fs';
 
 const criticalCssPath = 'public/critical.css';
+const cacheLifetime = 5 * 60 * 1000; // 5 minutes
 
+let cache = null;
+let cacheTimeout = null;
+
+/**
+ * Returns whether the critical CSS file exists or not.
+ *
+ * @returns {boolean}
+ */
 export const hasCriticalCss = () => {
   return existsSync(criticalCssPath) && lstatSync(criticalCssPath).isFile();
 };
 
 /**
- * Checks if there is a CSS file 'critical.css' inside the 'public' directory.
- * If it is there, returns its contents.
+ * Checks if there is a valid CSS file in the relative path `criticalCssPath`.
+ * If it is there, returns its contents after caching it for `cacheLifetime`
+ * milliseconds. If not, returns null and attempts to update the cache on each
+ * call.
+ *
+ * @returns {string|null}
+ */
+export const updateCriticalCss = () => {
+  if (!hasCriticalCss()) {
+    cache = null;
+    return null;
+  } else {
+    cache = readFileSync(criticalCssPath);
+  }
+
+  if (cacheTimeout) {
+    clearTimeout(cacheTimeout);
+  }
+  cacheTimeout = setTimeout(() => {
+    cache = null;
+  }, cacheLifetime);
+  return cache;
+};
+
+/**
+ * Reads the critical CSS cache. If well suited, updates the cache.
  *
  * @returns {string|null}
  */
 export const readCriticalCss = () => {
-  if (!hasCriticalCss()) {
-    return null;
+  if (cache) {
+    return cache;
   }
-  return readFileSync(criticalCssPath);
+  updateCriticalCss();
+  return cache;
 };
