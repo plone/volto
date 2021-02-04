03a596d373664d49c2a0dee46937d78e4f68e92f
nileshgulia1 <nileshgulia@gmail.com>
2021-02-05 01:17:42 +0530

    set only cache images in initial

--- src/express-middleware/image-proxy/file-cache.js
+++ src/express-middleware/image-proxy/file-cache.js
@@ -31,7 +31,7 @@ class FileCache {
       if (fs.statSync(dirPath + '/' + file).isDirectory()) {
         //if we move each file in a directory in future.
         initialize(dirPath + '/' + file);
-      } else {
+      } else if (!file.includes('.metadata')) {
         this.cache.set(file, 0);
       }
     });
