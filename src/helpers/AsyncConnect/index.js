b73c7338fca5606a6504d111d9bdf982e8e23c15
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-02-21 10:57:44 +0200

    Add some comments

--- src/helpers/AsyncConnect/index.js
+++ src/helpers/AsyncConnect/index.js
@@ -90,6 +90,22 @@ const getAsyncItems = (asyncItems, state) => {
   return foundAsyncItems;
 };
 
+/**
+ * A replacement for redux-connect's asyncConnect.
+ *
+ * It was needed because asyncConnect works as a direct wrapper on top of
+ * a Component, so the config registry is not yet initialized. To solve this
+ * problem we replace the original reduxAsyncConnect implementation (which was
+ * a list of asyncConnected "object promises") with as single promise exposed
+ * as { key: 'voltoLoadAsyncProps', promise: ... }.
+ *
+ * In more details, the original asyncConnect worked by replacing all our
+ * "object promises" with a special version that's integrated with its own
+ * Redux actions. We do another wrap, which takes the initial passed "object
+ * promises", finds registered, per route, "extenders", calls these extenders
+ * with the "object promises" and that's our "object promises" (which it calls
+ * internally "asyncItems").
+ */
 export function asyncConnect(
   asyncItems,
   mapStateToProps,
@@ -101,7 +117,7 @@ export function asyncConnect(
     Component.reduxAsyncConnect = addLoader(Component, asyncItems);
 
     const finalMapStateToProps = (state, ownProps) => {
-      const foundAsyncItems = getAsyncItems(asyncItems, state); // TODO: implement this?
+      const foundAsyncItems = getAsyncItems(asyncItems, state);
       const mutableState = getMutableState(state);
       const asyncStateToProps = foundAsyncItems.reduce((result, { key }) => {
         if (!key) {
