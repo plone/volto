72a600e6ee4fb510032bb6b1d779f97ff4884a5b
nzambello <nzambello@protonmail.com>
2021-03-08 12:48:39 +0100

    fix: sitemap url generation

--- src/helpers/Sitemap/Sitemap.js
+++ src/helpers/Sitemap/Sitemap.js
@@ -7,6 +7,7 @@ import superagent from 'superagent';
 import { map } from 'lodash';
 import cookie from 'react-cookie';
 import zlib from 'zlib';
+import { toPublicURL } from '@plone/volto/helpers';
 
 import { settings } from '~/config';
 
@@ -18,7 +19,6 @@ import { settings } from '~/config';
  */
 export const generateSitemap = (req) =>
   new Promise((resolve) => {
-    const url = `${req.protocol}://${req.get('Host')}`;
     const request = superagent.get(
       `${settings.apiPath}/@search?metadata_fields=modified&b_size=100000000`,
     );
@@ -34,10 +34,8 @@ export const generateSitemap = (req) =>
         const items = map(
           body.items,
           (item) =>
-            `  <url>\n    <loc>${item['@id'].replace(
-              settings.apiPath,
-              url,
-            )}</loc>\n    <lastmod>${item.modified}</lastmod>\n  </url>`,
+            `  <url>\n    <loc>${toPublicURL(item['@id'])}</loc>\n  
+            <lastmod>${item.modified}</lastmod>\n  </url>`,
         );
         const result = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n${items.join(
           '\n',
