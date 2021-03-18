0fb9108747a61d12f4ea4770d6e0b8c07c2b4753
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-03-18 20:12:27 +0100

    Reorganization and housecleaning of Cypress tests

---
+++ cypress/tests/guillotina/login.js
@@ -0,0 +1,12 @@
+describe('Login Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.contains('Log in').click();
+  });
+  it('As registered user I can login', function () {
+    cy.get('#login').type('admin').should('have.value', 'admin');
+    cy.get('#password').type('admin').should('have.value', 'admin');
+    cy.get('#login-form-submit').click();
+    cy.get('body').should('have.class', 'has-toolbar');
+  });
+});
