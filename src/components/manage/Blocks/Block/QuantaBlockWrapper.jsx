946127cf6d5c80d5450e5fbe62645a355519acb4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-04-02 00:01:00 +0300

    Add blockChooser and mutate button

--- src/components/manage/Blocks/Block/QuantaBlockWrapper.jsx
+++ src/components/manage/Blocks/Block/QuantaBlockWrapper.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Icon } from '@plone/volto/components';
-import { blockHasValue } from '@plone/volto/helpers';
-import dragSVG from '@plone/volto/icons/drag.svg';
 import { Button } from 'semantic-ui-react';
 import includes from 'lodash/includes';
 import isBoolean from 'lodash/isBoolean';
 import { defineMessages, injectIntl } from 'react-intl';
-import config from '@plone/volto/registry';
+import { Icon, BlockChooser } from '@plone/volto/components';
+import { blockHasValue } from '@plone/volto/helpers';
 import { Pluggable } from '@plone/volto/components/manage/Pluggable';
+import { doesNodeContainClick } from 'semantic-ui-react/dist/commonjs/lib';
 
+import config from '@plone/volto/registry';
+
+import addSVG from '@plone/volto/icons/circle-plus.svg';
+import dragSVG from '@plone/volto/icons/drag.svg';
 import trashSVG from '@plone/volto/icons/delete.svg';
 
 const messages = defineMessages({
@@ -18,13 +21,63 @@ const messages = defineMessages({
   },
 });
 
+const MutateBlockButton = ({ block, allowedBlocks, data, onMutateBlock }) => {
+  const { disableNewBlocks } = data;
+  const [addNewBlockOpened, setAddNewBlockOpened] = React.useState(false);
+
+  const blockChooserRef = React.useRef();
+
+  const handleClickOutside = React.useCallback((e) => {
+    if (
+      blockChooserRef.current &&
+      doesNodeContainClick(blockChooserRef.current, e)
+    )
+      return;
+    setAddNewBlockOpened(false);
+  }, []);
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside, false);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside, false);
+    };
+  }, [handleClickOutside]);
+
+  return (
+    <>
+      {!disableNewBlocks && !blockHasValue(data) && (
+        <Button
+          icon
+          basic
+          title="Add block"
+          onClick={() => setAddNewBlockOpened(true)}
+          className="group-block-add-button"
+        >
+          <Icon name={addSVG} className="" size="19px" />
+        </Button>
+      )}
+      {addNewBlockOpened && (
+        <BlockChooser
+          onMutateBlock={(id, value) => {
+            setAddNewBlockOpened(false);
+            onMutateBlock(id, value);
+          }}
+          currentBlock={block}
+          allowedBlocks={allowedBlocks}
+          ref={blockChooserRef}
+        />
+      )}
+    </>
+  );
+};
+
 const EditBlockWrapper = (props) => {
   const hideHandler = (data) => {
     return !!data.fixed || !blockHasValue(data);
   };
 
   const { intl, blockProps, draginfo, children } = props;
-  const { block, selected, type, onDeleteBlock, data } = blockProps;
+  const { block, data, onDeleteBlock, selected, type } = blockProps;
   const visible = selected && !hideHandler(data);
 
   const required = isBoolean(data.required)
@@ -38,7 +91,7 @@ const EditBlockWrapper = (props) => {
       className={`quanta-block quanta-block-editor-${data['@type']}`}
     >
       {selected && (
-        <div class="toolbar quanta-block-toolbar">
+        <div className="toolbar quanta-block-toolbar">
           <Button
             style={{
               visibility: visible ? 'visible' : 'visible',
@@ -50,6 +103,8 @@ const EditBlockWrapper = (props) => {
           >
             <Icon name={dragSVG} size="18px" />
           </Button>
+          <MutateBlockButton {...blockProps} />
+
           {!required && (
             <Button
               icon
