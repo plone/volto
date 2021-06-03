21e5bce25d7735db92fb22e8c913dc97cf366000
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-06-03 15:16:37 +0200

    updated Image from add_srcset_imgs branch

--- src/helpers/Image/Image.js
+++ src/helpers/Image/Image.js
@@ -4,8 +4,12 @@ import config from '@plone/volto/registry';
 /**
  * Get src-set list from image
  * @param {object | string} image - Image content object or url
+ * @param {object} options - { maxSize [number]: maximum size to render, useOriginal [boolean]: whether to render original img }
  */
-export const getImageAttributes = (image) => {
+export const getImageAttributes = (
+  image,
+  { maxSize = 10000, useOriginal = false } = {},
+) => {
   const imageScales = config.settings.imageScales;
   const minSize = Object.keys(imageScales).reduce((minSize, scale) => {
     if (!minSize || imageScales[scale] < imageScales[minSize]) {
@@ -29,13 +33,23 @@ export const getImageAttributes = (image) => {
     // Scales object from Plone restapi
     // ideal use of Plone images
     case 'imageObject':
-      let sortedScales = Object.values(image.scales).sort(
-        (a, b) => a.width - b.width,
-      );
+      let sortedScales = Object.values(image.scales)
+        .filter((scale) => scale.width <= maxSize)
+        .filter((scale) => scale.width < image.width) // avoid duplicates if image is small and original is smaller than scale
+        .sort((a, b) => {
+          if (a.width > b.width) return 1;
+          else if (a.width < b.width) return -1;
+          else return 0;
+        });
       attrs.src = sortedScales[0].download;
-      attrs.srcSet = sortedScales
-        .map((scale) => `${flattenToAppURL(scale.download)} ${scale.width}w`)
-        .concat(`${flattenToAppURL(image.download)} ${image.width}w`);
+      attrs.srcSet = sortedScales.map(
+        (scale) => `${flattenToAppURL(scale.download)} ${scale.width}w`,
+      );
+
+      if (useOriginal)
+        attrs.srcSet = attrs.srcSet.concat(
+          `${flattenToAppURL(image.download)} ${image.width}w`,
+        );
       break;
 
     // Internal URL
@@ -44,11 +58,11 @@ export const getImageAttributes = (image) => {
         image.endsWith('/') ? '' : '/'
       }@@images/image`;
       attrs.src = `${baseUrl}/${minSize}`;
-      attrs.srcSet = Object.keys(imageScales)
-        .reduce((srcSet, scale) => {
-          return [...srcSet, `${baseUrl}/${scale} ${imageScales[scale]}w`];
-        }, [])
-        .concat(`${baseUrl} 1200w`); // expect that is for desktop screens, I don't have actual size
+      attrs.srcSet = Object.keys(imageScales).reduce((srcSet, scale) => {
+        return [...srcSet, `${baseUrl}/${scale} ${imageScales[scale]}w`];
+      }, []);
+
+      if (useOriginal) attrs.srcSet = attrs.srcSet.concat(`${baseUrl} 1200w`); // expect that is for desktop screens, I don't have actual size
       break;
 
     // External URL or other element
