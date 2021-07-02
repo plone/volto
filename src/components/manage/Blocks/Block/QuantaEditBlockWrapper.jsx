8b90a8f106c8c4b0b8734586523c9595863bf835
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-07-02 23:45:13 +0300

    More work on toolbar

--- src/components/manage/Blocks/Block/QuantaEditBlockWrapper.jsx
+++ src/components/manage/Blocks/Block/QuantaEditBlockWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Dropdown } from 'semantic-ui-react';
 import includes from 'lodash/includes';
 import isBoolean from 'lodash/isBoolean';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, injectIntl, useIntl } from 'react-intl';
 import { Icon } from '@plone/volto/components';
 import { blockHasValue } from '@plone/volto/helpers';
 import { Pluggable, Plug } from '@plone/volto/components/manage/Pluggable';
@@ -27,17 +27,49 @@ const hideHandler = (data) => {
   return !!data.fixed || !blockHasValue(data);
 };
 
+const DefaultPlugs = (props) => {
+  const intl = useIntl();
+  const { blockProps } = props;
+  const { data, type, onDeleteBlock, block } = blockProps;
+  const required = isBoolean(data.required)
+    ? data.required
+    : includes(config.blocks.requiredBlocks, type);
+
+  return (
+    <>
+      <Plug pluggable="block-toolbar" id="mutate-block-button">
+        <MutateBlockButton
+          {...blockProps}
+          onInsertBlock={(id, value) => {
+            blockProps.onSelectBlock(blockProps.onInsertBlock(id, value));
+          }}
+          className="quanta-block-add-button"
+        />
+      </Plug>
+      <Plug pluggable="block-toolbar-more" id="delete-button">
+        {!required ? (
+          <Button
+            icon
+            basic
+            onClick={() => onDeleteBlock(block)}
+            className="delete-button"
+            aria-label={intl.formatMessage(messages.delete)}
+          >
+            <Icon name={trashSVG} size="18px" />
+          </Button>
+        ) : null}
+      </Plug>
+    </>
+  );
+};
+
 const QuantaEditBlockWrapper = (props) => {
-  const { intl, blockProps, draginfo, children, className } = props;
-  const { block, data, onDeleteBlock, selected, type } = blockProps;
+  const { blockProps, draginfo, children, className } = props;
+  const { data, selected, type } = blockProps;
 
   // if (usesClassicWrapper(data)) return <EditBlockWrapper {...props} />;
 
   const visible = selected && !hideHandler(data);
-
-  const required = isBoolean(data.required)
-    ? data.required
-    : includes(config.blocks.requiredBlocks, type);
 
   return (
     <div
@@ -51,94 +83,44 @@ const QuantaEditBlockWrapper = (props) => {
     >
       {selected && (
         <div className="toolbar quanta-block-toolbar">
+          <Button
+            style={{
+              visibility: visible ? 'visible' : 'visible',
+              display: 'inline-block',
+            }}
+            {...draginfo.dragHandleProps}
+            icon
+            basic
+          >
+            <Icon name={dragSVG} size="18px" />
+          </Button>
+
           <Pluggable name="block-toolbar" params={blockProps} />
-          <Plug pluggable="block-toolbar" id="drag-handle">
-            <Button
-              style={{
-                visibility: visible ? 'visible' : 'visible',
-                display: 'inline-block',
-              }}
-              {...draginfo.dragHandleProps}
-              icon
-              basic
-            >
-              <Icon name={dragSVG} size="18px" />
-            </Button>
-          </Plug>
-          <Plug pluggable="block-toolbar" id="mutate-block-button">
-            <>
-              <MutateBlockButton
-                {...blockProps}
-                onInsertBlock={(id, value) => {
-                  blockProps.onSelectBlock(blockProps.onInsertBlock(id, value));
-                }}
-                className="quanta-block-add-button"
-              />
-            </>
-          </Plug>
-          <Plug pluggable="block-toolbar" id="more-button">
-            <Pluggable name="block-toolbar-more">
-              {({ children }) => {
-                return children ? (
-                  <Button basic icon>
-                    <Dropdown
-                      item
-                      icon={<Icon name={moreSVG} size="18px" color="#826a6a" />}
-                      className=""
-                    >
-                      <Dropdown.Menu className="right">
-                        <Dropdown.Header content="More actions" />
-                        <Dropdown.Menu scrolling>
-                          <Dropdown.Item>something</Dropdown.Item>
-                          {/* <Dropdown.Item */}
-                          {/*   key={index} */}
-                          {/*   value={index} */}
-                          {/*   onClick={this.onSelectIndex} */}
-                          {/*   className="iconAlign" */}
-                          {/* > */}
-                          {/*   {this.state.index.values[index].selected ? ( */}
-                          {/*     <Icon */}
-                          {/*       name={checkboxCheckedSVG} */}
-                          {/*       size="24px" */}
-                          {/*       color="#007eb1" */}
-                          {/*       className={this.state.index.values[index].label} */}
-                          {/*     /> */}
-                          {/*   ) : ( */}
-                          {/*     <Icon */}
-                          {/*       name={checkboxUncheckedSVG} */}
-                          {/*       className={this.state.index.values[index].label} */}
-                          {/*       size="24px" */}
-                          {/*     /> */}
-                          {/*   )} */}
-                          {/*   <span> */}
-                          {/*     {' '} */}
-                          {/*     {this.props.intl.formatMessage({ */}
-                          {/*       id: this.state.index.values[index].label, */}
-                          {/*       defaultMessage: this.state.index.values[index] */}
-                          {/*         .label, */}
-                          {/*     })} */}
-                          {/*   </span> */}
-                          {/* </Dropdown.Item> */}
-                        </Dropdown.Menu>
+          <Pluggable name="block-toolbar-slots" params={blockProps} />
+          <Pluggable name="block-toolbar-more">
+            {(pluggables) => {
+              return pluggables?.length > 1 ? (
+                <Button basic icon>
+                  <Dropdown
+                    item
+                    icon={<Icon name={moreSVG} size="18px" color="#826a6a" />}
+                    className=""
+                  >
+                    <Dropdown.Menu className="right">
+                      <Dropdown.Header content="More actions" />
+                      <Dropdown.Menu scrolling>
+                        {pluggables.map((p) => p())}
                       </Dropdown.Menu>
-                    </Dropdown>
-                  </Button>
-                ) : (
-                  !required && (
-                    <Button
-                      icon
-                      basic
-                      onClick={() => onDeleteBlock(block)}
-                      className="delete-button"
-                      aria-label={intl.formatMessage(messages.delete)}
-                    >
-                      <Icon name={trashSVG} size="18px" />
-                    </Button>
-                  )
-                );
-              }}
-            </Pluggable>
-          </Plug>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Button>
+              ) : pluggables ? (
+                pluggables.map((p, i) => p())
+              ) : null;
+            }}
+          </Pluggable>
+
+          <DefaultPlugs {...props} />
         </div>
       )}
       <div className={`ui drag block inner ${type}`}>{children}</div>
@@ -147,3 +129,10 @@ const QuantaEditBlockWrapper = (props) => {
 };
 
 export default injectIntl(QuantaEditBlockWrapper);
+/*
+                        {children.map((child, i) => (
+                          <Dropdown.Item key={i}>{child}</Dropdown.Item>
+                        ))}
+
+      <Plug pluggable="block-toolbar-more" id="more-button" order={999}></Plug>
+                        */
--- src/components/manage/Slots/SlotEditBlockWrapper.jsx
+++ src/components/manage/Slots/SlotEditBlockWrapper.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import QuantaEditBlockWrapper from '@plone/volto/components/manage/Blocks/Block/QuantaEditBlockWrapper';
 import cx from 'classnames';
+import { Plug } from '@plone/volto/components/manage/Pluggable';
 
 const SlotEditBlockWrapper = (props) => {
   const { blockProps } = props;
@@ -9,14 +10,17 @@ const SlotEditBlockWrapper = (props) => {
   const inherited = data._v_inherit && 'slot-inherited';
 
   return (
-    <QuantaEditBlockWrapper
-      {...props}
-      className={cx(
-        'slot-editor',
-        `slot-editor-${data['@type']}`,
-        inherited || '',
-      )}
-    />
+    <>
+      <QuantaEditBlockWrapper
+        {...props}
+        className={cx(
+          'slot-editor',
+          `slot-editor-${data['@type']}`,
+          inherited || '',
+        )}
+      />
+      <Plug pluggable="block-toolbar" id="slot-buttons"></Plug>
+    </>
   );
 };
 
