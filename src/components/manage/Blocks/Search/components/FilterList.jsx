e4e0e1ceca454ad2857e78905d619eecb3b4d39e
kreafox <krisztina.n.elekes@gmail.com>
2021-08-17 10:52:32 +0300

    Add reset filters button

--- src/components/manage/Blocks/Search/components/FilterList.jsx
+++ src/components/manage/Blocks/Search/components/FilterList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Label, Icon } from 'semantic-ui-react';
+import { Label, Icon, Button } from 'semantic-ui-react';
 
 const FilterList = (props) => {
   const { data, facets, setFacets, isEditMode } = props;
@@ -10,41 +10,50 @@ const FilterList = (props) => {
         <div className="filter-list-wrapper" key={i}>
           {facets[facet].length > 0 && (
             <div className="filter-list-group">
-              <Label.Group>
-                <>
-                  {data?.facets.map((f, i) => {
-                    return facet === f?.field?.value ? (
-                      <span className="label-title" key={i}>
-                        {f.title || f?.field?.label}:
-                      </span>
-                    ) : (
-                      ''
-                    );
-                  })}
-                  {facets[facet].map((entry, i) => (
-                    <Label key={i}>
-                      {entry}
-                      <Icon
-                        name="delete"
-                        onClick={() => {
-                          const entries = facets[facet].filter(
-                            (item) => item !== entry,
-                          );
-                          !isEditMode &&
-                            setFacets({
-                              ...facets,
-                              [facet]: entries,
-                            });
-                        }}
-                      />
-                    </Label>
-                  ))}
-                </>
-              </Label.Group>
+              {data?.facets.map((f, i) => {
+                return facet === f?.field?.value ? (
+                  <span className="label-title" key={i}>
+                    {f.title || f?.field?.label}:
+                  </span>
+                ) : (
+                  ''
+                );
+              })}
+              {facets[facet].map((entry, i) => (
+                <Label size="small" key={i}>
+                  {entry}
+                  <Icon
+                    name="delete"
+                    onClick={() => {
+                      const entries = facets[facet].filter(
+                        (item) => item !== entry,
+                      );
+                      !isEditMode &&
+                        setFacets({
+                          ...facets,
+                          [facet]: entries,
+                        });
+                    }}
+                  />
+                </Label>
+              ))}
             </div>
           )}
         </div>
       ))}
+
+      {Object.keys(facets).length > 0 && (
+        <Button
+          basic
+          size="tiny"
+          onClick={() => {
+            !isEditMode && setFacets({});
+          }}
+        >
+          <Icon name="trash" />
+          Reset filters
+        </Button>
+      )}
     </div>
   );
 };
