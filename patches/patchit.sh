b413b472ab2d1ea0b7223f2260069637b986c167
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-09-18 07:38:55 +0300

    Add patch line

--- patches/patchit.sh
+++ patches/patchit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 patch --quiet -p0 -N node_modules/razzle/config/createJestConfig.js < patches/razzle-jest.patch
+git apply --directory=node_modules/formidable/ patches/fixformidable.diff
