1dcd0e48bda16d7beb4c6be67b8bf8ed845e9727
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-09-21 22:45:06 +0200

    Action should be a thunk...

--- src/components/theme/View/View.test.jsx
+++ src/components/theme/View/View.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
+import thunk from 'redux-thunk';
 
 import View from './View';
 import config from '@plone/volto/registry';
@@ -24,7 +25,7 @@ beforeAll(() => {
   config.settings.publicURL = 'https://plone.org';
 });
 
-const mockStore = configureStore();
+const mockStore = configureStore([thunk]);
 
 jest.mock('react-portal', () => ({
   Portal: jest.fn(() => <div id="Portal" />),
