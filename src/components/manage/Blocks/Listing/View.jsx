f607a4b7527382383ac533a258458b647435a67e
nileshgulia1 <nileshgulia@gmail.com>
2021-10-13 21:26:16 +0530

    add pathname from router in ListingBlock view

--- src/components/manage/Blocks/Listing/View.jsx
+++ src/components/manage/Blocks/Listing/View.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { withRouter } from 'react-router';
 import { withBlockExtensions } from '@plone/volto/helpers';
 
 import { ListingBlockBody as ListingBody } from '@plone/volto/components';
 
-const View = (props) => {
-  const { data, path, pathname } = props;
+const View = withRouter((props) => {
+  const {
+    data,
+    path,
+    location: { pathname },
+  } = props;
 
   return (
     <div className={cx('block listing', data.variation)}>
       <ListingBody {...props} path={path ?? pathname} />
     </div>
   );
-};
+});
 
 View.propTypes = {
   data: PropTypes.objectOf(PropTypes.any).isRequired,
