5bd771d8e6277d3ea2936fd31dc6c58b3cc79a6b
nzambello <nzambello@protonmail.com>
2021-10-27 11:15:12 +0200

    chore: update image scales from plone.volto sizes

--- src/config/ImageScales.jsx
+++ src/config/ImageScales.jsx
@@ -1,9 +1,12 @@
 export const imageScales = {
-  large: 768,
+  huge: 1600,
+  great: 1200,
+  larger: 1000,
+  large: 800,
+  teaser: 600,
   preview: 400,
   mini: 200,
   thumb: 128,
   tile: 64,
   icon: 32,
-  listing: 16,
 };
