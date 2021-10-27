1cb2b46d358fd56b7da9c7bd4e10a3221504e285
nzambello <nzambello@protonmail.com>
2021-10-27 11:49:32 +0200

    test: update scales in cy tests

--- cypress/tests/core/blocks-image.js
+++ cypress/tests/core/blocks-image.js
@@ -107,13 +107,13 @@ describe('Blocks Tests', () => {
       subjectType: 'input',
       encoding: 'utf8',
     });
-    cy.waitForResourceToLoad('image.png/@@images/image/listing');
+    cy.waitForResourceToLoad('image.png/@@images/image');
     cy.get('#toolbar-save').click();
 
     // then image src must be equal to image name
     cy.get('.block img')
       .should('have.attr', 'src')
-      .and('eq', '/my-page/image.png/@@images/image/listing');
+      .and('eq', '/my-page/image.png/@@images/image/icon');
 
     cy.get('.block img')
       .should('be.visible')
