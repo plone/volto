ba383977027491f025ff2ef552e6f85c10ce486e
nileshgulia1 <nileshgulia@gmail.com>
2021-11-01 12:23:19 +0530

    cypress:fix tests

--- cypress/tests/core/user-control-panel.js
+++ cypress/tests/core/user-control-panel.js
@@ -23,8 +23,8 @@ describe('User Control Panel Test', () => {
     // then the user section must contains a fullname when I searched the
     // same with the same username
     cy.get('input[id="user-search-input"]').clear().type('i');
-    cy.get('.icon.button:first').click();
-    cy.get('.fullname').should('have.text', 'Alok Kumar');
+    cy.get('.icon.zoom').click();
+    cy.contains('Alok Kumar');
   });
 
   it('Should show error from backend when add User fails', () => {
@@ -65,14 +65,13 @@ describe('User Control Panel Test', () => {
     cy.get('input[id="field-password"]').clear().type('test@test');
     cy.get('button[title="Save"]').click(-50, -50, { force: true });
 
-    // select first user with name, delete it and search if its exists or not!
-    cy.get('tr:nth-of-type(2) > td.fullname').should('have.text', 'Alok Kumar');
-    cy.get('tr:nth-of-type(2) div[role="listbox"]').click();
-    cy.get('tr:nth-of-type(2) div[role="option"]').click();
-    cy.contains('Delete User');
-    cy.get('button.ui.primary.button').should('have.text', 'OK').click();
+    cy.get('td:nth-child(1) > .icon').click();
+    cy.get('.delete').click();
+    cy.contains('iFlameing');
+    cy.get('div.modal .primary').click();
+
     cy.get('input[id="user-search-input"]').clear().type('i');
-    cy.get('.icon.button:first').click();
+    cy.get('.icon.zoom').click();
     cy.getIfExists('.fullname').should('not.have.text', 'Alok Kumar');
   });
 
@@ -83,16 +82,19 @@ describe('User Control Panel Test', () => {
     cy.waitForResourceToLoad('@actions');
     cy.waitForResourceToLoad('@types');
 
-    cy.get('[data-user="users"] input[type="checkbox"')
-      .first()
-      .check({ force: true });
+    cy.get('Button[id="toolbar-add"]').click();
+    cy.get('input[id="field-username"]').clear().type('iFlameing');
+    cy.get('input[id="field-fullname"]').clear().type('Alok Kumar');
+    cy.get('input[id ="field-email"]').clear().type('info@example.com');
+    cy.get('input[id="field-password"]').clear().type('test@test');
+    cy.get('button[title="Save"]').click(-50, -50, { force: true });
+
+    cy.get('td:nth-child(3) > .icon').click();
 
     cy.get('Button[id="toolbar-save"]').click();
 
     cy.reload();
 
-    cy.get('[data-user="users"] div.checkbox')
-      .first()
-      .should('have.class', 'checked');
+    cy.get('td:nth-child(3) > .icon').should('have.css', 'rgb');
   });
 });
