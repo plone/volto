9c42c95e8e8d525c5d964753714d48d833d6349e
mamico <mauro.amico@gmail.com>
2021-11-04 16:51:43 +0100

    change import settings for overrides

--- src/express-middleware/static.js
+++ src/express-middleware/static.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import settings from '../config/server';
+import config from '@plone/volto/registry';
 
 const staticMiddleware = express.static(process.env.RAZZLE_PUBLIC_DIR, {
   setHeaders: function (res, path) {
     const relpath = path.substr(process.env.RAZZLE_PUBLIC_DIR.length);
-    settings.staticFiles.some((elem) => {
+    config.settings.staticFiles.some((elem) => {
       if (relpath.match(elem.match)) {
         for (const name in elem.headers) {
           res.setHeader(name, elem.headers[name] || 'undefined');
