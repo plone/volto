39b434627b7e2d65994492204083bd562dc15198
Tiberiu Ichim <tiberiuichim@users.noreply.github.com>
2021-11-05 17:14:58 +0200

    Fix pagination in search block when changing filters (#2816)
    * Add usePrevious helper, fix pagination in search block when changing filters
    
    * Push usePagination in separate Volto helpers, to make it reusable
    
    * Really use defaultPage argument

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -85,3 +85,5 @@ export { asyncConnect } from './AsyncConnect';
 export { userHasRoles } from './User/User';
 // export { injectLazyLibs } from './Loadable/Loadable';
 export { useDetectClickOutside } from './Utils/useDetectClickOutside';
+export { usePrevious } from './Utils/usePrevious';
+export { usePagination } from './Utils/usePagination';
