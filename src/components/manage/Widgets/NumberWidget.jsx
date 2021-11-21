e585fdf493179886f04f583078c564aff4829750
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-11-21 12:38:01 +0200

    Remove jsdoc string

--- src/components/manage/Widgets/NumberWidget.jsx
+++ src/components/manage/Widgets/NumberWidget.jsx
@@ -11,9 +11,6 @@ import { injectIntl } from 'react-intl';
 
 /**
  * NumberWidget component class.
- * @function NumberWidget
- * @returns {string} Markup of the component.
- *
  *
  * To use it, in schema properties, declare a field like:
  *
