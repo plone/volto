b35c0d94d29c7555b2ce882205c7ce6b3ee4fca9
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-12-13 21:36:38 +0200

    Fix update of text block

--- src/components/manage/Blocks/Text/Edit.jsx
+++ src/components/manage/Blocks/Text/Edit.jsx
@@ -137,14 +137,20 @@ class Edit extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (!isEqual(this.props.data, prevProps.data)) {
+    if (
+      !isEqual(this.props.data, prevProps.data) &&
+      !isEqual(
+        convertToRaw(this.state.editorState.getCurrentContent()),
+        this.props.data.text,
+      )
+    ) {
       const editorState =
         this.props.data && this.props.data.text
           ? EditorState.createWithContent(convertFromRaw(this.props.data.text))
           : EditorState.createEmpty();
 
       this.setState({
-        editorState: EditorState.moveFocusToEnd(editorState),
+        editorState: editorState,
       });
     }
   }
