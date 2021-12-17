1ddbf6e026e739287f0be6e2bd8c52652335d803
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-12-17 07:22:54 +0200

    Move hotkeys support in useUndoManager hook

--- src/helpers/UndoManager/useUndoManager.js
+++ src/helpers/UndoManager/useUndoManager.js
@@ -4,7 +4,11 @@ import Undoo from 'undoo';
 // Code based on Apache-2.0 License
 // https://github.com/reaviz/reaflow/blob/78d60aa04f514947a17097c906efdbbd6bae5080/src/helpers/useUndo.ts
 
-const useUndoManager = (state, onUndoRedo, { maxUndoLevels }) => {
+const useUndoManager = (
+  state,
+  onUndoRedo,
+  { maxUndoLevels, enableHotKeys },
+) => {
   const [canUndo, setCanUndo] = React.useState(false);
   const [canRedo, setCanRedo] = React.useState(false);
   const manager = React.useRef(new Undoo({ maxLength: maxUndoLevels }));
@@ -55,6 +59,37 @@ const useUndoManager = (state, onUndoRedo, { maxUndoLevels }) => {
       });
     });
   }, []);
+
+  const handleKeys = React.useCallback(
+    (event) => {
+      const keyName = event.key;
+
+      if (keyName === 'Control' || keyName === 'Meta') {
+        // do not alert when only Control key is pressed.
+        return;
+      }
+      if (event.ctrlKey || event.metaKey) {
+        if (keyName === 'z') {
+          event.preventDefault();
+          event.stopPropagation();
+          doUndo();
+        } else if (keyName === 'y') {
+          event.preventDefault();
+          event.stopPropagation();
+          doRedo();
+        }
+      } else {
+        return;
+      }
+    },
+    [doUndo, doRedo],
+  );
+
+  React.useEffect(() => {
+    if (!enableHotKeys) return;
+    document.addEventListener('keydown', handleKeys);
+    return () => document.removeEventListener('keydown', handleKeys);
+  }, [enableHotKeys, handleKeys]);
 
   return {
     doUndo,
