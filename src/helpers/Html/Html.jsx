afc1e19e7590074aaf4c5083be60a4a87f181331
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-12-18 10:55:54 +0100

    Update favicons to Plone ones

--- src/helpers/Html/Html.jsx
+++ src/helpers/Html/Html.jsx
@@ -123,7 +123,24 @@ class Html extends Component {
             }}
           />
 
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="generator" content="Volto - http://plone.org" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
