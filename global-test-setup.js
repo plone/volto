52569454198630f0cb0791d906cc108f29895e3b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-01-04 16:36:41 +0200

    Try to fix problem on CI

--- global-test-setup.js
+++ global-test-setup.js
@@ -2,4 +2,5 @@
 // Seen here https://stackoverflow.com/questions/56261381/how-do-i-set-a-timezone-in-my-jest-config
 module.exports = async () => {
   process.env.TZ = 'UTC';
+  global.Intl = require('intl');
 };
