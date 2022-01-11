10d61269a70b06103bf70476119207772fd4b4b9
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-01-11 16:45:38 +0100

    one level dropdown-menu for main navigation

--- src/components/theme/Navigation/NavItems.jsx
+++ src/components/theme/Navigation/NavItems.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import NavItem from './NavItem';
+import { Dropdown } from 'semantic-ui-react';
 
 const NavItems = ({ items, lang }) => {
   return (
     <>
-      {items.map((item) => (
-        <NavItem item={item} lang={lang} key={item.url} />
-      ))}
+      {items.map((item) =>
+        item && item.items && item.items.length > 0 ? (
+          <Dropdown text={item.title} className="item" key={item.url}>
+            <Dropdown.Menu key={item.url}>
+              {item.items.map((dropdownitem) => (
+                <NavItem
+                  item={dropdownitem}
+                  lang={lang}
+                  key={dropdownitem.url}
+                />
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <NavItem item={item} lang={lang} key={item.url} />
+        ),
+      )}
     </>
   );
 };
