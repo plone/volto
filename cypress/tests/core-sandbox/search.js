00ba4c422179ecbf62377e54b5777fd49c2b0834
kreafox <krisztina.n.elekes@gmail.com>
2022-02-04 16:56:18 +0200

    Fix one more cypress test

--- cypress/tests/core-sandbox/search.js
+++ cypress/tests/core-sandbox/search.js
@@ -32,6 +32,8 @@ context('Search action tests', () => {
       cy.navigate('/newsitem/edit');
 
       // Add subject
+      // because of lazyloading wait for the element to reach an actionable state
+      cy.wait(1000);
       cy.get('.field-wrapper-subjects input')
         .type('garden', { force: true })
         .type('{enter}');
