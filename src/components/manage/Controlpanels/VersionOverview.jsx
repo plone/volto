9183915cfb14226bb2d5700176ccc13a916257ef
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-02-07 11:56:41 +0100

    Fix debug mode

--- src/components/manage/Controlpanels/VersionOverview.jsx
+++ src/components/manage/Controlpanels/VersionOverview.jsx
@@ -22,6 +22,7 @@ const messages = defineMessages({
 
 const VersionOverview = ({
   cmf_version,
+  debug_mode,
   pil_version,
   plone_version,
   plone_restapi_version,
@@ -57,12 +58,14 @@ const VersionOverview = ({
           <li>{`${addon} ${addonsInfo[addon].version || ''}`}</li>
         ))}
       </ul>
-      <p>
-        <FormattedMessage
-          id="Warning Regarding debug mode"
-          defaultMessage="You are running in 'debug mode'. This mode is intended for sites that are under development. This allows many configuration changes to be immediately visible, but will make your site run more slowly. To turn off debug mode, stop the server, set 'debug-mode=off' in your buildout.cfg, re-run bin/buildout and then restart the server process."
-        />
-      </p>
+      {debug_mode !== 'No' && (
+        <p>
+          <FormattedMessage
+            id="Warning Regarding debug mode"
+            defaultMessage="You are running in 'debug mode'. This mode is intended for sites that are under development. This allows many configuration changes to be immediately visible, but will make your site run more slowly. To turn off debug mode, stop the server, set 'debug-mode=off' in your buildout.cfg, re-run bin/buildout and then restart the server process."
+          />
+        </p>
+      )}
     </>
   );
 };
