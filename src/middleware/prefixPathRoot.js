37c06fa568e08357b03df367758222a7bd6f0b2c
Tiberiu Ichim <tiberiuichim@users.noreply.github.com>
2022-03-27 09:30:23 +0300

    Update src/middleware/prefixPathRoot.js

--- src/middleware/prefixPathRoot.js
+++ src/middleware/prefixPathRoot.js
@@ -2,7 +2,7 @@
  * A middleware that prefixes all paths with the relative root path.
  * To use the relative paths, set the RAZZLE_PREFIX_PATH env var.
  *
- * This middleware is meant to be catch route destinations that are hardcoded,
+ * This middleware is meant to catch route destinations that are hardcoded,
  * for example the Logo hardcodes its destination as "/".
  */
 
