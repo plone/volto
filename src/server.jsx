a98dbb42bdece22a00d541c3d6b402b89361ef54
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-04-02 12:11:03 +0200

    Temptative fix

--- src/server.jsx
+++ src/server.jsx
@@ -78,7 +78,7 @@ if (middleware.length) server.use('/', middleware);
 
 server.use(function (err, req, res, next) {
   if (err) {
-    const { store } = req.app.locals;
+    const { store } = res.app.locals;
     const errorPage = (
       <Provider store={store} onError={reactIntlErrorHandler}>
         <StaticRouter context={{}} location={req.url}>
@@ -105,6 +105,78 @@ server.use(function (err, req, res, next) {
 });
 
 function setupServer(req, res, next) {
+  const api = new Api(req);
+
+  const lang = new locale.Locales(
+    req.universalCookies.get('I18N_LANGUAGE') ||
+      config.settings.defaultLanguage ||
+      req.headers['accept-language'],
+  )
+    .best(supported)
+    .toString();
+
+  const initialState = {
+    form: req.body,
+    intl: {
+      defaultLocale: 'en',
+      locale: lang,
+      messages: locales[lang],
+    },
+  };
+
+  const history = createMemoryHistory({
+    initialEntries: [req.url],
+  });
+
+  // Create a new Redux store instance
+  const store = configureStore(initialState, history, api);
+
+  function errorHandler(error) {
+    const errorPage = (
+      <Provider store={store} onError={reactIntlErrorHandler}>
+        <StaticRouter context={{}} location={req.url}>
+          <ErrorPage message={error.message} />
+        </StaticRouter>
+      </Provider>
+    );
+
+    res.set({
+      'Cache-Control': 'public, max-age=60, no-transform',
+    });
+
+    /* Displays error in console
+     * TODO:
+     * - get ignored codes from Plone error_log
+     */
+    const ignoredErrors = [301, 302, 401, 404];
+    if (!ignoredErrors.includes(error.status)) console.error(error);
+
+    res
+      .status(error.status || 500) // If error happens in Volto code itself error status is undefined
+      .send(`<!doctype html> ${renderToString(errorPage)}`);
+  }
+
+  if (!process.env.RAZZLE_API_PATH && req.headers.host) {
+    res.app.locals.detectedHost = `${
+      req.headers['x-forwarded-proto'] || req.protocol
+    }://${req.headers.host}`;
+    config.settings.apiPath = res.app.locals.detectedHost;
+    config.settings.publicURL = res.app.locals.detectedHost;
+  }
+
+  res.app.locals = {
+    ...res.app.locals,
+    store,
+    api,
+    errorHandler,
+  };
+
+  next();
+}
+
+server.get('/*', (req, res) => {
+  const { errorHandler } = res.app.locals;
+
   const api = new Api(req);
 
   const browserdetect = detect(req.headers['user-agent']);
@@ -137,52 +209,6 @@ function setupServer(req, res, next) {
   const store = configureStore(initialState, history, api);
 
   persistAuthToken(store, req);
-
-  function errorHandler(error) {
-    const errorPage = (
-      <Provider store={store} onError={reactIntlErrorHandler}>
-        <StaticRouter context={{}} location={req.url}>
-          <ErrorPage message={error.message} />
-        </StaticRouter>
-      </Provider>
-    );
-
-    res.set({
-      'Cache-Control': 'public, max-age=60, no-transform',
-    });
-
-    /* Displays error in console
-     * TODO:
-     * - get ignored codes from Plone error_log
-     */
-    const ignoredErrors = [301, 302, 401, 404];
-    if (!ignoredErrors.includes(error.status)) console.error(error);
-
-    res
-      .status(error.status || 500) // If error happens in Volto code itself error status is undefined
-      .send(`<!doctype html> ${renderToString(errorPage)}`);
-  }
-
-  if (!process.env.RAZZLE_API_PATH && req.headers.host) {
-    req.app.locals.detectedHost = `${
-      req.headers['x-forwarded-proto'] || req.protocol
-    }://${req.headers.host}`;
-    config.settings.apiPath = req.app.locals.detectedHost;
-    config.settings.publicURL = req.app.locals.detectedHost;
-  }
-
-  req.app.locals = {
-    ...req.app.locals,
-    store,
-    api,
-    errorHandler,
-  };
-
-  next();
-}
-
-server.get('/*', (req, res) => {
-  const { store, api, errorHandler } = req.app.locals;
 
   // @loadable/server extractor
   const buildDir = process.env.BUILD_DIR || 'build';
@@ -249,10 +275,10 @@ server.get('/*', (req, res) => {
                   }
                   criticalCss={readCriticalCss(req)}
                   apiPath={
-                    req.app.locals.detectedHost || config.settings.apiPath
+                    res.app.locals.detectedHost || config.settings.apiPath
                   }
                   publicURL={
-                    req.app.locals.detectedHost || config.settings.publicURL
+                    res.app.locals.detectedHost || config.settings.publicURL
                   }
                 />,
               )}
@@ -268,10 +294,10 @@ server.get('/*', (req, res) => {
                   store={store}
                   criticalCss={readCriticalCss(req)}
                   apiPath={
-                    req.app.locals.detectedHost || config.settings.apiPath
+                    res.app.locals.detectedHost || config.settings.apiPath
                   }
                   publicURL={
-                    req.app.locals.detectedHost || config.settings.publicURL
+                    res.app.locals.detectedHost || config.settings.publicURL
                   }
                 />,
               )}
