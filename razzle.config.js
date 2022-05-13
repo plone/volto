85eb0d34bc8c56d7e291a86604fb460cd25849c6
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-05-13 16:18:39 +0200

    fix static paths in razzle

--- razzle.config.js
+++ razzle.config.js
@@ -348,6 +348,15 @@ const defaultModify = ({
     };
   }
 
+  const prefixPath = process.env.RAZZLE_PREFIX_PATH || '';
+
+  if (prefixPath) {
+    if (target === 'web' && dev) {
+      config.devServer.publicPath = prefixPath;
+    }
+    const pp = config.output.publicPath;
+    config.output.publicPath = `${pp}${prefixPath.slice(1)}/`;
+  }
   return config;
 };
 
