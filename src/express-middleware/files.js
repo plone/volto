8bce92ddd35130731464d0764232430735769cf7
mamico <mauro.amico@gmail.com>
2022-05-24 10:28:20 +0200

    range

--- src/express-middleware/files.js
+++ src/express-middleware/files.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { getAPIResourceWithAuth } from '@plone/volto/helpers';
 
-const HEADERS = ['content-type', 'content-disposition', 'cache-control'];
+const HEADERS = [
+  'cache-control',
+  'content-disposition',
+  'content-range',
+  'content-type',
+];
 
 function fileMiddleware(req, res, next) {
   getAPIResourceWithAuth(req)
