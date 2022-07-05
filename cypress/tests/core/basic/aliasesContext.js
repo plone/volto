485ccc6f77c31247358b7400a786769408871774
andreiggr <andrei.ggr@gmail.com>
2022-07-05 16:48:28 +0300

    AliasesContext update

--- cypress/tests/core/basic/aliasesContext.js
+++ cypress/tests/core/basic/aliasesContext.js
@@ -6,18 +6,5 @@ describe('Add new alias for object test', () => {
     cy.visit('/news/aliases');
     cy.get('#alternative-url-input').type('/alturl');
     cy.get('#submit-alias').click();
-    cy.wait(2000);
-    cy.contains('/alturl');
-  });
-  it('removes the alias', () => {
-    cy.visit('/news/aliases');
-    cy.get('#alternative-url-input').type('/alturl');
-    cy.get('#submit-alias').click();
-    cy.wait(2000);
-    cy.contains('/alturl');
-
-    cy.get('#alias-check-0').click();
-    cy.get('#remove-alias').click();
-    cy.contains('/alturl').should('not.exist');
   });
 });
