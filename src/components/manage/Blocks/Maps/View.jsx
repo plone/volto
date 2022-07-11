76bdc3209e2bc80d33ec4cebadebd6236ac4b1d7
iRohitSingh <isinghrohit0@gmail.com>
2022-07-11 22:14:10 +0530

    remove prefix

--- src/components/manage/Blocks/Maps/View.jsx
+++ src/components/manage/Blocks/Maps/View.jsx
@@ -4,17 +4,11 @@
  */
 
 import React from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { compose } from 'redux';
 import { withBlockExtensions } from '@plone/volto/helpers';
-const messages = defineMessages({
-  EmbededGoogleMaps: {
-    id: 'Embeded Google Maps Of',
-    defaultMessage: 'Embeded Google Maps Of',
-  },
-});
 
 /**
  * View image block class.
@@ -38,9 +32,7 @@ const View = ({ data, intl }) => (
       })}
     >
       <iframe
-        title={`${intl.formatMessage(messages.EmbededGoogleMaps)} ${
-          data.title
-        }`}
+        title={data.title}
         src={data.url}
         className="google-map"
         frameBorder="0"
