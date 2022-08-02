a1080d52a1c5a8c001d4e9d0a98952c7aa19ea2a
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-02 08:29:13 +0200

    actions and reducers

--- src/actions/navroot/navroot.js
+++ src/actions/navroot/navroot.js
@@ -6,9 +6,12 @@ import { GET_NAVROOT } from '@plone/volto/constants/ActionTypes';
  * @returns {Object} Get the Navroot information
  */
 export function getNavroot(url) {
+  console.log('getNavRoot', url);
   return {
     type: GET_NAVROOT,
-    op: 'get',
-    request: `${url}/@navroot`,
+    request: {
+      op: 'get',
+      path: `${url}/@navroot`,
+    },
   };
 }
