61e01d9bcb78903de366b9b5763ea51a98d4d8d3
MdSahil-oss <mohdssahil1@gmail.com>
2022-08-09 17:31:02 +0530

    modified as per @robgietema suggestion

--- src/components/manage/Controlpanels/UndoControlpanel.test.jsx
+++ src/components/manage/Controlpanels/UndoControlpanel.test.jsx
@@ -19,61 +19,56 @@ describe('UndoControlpanel', () => {
         transactions_recieved: [
           {
             description: 'Added default view for root object',
-            id:
-              'QStrR0RRc0M5TjA9\n 2022-06-19T22:15:02 Added default view for root object',
+            id: 'QStrR0RRc0M5TjA9',
             size: 2216,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'Added virtual_hosting',
-            id: 'QStrR0RRcTVrbFU9\n 2022-06-19T22:15:02 Added virtual_hosting',
+            id: 'QStrR0RRcTVrbFU9',
             size: 1369,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'Added site error_log at /error_log',
-            id:
-              'QStrR0RRcVh4cGs9\n 2022-06-19T22:15:02 Added site error_log at /error_log',
+            id: 'QStrR0RRcVh4cGs9',
             size: 1130,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'Added session_data_manager',
-            id:
-              'QStrR0RRcDlFaUk9\n 2022-06-19T22:15:02 Added session_data_manager',
+            id: 'QStrR0RRcDlFaUk9',
             size: 1405,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'Added browser_id_manager',
-            id:
-              'QStrR0RRcFU2KzQ9\n 2022-06-19T22:15:02 Added browser_id_manager',
+            id: 'QStrR0RRcFU2KzQ9',
             size: 840,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'Created initial user',
-            id: 'QStrR0RRbUkzKzQ9\n 2022-06-19T22:15:02 Created initial user',
+            id: 'QStrR0RRbUkzKzQ9',
             size: 403,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: '',
-            id: 'QStrR0RRbEVVa1E9\n 2022-06-19T22:15:02',
+            id: 'QStrR0RRbEVVa1E9',
             size: 717,
             time: '2022-06-19T22:15:02',
             user_name: '',
           },
           {
             description: 'initial database creation',
-            id:
-              'QStrR0ROWlo5YW89\n 2022-06-19T22:14:50 initial database creation',
+            id: 'QStrR0ROWlo5YW89',
             size: 154,
             time: '2022-06-19T22:14:50',
             user_name: '',
