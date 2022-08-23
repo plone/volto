efd04f196bab30904490760921e1d959fbd005c0
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2022-08-23 01:00:42 +0100

    Add test for searching for a user and sharing with them

--- cypress/tests/core/basic/sharing.js
+++ cypress/tests/core/basic/sharing.js
@@ -8,8 +8,7 @@ describe('Sharing Tests', () => {
     cy.waitForResourceToLoad('@actions');
     cy.waitForResourceToLoad('@types');
     cy.waitForResourceToLoad('');
-  });
-  it('As editor I can add a page and access sharing', function () {
+
     // I add a page
     cy.get('#toolbar-add').click();
     cy.get('#toolbar-add-document').click();
@@ -20,7 +19,8 @@ describe('Sharing Tests', () => {
     cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
 
     cy.get('.navigation .item.active').should('have.text', 'My Page');
-
+  });
+  it('As editor I can add a page and access sharing', function () {
     // then I click on the Toolbar > More
     cy.get('#toolbar-more').click();
     cy.get('.menu-more').contains('Sharing');
@@ -29,5 +29,43 @@ describe('Sharing Tests', () => {
     cy.get('.menu-more a[href*="/sharing"]').contains('Sharing').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/my-page/sharing');
     cy.contains('Sharing for');
+  });
+  it('As editor, I can share a page to another user', function () {
+    cy.intercept('/**/@logout').as('logout');
+
+    // Click on the Toolbar > More
+    cy.findByRole('button', { name: /more/i }).click();
+    cy.findByRole('link', { name: /sharing/i }).click();
+    cy.waitForResourceToLoad('@sharing');
+
+    // TODO: Need a unique label here. Site search name is also 'SearchableText'
+    // Search for the test user
+    cy.findByPlaceholderText(/search for user or group/i)
+      .type('test')
+      .closest('form')
+      .submit();
+
+    // Give the test user view permissions
+    cy.findByRole('cell', { name: 'test_user_1_ (test-user)' })
+      .parents('tr')
+      .within(() => {
+        // TODO: Manually pressing the label is needed because the label isn't associated with the input!
+        // See https://github.com/plone/volto/pull/1415 for a fix.
+        // The 'view' role is the 2nd row. Cypress isn't great with tables
+        cy.findAllByRole('checkbox', { value: 'test_user_1_:Reviewer' })
+          .eq(2)
+          .parents('.checkbox')
+          .click();
+      });
+
+    cy.findByRole('button', { name: /save/i }).click();
+    cy.waitForResourceToLoad('@sharing');
+
+    cy.visit('/logout');
+    cy.wait('@logout');
+
+    cy.autologin('test-user');
+    cy.visit('/my-page');
+    cy.findByText(/my page/i).should('exist');
   });
 });
