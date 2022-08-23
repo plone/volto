aa82c6156ca5f7c2e401f933a25d67d99b6c18cc
David Glick <david@glicksoftware.com>
2022-08-22 20:03:05 -0700

    Update @plone/scripts as well

--- razzle.config.js
+++ razzle.config.js
@@ -15,7 +15,7 @@ const fileLoaderFinder = makeLoaderFinder('file-loader');
 
 const projectRootPath = path.resolve('.');
 const languages = require('./src/constants/Languages');
-const { poToJson } = require('./packages/scripts/i18n'); // FIXME
+const { poToJson } = require('@plone/scripts/i18n');
 
 const packageJson = require(path.join(projectRootPath, 'package.json'));
 
