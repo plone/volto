6550447b5806aeadf9bb2d93bf3db1dfb9454c36
nileshgulia1 <nileshgulia@gmail.com>
2022-09-16 13:33:16 +0530

    refactor(menu): use dropdowns instead of selects

--- packages/volto-slate/src/editor/plugins/StyleMenu/StyleMenu.jsx
+++ packages/volto-slate/src/editor/plugins/StyleMenu/StyleMenu.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import { useSlate } from 'slate-react';
-import loadable from '@loadable/component';
+import { Dropdown } from 'semantic-ui-react';
 import { useIntl, defineMessages } from 'react-intl';
 import { isBlockStyleActive, isInlineStyleActive, toggleStyle } from './utils';
 import config from '@plone/volto/registry';
 import { ToolbarButton } from '@plone/volto-slate/editor/ui';
 import paintSVG from '@plone/volto/icons/paint.svg';
 
-const Select = loadable(() => import('react-select'));
-
 const messages = defineMessages({
-  fontStyle: {
+  inlineStyle: {
     id: 'Inline Style',
     defaultMessage: 'Inline Style',
   },
@@ -24,45 +22,45 @@ const messages = defineMessages({
   },
 });
 
-const selectStyles = {
-  input: (provided, state) => {
-    return {
-      ...provided,
-      display: 'none',
-    };
-  },
-  menu: (provided, state) => {
-    return {
-      ...provided,
-      width: '200px',
-    };
-  },
-  option: (provided, state) => {
-    return {
-      ...provided,
-      fontSize: '1rem',
-      cursor: 'pointer',
-      display: 'inline-flex',
-      justifyContent: 'flex-start',
-      verticalAlign: 'middle',
-      ':not:hover': {
-        backgroundColor: null,
-      },
-    };
-  },
-  noOptionsMessage: (provided, state) => {
-    return {
-      ...provided,
-      fontSize: '1rem',
-    };
-  },
-  group: (provided, state) => {
-    return {
-      ...provided,
-      fontSize: '1rem',
-    };
-  },
-};
+// const selectStyles = {
+//   input: (provided, state) => {
+//     return {
+//       ...provided,
+//       display: 'none',
+//     };
+//   },
+//   menu: (provided, state) => {
+//     return {
+//       ...provided,
+//       width: '200px',
+//     };
+//   },
+//   option: (provided, state) => {
+//     return {
+//       ...provided,
+//       fontSize: '1rem',
+//       cursor: 'pointer',
+//       display: 'inline-flex',
+//       justifyContent: 'flex-start',
+//       verticalAlign: 'middle',
+//       ':not:hover': {
+//         backgroundColor: null,
+//       },
+//     };
+//   },
+//   noOptionsMessage: (provided, state) => {
+//     return {
+//       ...provided,
+//       fontSize: '1rem',
+//     };
+//   },
+//   group: (provided, state) => {
+//     return {
+//       ...provided,
+//       fontSize: '1rem',
+//     };
+//   },
+// };
 
 const StyleMenuButton = ({ icon, active, ...props }) => (
   <ToolbarButton {...props} icon={icon} active={active} />
@@ -71,176 +69,208 @@ const StyleMenuButton = ({ icon, active, ...props }) => (
 const StylingsButton = (props) => {
   const editor = useSlate();
   const intl = useIntl();
-  const [open, setOpen] = useState(false);
 
-  // Converting the settings to a format that is required by react-select.
-  const rawOpts = [
+  // Converting the settings to a format that is required by dropdowns.
+  const inlineOpts = [
     ...config.settings.slate.styleMenu.inlineStyles.map((def) => {
       return {
         value: def.cssClass,
-        label: def.label,
+        text: def.label,
         icon: def.icon,
         isBlock: false,
       };
     }),
+  ];
+  const blockOpts = [
     ...config.settings.slate.styleMenu.blockStyles.map((def) => {
       return {
         value: def.cssClass,
-        label: def.label,
+        text: def.label,
         icon: def.icon,
         isBlock: true,
       };
     }),
   ];
 
-  const opts = [
-    {
-      label: intl.formatMessage(messages.paragraphStyle),
-      options: rawOpts.filter((x) => x.isBlock),
-    },
-    {
-      label: intl.formatMessage(messages.fontStyle),
-      options: rawOpts.filter((x) => !x.isBlock),
-    },
-  ];
-
   // Calculating the initial selection.
   const toSelect = [];
   // block styles
-  for (const val of opts[0].options) {
+  for (const val of blockOpts) {
     const ia = isBlockStyleActive(editor, val.value);
     if (ia) {
       toSelect.push(val);
     }
   }
   // inline styles
-  for (const val of opts[1].options) {
+  for (const val of inlineOpts) {
     const ia = isInlineStyleActive(editor, val.value);
     if (ia) {
       toSelect.push(val);
     }
   }
 
-  return rawOpts.length > 0 ? (
-    <Select
-      options={opts}
-      value={toSelect}
-      menuIsOpen={open}
-      onBlur={() => {
-        setOpen(false);
-      }}
-      styles={selectStyles}
-      isMulti={true}
-      hideSelectedOptions={false}
-      components={{
-        Control: (props) => {
-          const {
-            cx,
-            className,
-            isDisabled,
-            isFocused,
-            innerRef,
-            innerProps,
-            menuIsOpen,
-          } = props;
-          return (
-            <StyleMenuButton
-              ref={innerRef}
-              title={intl.formatMessage(messages.additionalStyles)}
-              icon={paintSVG}
-              active={toSelect.length > 0}
-              className={cx(
-                {
-                  control: true,
-                  'control--is-disabled': isDisabled,
-                  'control--is-focused': isFocused,
-                  'control--menu-is-open': menuIsOpen,
-                },
-                className,
-              )}
-              onClick={() => {
-                setOpen(!open);
-              }}
-              {...innerProps}
-            />
-          );
-        },
-        Menu: (props) => {
-          const {
-            children,
-            getStyles,
-            className,
-            innerRef,
-            innerProps,
-          } = props;
-          return (
-            <div
-              ref={innerRef}
-              role="presentation"
-              style={getStyles('menu', props)}
-              className={className}
-              {...innerProps}
-            >
-              {children}
-            </div>
-          );
-        },
-        Option: (props) => {
-          const {
-            getStyles,
-            className,
-            innerRef,
-            data: { icon = null, label = '' } = {},
-            innerProps,
-          } = props;
-          const OptionIcon = icon;
-          return (
-            <div
-              ref={innerRef}
-              style={getStyles('option', props)}
-              className={className}
-              {...innerProps}
-            >
-              <span style={{ paddingRight: '0.5em' }}>
-                {OptionIcon && <OptionIcon {...props} />}
-              </span>
-              <span style={{ verticalAlign: 'middle', lineHeight: 'normal' }}>
-                {label}
-              </span>
-            </div>
-          );
-        },
-        Placeholder: (props) => {
-          return null;
-        },
-        DropdownIndicator: (props) => {
-          return null;
-        },
-      }}
-      theme={(theme) => {
-        return {
-          ...theme,
-          colors: {
-            ...theme.colors,
-            primary: '#68778D', // 100% opaque @brown
-            primary75: '#826A6Abf', // 75% opaque @brown
-            primary50: '#826A6A7f', // 50% opaque @brown
-            primary25: '#826A6A40', // 25% opaque @brown
-            ...(config.settings.slate?.styleMenu?.themeColors || {}),
-          },
-        };
-      }}
-      onChange={(selItem, meta) => {
-        for (const item of rawOpts) {
-          const isRequested = selItem.includes(item);
-          toggleStyle(editor, {
-            cssClass: item.value,
-            isBlock: item.isBlock,
-            isRequested,
-          });
-        }
-      }}
-    ></Select>
+  const showMenu = inlineOpts.length || blockOpts.length;
+
+  return showMenu ? (
+    // <Select
+    //   options={opts}
+    //   value={toSelect}
+    //   menuIsOpen={open}
+    //   onBlur={() => {
+    //     setOpen(false);
+    //   }}
+    //   styles={selectStyles}
+    //   isMulti={true}
+    //   hideSelectedOptions={false}
+    //   components={{
+    //     Control: (props) => {
+    //       const {
+    //         cx,
+    //         className,
+    //         isDisabled,
+    //         isFocused,
+    //         innerRef,
+    //         innerProps,
+    //         menuIsOpen,
+    //       } = props;
+    //       return (
+    //         <StyleMenuButton
+    //           ref={innerRef}
+    //           title={intl.formatMessage(messages.additionalStyles)}
+    //           icon={paintSVG}
+    //           active={toSelect.length > 0}
+    //           className={cx(
+    //             {
+    //               control: true,
+    //               'control--is-disabled': isDisabled,
+    //               'control--is-focused': isFocused,
+    //               'control--menu-is-open': menuIsOpen,
+    //             },
+    //             className,
+    //           )}
+    //           onClick={() => {
+    //             setOpen(!open);
+    //           }}
+    //           {...innerProps}
+    //         />
+    //       );
+    //     },
+    //     Menu: (props) => {
+    //       const {
+    //         children,
+    //         getStyles,
+    //         className,
+    //         innerRef,
+    //         innerProps,
+    //       } = props;
+    //       return (
+    //         <div
+    //           ref={innerRef}
+    //           role="presentation"
+    //           style={getStyles('menu', props)}
+    //           className={className}
+    //           {...innerProps}
+    //         >
+    //           {children}
+    //         </div>
+    //       );
+    //     },
+    //     Option: (props) => {
+    //       const {
+    //         getStyles,
+    //         className,
+    //         innerRef,
+    //         data: { icon = null, label = '' } = {},
+    //         innerProps,
+    //       } = props;
+    //       const OptionIcon = icon;
+    //       return (
+    //         <div
+    //           ref={innerRef}
+    //           style={getStyles('option', props)}
+    //           className={className}
+    //           {...innerProps}
+    //         >
+    //           <span style={{ paddingRight: '0.5em' }}>
+    //             {OptionIcon && <OptionIcon {...props} />}
+    //           </span>
+    //           <span style={{ verticalAlign: 'middle', lineHeight: 'normal' }}>
+    //             {label}
+    //           </span>
+    //         </div>
+    //       );
+    //     },
+    //     Placeholder: (props) => {
+    //       return null;
+    //     },
+    //     DropdownIndicator: (props) => {
+    //       return null;
+    //     },
+    //   }}
+    //   theme={(theme) => {
+    //     return {
+    //       ...theme,
+    //       colors: {
+    //         ...theme.colors,
+    //         primary: '#68778D', // 100% opaque @brown
+    //         primary75: '#826A6Abf', // 75% opaque @brown
+    //         primary50: '#826A6A7f', // 50% opaque @brown
+    //         primary25: '#826A6A40', // 25% opaque @brown
+    //         ...(config.settings.slate?.styleMenu?.themeColors || {}),
+    //       },
+    //     };
+    //   }}
+    //   onChange={(selItem, meta) => {
+    //     for (const item of rawOpts) {
+    //       const isRequested = selItem.includes(item);
+    //       toggleStyle(editor, {
+    //         cssClass: item.value,
+    //         isBlock: item.isBlock,
+    //         isRequested,
+    //       });
+    //     }
+    //   }}
+    // ></Select>
+    <Dropdown
+      multiple
+      disabled={config.settings.slate.styleMenu.disabled ?? false}
+      additionLabel={intl.formatMessage(messages.additionalStyles)}
+      closeOnBlur
+      // onChange={(event, selItem) => {
+      //       for (const item of rawOpts) {
+      //         const isRequested = selItem.includes(item);
+      //         toggleStyle(editor, {
+      //           cssClass: item.value,
+      //           isBlock: item.isBlock,
+      //           isRequested,
+      //         });
+      //       }
+      //     }}
+      trigger={
+        <StyleMenuButton
+          title={intl.formatMessage(messages.additionalStyles)}
+          icon={paintSVG}
+          active={toSelect.length > 0}
+          // onClick={() => {
+          //   setOpen(!open);
+          // }}
+        />
+      }
+    >
+      <Dropdown.Menu>
+        <Dropdown.Header content={intl.formatMessage(messages.inlineStyle)} />
+        {inlineOpts.map((option) => (
+          <Dropdown.Item as="span" active {...option} />
+        ))}
+
+        <Dropdown.Header
+          content={intl.formatMessage(messages.paragraphStyle)}
+        />
+        {blockOpts.map((option) => (
+          <Dropdown.Item active {...option} />
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   ) : (
     ''
   );
