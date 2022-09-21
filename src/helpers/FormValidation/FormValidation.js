97d83b6d7a158ad265ef236f707c424fae8e755b
Jon Pentland <jon.pentland@pretagov.co.uk>
2022-09-21 11:23:16 +0100

    Use regex from WHATWG living standard: https://html.spec.whatwg.org/multipage/input.html#e-mail-state-(type=email)

--- src/helpers/FormValidation/FormValidation.js
+++ src/helpers/FormValidation/FormValidation.js
@@ -40,7 +40,7 @@ const isMinPropertyValid = (value, valueToCompare, maxCriterion, intlFunc) => {
 const widgetValidation = {
   email: {
     isValidEmail: (emailValue, emailObj, intlFunc) => {
-      const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+      const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
       const isValid = emailRegex.test(emailValue);
       return !isValid ? intlFunc(messages.isValidEmail) : null;
     },
