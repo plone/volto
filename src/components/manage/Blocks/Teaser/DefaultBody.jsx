7dd5b316ef6a70dea7b9e2078a6f64beb41099a5
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-10-01 17:31:59 +0200

    First implementation

---
+++ src/components/manage/Blocks/Teaser/DefaultBody.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Message } from 'semantic-ui-react';
+import { defineMessages, useIntl } from 'react-intl';
+import imageBlockSVG from '@plone/volto/components/manage/Blocks/Image/block-image.svg';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { getTeaserImageURL } from './utils';
+import { MaybeWrap } from '@plone/volto/components';
+import { UniversalLink } from '@plone/volto/components';
+import cx from 'classnames';
+
+const messages = defineMessages({
+  PleaseChooseContent: {
+    id: 'Please choose an existing content as source for this element',
+    defaultMessage:
+      'Please choose an existing content as source for this element',
+  },
+});
+
+const TeaserDefaultTemplate = (props) => {
+  const { className, data, isEditMode } = props;
+  const intl = useIntl();
+  const href = data.href?.[0];
+  const image = data.preview_image?.[0];
+
+  return (
+    <div className={cx('block teaser', className)}>
+      <>
+        {!href && isEditMode && (
+          <Message>
+            <div className="grid-teaser-item placeholder">
+              <img src={imageBlockSVG} alt="" />
+              <p>{intl.formatMessage(messages.PleaseChooseContent)}</p>
+            </div>
+          </Message>
+        )}
+        {href && (
+          <MaybeWrap
+            condition={!isEditMode}
+            as={UniversalLink}
+            href={href['@id']}
+            target={data.openLinkInNewTab ? '_blank' : null}
+          >
+            <div className="grid-teaser-item default">
+              {(href.hasPreviewImage || href.image_field || image) && (
+                <div className="grid-image-wrapper">
+                  <img
+                    src={flattenToAppURL(getTeaserImageURL(href, image))}
+                    alt=""
+                    loading="lazy"
+                  />
+                </div>
+              )}
+              <div className="content">
+                {data?.head_title && (
+                  <div className="headline">{data.head_title}</div>
+                )}
+                <h2>{data?.title}</h2>
+                {!data.hide_description && <p>{data?.description}</p>}
+              </div>
+            </div>
+          </MaybeWrap>
+        )}
+      </>
+    </div>
+  );
+};
+
+TeaserDefaultTemplate.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+  isEditMode: PropTypes.bool,
+};
+
+export default TeaserDefaultTemplate;
--- src/config/Blocks.jsx
+++ src/config/Blocks.jsx
@@ -40,6 +40,7 @@ import tableSVG from '@plone/volto/icons/table.svg';
 import listBulletSVG from '@plone/volto/icons/list-bullet.svg';
 import tocSVG from '@plone/volto/icons/list-bullet.svg';
 import searchSVG from '@plone/volto/icons/zoom.svg';
+import imagesSVG from '@plone/volto/icons/images.svg';
 
 import ImageGalleryListingBlockTemplate from '@plone/volto/components/manage/Blocks/Listing/ImageGallery';
 import BlockSettingsSchema from '@plone/volto/components/manage/Blocks/Block/Schema';
@@ -70,6 +71,11 @@ import ListingBlockSchema from '@plone/volto/components/manage/Blocks/Listing/sc
 import SearchBlockSchema from '@plone/volto/components/manage/Blocks/Search/schema';
 
 import ToCVariations from '@plone/volto/components/manage/Blocks/ToC/variations';
+
+import TeaserViewBlock from '@plone/volto/components/manage/Blocks/Teaser/View';
+import TeaserEditBlock from '@plone/volto/components/manage/Blocks/Teaser/Edit';
+import TeaserBlockDefaultBody from '@plone/volto/components/manage/Blocks/Teaser/DefaultBody';
+import { TeaserSchema } from '@plone/volto/components/manage/Blocks/Teaser/schema';
 
 defineMessages({
   title: {
@@ -482,6 +488,26 @@ const blocksConfig = {
         ],
       },
     },
+  },
+  teaser: {
+    id: 'teaser',
+    title: 'Teaser',
+    icon: imagesSVG,
+    group: 'common',
+    view: TeaserViewBlock,
+    edit: TeaserEditBlock,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: 1,
+    blockSchema: TeaserSchema,
+    variations: [
+      {
+        id: 'default',
+        isDefault: true,
+        title: 'Default',
+        template: TeaserBlockDefaultBody,
+      },
+    ],
   },
 };
 
