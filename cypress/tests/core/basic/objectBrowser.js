89264ecbfcd61e83b51eb69aa3df1aa928d2dd38
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-10-05 16:14:33 +0200

    Refine tests for seamless

--- cypress/tests/core/basic/objectBrowser.js
+++ cypress/tests/core/basic/objectBrowser.js
@@ -52,7 +52,7 @@ describe('Object Browser Tests', () => {
     cy.get('.ui.basic.icon.button.image').contains('Image').click();
     cy.get('.toolbar-inner button.ui.basic.icon.button').click();
     cy.findByLabelText('Search SVG').click();
-    cy.get('.ui.input.search').type('http://localhost:55001/my-page-1');
+    cy.get('.ui.input.search').type(`${Cypress.config().baseUrl}/my-page-1`);
     cy.findByLabelText('Select My Image').dblclick();
     cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
