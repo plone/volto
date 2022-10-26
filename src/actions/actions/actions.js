2f0e33414be7c8a6e3509533e623c07a2245a90b
iRohitSingh <isinghrohit0@gmail.com>
2022-10-26 23:10:14 +0530

    this is test

--- src/actions/actions/actions.js
+++ src/actions/actions/actions.js
@@ -10,6 +10,7 @@ import { LIST_ACTIONS } from '@plone/volto/constants/ActionTypes';
  * @function listActions
  * @param {string} url Content url.
  * @returns {Object} List actions action.
+ * 
  */
 export function listActions(url) {
   return {
