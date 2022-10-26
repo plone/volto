daa4f505ab2ef519d08a86734c28090bfab53beb
iRohitSingh <isinghrohit0@gmail.com>
2022-10-26 23:36:30 +0530

    th

--- src/actions/actions/actions.js
+++ src/actions/actions/actions.js
@@ -10,7 +10,6 @@ import { LIST_ACTIONS } from '@plone/volto/constants/ActionTypes';
  * @function listActions
  * @param {string} url Content url.
  * @returns {Object} List actions action.
- * 
  */
 export function listActions(url) {
   return {
