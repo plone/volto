f0057820e0d8fdf122ba49d619b0070a61902bf3
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-11-15 10:55:32 -0600

    Story draft

--- src/components/theme/Logout/Logout.stories.jsx
+++ src/components/theme/Logout/Logout.stories.jsx
@@ -2,25 +2,11 @@ import React from 'react';
 import Logout from './Logout';
 import Wrapper from '@plone/volto/storybook';
 
-const { Provider } = require('react-intl-redux');
-const configureStore = require('redux-mock-store').default;
-const store = configureStore()({
-  userSession: {
-    login: {},
-  },
-  intl: {
-    locale: 'en',
-    messages: {},
-  },
-});
-
 const StoryComponent = (args) => {
   return (
-    <Provider store={store}>
-      <Wrapper>
-        <Logout location={{ pathname: '' }} />
-      </Wrapper>
-    </Provider>
+    <Wrapper>
+      <Logout location={{ pathname: '' }} />
+    </Wrapper>
   );
 };
 
@@ -28,7 +14,6 @@ export const Default = StoryComponent.bind({});
 Default.args = {
   logout: () => {},
   purgeMessages: () => {},
-  location: { query: { return_url: '/' } },
 };
 
 export default {
