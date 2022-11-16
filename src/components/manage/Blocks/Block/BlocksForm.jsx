864e330ce7a87b082e56d8d73ff856a84e37883b
David Glick <david@glicksoftware.com>
2022-11-15 21:59:04 -0800

    move useEvent to helpers

--- src/components/manage/Blocks/Block/BlocksForm.jsx
+++ src/components/manage/Blocks/Block/BlocksForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import EditBlock from './Edit';
 import { DragDropList } from '@plone/volto/components';
 import { getBlocks } from '@plone/volto/helpers';
@@ -15,20 +15,8 @@ import {
 import EditBlockWrapper from './EditBlockWrapper';
 import { setSidebarTab } from '@plone/volto/actions';
 import { useDispatch } from 'react-redux';
-import { useDetectClickOutside } from '@plone/volto/helpers';
+import { useDetectClickOutside, useEvent } from '@plone/volto/helpers';
 import config from '@plone/volto/registry';
-
-const useEvent = (event, handler) => {
-  useEffect(() => {
-    // initiate the event handler
-    document.addEventListener(event, handler);
-
-    // this will clean up the event every time the component is re-rendered
-    return function cleanup() {
-      document.removeEventListener(event, handler);
-    };
-  });
-};
 
 const BlocksForm = (props) => {
   const {
