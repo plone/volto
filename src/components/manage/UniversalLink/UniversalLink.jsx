b77968038690c5d56d41bbe99a9c72f52ad5610d
jackahl <kahl@kitconcept.com>
2022-12-21 17:37:16 +0100

    replace router link with hashlink in Universal link

--- src/components/manage/UniversalLink/UniversalLink.jsx
+++ src/components/manage/UniversalLink/UniversalLink.jsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 import { useSelector } from 'react-redux';
 import {
   flattenToAppURL,
