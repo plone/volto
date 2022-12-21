fa7250cce3ff0b05018142c34037cfd3372c640c
bipoza <bipoza@gmail.com>
2022-12-21 11:38:02 +0100

    Fixes: #3868 - Added the current page parameter in route in Listing and Search block

--- src/components/manage/Blocks/Listing/withQuerystringResults.jsx
+++ src/components/manage/Blocks/Listing/withQuerystringResults.jsx
@@ -30,7 +30,7 @@ export default function withQuerystringResults(WrappedComponent) {
     const [initialPath] = React.useState(getBaseUrl(path));
 
     const copyFields = ['limit', 'query', 'sort_on', 'sort_order', 'depth'];
-
+    const { currentPage, setCurrentPage } = usePagination(querystring, 1);
     const adaptedQuery = Object.assign(
       variation?.fullobjects ? { fullobjects: 1 } : { metadata_fields: '_all' },
       {
@@ -42,7 +42,6 @@ export default function withQuerystringResults(WrappedComponent) {
           : {},
       ),
     );
-    const { currentPage, setCurrentPage } = usePagination(querystring, 1);
     const querystringResults = useSelector(
       (state) => state.querystringsearch.subrequests,
     );
