43d7f310b7f9ebacd4dcded9dd4da870aa5f47c6
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-01-04 10:53:27 +0100

    Improve matching in keyboard slash menu (#4187) (#4216)
    Co-authored-by: David Glick <david@glicksoftware.com>

--- cypress/tests/core/volto-slate/09-block-slate-slashmenu.js
+++ cypress/tests/core/volto-slate/09-block-slate-slashmenu.js
@@ -3,16 +3,12 @@ import { slateBeforeEach } from '../../../support/e2e';
 describe('SlashMenu Test: Shortcuts', () => {
   beforeEach(slateBeforeEach);
 
-  it('As editor I can create a Description block using the SlashMenu shortcut', function () {
-    // Use SlashMenu shortcut to create a Description block and type some text in it
-    cy.getSlateEditorAndType('/Desc').type('{enter}');
-    cy.get('.documentDescription').type('This is a description.');
-    cy.get('.documentDescription').type('{enter}');
-
-    // Save
+  it('As editor I can create a Table block using the SlashMenu shortcut', function () {
+    // Use SlashMenu shortcut to create a table block
+    cy.getSlateEditorAndType('/t').type('{enter}');
     cy.toolbarSave();
 
-    // then the page view should contain a Description with the right text in it
-    cy.get('.documentDescription').contains('This is a description.');
+    // then the page view should contain a table
+    cy.get('#page-document table').should('be.visible');
   });
 });
