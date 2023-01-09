ccc6ec766810bdb8c5b84a0e8174afbbe2571c07
Katja Süss <k.suess@rohberg.ch>
2023-01-09 10:52:57 +0100

    Synchronize redundant block id in listing block on pasting: blocks[blockid].block

--- src/components/manage/Form/BlocksToolbar.jsx
+++ src/components/manage/Form/BlocksToolbar.jsx
@@ -100,7 +100,15 @@ export class BlocksToolbarComponent extends React.Component {
             : [uuid(), blockData]
           : [uuid(), blockData]; // if cut/pasting blocks, we don't clone
       })
-      .filter((info) => !!info); // some blocks may refuse to be copied
+      .filter((info) => !!info) // some blocks may refuse to be copied
+      .map(([id, data]) => {
+        // sync redundant block id in listing block: blocks[blockid].block
+        if (data.block) {
+          let new_data = Object.assign({}, data);
+          new_data.block = id;
+          return [id, new_data];
+        } else return [id, data];
+      });
     const blocksFieldname = getBlocksFieldname(formData);
     const blocksLayoutFieldname = getBlocksLayoutFieldname(formData);
     const selectedIndex =
