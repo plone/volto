1486a083ab777804e4d99c3568b04d40f56f5060
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-02-04 18:06:57 +0100

    Baseline config

---
+++ packages/actions/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaVersion: 'ESNext', // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
---
+++ packages/actions/README.md
@@ -0,0 +1,3 @@
+# @plone/actions (core) add-on
+
+This package is a library of actions to query Plone 6 backend.
