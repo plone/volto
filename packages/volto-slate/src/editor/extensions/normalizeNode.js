ec5183e4ea23b766d638027ab5e16d2103855a6c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-03-21 21:37:39 +0200

    WIP

--- packages/volto-slate/src/editor/extensions/normalizeNode.js
+++ packages/volto-slate/src/editor/extensions/normalizeNode.js
@@ -1,8 +1,8 @@
 import { Text, Transforms, Element, Node } from 'slate'; // Editor,
 import config from '@plone/volto/registry';
+import { isEqual } from 'lodash';
 
 export const normalizeNode = (editor) => {
-  // enforce list rules (no block elements, only ol/ul/li as possible children
   const { normalizeNode } = editor;
   const { slate } = config.settings;
 
@@ -25,45 +25,46 @@ export const normalizeNode = (editor) => {
     }
 
     if (node.type === slate.listItemType) {
+      // don't allow slate default normalizeNode, as it will remove the blocks
+      // when there's combined inline + block elements and the first child is
+      // inline
+      // See https://github.com/ianstormtaylor/slate/blob/1d8010be8500ef5c98dbd4179354db1986d5c8f4/packages/slate/src/create-editor.ts#L253-L256
+
+      const toWrap = [];
       for (const [child, childPath] of Node.children(editor, path)) {
         if (Text.isText(child) || editor.isInline(child)) {
-          // don't allow slate default normalizeNode, as it will remove the
-          // blocks after this
-          const newParent = { type: 'span', children: [] };
-          Transforms.wrapNodes(editor, newParent, { at: childPath });
+          toWrap.push([child, childPath]);
+        } else {
+          break;
+        }
+      }
+
+      const paths = toWrap.map(([, childPath]) => childPath);
+      if (toWrap.length) {
+        const newParent = { type: 'span', children: [] };
+        Transforms.wrapNodes(editor, newParent, {
+          at: path,
+          match: (child, childPath) => {
+            const is = paths.find((p) => isEqual(p, childPath));
+            return !!is;
+          },
+        });
+        return;
+      }
+    }
+
+    if (isElementNode && isListTypeNode) {
+      // lift all child nodes of ul/ol that are not ul/ol/li
+      for (const [child, childPath] of Node.children(editor, path)) {
+        if (
+          !validListElements.includes(child.type) &&
+          !validListElements.includes(node.type)
+        ) {
+          Transforms.liftNodes(editor, { at: childPath, split: true });
           return;
         }
       }
     }
-
-    // console.log(editor.children);
-
-    // if (node.type === slate.listItemType) {
-    //   for (const [child] of Node.children(editor, path)) {
-    //     if (Text.isText(child) || editor.isInline(child)) {
-    //       // don't allow slate default normalizeNode, as it will remove the
-    //       // blocks after this
-    //       return;
-    //     }
-    //   }
-    // }
-
-    // if (isElementNode && isListTypeNode) {
-    //   // lift all child nodes of ul/ol that are not ul/ol/li
-    //   for (const [child, childPath] of Node.children(editor, path)) {
-    //     if (
-    //       !validListElements.includes(child.type) &&
-    //       !validListElements.includes(node.type)
-    //     ) {
-    //       Transforms.liftNodes(editor, { at: childPath, split: true });
-    //
-    //       // Alternate strategy, need to investigate
-    //       // const newParent = { type: slate.defaultBlockType, children: [] };
-    //       // Transforms.wrapNodes(editor, newParent, { at: childPath });
-    //       return;
-    //     }
-    //   }
-    // }
 
     normalizeNode(entry);
   };
