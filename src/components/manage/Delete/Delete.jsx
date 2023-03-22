1a3cf2c1f995e1b6eb3f6bcb24aebee5c54fa9df
TajAlasfiyaa Ishag <87016999+TajAlasfiyaa@users.noreply.github.com>
2023-03-22 11:30:24 +0200

    I've refactored a React class component to a function component

--- src/components/manage/Delete/Delete.jsx
+++ src/components/manage/Delete/Delete.jsx
@@ -1,9 +1,9 @@
+
 /**
  * Delete container.
  * @module components/manage/Delete/Delete
  */
-
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from '@plone/volto/helpers';
 import { connect } from 'react-redux';
@@ -13,7 +13,6 @@ import { Portal } from 'react-portal';
 import { Button, Container, List, Segment } from 'semantic-ui-react';
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 import qs from 'query-string';
-
 import { deleteContent, getContent } from '@plone/volto/actions';
 import { Toolbar } from '@plone/volto/components';
 
@@ -33,160 +32,155 @@ const messages = defineMessages({
 });
 
 /**
- * Delete container class.
- * @class Delete
- * @extends Component
+ * Delete container fuction.
+ * @function Delete
+ * 
  */
-class Delete extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    deleteContent: PropTypes.func.isRequired,
-    getContent: PropTypes.func.isRequired,
-    deleteRequest: PropTypes.shape({
-      loading: PropTypes.bool,
-      loaded: PropTypes.bool,
-    }).isRequired,
-    pathname: PropTypes.string.isRequired,
-    content: PropTypes.shape({
-      title: PropTypes.string,
-    }),
-    returnUrl: PropTypes.string,
-  };
-
-  /**
-   * Default properties
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    content: null,
-    returnUrl: null,
-  };
-
-  /**
-   * Constructor
-   * @method constructor
-   * @param {Object} props Component properties
-   * @constructs WysiwygEditor
-   */
-  constructor(props) {
-    super(props);
-    this.onCancel = this.onCancel.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.state = { isClient: false };
-  }
-
-  /**
-   * Component will receive props
-   * @method componentWillReceiveProps
-   * @param {Object} nextProps Next properties
-   * @returns {undefined}
-   */
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (this.props.deleteRequest.loading && nextProps.deleteRequest.loaded) {
-      this.props.history.push(
-        this.props.returnUrl ||
-          this.props.pathname.replace('/delete', '').replace(/\/[^/]*$/, ''),
-      );
-    }
-  }
+const Delete = ({
+  deleteContent,
+  getContent,
+  deleteRequest,
+  pathname,
+  content,
+  returnUrl,
+  history,
+  location,
+}) => {
+  const [isClient, setIsClient] = useState(false);
+  const intl = useIntl();
 
   /**
    * Component did mount
    * @method componentDidMount
    * @returns {undefined}
    */
-  componentDidMount() {
-    this.props.getContent(this.props.pathname.split('/delete')[0]);
-    this.setState({ isClient: true });
-  }
+  useEffect(() => {
+    getContent(pathname.split('/delete')[0]);
+    setIsClient(true);
+  }, [getContent, pathname]);
+
+  /**
+   * Component will receive props
+   * @method useEffect
+   * @param {Object} nextProps Next properties
+   * @returns {undefined}
+   */
+  useEffect(() => {
+    if (deleteRequest.loading && !deleteRequest.loaded) {
+      history.push(
+        returnUrl ||
+          pathname.replace('/delete', '').replace(/\/[^/]*$/, ''),
+      );
+    }
+  }, [deleteRequest.loading, deleteRequest.loaded, history, pathname, returnUrl]);
 
   /**
    * Submit handler
    * @method onSubmit
    * @returns {undefined}
    */
-  onSubmit() {
-    this.props.deleteContent(this.props.pathname.replace('/delete', ''));
-  }
+  const onSubmit = () => {
+    deleteContent(pathname.replace('/delete', ''));
+  };
 
   /**
    * Cancel handler
    * @method onCancel
    * @returns {undefined}
    */
-  onCancel() {
-    this.props.history.push(this.props.pathname.replace('/delete', ''));
-  }
+  const onCancel = () => {
+    history.push(pathname.replace('/delete', ''));
+  };
 
   /**
-   * Render method.
+   * deprcate Render method.
    * @method render
    * @returns {string} Markup for the component.
    */
-  render() {
-    if (this.props.content) {
-      return (
-        <div id="page-delete">
-          <Helmet title={this.props.intl.formatMessage(messages.delete)} />
-          <Container>
-            <Segment.Group raised>
-              <Segment className="primary">
-                <FormattedMessage
-                  id="Do you really want to delete this item?"
-                  defaultMessage="Do you really want to delete this item?"
-                />
-              </Segment>
-              <Segment attached>
-                <List bulleted>
-                  <List.Item>{this.props.content.title}</List.Item>
-                </List>
-              </Segment>
-              <Segment className="actions" clearing>
-                <Button
-                  basic
-                  circular
-                  primary
-                  floated="right"
-                  icon="arrow right"
-                  aria-label={this.props.intl.formatMessage(messages.ok)}
-                  title={this.props.intl.formatMessage(messages.ok)}
-                  size="big"
-                  onClick={this.onSubmit}
-                />
-                <Button
-                  basic
-                  circular
-                  secondary
-                  icon="remove"
-                  aria-label={this.props.intl.formatMessage(messages.cancel)}
-                  title={this.props.intl.formatMessage(messages.cancel)}
-                  floated="right"
-                  size="big"
-                  onClick={this.onCancel}
-                />
-              </Segment>
-            </Segment.Group>
-          </Container>
-          {this.state.isClient && (
-            <Portal node={document.getElementById('toolbar')}>
-              <Toolbar
-                pathname={this.props.pathname}
-                hideDefaultViewButtons
-                inner={<span />}
+  if (content) {
+    return (
+      <div id="page-delete">
+        <Helmet title={intl.formatMessage(messages.delete)} />
+        <Container>
+          <Segment.Group raised>
+            <Segment className="primary">
+              <FormattedMessage
+                id="Do you really want to delete this item?"
+                defaultMessage="Do you really want to delete this item?"
               />
-            </Portal>
-          )}
-        </div>
-      );
-    }
-    return <div />;
+            </Segment>
+            <Segment attached>
+              <List bulleted>
+                <List.Item>{content.title}</List.Item>
+              </List>
+            </Segment>
+            <Segment className="actions" clearing>
+              <Button
+                basic
+                circular
+                primary
+                floated="right"
+                icon="arrow right"
+                aria-label={intl.formatMessage(messages.ok)}
+                title={intl.formatMessage(messages.ok)}
+                size="big"
+                onClick={onSubmit}
+              />
+              <Button
+                basic
+                circular
+                secondary
+                icon="remove"
+                aria-label={intl.formatMessage(messages.cancel)}
+                title={intl.formatMessage(messages.cancel)}
+                floated="right"
+                size="big"
+                onClick={onCancel}
+              />
+            </Segment>
+          </Segment.Group>
+        </Container>
+        {isClient && (
+          <Portal node={document.getElementById('toolbar')}>
+            <Toolbar
+              pathname={pathname}
+              hideDefaultViewButtons
+              inner={<span />}
+            />
+          </Portal>
+        )}
+      </div>
+    );
   }
-}
+  return <div />;
+};
+
+
+/**
+   * Property types.
+   * @property {Object} propTypes Property types.
+ */
+Delete.propTypes = {
+  deleteContent: PropTypes.func.isRequired,
+  getContent: PropTypes.func.isRequired,
+  deleteRequest: PropTypes.shape({
+    loading: PropTypes.bool,
+    loaded: PropTypes.bool,
+  }).isRequired,
+  pathname: PropTypes.string.isRequired,
+  content: PropTypes.shape({
+    title: PropTypes.string,
+  }),
+  returnUrl: PropTypes.string,
+};
+/**
+     * Default properties
+     * @property {Object} defaultProps Default properties.
+ */
+Delete.defaultProps = {
+  content: null,
+  returnUrl: null,
+};
 
 export default compose(
   withRouter,
