cc9b1cd61b67cb54423812c2d98da744c9cb8e8a
ionlizarazu <ilizarazu@codesyntax.com>
2023-03-30 16:54:22 +0200

    fix and add tests for usePagination.js

--- src/helpers/Utils/usePagination.test.js
+++ src/helpers/Utils/usePagination.test.js
@@ -2,66 +2,34 @@ import { renderHook } from '@testing-library/react-hooks';
 import { usePagination } from './usePagination';
 import * as redux from 'react-redux';
 import routeData from 'react-router';
+import { slugify } from '@plone/volto/helpers/Utils/Utils';
 
-const state = {
+const searchBlockId = '545b33de-92cf-4747-969d-68851837b317';
+const searchBlockId2 = '454b33de-92cf-4747-969d-68851837b713';
+const searchBlock = {
+  '@type': 'search',
+  query: {
+    b_size: '4',
+    query: [
+      {
+        i: 'path',
+        o: 'plone.app.querystring.operation.string.relativePath',
+        v: '',
+      },
+    ],
+    sort_order: 'ascending',
+  },
+  showSearchInput: true,
+  showTotalResults: true,
+};
+let state = {
   content: {
     data: {
       blocks: {
-        '1a4ebb48-8095-4182-98a5-d7ccf5761bd2': {
-          '@type': 'title',
-        },
-        '545b33de-92cf-4747-969d-68851837b317': {
-          '@type': 'search',
-          query: {
-            b_size: '4',
-            query: [
-              {
-                i: 'path',
-                o: 'plone.app.querystring.operation.string.relativePath',
-                v: '',
-              },
-            ],
-            sort_order: 'ascending',
-          },
-          showSearchInput: true,
-          showTotalResults: true,
-        },
-        '454b33de-92cf-4747-969d-68851837b317': {
-          '@type': 'search',
-          query: {
-            b_size: '4',
-            query: [
-              {
-                i: 'path',
-                o: 'plone.app.querystring.operation.string.relativePath',
-                v: '',
-              },
-            ],
-            sort_order: 'ascending',
-          },
-          showSearchInput: true,
-          showTotalResults: true,
-        },
-        '81509424-dcbc-4478-8db0-903c74b28b3d': {
-          '@type': 'slate',
-          plaintext: '',
-          value: [
-            {
-              children: [
-                {
-                  text: '',
-                },
-              ],
-              type: 'p',
-            },
-          ],
-        },
+        [searchBlockId]: searchBlock,
       },
       blocks_layout: {
-        items: [
-          '545b33de-92cf-4747-969d-68851837b317',
-          '81509424-dcbc-4478-8db0-903c74b28b3d',
-        ],
+        items: [searchBlockId],
       },
     },
   },
@@ -72,76 +40,76 @@ let mockUseLocationValue = {
   search: '',
 };
 
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useLocation: () => ({
-//     pathname: 'localhost:3000/example/path',
-//   }),
-// }));
-
-const setUp = (searchParam) => {
+const setUp = (searchParam, numberOfSearches) => {
   mockUseLocationValue.search = searchParam;
-  return renderHook(
-    ({ blockId, defaultPage }) => usePagination(blockId, defaultPage),
-    {
-      initialProps: {
-        blockId: '545b33de-92cf-4747-969d-68851837b317',
-        defaultPage: 1,
-      },
+  if (numberOfSearches > 1) {
+    state.content.data.blocks[searchBlockId2] = searchBlock;
+    state.content.data.blocks_layout.items.push(searchBlockId2);
+  }
+  return renderHook(({ id, defaultPage }) => usePagination(id, defaultPage), {
+    initialProps: {
+      id: searchBlockId,
+      defaultPage: 1,
     },
-  );
+  });
 };
 
-// jest.mock('react-router-dom', () => ({
-//   useHistory: () => ({ replace: jest.fn() }),
-//   useLocation: () => ({
-//     search: jest.fn().mockImplementation(() => {
-//       return mockUseLocationValue;
-//     }),
-//   }),
-// }));
-
-jest.spyOn(redux, 'useSelector').mockImplementation((cb) => cb(state));
-describe('Utils tests', () => {
+describe(`Tests for usePagination, for the block ${searchBlockId}`, () => {
   const useLocation = jest.spyOn(routeData, 'useLocation');
   const useHistory = jest.spyOn(routeData, 'useHistory');
   const useSelector = jest.spyOn(redux, 'useSelector');
   beforeEach(() => {
     useLocation.mockReturnValue(mockUseLocationValue);
     useHistory.mockReturnValue({ replace: jest.fn() });
-    // useSelector.mockReturnValue((cb) => cb(state));
+    useSelector.mockImplementation((cb) => cb(state));
   });
 
-  it('with blockId and defaultPage 1 - shoud be 1', () => {
+  it('1 paginated block with id and defaultPage 1 - shoud be 1', () => {
     const { result } = setUp();
     expect(result.current.currentPage).toBe(1);
   });
 
-  it('without params - shoud be 1', () => {
+  it('1 paginated block without params - shoud be 1', () => {
     const { result } = setUp();
     expect(result.current.currentPage).toBe(1);
   });
 
-  it('with page 2 param - shoud be 2', () => {
-    const { result } = setUp('?page=2');
+  const param1 = '?page=2';
+  it(`1 paginated block with params: ${param1} - shoud be 2`, () => {
+    const { result } = setUp(param1);
     expect(result.current.currentPage).toBe(2);
   });
 
-  it('first time with page 2 - shoud be 2', () => {
-    const { result } = setUp('?page-545b33de-92cf-4747-969d-68851837b317=2');
+  const param2 = `?${slugify(`page-${searchBlockId}`)}=2`;
+  it(`2 paginated blocks with current block in the params: ${param2} - shoud be 2`, () => {
+    const { result } = setUp(param2, 2);
     expect(result.current.currentPage).toBe(2);
+  });
+
+  const param3 = `?${slugify(`page-${searchBlockId2}`)}=2`;
+  it(`2 paginated blocks with the other block in the params: ${param3} - shoud be 1`, () => {
+    const { result } = setUp(param3, 2);
+    expect(result.current.currentPage).toBe(1);
+  });
+
+  const param4 = `?${slugify(`page-${searchBlockId}`)}=2&${slugify(
+    `page-${searchBlockId2}`,
+  )}=1`;
+  it(`2 paginated blocks with both blocks in the params, current 2: ${param4} - shoud be 2`, () => {
+    const { result } = setUp(param4, 2);
+    expect(result.current.currentPage).toBe(2);
+  });
+
+  const param5 = `?${slugify(`page-${searchBlockId}`)}=1&${slugify(
+    `page-${searchBlockId2}`,
+  )}=2`;
+  it(`2 paginated blocks with both blocks in the params, current 1: ${param5} - shoud be 1`, () => {
+    const { result } = setUp(param5, 2);
+    expect(result.current.currentPage).toBe(1);
+  });
+
+  it(`2 paginated blocks with wrong page param: ${param1} - shoud be 1`, () => {
+    const { result } = setUp(param1, 2);
+    expect(result.current.currentPage).toBe(1);
   });
 });
-
-// it('should always return previous state after each update', () => {
-//   const { result, rerender } = setUp();
-
-//   rerender({ state: 2 });
-//   expect(result.current).toBe(0);
-
-//   rerender({ state: 4 });
-//   expect(result.current).toBe(2);
-
-//   rerender({ state: 6 });
-//   expect(result.current).toBe(4);
-// });
