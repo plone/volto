51095a7f1bd10dc0f8bc8cb76ef6485899524d85
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-04-18 09:32:20 +0200

    Refactor faulty reorder elements in ObjectBrowserList widget (#4703)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -86,6 +86,7 @@ export {
   hasApiExpander,
   replaceItemOfArray,
   cloneDeepSchema,
+  reorderArray,
 } from '@plone/volto/helpers/Utils/Utils';
 export { messages } from './MessageLabels/MessageLabels';
 export {
