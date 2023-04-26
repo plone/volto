9ae105003fff9624f3bfe7c9f8184caaa2a29234
Miu Razvan <miu.razvan28@gmail.com>
2023-04-26 18:51:39 +0300

    update

--- src/helpers/Url/Url.js
+++ src/helpers/Url/Url.js
@@ -261,9 +261,9 @@ export const getFieldURL = (data) => {
   let url = data;
   const _isObject = data && isObject(data) && !isArray(data);
   if (_isObject && data['@type'] === 'URL') {
-    url = data['value'] || data['url'] || data['href'] || '';
+    url = data['value'] || data['url'] || data['href'] || data;
   } else if (_isObject) {
-    url = data['@id'] || data['url'] || data['href'] || '';
+    url = data['@id'] || data['url'] || data['href'] || data;
   }
   if (isArray(data)) {
     url = data.map((item) => getFieldURL(item));
