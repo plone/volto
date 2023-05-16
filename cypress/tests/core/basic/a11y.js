dee54eea2c7388e45f9e856c350627ad5f6f993f
iRohitSingh <isinghrohit0@gmail.com>
2023-05-16 20:09:10 +0530

    Add Accessibility acceptance tests for content-types(#770)

--- cypress/tests/core/basic/a11y.js
+++ cypress/tests/core/basic/a11y.js
@@ -1,15 +1,112 @@
+/* eslint-disable prettier/prettier */
 describe('Accessibility Tests', () => {
   beforeEach(() => {
+    cy.autologin();
     cy.visit('/');
     cy.injectAxe(); // make sure axe is available on the page
+
+    cy.waitForResourceToLoad('@navigation');
+    cy.waitForResourceToLoad('@breadcrumbs');
+    cy.waitForResourceToLoad('@actions');
+    cy.waitForResourceToLoad('@types');
+    cy.waitForResourceToLoad('');
   });
 
   it('Front page has not a11y violations', () => {
     cy.checkA11y(); // fail for a11y violations
   });
 
-  it('Contact form has not a11y violations', () => {
+  /*
+     it('Contact form has not a11y violations', () => {
     cy.navigate('/contact-form');
+    */
+
+  //  content-types
+
+  it('File has no a11y violations', () => {
+    // Creating the File content type
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-file').click();
+    cy.get('#field-title').type('File');
+    cy.get('#field-description').type('A11y cypress test for File content type ');
+
+    cy.get('input[id="field-file"]').attachFile('file.pdf', {
+      subjectType: 'input',
+    });
+    cy.wait(1000);
+
+    cy.get('#toolbar-save').focus().click();
+    cy.waitForResourceToLoad('file.pdf');
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+
+  it('Link has no a11y violations', () => {
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-link').click();
+    cy.get('#field-title').type('Link');
+    cy.get('#field-description').type('A11y cypress test for Link content type');
+    cy.get('#field-remoteUrl').type('https://google.com');
+    cy.get('#toolbar-save').click();
+    cy.wait(1000);
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+  it('Image has no a11y violations', () => {
+    // Creating the Image content type
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-image').click();
+    cy.get('#field-title').type('Image ');
+    cy.get('#field-description').type('Image content type ');
+    cy.fixture('image.png', 'base64')
+      .then((fc) => {
+        return Cypress.Blob.base64StringToBlob(fc);
+      })
+      .then((fileContent) => {
+        cy.get('input#field-image').attachFile({ fileContent, fileName: 'image.png', mimeType: 'image/png' }, { subjectType: 'input' });
+        cy.get('#field-image-image').parent().parent().contains('image.png');
+      });
+    cy.get('#toolbar-save').click();
+    cy.wait(1000);
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+
+  it('Event has no a11y violations', () => {
+    // Creating the Event content type
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-event').click();
+    cy.get('.documentFirstHeading').type('My event');
+    cy.get('#toolbar-save').click();
+    cy.wait(1000);
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+  it('Page has no a11y violations', () => {
+    // Creating the Page content type
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-document').click();
+    cy.get('.documentFirstHeading').type('My Page');
+    cy.get('#toolbar-save').click();
+    cy.wait(1000);
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+
+  it('News Item has no a11y violations', () => {
+    // Creating the News Item content type
+    cy.visit('/');
+    cy.get('#toolbar-add').click();
+    cy.get('#toolbar-add-news-item').click();
+    cy.get('.documentFirstHeading').type('My News Item');
+    cy.get('#toolbar-save').click();
+    cy.wait(1000);
+    cy.injectAxe();
     cy.checkA11y();
   });
 
