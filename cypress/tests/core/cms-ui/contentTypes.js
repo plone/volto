372749777991587144720094c44e7580bb1543ed
Wagner Trezub <wagner.trezub@redturtle.it>
2023-05-18 11:26:20 +0200

    drag and drop button re-arranged

--- cypress/tests/core/cms-ui/contentTypes.js
+++ cypress/tests/core/cms-ui/contentTypes.js
@@ -5,7 +5,7 @@ describe('Accessibility Tests', () => {
     cy.injectAxe(); // make sure axe is available on the page
   });
 
-  it('As editor can edit a contentType "News Item" using keyboard', () => {
+  it('As an user can edit, for the first time, a text block inside a new contentType (News Item) using keyboard', () => {
     cy.createContent({
       contentType: 'News Item',
       contentId: 'test-news-item',
@@ -17,8 +17,11 @@ describe('Accessibility Tests', () => {
     cy.waitForResourceToLoad('@breadcrumbs');
     cy.waitForResourceToLoad('@actions');
     cy.waitForResourceToLoad('@types');
-    cy.focused().tab();
+    cy.focused().tab().tab(); //drag-and-drop button -> next text block
     cy.focused().wait(1000).type('New content text');
-    cy.focused().tab().should('have.class', 'delete-button');
+    cy.focused().tab().should('have.class', 'handle wrapper');
   });
+
+  //IMPORTANT TO DO
+  /**focus when the text block is already fullfiled does not active the select class */
 });
