52df7ad8bbee7c4e3bd1131c686cd19d05e3c4d5
Miu Razvan <miu.razvan28@gmail.com>
2023-05-26 16:05:44 +0300

    tests: add unit tests for getFiedURL helper function

--- src/helpers/Url/Url.js
+++ src/helpers/Url/Url.js
@@ -261,9 +261,9 @@ export const getFieldURL = (data) => {
   let url = data;
   const _isObject = data && isObject(data) && !isArray(data);
   if (_isObject && data['@type'] === 'URL') {
-    url = data['value'] || data['url'] || data['href'] || data;
+    url = data['value'] ?? data['url'] ?? data['href'] ?? data;
   } else if (_isObject) {
-    url = data['@id'] || data['url'] || data['href'] || data;
+    url = data['@id'] ?? data['url'] ?? data['href'] ?? data;
   }
   if (isArray(data)) {
     url = data.map((item) => getFieldURL(item));
