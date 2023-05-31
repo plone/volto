222e9b6c0cd71846a06047513df908b98c40434e
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-05-31 11:12:37 +0100

    Strip `++api++` from location changes

--- src/middleware/blacklistRoutes.js
+++ src/middleware/blacklistRoutes.js
@@ -8,18 +8,30 @@ const blacklistRoutes = ({ dispatch, getState }) => (next) => (action) => {
 
   switch (action.type) {
     case '@@router/LOCATION_CHANGE':
-      const { pathname } = action.payload.location;
+      let { pathname } = action.payload.location;
       const { externalRoutes = [] } = config.settings;
 
       const route = externalRoutes.find((route) =>
         matchPath(pathname, route.match),
       );
 
+      let actionToSend = action;
+      if (pathname.startsWith('/++api++')) {
+        actionToSend.payload.location.pathname = actionToSend.payload.location.pathname.substring(
+          8,
+        );
+        // To handle the `window.location.replace`
+        pathname = actionToSend.payload.location.pathname;
+        if (window.history) {
+          window.history.replaceState(window.history.state, '', pathname);
+        }
+      }
+
       if (!route) {
-        return next(action);
+        return next(actionToSend);
       } else {
         window.location.replace(
-          route.url ? route.url(action.payload) : pathname,
+          route.url ? route.url(actionToSend.payload) : pathname,
         );
       }
       break;
