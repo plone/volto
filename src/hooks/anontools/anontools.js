15bc9ad83cca5c08101fa961e2e384aed80b1848
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-07 10:29:45 +0530

    Refactor anontools

--- src/hooks/anontools/anontools.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export function useToken() {
-  return useSelector((state) => state.userSession.token);
-}
-
-export function useContent() {
-  return useSelector((state) => state.content.data);
-}
