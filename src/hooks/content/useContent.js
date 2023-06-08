384cea5bab7de9c72af66c5e769c02430f6ace28
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-06-08 12:40:26 +0200

    Refactored useContent

--- src/hooks/content/useContent.js
+++ src/hooks/content/useContent.js
@@ -1,5 +1,22 @@
 import { useSelector, shallowEqual } from 'react-redux';
 
 export function useContent() {
-  return useSelector((state) => state.content.data, shallowEqual);
+  const data = useSelector((state) => state.content.data, shallowEqual);
+  const loading = useSelector((state) => state.content.get.loading);
+  const loaded = useSelector((state) => state.content.get.loaded);
+  const error = useSelector((state) => state.content.get.error, shallowEqual);
+
+  return { data, loading, loaded, error };
 }
+
+// Potential future useQuery version
+// export function useGetContent() {
+//   // the cache will need to know the current location
+//   const pathname = useLocation();
+//   const query = useQuery(getContentQuery({ path }))
+
+//   // This might not be needed if we rename the properties
+//   const {isLoading: loading, isSuccess: loaded, ...rest} = query;
+
+//   return { loading, loaded, ...rest };
+// }
