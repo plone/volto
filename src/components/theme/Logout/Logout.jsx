262a31c5916ed95ec5016852ef442474ec82ec02
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-09 18:34:42 +0530

    UseMemo() added

--- src/components/theme/Logout/Logout.jsx
+++ src/components/theme/Logout/Logout.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect ,useMemo} from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { defineMessages, useIntl } from 'react-intl';
@@ -23,10 +23,10 @@ const messages = defineMessages({
 const Logout = ({ location, history }) => {
   const token = useToken();
 
-  const returnUrl =
-    qs.parse(location.search).return_url ||
-    location.pathname.replace(/\/login\/?$/, '').replace(/\/logout\/?$/, '') ||
-    '/';
+  const returnUrl =useMemo(()=>    qs.parse(location.search).return_url ||
+  location.pathname.replace(/\/login\/?$/, '').replace(/\/logout\/?$/, '') ||
+  '/'
+,[location])
 
   const dispatch = useDispatch();
   const intl = useIntl();
