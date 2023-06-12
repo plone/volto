91321e027c0b42855e8f5cfcae51e2d7e6f24912
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-12 20:44:15 +0530

    Refactor

--- src/components/theme/View/AlbumView.jsx
+++ src/components/theme/View/AlbumView.jsx
@@ -1,4 +1,4 @@
-import {useState}from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Container, GridColumn, Segment } from 'semantic-ui-react';
 import { Button, Modal, Grid } from 'semantic-ui-react';
@@ -8,50 +8,96 @@ import openSVG from '@plone/volto/icons/open.svg';
 import aheadSVG from '@plone/volto/icons/ahead.svg';
 import backSVG from '@plone/volto/icons/back.svg';
 
+const AlbumView = ({ content }) => {
+  const [openIndex, setopenIndex] = useState(undefined);
 
-const AlbumView =({content})=> {
-
-  const [openIndex,setopenIndex]=useState(undefined);
-
-  const closeModal=()=> {
+  const closeModal = () => {
     setopenIndex(-1);
-  }
+  };
 
-  const nextImage=()=> {
-    const openIndex =
-      (openIndex + 1) % content.items.length;
-      setopenIndex(openIndex);
-  }
+  const nextImage = () => {
+    const openIndex = (openIndex + 1) % content.items.length;
+    setopenIndex(openIndex);
+  };
 
-  const prevImage=()=> {
-    const openIndex =
-      (openIndex - 1) % content.items.length;
-      setopenIndex(openIndex);
-  }
+  const prevImage = () => {
+    const openIndex = (openIndex - 1) % content.items.length;
+    setopenIndex(openIndex);
+  };
 
-
-    return (
-      <Container className="view-wrapper">
-        <article id="content">
-          <header>
-            <h1 className="documentFirstHeading">{content.title}</h1>
-            {content.description && (
-              <p className="documentDescription">{content.description}</p>
-            )}
-          </header>
-          <section id="content-core">
-            <Grid doubling stackable columns={4}>
-              {content.items &&
-                content.items.map((item, index) => (
-                  <React.Fragment key={item.url}>
-                    {item.image_field && (
-                      <Modal
-                        className="gallery"
-                        onClose={closeModal}
-                        open={openIndex === index}
-                        trigger={
-                          <Grid.Column>
-                            <Segment className="imageborder">
+  return (
+    <Container className="view-wrapper">
+      <article id="content">
+        <header>
+          <h1 className="documentFirstHeading">{content.title}</h1>
+          {content.description && (
+            <p className="documentDescription">{content.description}</p>
+          )}
+        </header>
+        <section id="content-core">
+          <Grid doubling stackable columns={4}>
+            {content.items &&
+              content.items.map((item, index) => (
+                <React.Fragment key={item.url}>
+                  {item.image_field && (
+                    <Modal
+                      className="gallery"
+                      onClose={closeModal}
+                      open={openIndex === index}
+                      trigger={
+                        <Grid.Column>
+                          <Segment className="imageborder">
+                            <PreviewImage
+                              item={item}
+                              alt={
+                                item.image_caption
+                                  ? item.image_caption
+                                  : item.title
+                              }
+                              onClick={() => {
+                                setopenIndex(index);
+                              }}
+                              className="ui middle aligned image"
+                            />
+                          </Segment>
+                        </Grid.Column>
+                      }
+                      closeIcon
+                    >
+                      <Modal.Header>
+                        <Grid>
+                          <Grid.Row>
+                            <GridColumn width={10}>{item.title}</GridColumn>
+                            <GridColumn width={2} textAlign="right">
+                              <UniversalLink
+                                href={item.url}
+                                title={item['@type']}
+                                onClick={closeModal}
+                              >
+                                <Icon size="30px" fitted name={openSVG} />
+                              </UniversalLink>
+                            </GridColumn>
+                          </Grid.Row>
+                        </Grid>
+                      </Modal.Header>
+                      <Grid centered verticalAlign="middle">
+                        <Grid.Row>
+                          <Grid.Column width={2} textAlign="center">
+                            <Button
+                              className="gallery noborder"
+                              onClick={nextImage}
+                              style={{ margin: 0 }}
+                            >
+                              <Icon
+                                name={backSVG}
+                                className="circled"
+                                size="30px"
+                                style={{ margin: 0 }}
+                              />
+                            </Button>
+                          </Grid.Column>
+                          <Grid.Column width={8}>
+                            <Modal.Content image>
                               <PreviewImage
                                 item={item}
                                 alt={
@@ -62,94 +108,41 @@ const AlbumView =({content})=> {
                                 onClick={() => {
                                   setopenIndex(index);
                                 }}
-                                className="ui middle aligned image"
+                                size="large"
+                                className="ui image"
                               />
-                            </Segment>
+
+                              <Modal.Description>
+                                <p>{item.description}</p>
+                              </Modal.Description>
+                            </Modal.Content>
                           </Grid.Column>
-                        }
-                        closeIcon
-                      >
-                        <Modal.Header>
-                          <Grid>
-                            <Grid.Row>
-                              <GridColumn width={10}>{item.title}</GridColumn>
-                              <GridColumn width={2} textAlign="right">
-                                <UniversalLink
-                                  href={item.url}
-                                  title={item['@type']}
-                                  onClick={closeModal}
-                                >
-                                  <Icon size="30px" fitted name={openSVG} />
-                                </UniversalLink>
-                              </GridColumn>
-                            </Grid.Row>
-                          </Grid>
-                        </Modal.Header>
-                        <Grid centered verticalAlign="middle">
-                          <Grid.Row>
-                            <Grid.Column width={2} textAlign="center">
-                              <Button
-                                className="gallery noborder"
-                                onClick={nextImage}
+                          <Grid.Column width={2} textAlign="center">
+                            <Button
+                              onClick={nextImage}
+                              className="gallery noborder"
+                              style={{ margin: 0 }}
+                            >
+                              <Icon
+                                name={aheadSVG}
+                                className="circled"
+                                size="30px"
                                 style={{ margin: 0 }}
-                              >
-                                <Icon
-                                  name={backSVG}
-                                  className="circled"
-                                  size="30px"
-                                  style={{ margin: 0 }}
-                                />
-                              </Button>
-                            </Grid.Column>
-                            <Grid.Column width={8}>
-                              <Modal.Content image>
-                                <PreviewImage
-                                  item={item}
-                                  alt={
-                                    item.image_caption
-                                      ? item.image_caption
-                                      : item.title
-                                  }
-                                  onClick={() => {
-                                    setopenIndex(index);
-                                  }}
-                                  size="large"
-                                  className="ui image"
-                                />
-
-                                <Modal.Description>
-                                  <p>{item.description}</p>
-                                </Modal.Description>
-                              </Modal.Content>
-                            </Grid.Column>
-                            <Grid.Column width={2} textAlign="center">
-                              <Button
-                                onClick={nextImage}
-                                className="gallery noborder"
-                                style={{ margin: 0 }}
-                              >
-                                <Icon
-                                  name={aheadSVG}
-                                  className="circled"
-                                  size="30px"
-                                  style={{ margin: 0 }}
-                                />
-                              </Button>
-                            </Grid.Column>
-                          </Grid.Row>
-                        </Grid>
-                      </Modal>
-                    )}
-                  </React.Fragment>
-                ))}
-            </Grid>
-          </section>
-        </article>
-      </Container>
-    );
-  }
-
-
+                              />
+                            </Button>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
+                    </Modal>
+                  )}
+                </React.Fragment>
+              ))}
+          </Grid>
+        </section>
+      </article>
+    </Container>
+  );
+};
 
 AlbumView.propTypes = {
   /**
@@ -198,5 +191,4 @@ AlbumView.propTypes = {
   }).isRequired,
 };
 
- export default AlbumView;
- 
+export default AlbumView;
