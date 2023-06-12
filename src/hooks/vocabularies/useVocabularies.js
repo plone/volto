092bfe814b825aef81472eda3d113eb8b9f15535
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-12 16:47:50 +0530

    Adding facade hook

---
+++ src/hooks/vocabularies/useVocabularies.js
@@ -0,0 +1,10 @@
+import { useSelector, shallowEqual } from 'react-redux';
+const vocabulary = 'plone.app.vocabularies.Keywords';
+
+export function useVocabularies() {
+  const items= useSelector((state) =>  state.vocabularies[vocabulary] && state.vocabularies[vocabulary].items
+  ? state.vocabularies[vocabulary].items
+  : [], shallowEqual);
+
+  return items;
+}
