7950a5621656212f3df9f8ccca4f35d7bf7f8910
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-12 20:35:32 +0530

    prettier

--- src/components/theme/View/LinkView.jsx
+++ src/components/theme/View/LinkView.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { isInternalURL, flattenToAppURL } from '@plone/volto/helpers';
 import { Container } from 'semantic-ui-react';
@@ -6,49 +6,45 @@ import { UniversalLink } from '@plone/volto/components';
 import { FormattedMessage } from 'react-intl';
 import config from '@plone/volto/registry';
 
-
-const LinkView=( { token ,content ,history})=>{
-  
+const LinkView = ({ token, content, history }) => {
   const { remoteUrl } = content;
-  useEffect(()=>{
+  useEffect(() => {
     if (!token) {
       if (isInternalURL(remoteUrl)) {
-         history.replace(flattenToAppURL(remoteUrl));
+        history.replace(flattenToAppURL(remoteUrl));
       } else if (!__SERVER__) {
         window.location.href = flattenToAppURL(remoteUrl);
       }
     }
-  },[history,remoteUrl,token])
+  }, [history, remoteUrl, token]);
 
   const { openExternalLinkInNewTab } = config.settings;
 
-    return (
-      <Container id="page-document">
-        <h1 className="documentFirstHeading">{content.title}</h1>
-        {content.description && (
-          <p className="documentDescription">
-            {content.description}
-          </p>
-        )}
-        {remoteUrl && (
-          <p>
-            <FormattedMessage
-              id="The link address is:"
-              defaultMessage="The link address is:"
-            />{' '}
-            <UniversalLink
-              href={remoteUrl}
-              openLinkInNewTab={
-                openExternalLinkInNewTab && !isInternalURL(remoteUrl)
-              }
-            >
-              {flattenToAppURL(remoteUrl)}
-            </UniversalLink>
-          </p>
-        )}
-      </Container>
-    );
-  }
+  return (
+    <Container id="page-document">
+      <h1 className="documentFirstHeading">{content.title}</h1>
+      {content.description && (
+        <p className="documentDescription">{content.description}</p>
+      )}
+      {remoteUrl && (
+        <p>
+          <FormattedMessage
+            id="The link address is:"
+            defaultMessage="The link address is:"
+          />{' '}
+          <UniversalLink
+            href={remoteUrl}
+            openLinkInNewTab={
+              openExternalLinkInNewTab && !isInternalURL(remoteUrl)
+            }
+          >
+            {flattenToAppURL(remoteUrl)}
+          </UniversalLink>
+        </p>
+      )}
+    </Container>
+  );
+};
 
 LinkView.propTypes = {
   content: PropTypes.shape({
@@ -59,9 +55,9 @@ LinkView.propTypes = {
   token: PropTypes.string,
 };
 
-  LinkView.defaultProps = {
-    content: null,
-    token: null,
-  };
+LinkView.defaultProps = {
+  content: null,
+  token: null,
+};
 
 export default LinkView;
