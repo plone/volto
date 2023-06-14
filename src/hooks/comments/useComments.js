aec97d2bee8e26c75be613d6e6b1b3cdb35aeebc
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-14 12:38:11 +0530

    Prettier

--- src/hooks/comments/useComments.js
+++ src/hooks/comments/useComments.js
@@ -1,7 +1,6 @@
 import { useSelector, shallowEqual } from 'react-redux';
 
 export function useComments() {
-
   const request = useSelector((state) => state.comments.update, shallowEqual);
 
   return { request };
