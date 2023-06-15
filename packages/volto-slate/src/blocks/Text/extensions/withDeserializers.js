e29280ac3843dcf3e18569e99c49e09c5262abfd
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-06-15 21:24:25 +0200

    Enforce max upload size (#4868) (#4889)
    Co-authored-by: David Glick <david@glicksoftware.com>
    Co-authored-by: Timo Stollenwerk <stollenwerk@kitconcept.com>

--- packages/volto-slate/src/blocks/Text/extensions/withDeserializers.js
+++ packages/volto-slate/src/blocks/Text/extensions/withDeserializers.js
@@ -1,7 +1,7 @@
 import isUrl from 'is-url';
 import imageExtensions from 'image-extensions';
 import { blockTagDeserializer } from '@plone/volto-slate/editor/deserialize';
-import { getBaseUrl } from '@plone/volto/helpers';
+import { getBaseUrl, validateFileUploadSize } from '@plone/volto/helpers';
 import { v4 as uuid } from 'uuid';
 import { Transforms } from 'slate';
 
@@ -66,7 +66,9 @@ export const withDeserializers = (editor) => {
     ...editor.dataTransferHandlers,
     files: (files) => {
       const unprocessed = [];
+      const { intl } = editor.getBlockProps();
       for (const file of files) {
+        if (!validateFileUploadSize(file, intl.formatMessage)) return;
         const reader = new FileReader();
         const [mime] = file.type.split('/');
         if (mime === 'image') {
