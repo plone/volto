b13c01d443c8336a25228b62d8df91f972f02ebf
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-16 11:23:36 +0530

    prettier

--- src/hooks/content/useContent.js
+++ src/hooks/content/useContent.js
@@ -2,7 +2,7 @@ import { useSelector, shallowEqual } from 'react-redux';
 
 export function useContent() {
   const content = useSelector((state) => state.content?.data, shallowEqual);
-  const deleteRequest=useSelector((state)=>state.content?.delete);
+  const deleteRequest = useSelector((state) => state.content?.delete);
   const error = useSelector((state) => state.content.get?.error);
   return { content, deleteRequest, error };
 }
