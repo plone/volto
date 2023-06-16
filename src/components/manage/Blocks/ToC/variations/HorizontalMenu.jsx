e66b7c2b2e8d2f07c0aa755c668bcabca0c700b8
Cretu Mihaela <cretu.mihaelaa11@gmail.com>
2023-06-16 17:55:16 +0300

    implemented a partial functionality for a dropdown menu for toc

--- src/components/manage/Blocks/ToC/variations/HorizontalMenu.jsx
+++ src/components/manage/Blocks/ToC/variations/HorizontalMenu.jsx
@@ -3,10 +3,10 @@
  * @module components/manage/Blocks/ToC/View
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { map } from 'lodash';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Dropdown } from 'semantic-ui-react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
@@ -26,12 +26,73 @@ const RenderMenuItems = ({ items }) => {
   });
 };
 
-/**
- * View toc block class.
- * @class View
- * @extends Component
- */
 const View = ({ data, tocEntries }) => {
+  const [fittingItems, setFittingItems] = useState([]);
+  const [nonFittingItems, setNonFittingItems] = useState([]);
+  const menuElement = document.querySelector('.table-of-contents');
+  const containerWidth = menuElement.offsetWidth;
+
+  useEffect(() => {
+    const handleResize = () => {
+      const fittingItems = [];
+      const nonFittingItems = [];
+      const nested = document.querySelectorAll('.responsive-menu .item');
+      let totalWidth = 0;
+
+      console.log({ nested });
+
+      tocEntries.forEach((item, index) => {
+        const menuItemWidth = nested[index]?.offsetWidth;
+        // console.log(
+        //   { totalWidth, containerWidth, menuItemWidth },
+        //   nested[index],
+        // );
+        if (
+          parseInt(totalWidth) + parseInt(menuItemWidth) <=
+          parseInt(containerWidth)
+        ) {
+          fittingItems.push(item);
+          totalWidth = parseInt(totalWidth) + parseInt(menuItemWidth);
+          console.log(
+            { containerWidth, menuItemWidth, totalWidth },
+            nested[index],
+            item.title,
+          );
+        } else {
+          nonFittingItems.push(item);
+        }
+      });
+
+      setFittingItems(fittingItems);
+      setNonFittingItems(nonFittingItems);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [tocEntries, containerWidth]);
+  // console.log({ fittingItems, nonFittingItems });
+  const renderMenuItems = () => {
+    // dropdown visible
+    if (nonFittingItems.length > 0) {
+      return (
+        <>
+          <RenderMenuItems items={fittingItems} />
+          <Dropdown item text="More">
+            <Dropdown.Menu>
+              <RenderMenuItems items={nonFittingItems} />
+            </Dropdown.Menu>
+          </Dropdown>
+        </>
+      );
+    } else {
+      return <RenderMenuItems items={tocEntries} />;
+    }
+  };
+
   return (
     <>
       {data.title && !data.hide_title ? (
@@ -46,18 +107,11 @@ const View = ({ data, tocEntries }) => {
       ) : (
         ''
       )}
-      <Menu>
-        <RenderMenuItems items={tocEntries} />
-      </Menu>
+      <Menu className="responsive-menu">{renderMenuItems()}</Menu>
     </>
   );
 };
 
-/**
- * Property types.
- * @property {Object} propTypes Property types.
- * @static
- */
 View.propTypes = {
   properties: PropTypes.objectOf(PropTypes.any).isRequired,
 };
