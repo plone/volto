f454c46769d881e2dc09e8cb19942865d82f92f9
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-19 15:09:52 +0530

    Assert auth_key is null

--- cypress/tests/core/basic/logout.js
+++ cypress/tests/core/basic/logout.js
@@ -13,8 +13,8 @@ describe('Logout Tests', () => {
     cy.get('body').should('have.class', 'has-toolbar');
   });
   it('As registered user I can logout', function () {
-    cy.getCookie('auth_key').should('not.exist');
     cy.get('#toolbar-personal').click();
     cy.get('#toolbar-logout').click();
+    cy.getCookie('auth_key').should('not.exist');
   });
 });
