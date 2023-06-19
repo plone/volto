fcbea41dbc92bd86305383ec8a989e9a2fe4c196
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-19 12:23:07 +0530

    Fix cypress test

--- cypress/tests/core/basic/contactForm.js
+++ cypress/tests/core/basic/contactForm.js
@@ -4,12 +4,12 @@ describe('Contact Form Tests', () => {
     });
     it('renders contact Form', function () {
       cy.get('a:contains("Contact")').click();
-      cy.get('#fieldset-undefined-field-label-name').type('Myname');
-      cy.get('#fieldset-undefined-field-label-from').type('admin@admin.com');
-      cy.get('#fieldset-undefined-field-label-subject').type('Loreum Ipsium');
-      cy.get('#fieldset-undefined-field-label-message').type(
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      );
+      cy.get('input[name="name"]').type('Myname');
+      cy.get('input[name="from"]').type('admin@admin.com');
+      cy.get('input[name="subject"]').type('Loreum Ipsium');
+      cy.get('textarea[name="message"]').type('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
+     
+  
       cy.get('button[class="ui basic primary right floated button"]').click();
     });
   });
