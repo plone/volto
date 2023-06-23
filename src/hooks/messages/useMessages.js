ab44f56d0b85f2ffde3ca0222ff4bdfa1ebd28af
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-23 09:42:39 +0530

    Refactor messages

---
+++ src/hooks/messages/useMessages.js
@@ -0,0 +1,19 @@
+import { useSelector, shallowEqual } from 'react-redux';
+
+/**
+ * useMessages hook
+ *
+ * This hook returns the current messages that is stored in the Redux store in the
+ * `messages` reducer, and returns it along with the related state (messages).
+ *
+ * @export
+ * @return {{ messages }}
+ */
+export function useMessages() {
+  const messages = useSelector(
+    (state) => state.messages.messages,
+    shallowEqual
+  );
+
+  return { messages };
+}
