90bbe119dc23300b48545c979b85d9d4a702a1cc
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-25 18:58:02 +0530

    Revert

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-export { useClipboard } from '@plone/volto/hooks/clipboard/useClipboard';
+export useClipboard  from '@plone/volto/hooks/clipboard/useClipboard';
 export { useToken } from '@plone/volto/hooks/userSession/useToken';
 export { useContent } from '@plone/volto/hooks/content/useContent';
 export { useClient } from '@plone/volto/hooks/client/useClient';
