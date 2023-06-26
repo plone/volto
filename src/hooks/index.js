19d3c8060c59cf5af486a05cb8a3ebb2af03781c
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-26 12:48:38 +0530

    prettier

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,3 @@
 export { useContent } from '@plone/volto/hooks/content/useContent';
 export { useActions } from '@plone/volto/hooks/actions/useActions';
-export useClipboard   from '@plone/volto/hooks/clipboard/useClipboard';
-
+export useClipboard from '@plone/volto/hooks/clipboard/useClipboard';
