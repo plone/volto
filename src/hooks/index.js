abca8b9a0cb4c713966a08bd6e10b16790561ba0
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-26 11:31:42 +0530

    hooks/index

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,2 @@
-export  useClipboard  from '@plone/volto/hooks/clipboard/useClipboard';
-export  useToken  from '@plone/volto/hooks/userSession/useToken';
+export useClipboard from '@plone/volto/hooks/clipboard/useClipboard';
+export useToken from '@plone/volto/hooks/userSession/useToken';
