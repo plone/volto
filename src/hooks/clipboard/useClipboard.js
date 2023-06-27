91aeabf39087df81db5940b8065ba8d500760702
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-27 18:28:40 +0530

    fix error

--- src/hooks/clipboard/useClipboard.js
+++ src/hooks/clipboard/useClipboard.js
@@ -1,11 +1,9 @@
 import { useState, useRef, useEffect, useCallback } from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
 
 export default function useClipboard(clipboardText = '') {
   const stringToCopy = useRef(clipboardText);
   const [copied, setCopied] = useState(false);
-  const action = useSelector((state) => state.clipboard.action);
-  const source = useSelector((state) => state.clipboard.source, shallowEqual);
+
   //synchronous: window.clipboardData.setData(options.format || "text", text);
   const copyToClipboard = async (text) => {
     if ('clipboard' in navigator) {
