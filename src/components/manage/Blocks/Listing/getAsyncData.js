a9442d3cf542452e78cdb28c961f78cc918a4107
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-06-28 10:02:51 +0200

    Remove anonymous function calls. Remove default exports from. (#4917) (#4928)

--- src/components/manage/Blocks/Listing/getAsyncData.js
+++ src/components/manage/Blocks/Listing/getAsyncData.js
@@ -1,7 +1,12 @@
 import { getQueryStringResults } from '@plone/volto/actions';
 import { resolveBlockExtensions } from '@plone/volto/helpers';
 
-export default ({ dispatch, data, path, blocksConfig }) => {
+export default function getListingBlockAsyncData({
+  dispatch,
+  data,
+  path,
+  blocksConfig,
+}) {
   const { resolvedExtensions } = resolveBlockExtensions(data, blocksConfig);
 
   return [
@@ -18,4 +23,4 @@ export default ({ dispatch, data, path, blocksConfig }) => {
       ),
     ),
   ];
-};
+}
