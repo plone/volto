7737e6181179a77f87310b9cf3858426366c3f04
Tishasoumya-02 <tishasoumya@gmail.com>
2023-06-29 10:18:03 +0530

    refactor

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,1 @@
-export useClipboard from '@plone/volto/hooks/clipboard/useClipboard';
-export useToken from '@plone/volto/hooks/userSession/useToken';
+export { useUsers } from '@plone/volto/hooks/users/useUsers';
