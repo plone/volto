e0dba3d29aab0d2428e63fc44a8a1b1effa8d434
Tishasoumya-02 <tishasoumya@gmail.com>
2023-07-06 14:07:27 +0530

    delete index.js

--- src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { useContent } from '@plone/volto/hooks/content/useContent';
-export { useActions } from '@plone/volto/hooks/actions/useActions';
-export { useCopyContent } from '@plone/volto/hooks/copyContent/useCopyContent';
