ca2d08e2e4ea3153a02a2710ac8be56621c0651e
Tishasoumya-02 <tishasoumya@gmail.com>
2023-07-12 18:58:44 +0530

    Refactor

--- src/components/manage/Controlpanels/ContentType.jsx
+++ src/components/manage/Controlpanels/ContentType.jsx
@@ -1,23 +1,18 @@
-/**
- * Content Type component.
- * @module components/manage/Controlpanels/ContentType
- */
-
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { getParentUrl } from '@plone/volto/helpers';
 import { Portal } from 'react-portal';
 import { Button, Header } from 'semantic-ui-react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 import { toast } from 'react-toastify';
 import { last, nth, join } from 'lodash';
 import { Error, Form, Icon, Toolbar, Toast } from '@plone/volto/components';
 import { getControlpanel, updateControlpanel } from '@plone/volto/actions';
-
+import { usePrevious } from '@plone/volto/helpers';
+import { useClient } from '@plone/volto/hooks';
 import saveSVG from '@plone/volto/icons/save.svg';
 import clearSVG from '@plone/volto/icons/clear.svg';
+import { useHistory, useLocation } from 'react-router';
 
 const messages = defineMessages({
   title: {
@@ -46,228 +41,153 @@ const messages = defineMessages({
   },
 });
 
-/**
- * ContentType class.
- * @class ContentType
- * @extends Component
- */
-class ContentType extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    updateControlpanel: PropTypes.func.isRequired,
-    getControlpanel: PropTypes.func.isRequired,
-    id: PropTypes.string.isRequired,
-    parent: PropTypes.string.isRequired,
-    cpanelRequest: PropTypes.objectOf(PropTypes.any).isRequired,
-    controlpanel: PropTypes.shape({
-      '@id': PropTypes.string,
-      data: PropTypes.object,
-      schema: PropTypes.object,
-      title: PropTypes.string,
-    }),
-    pathname: PropTypes.string.isRequired,
-  };
+const ContentType = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+  const intl = useIntl();
+  const dispatch = useDispatch();
 
-  /**
-   * Default properties.
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    controlpanel: null,
-  };
+  const [visual] = useState(false);
+  const [error, setError] = useState(null);
 
-  /**
-   * Constructor
-   * @method constructor
-   * @param {Object} props Component properties
-   * @constructs ContentType
-   */
-  constructor(props) {
-    super(props);
+  const isClient = useClient();
 
-    this.state = {
-      visual: false,
-      error: null,
-      isClient: false,
-    };
+  const form = useRef();
 
-    this.onCancel = this.onCancel.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.form = React.createRef();
-  }
+  const cpanelRequest = useSelector(
+    (state) => state.controlpanels,
+    shallowEqual,
+  );
+  const controlpanel = useSelector((state) => state.controlpanels.controlpanel);
 
-  /**
-   * Component did mount
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
-    this.props.getControlpanel(join([this.props.parent, this.props.id], '/'));
-    this.setState({ isClient: true });
-  }
+  const id = last(pathname.split('/'));
+  const parent = nth(pathname.split('/'), -2);
 
-  /**
-   * Component will receive props
-   * @method componentWillReceiveProps
-   * @param {Object} nextProps Next properties
-   * @returns {undefined}
-   */
-  UNSAFE_componentWillReceiveProps(nextProps) {
+  const prevcpanelGetRequestLoading = usePrevious(cpanelRequest.get.loading);
+  const prevcpanelUpdateRequestLoading = usePrevious(
+    cpanelRequest.update.loading,
+  );
+
+  useEffect(() => {
+    dispatch(getControlpanel(join([parent, id], '/')));
+  }, [dispatch, id, parent]);
+
+  useEffect(() => {
     // Control Panel GET
-    if (
-      this.props.cpanelRequest.get.loading &&
-      nextProps.cpanelRequest.get.error
-    ) {
-      this.setState({
-        error: nextProps.cpanelRequest.get.error,
-      });
+    if (prevcpanelGetRequestLoading && cpanelRequest.get.error) {
+      setError(cpanelRequest.get.error);
     }
 
     // Control Panel PATCH
-    if (
-      this.props.cpanelRequest.update.loading &&
-      nextProps.cpanelRequest.update.loaded
-    ) {
+    if (prevcpanelUpdateRequestLoading && cpanelRequest.update.loaded) {
       toast.info(
         <Toast
           info
-          title={this.props.intl.formatMessage(messages.info)}
-          content={this.props.intl.formatMessage(messages.changesSaved)}
+          title={intl.formatMessage(messages.info)}
+          content={intl.formatMessage(messages.changesSaved)}
         />,
       );
     }
+  }, [
+    prevcpanelGetRequestLoading,
+    prevcpanelUpdateRequestLoading,
+    cpanelRequest.get.error,
+    cpanelRequest.update.loaded,
+    intl,
+  ]);
+
+  const onSubmit = (data) => {
+    dispatch(updateControlpanel(controlpanel['@id'], data));
+  };
+
+  const onCancel = () => {
+    history.push(getParentUrl(pathname));
+  };
+
+  // Error
+  if (error) {
+    return <Error error={error} />;
   }
 
-  /**
-   * Submit handler
-   * @method onSubmit
-   * @param {object} data Form data.
-   * @returns {undefined}
-   */
-  onSubmit(data) {
-    this.props.updateControlpanel(this.props.controlpanel['@id'], data);
-  }
-
-  /**
-   * Cancel handler
-   * @method onCancel
-   * @returns {undefined}
-   */
-  onCancel() {
-    this.props.history.push(getParentUrl(this.props.pathname));
-  }
-
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    // Error
-    if (this.state.error) {
-      return <Error error={this.state.error} />;
+  if (controlpanel) {
+    if (controlpanel?.data?.filter_content_types === false) {
+      controlpanel.data.filter_content_types = { title: 'all', token: 'all' };
     }
-
-    if (this.props.controlpanel) {
-      let controlpanel = this.props.controlpanel;
-      if (controlpanel?.data?.filter_content_types === false) {
-        controlpanel.data.filter_content_types = { title: 'all', token: 'all' };
+    if (controlpanel?.data?.filter_content_types === true) {
+      if ((controlpanel?.data?.allowed_content_types || []).length) {
+        controlpanel.data.filter_content_types = {
+          title: 'some',
+          token: 'some',
+        };
+      } else {
+        controlpanel.data.filter_content_types = {
+          title: 'none',
+          token: 'none',
+        };
       }
-      if (controlpanel?.data?.filter_content_types === true) {
-        if ((controlpanel?.data?.allowed_content_types || []).length) {
-          controlpanel.data.filter_content_types = {
-            title: 'some',
-            token: 'some',
-          };
-        } else {
-          controlpanel.data.filter_content_types = {
-            title: 'none',
-            token: 'none',
-          };
-        }
-      }
-      return (
-        <div id="page-controlpanel" className="ui container">
-          <Header disabled>
-            {this.props.intl.formatMessage(messages.title, {
-              id: controlpanel.title,
-            })}
-          </Header>
-          <Form
-            isEditForm
-            ref={this.form}
-            schema={controlpanel.schema}
-            formData={controlpanel.data}
-            onSubmit={this.onSubmit}
-            onCancel={this.onCancel}
-            pathname={this.props.pathname}
-            visual={this.state.visual}
-            hideActions
-            loading={this.props.cpanelRequest.update.loading}
-          />
-          {this.state.isClient && (
-            <Portal node={document.getElementById('toolbar')}>
-              <Toolbar
-                pathname={this.props.pathname}
-                hideDefaultViewButtons
-                inner={
-                  <>
-                    <Button
-                      id="toolbar-save"
-                      className="save"
-                      aria-label={this.props.intl.formatMessage(messages.save)}
-                      onClick={() => this.form.current.onSubmit()}
-                      disabled={this.props.cpanelRequest.update.loading}
-                      loading={this.props.cpanelRequest.update.loading}
-                    >
-                      <Icon
-                        name={saveSVG}
-                        className="circled"
-                        size="30px"
-                        title={this.props.intl.formatMessage(messages.save)}
-                      />
-                    </Button>
-                    <Button
-                      className="cancel"
-                      aria-label={this.props.intl.formatMessage(
-                        messages.cancel,
-                      )}
-                      onClick={() => this.onCancel()}
-                    >
-                      <Icon
-                        name={clearSVG}
-                        className="circled"
-                        size="30px"
-                        title={this.props.intl.formatMessage(messages.cancel)}
-                      />
-                    </Button>
-                  </>
-                }
-              />
-            </Portal>
-          )}
-        </div>
-      );
     }
-    return <div />;
+    return (
+      <div id="page-controlpanel" className="ui container">
+        <Header disabled>
+          {intl.formatMessage(messages.title, {
+            id: controlpanel.title,
+          })}
+        </Header>
+        <Form
+          isEditForm
+          ref={form}
+          schema={controlpanel.schema}
+          formData={controlpanel.data}
+          onSubmit={onSubmit}
+          onCancel={onCancel}
+          pathname={pathname}
+          visual={visual}
+          hideActions
+          loading={cpanelRequest.update.loading}
+        />
+        {isClient && (
+          <Portal node={document.getElementById('toolbar')}>
+            <Toolbar
+              pathname={pathname}
+              hideDefaultViewButtons
+              inner={
+                <>
+                  <Button
+                    id="toolbar-save"
+                    className="save"
+                    aria-label={intl.formatMessage(messages.save)}
+                    onClick={() => form.current.onSubmit()}
+                    disabled={cpanelRequest.update.loading}
+                    loading={cpanelRequest.update.loading}
+                  >
+                    <Icon
+                      name={saveSVG}
+                      className="circled"
+                      size="30px"
+                      title={intl.formatMessage(messages.save)}
+                    />
+                  </Button>
+                  <Button
+                    className="cancel"
+                    aria-label={intl.formatMessage(messages.cancel)}
+                    onClick={() => onCancel()}
+                  >
+                    <Icon
+                      name={clearSVG}
+                      className="circled"
+                      size="30px"
+                      title={intl.formatMessage(messages.cancel)}
+                    />
+                  </Button>
+                </>
+              }
+            />
+          </Portal>
+        )}
+      </div>
+    );
   }
-}
+  return <div />;
+};
 
-export default compose(
-  injectIntl,
-  connect(
-    (state, props) => ({
-      controlpanel: state.controlpanels.controlpanel,
-      cpanelRequest: state.controlpanels,
-      pathname: props.location.pathname,
-      id: last(props.location.pathname.split('/')),
-      parent: nth(props.location.pathname.split('/'), -2),
-    }),
-    { getControlpanel, updateControlpanel },
-  ),
-)(ContentType);
+export default ContentType;
