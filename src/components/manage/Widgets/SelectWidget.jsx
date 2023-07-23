fc17d42178453432260ded38bda2e0a5a881121b
Tishasoumya-02 <tishasoumya@gmail.com>
2023-07-23 10:49:05 +0530

    Refactor in progress

--- src/components/manage/Widgets/SelectWidget.jsx
+++ src/components/manage/Widgets/SelectWidget.jsx
@@ -1,14 +1,9 @@
-/**
- * SelectWidget component.
- * @module components/manage/Widgets/SelectWidget
- */
-
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector,useDispatch,shallowEqual } from 'react-redux';
 import { compose } from 'redux';
 import { map } from 'lodash';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 import {
   getVocabFromHint,
   getVocabFromField,
@@ -76,111 +71,62 @@ const messages = defineMessages({
   },
 });
 
-/**
- * SelectWidget component class.
- * @function SelectWidget
- * @returns {string} Markup of the component.
- */
-class SelectWidget extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    required: PropTypes.bool,
-    error: PropTypes.arrayOf(PropTypes.string),
-    getVocabulary: PropTypes.func.isRequired,
-    getVocabularyTokenTitle: PropTypes.func.isRequired,
-    choices: PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-    ),
-    items: PropTypes.shape({
-      vocabulary: PropTypes.object,
-    }),
-    widgetOptions: PropTypes.shape({
-      vocabulary: PropTypes.object,
-    }),
-    value: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string,
-      PropTypes.bool,
-      PropTypes.func,
-      PropTypes.array,
-    ]),
-    onChange: PropTypes.func.isRequired,
-    onBlur: PropTypes.func,
-    onClick: PropTypes.func,
-    onEdit: PropTypes.func,
-    onDelete: PropTypes.func,
-    wrapped: PropTypes.bool,
-    noValueOption: PropTypes.bool,
-    customOptionStyling: PropTypes.any,
-    isMulti: PropTypes.bool,
-    placeholder: PropTypes.string,
-  };
+const SelectWidget=(props)=> {
+  const { id, choices, value, intl, onChange } = props;
 
-  /**
-   * Default properties
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    description: null,
-    required: false,
-    items: {
-      vocabulary: null,
-    },
-    widgetOptions: {
-      vocabulary: null,
-    },
-    error: [],
-    choices: [],
-    value: null,
-    onChange: () => {},
-    onBlur: () => {},
-    onClick: () => {},
-    onEdit: null,
-    onDelete: null,
-    noValueOption: true,
-    customOptionStyling: null,
-  };
+  const dispatch=useDispatch();
+  const vocabBaseUrl = !props.choices
+  ? getVocabFromHint(props) ||
+    getVocabFromField(props) ||
+    getVocabFromItems(props)
+  : '';
 
-  /**
-   * Component did mount
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
+const vocabState =useSelector((state)=>
+  state.vocabularies?.[vocabBaseUrl]?.subrequests?.[state.intl.locale]);
+
+      // If the schema already has the choices in it, then do not try to get the vocab,
+      // even if there is one
+      if (props.choices) {
+        return {
+          choices: props.choices,
+          lang: state.intl.locale,
+        };
+      } else if (vocabState) {
+        return {
+          vocabBaseUrl,
+          choices: vocabState?.items ?? [],
+          lang: state.intl.locale,
+        };
+        // There is a moment that vocabState is not there yet, so we need to pass the
+        // vocabBaseUrl to the component.
+      } else if (vocabBaseUrl) {
+        return {
+          vocabBaseUrl,
+          lang: state.intl.locale,
+        };
+      }
+      
+  useEffect(()=>{
     if (
-      (!this.props.choices || this.props.choices?.length === 0) &&
-      this.props.vocabBaseUrl
+      (!choices || choices?.length === 0) &&
+      props.vocabBaseUrl
     ) {
-      this.props.getVocabulary({
-        vocabNameOrURL: this.props.vocabBaseUrl,
+      dispatch(getVocabulary({
+        vocabNameOrURL: props.vocabBaseUrl,
         size: -1,
-        subrequest: this.props.lang,
-      });
+        subrequest: props.lang,
+      }));
     }
-  }
+  },[]);
 
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    const { id, choices, value, intl, onChange } = this.props;
+   
     const normalizedValue = normalizeValue(choices, value, intl);
     // Make sure that both disabled and isDisabled (from the DX layout feat work)
-    const disabled = this.props.disabled || this.props.isDisabled;
-    const Select = this.props.reactSelect.default;
+    const disabled = props.disabled || props.isDisabled;
+    const Select = props.reactSelect.default;
 
-    let options = this.props.vocabBaseUrl
-      ? this.props.choices
+    let options = props.vocabBaseUrl
+      ? props.choices
       : [
           ...map(choices, (option) => ({
             value: option[0],
@@ -190,22 +136,22 @@ class SelectWidget extends Component {
           })),
           // Only set "no-value" option if there's no default in the field
           // TODO: also if this.props.defaultValue?
-          ...(this.props.noValueOption && !this.props.default
+          ...(props.noValueOption && !props.default
             ? [
                 {
-                  label: this.props.intl.formatMessage(messages.no_value),
+                  label: intl.formatMessage(messages.no_value),
                   value: 'no-value',
                 },
               ]
             : []),
         ];
 
-    const isMulti = this.props.isMulti
-      ? this.props.isMulti
-      : id === 'roles' || id === 'groups' || this.props.type === 'array';
+    const isMulti = props.isMulti
+      ? props.isMulti
+      : id === 'roles' || id === 'groups' || props.type === 'array';
 
     return (
-      <FormFieldWrapper {...this.props}>
+      <FormFieldWrapper {...props}>
         <Select
           id={`field-${id}`}
           key={choices}
@@ -226,12 +172,12 @@ class SelectWidget extends Component {
             MultiValueContainer,
             DropdownIndicator,
             ClearIndicator,
-            Option: this.props.customOptionStyling || Option,
+            Option: props.customOptionStyling || Option,
           }}
           value={normalizedValue}
           placeholder={
-            this.props.placeholder ??
-            this.props.intl.formatMessage(messages.select)
+            props.placeholder ??
+            intl.formatMessage(messages.select)
           }
           onChange={(selectedOption) => {
             if (isMulti) {
@@ -252,8 +198,63 @@ class SelectWidget extends Component {
       </FormFieldWrapper>
     );
   }
-}
 
+SelectWidget.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  required: PropTypes.bool,
+  error: PropTypes.arrayOf(PropTypes.string),
+  getVocabulary: PropTypes.func.isRequired,
+  getVocabularyTokenTitle: PropTypes.func.isRequired,
+  choices: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  ),
+  items: PropTypes.shape({
+    vocabulary: PropTypes.object,
+  }),
+  widgetOptions: PropTypes.shape({
+    vocabulary: PropTypes.object,
+  }),
+  value: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.string,
+    PropTypes.bool,
+    PropTypes.func,
+    PropTypes.array,
+  ]),
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  onClick: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  wrapped: PropTypes.bool,
+  noValueOption: PropTypes.bool,
+  customOptionStyling: PropTypes.any,
+  isMulti: PropTypes.bool,
+  placeholder: PropTypes.string,
+};
+
+SelectWidget.defaultProps = {
+  description: null,
+  required: false,
+  items: {
+    vocabulary: null,
+  },
+  widgetOptions: {
+    vocabulary: null,
+  },
+  error: [],
+  choices: [],
+  value: null,
+  onChange: () => {},
+  onBlur: () => {},
+  onClick: () => {},
+  onEdit: null,
+  onDelete: null,
+  noValueOption: true,
+  customOptionStyling: null,
+};
 export const SelectWidgetComponent = injectIntl(SelectWidget);
 
 export default compose(
