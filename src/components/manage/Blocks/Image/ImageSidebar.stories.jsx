b5f07d48680df393af81f25f7bee432bd9dcb003
Tishasoumya-02 <tishasoumya@gmail.com>
2023-08-01 19:58:02 +0530

    storybook

---
+++ src/components/manage/Blocks/Image/ImageSidebar.stories.jsx
@@ -0,0 +1,48 @@
+import { injectIntl } from 'react-intl';
+import React from 'react';
+import ImageSidebarComponent from './ImageSidebar';
+import { RealStoreWrapper as Wrapper } from '@plone/volto/storybook';
+
+const IntlImageSidebarComponent = injectIntl(ImageSidebarComponent);
+
+function StoryComponent(args) {
+  return (
+    <Wrapper
+      customStore={{
+        content: {
+            create: {},
+            data: {},
+          },
+          intl: {
+            locale: 'en',
+            messages: {},
+          },
+      }}
+    >
+      <div id="toolbar" style={{ display: 'none' }} />
+      <IntlImageSidebarComponent 
+      {...args}    
+      data={{ url: 'https://6.docs.plone.org/_static/logo.svg', alt: 'Plone Logo' }}
+      block="1234"
+      pathname="/news"
+      onChangeBlock={() => {}}
+      openObjectBrowser={() => {}}
+      resetSubmitUrl={() => {}}/>
+    </Wrapper>
+  );
+}
+
+export const ImageSidebar = StoryComponent.bind({});
+
+export default {
+  title: 'Public components/Image/ImageSidebar',
+  component: ImageSidebar,
+  decorators: [
+    (Story) => (
+      <div className="ui segment form attached" style={{ width: '400px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {},
+};
