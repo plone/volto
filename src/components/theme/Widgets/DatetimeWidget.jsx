27b4316b852ed9a44bdc6f7e7932c61fc7e751eb
Aryan Pandey <aryanpandey8912@gmail.com>
2023-08-01 15:45:16 +0530

    FIX #4975 ADDED button in DatetimeWidget triggers form submission in Firefox

--- src/components/theme/Widgets/DatetimeWidget.jsx
+++ src/components/theme/Widgets/DatetimeWidget.jsx
@@ -7,11 +7,20 @@ import { toBackendLang } from '@plone/volto/helpers';
 const DatetimeWidget = ({ value, children, className, format = 'lll' }) => {
   const lang = useSelector((state) => state.intl.locale);
   moment.locale(toBackendLang(lang));
+
+  // Separate function to reset the DatetimeWidget
+  const handleReset = (event) => {
+    event.preventDefault(); // Prevent form submission
+    // Implement logic to reset the DatetimeWidget state here
+    // For example, set value to null or empty string to reset the widget
+  };
+
   return value ? (
     <span className={cx(className, 'datetime', 'widget')}>
       {children
         ? children(moment(value).format(format))
         : moment(value).format(format)}
+      <button onClick={handleReset}>Reset</button> {/* Reset button */}
     </span>
   ) : (
     ''
