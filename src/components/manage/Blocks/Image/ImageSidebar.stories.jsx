ab5ffbf1dee10f4531baf07d3a7f4cc8a00e3f07
Tishasoumya-02 <tishasoumya@gmail.com>
2023-08-10 13:20:03 +0530

    storybook

--- src/components/manage/Blocks/Image/ImageSidebar.stories.jsx
+++ src/components/manage/Blocks/Image/ImageSidebar.stories.jsx
@@ -2,6 +2,7 @@ import { injectIntl } from 'react-intl';
 import React from 'react';
 import ImageSidebarComponent from './ImageSidebar';
 import { RealStoreWrapper as Wrapper } from '@plone/volto/storybook';
+import { action } from '@storybook/addon-actions';
 
 const IntlImageSidebarComponent = injectIntl(ImageSidebarComponent);
 
@@ -21,14 +22,10 @@ function StoryComponent(args) {
     >
       <div id="toolbar" style={{ display: 'none' }} />
       <IntlImageSidebarComponent
-        {...args}
-        data={{
-          url: 'https://6.docs.plone.org/_static/logo.svg',
-          alt: 'Plone Logo',
-        }}
+        data={{ ...args }}
         block="1234"
         pathname="/news"
-        onChangeBlock={() => {}}
+        onChangeBlock={action('Click Handler')}
         openObjectBrowser={() => {}}
         resetSubmitUrl={() => {}}
       />
@@ -38,15 +35,23 @@ function StoryComponent(args) {
 
 export const ImageSidebar = StoryComponent.bind({});
 
+ImageSidebar.args = {
+  url:
+    'https://plone.org/news-and-events/news/2023/plone-6-0-1-released/@@images/image/preview',
+  alt: 'Plone 6',
+};
+
 export default {
   title: 'Public components/Image/ImageSidebar',
   component: ImageSidebar,
+
   decorators: [
     (Story) => (
-      <div className="ui segment form attached" style={{ width: '400px' }}>
+      <div className="ui segment form attached" style={{ width: '600px' }}>
         <Story />
       </div>
     ),
   ],
+
   argTypes: {},
 };
