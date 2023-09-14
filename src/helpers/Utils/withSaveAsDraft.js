5879990167bc831b8bcfad7ebe8dbe24401f0015
rexalex <alex.medesan@gmail.com>
2023-09-14 13:45:10 +0300

    Update src/helpers/Utils/withSaveAsDraft.js
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- src/helpers/Utils/withSaveAsDraft.js
+++ src/helpers/Utils/withSaveAsDraft.js
@@ -22,7 +22,7 @@ const messages = defineMessages({
     id:
       'The version found is less recent than the server, do you want to load it (you can undo if you change your mind)?',
     defaultMessage:
-      'The version found is less recent than the server, do you want to load it (you can undo if you change your mind)?',
+      'The version of the autosaved content I found in your browser is older than that stored on the server. Do you want to load the autosaved content? (You can undo the autosaved content and revert to the server version.)',
   },
 });
 
