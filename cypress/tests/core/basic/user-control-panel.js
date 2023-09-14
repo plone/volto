1af5a3d634a17168cc772724c019fb4528e38aa1
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-09-14 10:11:51 +0200

    User Control Panel improvements (#4572)
    Co-authored-by: Ion Lizarazu <ilizarazu@codesyntax.com>
    Co-authored-by: nileshgulia1 <nileshgulia@gmail.com>
    Co-authored-by: Víctor Fernández de Alba <sneridagh@gmail.com>

--- cypress/tests/core/basic/user-control-panel.js
+++ cypress/tests/core/basic/user-control-panel.js
@@ -36,7 +36,7 @@ describe('User Control Panel Test', () => {
     // same with the same username
     cy.get('input[id="user-search-input"]').clear().type('i');
     cy.get('.icon.button:first').click();
-    cy.get('.fullname').should('have.text', 'Alok Kumar');
+    cy.get('.fullname').should('have.text', 'Alok Kumar (iFlameing)');
   });
 
   it('Should show error from backend when add User fails', () => {
@@ -61,7 +61,7 @@ describe('User Control Panel Test', () => {
     });
   });
 
-  it('Should delete User from controlPanel', () => {
+  it('Should edit and delete User from controlPanel', () => {
     cy.visit('/controlpanel/users');
     cy.waitForResourceToLoad('@navigation');
     cy.waitForResourceToLoad('@breadcrumbs');
@@ -77,15 +77,37 @@ describe('User Control Panel Test', () => {
     cy.get('input[id="field-password"]').clear().type('test@test');
     cy.get('button[title="Save"]').click(-50, -50, { force: true });
 
-    // select first user with name, delete it and search if its exists or not!
-    cy.get('tr:nth-of-type(2) > td.fullname').should('have.text', 'Alok Kumar');
+    // select first user with name and edit
+    cy.get('tr:nth-of-type(2) > td.fullname').should(
+      'have.text',
+      'Alok Kumar (iFlameing)',
+    );
     cy.get('tr:nth-of-type(2) div[role="listbox"]').click();
-    cy.get('tr:nth-of-type(2) div[role="option"]').click();
+    cy.get('tr:nth-of-type(2) div[role="option"]#edit-user-button').click();
+    cy.get('.ui.page.modals .ui.header').contains('Update User');
+    cy.get('.ui.page.modals form input#field-fullname')
+      .clear()
+      .type('Ion Lizarazu');
+    cy.get('.ui.page.modals .actions button[title="Save"]').click();
+    cy.get('input[id="user-search-input"]').clear().type('Ion');
+    cy.get('.icon.button:first').click();
+    cy.get('tr:nth-of-type(1) > td.fullname').should(
+      'have.text',
+      'Ion Lizarazu (iFlameing)',
+    );
+    cy.get('input[id="user-search-input"]').clear();
+    cy.get('.icon.button:first').click();
+
+    // delete it
+    cy.get('tr:nth-of-type(2) div[role="listbox"]').click();
+    cy.get('tr:nth-of-type(2) div[role="option"]#delete-user-button').click();
     cy.contains('Delete User');
     cy.get('button.ui.primary.button').should('have.text', 'OK').click();
-    cy.get('input[id="user-search-input"]').clear().type('i');
+
+    // search if its exists or not!
+    cy.get('input[id="user-search-input"]').clear().type('Ion');
     cy.get('.icon.button:first').click();
-    cy.getIfExists('.fullname').should('not.have.text', 'Alok Kumar');
+    cy.getIfExists('.fullname').should('not.have.text', 'Ion Lizarazu');
   });
 
   it('Should update user roles', () => {
@@ -108,7 +130,7 @@ describe('User Control Panel Test', () => {
       .should('have.class', 'checked');
   });
 });
-describe('User Control Panel test for  many users', () => {
+describe('User Control Panel test for many users', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.autologin();
