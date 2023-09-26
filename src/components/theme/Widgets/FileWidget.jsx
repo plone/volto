14345fec552bdc7fcf88362ace5eea39a10b9ad0
Jon Pentland <jon.pentland@pretagov.co.uk>
2023-09-26 12:42:30 +0100

    Convert components/theme/Widgets/FileWidget.jsx

--- src/components/theme/Widgets/FileWidget.jsx
+++ src/components/theme/Widgets/FileWidget.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
-import { flattenToAppURL } from '@plone/volto/helpers';
+import { useUrlHelpers } from '@plone/volto/helpers';
 import { UniversalLink } from '@plone/volto/components';
 
 const niceBytes = (bytes) => {
@@ -21,6 +21,7 @@ const FileWidget = ({ value, children, className }) => {
     return '';
   }
 
+  const { flattenToAppURL } = useUrlHelpers();
   const url = flattenToAppURL(value.download || value.filename || value);
   const filename = value.filename || url;
   const size = value.data ? value.data.length * 0.75 : value.size || 0;
