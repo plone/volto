b79fd43caf4f3d43ab289e4461f76faac2fd4fba
Jon Pentland <jon.pentland@pretagov.co.uk>
2023-09-26 16:07:18 +0100

    Create other path/url helper functions

--- src/helpers/Url/urlHelpers.js
+++ src/helpers/Url/urlHelpers.js
@@ -1,6 +1,10 @@
 import {
   flattenHTMLToAppURL as classicFlattenHTMLToAppURL,
   flattenToAppURL as classicFlattenToAppURL,
+  toPublicURL as classicToPublicURL,
+  addAppURL as classicAddAppURL,
+  expandToBackendURL as classicExpandToBackendURL,
+  isInternalURL as classicIsInternalURL,
 } from '@plone/volto/helpers';
 import config from '@plone/volto/registry';
 import hoistNonReactStatics from 'hoist-non-react-statics';
@@ -34,6 +38,12 @@ export function useUrlHelpers() {
         .replace(apiHeaders.apiPath, '')
         .replace(apiHeaders.publicURL, '')
     );
+
+  function toPublicURL(url) {
+    if (!hasApiHeaders || !apiHeaders.publicUrl) {
+      return classicToPublicURL(url);
+    }
+    return apiHeaders.publicURL.concat(flattenToAppURL(url));
   }
 
   function flattenHTMLToAppURL(html) {
@@ -47,18 +57,72 @@ export function useUrlHelpers() {
       : html.replace(new RegExp(apiHeaders.apiPath, 'g'), '');
   }
 
-  function stub() {
-    return 'stub';
+  function addAppURL(url) {
+    if (!hasApiHeaders) {
+      return classicAddAppURL(url);
+    }
+    const apiPath = getApiPath();
+    return url.indexOf(apiPath) === 0
+      ? url
+      : `${apiPath}${url}`;
+  }
+
+  function expandToBackendURL(url) {
+    if (!hasApiHeaders) {
+      return classicExpandToBackendURL(url);
+    }
+    const APISUFIX = settings.legacyTraverse ? '' : '/++api++';
+    let adjustedPath;
+    if (path.startsWith('http://') || path.startsWith('https://')) {
+      // flattenToAppURL first if we get a full URL
+      adjustedPath = flattenToAppURL(path);
+    } else {
+      // Next adds a / in front if not a full URL to make sure it's a valid relative path
+      adjustedPath = path[0] !== '/' ? `/${path}` : path;
+    }
+    const apiPath = getApiPath();
+    return `${apiPath}${APISUFIX}${adjustedPath}`;
+  }
+
+  function isInternalURL(url) {
+    if (!hasApiHeaders) {
+      return classicIsInternalURL(url);
+    }
+    const { settings } = config;
+
+    const isMatch = (config.settings.externalRoutes ?? []).find((route) => {
+      if (typeof route === 'object') {
+        return matchPath(flattenToAppURL(url), route.match);
+      }
+      return matchPath(flattenToAppURL(url), route);
+    });
+
+    const isExcluded = isMatch && Object.keys(isMatch)?.length > 0;
+    const apiPath = getApiPath();
+    const internalURL =
+      url &&
+      (url.indexOf(settings.publicURL) !== -1 ||
+        (apiHeaders.internalApiPath &&
+          url.indexOf(apiHeaders.internalApiPath) !== -1) ||
+        url.indexOf(apiPath) !== -1 ||
+        url.charAt(0) === '/' ||
+        url.charAt(0) === '.' ||
+        url.startsWith('#'));
+
+    if (internalURL && isExcluded) {
+      return false;
+    }
+    return internalURL;
   }
 
   return {
     getApiPath: getApiPath,
     flattenToAppURL: flattenToAppURL,
-    toPublicURL: stub,
+    toPublicURL: toPublicURL,
     flattenHTMLToAppURL: flattenHTMLToAppURL,
-    addAppURL: stub,
-    expandToBackendURL: stub,
-    isInternalURL: stub,
+    addAppURL: addAppURL,
+    expandToBackendURL: expandToBackendURL,
+    isInternalURL: isInternalURL,
   };
 }
 
