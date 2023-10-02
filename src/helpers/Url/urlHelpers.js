ce8f066e091f8c6c5b97e86ffa354d1679417de3
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-10-02 14:25:29 +0200

    Use IPV4 in urlHelpers

--- src/helpers/Url/urlHelpers.js
+++ src/helpers/Url/urlHelpers.js
@@ -173,7 +173,7 @@ export function calculateApiPath(headers) {
     }
     // Fallback to the default
     else {
-      apiPathValue = 'http://localhost:8080/Plone';
+      apiPathValue = 'http://127.0.01:8080/Plone';
     }
   }
   // CLIENT
@@ -187,7 +187,7 @@ export function calculateApiPath(headers) {
     } else {
       apiPathValue =
         settings.apiPath === undefined
-          ? 'http://localhost:8080/Plone'
+          ? 'http://127.0.01:8080/Plone'
           : settings.apiPath;
     }
   }
