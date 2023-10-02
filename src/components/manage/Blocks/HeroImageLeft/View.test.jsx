d2dd2b1e362a89b46ce43abd802c3a4246724381
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-10-02 12:27:00 +0200

    A redux few test fixes

--- src/components/manage/Blocks/HeroImageLeft/View.test.jsx
+++ src/components/manage/Blocks/HeroImageLeft/View.test.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
 import renderer from 'react-test-renderer';
 import View from './View';
 
+import { Provider } from 'react-intl-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore();
+const store = mockStore({
+  intl: {
+    locale: 'en',
+    messages: {},
+  },
+});
+
 test('renders a view hero component', () => {
-  const component = renderer.create(<View data={{ url: 'heroimage.jpg' }} />);
+  const component = renderer.create(
+    <Provider store={store}>
+      <View data={{ url: 'heroimage.jpg' }} />
+    </Provider>,
+  );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
 });
