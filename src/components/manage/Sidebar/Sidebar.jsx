a210929c4b1a0534ae1b3b01f766ef1cd98429a5
David Ichim <ichim.david@gmail.com>
2023-10-07 17:38:28 +0200

    Use buttons for sidebar formtabs for proper keyboard and screen reader support

--- src/components/manage/Sidebar/Sidebar.jsx
+++ src/components/manage/Sidebar/Sidebar.jsx
@@ -137,7 +137,10 @@ const Sidebar = (props) => {
           onTabChange={onTabChange}
           panes={[
             !!documentTab && {
-              menuItem: type || intl.formatMessage(messages.document),
+              menuItem: {
+                as: 'button',
+                content: type || intl.formatMessage(messages.document),
+              },
               pane: (
                 <Tab.Pane
                   key="metadata"
@@ -147,7 +150,10 @@ const Sidebar = (props) => {
               ),
             },
             !!blockTab && {
-              menuItem: intl.formatMessage(messages.block),
+              menuItem: {
+                as: 'button',
+                content: intl.formatMessage(messages.block),
+              },
               pane: (
                 <Tab.Pane
                   key="properties"
