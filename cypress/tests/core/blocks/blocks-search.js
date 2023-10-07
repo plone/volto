ba315afb2a70307def8328a1dec96160585917f5
Ion Lizarazu <ilizarazu@codesyntax.com>
2023-10-07 18:38:25 +0200

    feat: add cypress test for search block via url

--- cypress/tests/core/blocks/blocks-search.js
+++ cypress/tests/core/blocks/blocks-search.js
@@ -113,6 +113,32 @@ describe('Search Block Tests', () => {
     );
     // clear facets
     cy.get('.block.search .filter-list-header .ui.button').click();
+    cy.url().should(
+      'not.contain',
+      '%7B%22i%22%3A%22portal_type%22%2C%22o%22%3A%22paqo.list.contains%22%2C%22v%22%3A%5B%22Event%22%5D%7D',
+    );
+
+    // navigate to the searched url
+    cy.navigate(
+      '/my-search-page?query=%5B%7B%22i%22%3A%22portal_type%22%2C%22o%22%3A%22paqo.list.contains%22%2C%22v%22%3A%5B%22Event%22%5D%7D%5D',
+    );
+    cy.reload();
+    cy.wait(500);
+    cy.get('.search-details').should('contain', 'Search results: 1');
+
+    //navigate to home
+    cy.navigate('/');
+    cy.wait(500);
+
+    // navigate to the searched url
+    cy.navigate(
+      // '/my-search-page?query=%5B%7B%22i%22%3A%22portal_type%22%2C%22o%22%3A%22paqo.list.contains%22%2C%22v%22%3A%5B%22Event%22%5D%7D%5D',
+      '/my-search-page?query=%5B%7B%22i%22%3A%22portal_type%22%2C%22o%22%3A%22paqo.list.contains%22%2C%22v%22%3A%5B%22Event%22%5D%7D%5D',
+    );
+    cy.get('.search-details').should('contain', 'Search results: 1');
+
+    cy.reload();
+    cy.get('.search-details').should('contain', 'Search results: 1');
   });
 
   it('Search block - test date range facet', () => {
@@ -173,11 +199,7 @@ describe('Search Block Tests', () => {
     );
   });
 
-  // Sept-2023 - @sneridagh:
-  // Skipped for now, it behaves quite weird and different than the "live" one
-  // Still due to be investigated, I could not pinpoint why
-  // See: https://github.com/plone/volto/issues/5219
-  it.skip('Search block - test live searchbox', () => {
+  it('Search block - test live searchbox', () => {
     cy.visit('/');
     cy.get('#toolbar-add > .icon').click();
     cy.get('#toolbar-add-document').click();
