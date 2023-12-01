5887224ff8133495d681a7c9c25f04dda6f992bd
Victor Fernandez de Alba <sneridagh@gmail.com>
2023-12-01 21:04:23 +0100

    Improve the prerelease script

--- packages/scripts/prepublish.js
+++ packages/scripts/prepublish.js
@@ -1,24 +1,65 @@
 import { Plugin } from 'release-it';
 
+const NPM_BASE_URL = 'https://www.npmjs.com';
+const NPM_PUBLIC_PATH = 'package';
+
 class PrePublishReleaseItPlugin extends Plugin {
   constructor(...args) {
     super(...args);
+
     this.registerPrompts({
       prePublishPrompt: {
         type: 'confirm',
-        message: () => 'Are you sure?',
+        message: (context) =>
+          `Are you sure you want to publish ${context.npm.name}${
+            context.npm.tag === null || context.npm.tag === 'latest'
+              ? ''
+              : `@${context.npm.tag}`
+          } to npm?`,
+        default: true,
       },
     });
   }
 
-  async release() {
+  getName() {
+    return this.config.getContext().npm.name;
+  }
+
+  getPackageUrl() {
+    const baseUrl = NPM_BASE_URL;
+    const publicPath = NPM_PUBLIC_PATH;
+    return `${baseUrl}/${publicPath}/${this.getName()}`;
+  }
+
+  async beforeRelease() {
+    const context = this.config.getContext();
+    const tag =
+      context.npm.tag === null || context.npm.tag === 'latest'
+        ? ''
+        : context.npm.tag;
+
     await this.step({
       enabled: true,
       task: () =>
-        this.exec('pnpm publish --dry-run', { options: { write: false } }),
-      label: 'Doing task',
+        this.exec(
+          `pnpm publish${tag ? ` --tag ${tag}` : ''} --dry-run --no-git-checks`,
+          {
+            options: { write: false },
+          },
+        ).then(() => {
+          this.setContext({ isReleased: true });
+        }),
+      label: 'Publishing to npm...',
       prompt: 'prePublishPrompt',
     });
+  }
+
+  afterRelease() {
+    const { isReleased } = this.getContext();
+    console.log();
+    if (isReleased) {
+      this.log.log(`🔗 ${this.getPackageUrl()}`);
+    }
   }
 }
 
