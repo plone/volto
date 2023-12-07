5fa6e6dfe64418cdf5e721e81e576163d1bb2603
Víctor Fernández de Alba <sneridagh@gmail.com>
2023-12-07 18:01:18 +0100

    Add conditional variations support (#5424)
    Co-authored-by: Rob Gietema <rob.gietema@gmail.com>
    Co-authored-by: David Glick <david@glicksoftware.com>

--- packages/coresandbox/src/components/Blocks/TestBlock/Data.jsx
+++ packages/coresandbox/src/components/Blocks/TestBlock/Data.jsx
@@ -2,7 +2,8 @@ import { useIntl } from 'react-intl';
 import { BlockDataForm } from '@plone/volto/components';
 
 const TestBlockData = (props) => {
-  const { block, blocksConfig, data, onChangeBlock } = props;
+  const { block, blocksConfig, contentType, data, navRoot, onChangeBlock } =
+    props;
   const intl = useIntl();
   const schema = blocksConfig[data['@type']].blockSchema({ intl });
 
@@ -19,6 +20,9 @@ const TestBlockData = (props) => {
       }}
       onChangeBlock={onChangeBlock}
       formData={data}
+      blocksConfig={blocksConfig}
+      navRoot={navRoot}
+      contentType={contentType}
     />
   );
 };
