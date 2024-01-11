7b436ddc8aa59762961d7373a55576de2baec22c
Dhruv Gupta <gdhruv579@gmail.com>
2024-01-11 14:58:45 +0530

    I've replaced the class component with a functiona l component. I'm using the useState hook for managing state (selectedOption and search). I've replaced the componentDidMount lifecycle method with the useEffect hook. I'm using the useIntl hook for internationalization. Removed the propTypes and defaultProps static properties and placed them outside the component.

--- packages/volto/src/components/manage/Blocks/Search/widgets/SelectMetadataField.jsx
+++ packages/volto/src/components/manage/Blocks/Search/widgets/SelectMetadataField.jsx
@@ -1,17 +1,9 @@
-/**
- * SelectWidget component.
- * @module components/manage/Widgets/SelectWidget
- */
-
-import { map, intersection, filter, toPairs, groupBy } from 'lodash';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { map, intersection, filter, toPairs, groupBy } from 'lodash';
 import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
 import { compose } from 'redux';
-
-import { FormFieldWrapper } from '@plone/volto/components';
-import withQueryString from './../hocs/withQueryString';
-import { defineMessages } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 
 import {
   Option,
@@ -19,6 +11,11 @@ import {
   selectTheme,
   customSelectStyles,
 } from '@plone/volto/components/manage/Widgets/SelectStyling';
+
+import {
+  FormFieldWrapper,
+  withQueryString,
+} from '@plone/volto/components';
 
 const identity = (a) => a;
 
@@ -28,106 +25,65 @@ const messages = defineMessages({
     defaultMessage: 'Select…',
   },
 });
-
 /**
  * SelectWidget component class.
  * @function SelectWidget
- * @returns {string} Markup of the component.
+ *  
+ * @returns {string} Markup of the component 
  */
-class SelectWidget extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    required: PropTypes.bool,
-    error: PropTypes.arrayOf(PropTypes.string),
-    loading: PropTypes.bool,
-    value: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string,
-      PropTypes.bool,
-    ]),
-    onChange: PropTypes.func.isRequired,
-    onBlur: PropTypes.func,
-    onClick: PropTypes.func,
-    onEdit: PropTypes.func,
-    onDelete: PropTypes.func,
-    wrapped: PropTypes.bool,
-    querystring: PropTypes.object,
-  };
+const SelectWidget = ({
+  id,
+  title,
+  description,
+  required,
+  error,
+  loading,
+  value,
+  onChange,
+  onBlur,
+  onClick,
+  onEdit,
+  onDelete,
+  wrapped,
+  querystring,
+  reactSelect,
+}) => {
+  const intl = useIntl();
 
-  /**
-   * Default properties
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    description: null,
-    required: false,
-    items: {
-      vocabulary: null,
-    },
-    widgetOptions: {
-      vocabulary: null,
-    },
-    error: [],
-    choices: [],
-    loading: false,
-    value: null,
-    onChange: () => {},
-    onBlur: () => {},
-    onClick: () => {},
-    onEdit: null,
-    onDelete: null,
-  };
+  const [selectedOption, setSelectedOption] = useState(
+    value
+      ? { label: value.title, value: value.value }
+      : {}
+  );
 
-  state = {
-    selectedOption: this.props.value
-      ? { label: this.props.value.title, value: this.props.value.value }
-      : {},
-  };
+  const [search, setSearch] = useState('');
 
-  /**
-   * Component did mount
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
-    if (!this.props.choices && this.props.vocabBaseUrl) {
-      this.props.getVocabulary({ vocabNameOrURL: this.props.vocabBaseUrl });
+  useEffect(() => {
+    if (!reactSelect.choices && reactSelect.vocabBaseUrl) {
+      reactSelect.getVocabulary({
+        vocabNameOrURL: reactSelect.vocabBaseUrl,
+      });
     }
-  }
+  }, [reactSelect.choices, reactSelect.vocabBaseUrl]);
 
-  /**
-   * Initiate search with new query
-   * @method loadOptions
-   * @param {string} search Search query.
-   * @param {string} previousOptions The previous options rendered.
-   * @param {string} additional Additional arguments to pass to the next loadOptions.
-   * @returns {undefined}
-   */
-  loadOptions = (search, previousOptions, additional) => {
-    let hasMore = this.props.itemsTotal > previousOptions.length;
+  const loadOptions = (search, previousOptions, additional) => {
+    let hasMore = reactSelect.itemsTotal > previousOptions.length;
+
     if (hasMore) {
-      const offset = this.state.search !== search ? 0 : additional.offset;
-      this.props.getVocabulary({
-        vocabNameOrURL: this.props.vocabBaseUrl,
+      const offset = search !== search ? 0 : additional.offset;
+      reactSelect.getVocabulary({
+        vocabNameOrURL: reactSelect.vocabBaseUrl,
         query: search,
         start: offset,
       });
-      this.setState({ search });
+      setSearch(search);
 
       return {
         options:
-          intersection(previousOptions, this.props.choices).length ===
-          this.props.choices.length
+          intersection(previousOptions, reactSelect.choices).length ===
+          reactSelect.choices.length
             ? []
-            : this.props.choices,
+            : reactSelect.choices,
         hasMore: hasMore,
         additional: {
           offset: offset === additional.offset ? offset + 25 : offset,
@@ -137,80 +93,122 @@ class SelectWidget extends Component {
     return null;
   };
 
-  /* Customized to pass object instead of plain string value */
-  handleChange = (selectedOption) => {
-    this.setState({ selectedOption });
-    this.props.onChange(this.props.id, {
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+    onChange(id, {
       value: selectedOption.value,
       title: selectedOption.label,
     });
   };
 
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    const {
-      id,
-      // choices,
-      value,
-      onChange,
-      placeholder,
-      querystring,
-      filterOptions = identity,
-    } = this.props;
-    const isDisabled = false;
-    const { indexes = [] } = querystring;
-
-    const Select = this.props.reactSelect.default;
-
-    return (
-      <FormFieldWrapper {...this.props}>
-        <Select
-          id={`field-${id}`}
-          name={id}
-          placeholder={
-            placeholder ?? this.props.intl.formatMessage(messages.select)
-          }
-          isDisabled={isDisabled}
-          className="react-select-container"
-          classNamePrefix="react-select"
-          options={map(
-            toPairs(
-              groupBy(toPairs(filterOptions(indexes)), (item) => item[1].group),
+  return (
+    <FormFieldWrapper
+      id={id}
+      title={title}
+      description={description}
+      required={required}
+      error={error}
+      loading={loading}
+      wrapped={wrapped}
+      onEdit={onEdit}
+      onDelete={onDelete}
+    >
+      <reactSelect.default
+        id={`field-${id}`}
+        name={id}
+        placeholder={
+          placeholder ?? intl.formatMessage(messages.select)
+        }
+        isDisabled={false}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        options={map(
+          toPairs(
+            groupBy(
+              toPairs(filterOptions(querystring.indexes)),
+              (item) => item[1].group
+            )
+          ),
+          (group) => ({
+            label: group[0],
+            options: map(
+              filter(group[1], (item) => item[1].enabled),
+              (field) => ({
+                label: field[1].title,
+                value: field[0],
+              })
             ),
-            (group) => ({
-              label: group[0],
-              options: map(
-                filter(group[1], (item) => item[1].enabled),
-                (field) => ({
-                  label: field[1].title,
-                  value: field[0],
-                }),
-              ),
-            }),
-          )}
-          styles={customSelectStyles}
-          theme={selectTheme}
-          components={{ DropdownIndicator, Option }}
-          value={{ value: value?.value, label: indexes[value?.value]?.title }}
-          onChange={(data) => {
-            let dataValue = [];
-            if (Array.isArray(data)) {
-              for (let obj of data) {
-                dataValue.push(obj.value);
-              }
-              return onChange(id, dataValue);
+          })
+        )}
+        styles={customSelectStyles}
+        theme={selectTheme}
+        components={{ DropdownIndicator, Option }}
+        value={{
+          value: value?.value,
+          label: querystring.indexes[value?.value]?.title,
+        }}
+        onChange={(data) => {
+          let dataValue = [];
+          if (Array.isArray(data)) {
+            for (let obj of data) {
+              dataValue.push(obj.value);
             }
-            return onChange(id, data);
-          }}
-        />
-      </FormFieldWrapper>
-    );
-  }
-}
+            return onChange(id, dataValue);
+          }
+          return onChange(id, data);
+        }}
+      />
+    </FormFieldWrapper>
+  );
+};
+
+SelectWidget.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  required: PropTypes.bool,
+  error: PropTypes.arrayOf(PropTypes.string),
+  loading: PropTypes.bool,
+  value: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  onClick: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  wrapped: PropTypes.bool,
+  querystring: PropTypes.object,
+  reactSelect: PropTypes.shape({
+    choices: PropTypes.array,
+    vocabBaseUrl: PropTypes.string,
+    itemsTotal: PropTypes.number,
+    getVocabulary: PropTypes.func,
+    reactSelect: PropTypes.object,
+  }),
+};
+
+SelectWidget.defaultProps = {
+  description: null,
+  required: false,
+  items: {
+    vocabulary: null,
+  },
+  widgetOptions: {
+    vocabulary: null,
+  },
+  error: [],
+  choices: [],
+  loading: false,
+  value: null,
+  onChange: () => {},
+  onBlur: () => {},
+  onClick: () => {},
+  onEdit: null,
+  onDelete: null,
+};
 
 export default compose(
   withQueryString,
