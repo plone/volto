0827cc3ed23cc9151885b26e2c387433ea8d172b
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-01 23:35:50 +0100

    better tests

--- packages/volto/cypress/tests/core/blocks/blocks-autofocus.js
+++ packages/volto/cypress/tests/core/blocks/blocks-autofocus.js
@@ -1,7 +1,7 @@
 describe('New Block Auto Focus Tests', () => {
   beforeEach(() => {
-    cy.intercept('GET', `/**/*?expand*`).as('content');
     cy.intercept('GET', '/**/my-page').as('content');
+    cy.intercept('GET', '/**/Document').as('schema');
     cy.intercept('PATCH', '*').as('save');
 
     // given a logged in editor and a page in edit mode
@@ -16,11 +16,13 @@ describe('New Block Auto Focus Tests', () => {
     cy.wait('@content');
 
     cy.navigate('/my-page/edit');
+    cy.wait('@schema');
   });
 
   it('Press Enter on a description block adds new autofocused default block', () => {
     cy.addNewBlock('description');
     cy.get('.documentDescription').first().click().type('{enter}');
+    cy.get('.block-editor-description + .block-editor-slate');
     cy.get('*[class^="block-editor"]')
       .eq(2)
       .within(() => {
@@ -34,6 +36,7 @@ describe('New Block Auto Focus Tests', () => {
     cy.get('.blocks-chooser .title').contains('Text').click({ force: true });
     cy.get('.blocks-chooser .text').contains('Text').click({ force: true });
     cy.get('.text-slate-editor-inner').first().click().type('{enter}');
+    cy.get('.block-editor-slate + .block-editor-slate');
     cy.get('*[class^="block-editor"]')
       .eq(2)
       .within(() => {
@@ -43,7 +46,14 @@ describe('New Block Auto Focus Tests', () => {
 
   it('Press Enter on a image block adds new autofocused default block', () => {
     cy.addNewBlock('image');
-    cy.get('.block-editor-image').first().click().type('{enter}');
+    // Timing issues ahead :(
+    cy.get('.block-editor-image .no-image-wrapper img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+    cy.get('.block-editor-image').wait(500).click('topLeft').type('{enter}');
     cy.get('*[class^="block-editor"]')
       .eq(2)
       .within(() => {
@@ -53,6 +63,12 @@ describe('New Block Auto Focus Tests', () => {
 
   it('Press Enter on a video block adds new autofocused default block', () => {
     cy.addNewBlock('video');
+    cy.get('.block-editor-video .message img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
     cy.get('.block-editor-video').first().click().type('{enter}');
     cy.get('*[class^="block-editor"]')
       .eq(2)
@@ -83,6 +99,12 @@ describe('New Block Auto Focus Tests', () => {
 
   it('Press Enter on a maps block adds new autofocused default block', () => {
     cy.addNewBlock('maps');
+    cy.get('.block-editor-maps .message img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
     cy.get('.block-editor-maps').first().click().type('{enter}');
     cy.get('*[class^="block-editor"]')
       .eq(2)
