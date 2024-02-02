c734f535bdad549962c0db8cf930d972edd74134
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-02-02 13:27:01 +0100

    Support for passing whole data object in the initial block config (#5718)
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- packages/volto/src/helpers/index.js
+++ packages/volto/src/helpers/index.js
@@ -62,6 +62,10 @@ export {
   getPreviousNextBlock,
   findBlocks,
 } from '@plone/volto/helpers/Blocks/Blocks';
+export {
+  getSimpleDefaultBlocks,
+  getDefaultBlocks,
+} from '@plone/volto/helpers/Blocks/defaultBlocks';
 export { default as BodyClass } from '@plone/volto/helpers/BodyClass/BodyClass';
 export { default as ScrollToTop } from '@plone/volto/helpers/ScrollToTop/ScrollToTop';
 export {
