51a156880e844f1138cd81712a94a3d844acb1a8
Rob Gietema <rob.gietema@gmail.com>
2024-02-13 16:16:00 +0100

    Add accordion to metadata form.

--- packages/volto/src/reducers/sidebar/sidebar.test.js
+++ packages/volto/src/reducers/sidebar/sidebar.test.js
@@ -1,10 +1,52 @@
 import sidebar from './sidebar';
-import { SET_SIDEBAR_TAB } from '@plone/volto/constants/ActionTypes';
+import {
+  SET_METADATA_FIELDSETS,
+  SET_METADATA_FOCUS,
+  RESET_METADATA_FOCUS,
+  SET_SIDEBAR_TAB,
+} from '@plone/volto/constants/ActionTypes';
 
 describe('Sidebar reducer', () => {
   it('should return the initial state', () => {
     expect(sidebar()).toEqual({
       tab: 0,
+    });
+  });
+
+  it('should handle SET_METADATA_FIELDSETS', () => {
+    expect(
+      sidebar(undefined, {
+        type: SET_METADATA_FIELDSETS,
+        fieldsets: ['default'],
+      }),
+    ).toEqual({
+      fieldsets: ['default'],
+    });
+  });
+
+  it('should handle SET_METADATA_FOCUS', () => {
+    expect(
+      sidebar(undefined, {
+        type: SET_METADATA_FOCUS,
+        fieldset: 'default',
+        field: 'title',
+      }),
+    ).toEqual({
+      metadataFieldsets: ['default'],
+      title: 'title',
+    });
+  });
+
+  it('should handle RESET_METADATA_FOCUS', () => {
+    expect(
+      sidebar(
+        { metadataFieldFocus: 'title' },
+        {
+          type: RESET_METADATA_FOCUS,
+        },
+      ),
+    ).toEqual({
+      metadataFieldFocus: '',
     });
   });
 
