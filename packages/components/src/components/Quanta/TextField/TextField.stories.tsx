7de9bdc76ef3425c1f44b769e43ea2dc92161a08
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-14 17:16:38 +0100

    Quanta TextField in simple plain CSS, overriding BasiQ CSS

--- packages/components/src/components/Quanta/TextField/TextField.stories.tsx
+++ packages/components/src/components/Quanta/TextField/TextField.stories.tsx
@@ -27,3 +27,50 @@ export const Default: Story = {
     placeholder: 'Type something…',
   },
 };
+
+export const Required: Story = {
+  args: {
+    ...Default.args,
+    name: 'required',
+    isRequired: true,
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    ...Default.args,
+    name: 'filled',
+    defaultValue: 'Filled with value A',
+    isRequired: true,
+  },
+};
+
+export const Errored: Story = {
+  args: {
+    ...Default.args,
+    name: 'errored',
+    defaultValue: 'Filled with value A',
+    errorMessage: 'This is the error',
+    isInvalid: true,
+    isRequired: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    name: 'disabled',
+    label: 'Disabled field title',
+    isDisabled: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    name: 'readonly',
+    label: 'Readonly field title',
+    defaultValue: 'Filled with value A',
+    isReadOnly: true,
+  },
+};
