6bf883ee49f4feb6552d6f66ecbba39aa78c6c45
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-21 20:35:54 +0100

    Use latest @types/react and @types/react-dom

--- packages/volto/src/components/theme/SlotRenderer/SlotRenderer.tsx
+++ packages/volto/src/components/theme/SlotRenderer/SlotRenderer.tsx
@@ -29,8 +29,7 @@ const SlotRenderer = ({
     <>
       {slots.map((component) => {
         const id = uuid();
-        const SlotComponent = component as React.ElementType;
-        //@ts-ignore - Probably related to an old @types/react dep :(
+        const SlotComponent = component;
         return <SlotComponent key={id} id={id} />;
       })}
     </>
