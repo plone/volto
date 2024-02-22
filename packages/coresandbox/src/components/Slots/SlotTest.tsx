2ad12dc4a07ed9870c849d6adc8c23123060a635
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-22 14:27:07 +0100

    Add Cypress test

--- packages/coresandbox/src/components/Slots/SlotTest.tsx
+++ packages/coresandbox/src/components/Slots/SlotTest.tsx
@@ -1,6 +1,6 @@
 const SlotComponentTest = () => {
   return (
-    <div style={{ margin: '0 auto 20px auto !important', width: '1127px' }}>
+    <div style={{ margin: '0 auto 20px auto', width: '1127px' }}>
       <h2>This is a test slot component</h2>
       <p>It should appear above the Content</p>
     </div>
