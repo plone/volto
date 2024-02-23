30f2508df978cd6e586084f25caef347c18e163d
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-02-23 11:16:22 +0100

    Added global form state (#5721)

--- src/reducers/form/form.test.js
+++ src/reducers/form/form.test.js
@@ -1,7 +1,19 @@
 import form from './form';
+import { SET_FORM_DATA } from '@plone/volto/constants/ActionTypes';
 
 describe('Form reducer', () => {
   it('should return the initial state', () => {
-    expect(form()).toEqual({});
+    expect(form()).toEqual({ global: {} });
+  });
+
+  it('should handle SET_FORM_DATA', () => {
+    expect(
+      form(undefined, {
+        type: SET_FORM_DATA,
+        data: { foo: 'bar' },
+      }),
+    ).toEqual({
+      global: { foo: 'bar' },
+    });
   });
 });
