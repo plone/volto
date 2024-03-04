0a457b58186785d9e659c5086ca474069ad9711d
wesleybl <wesleybl@gmail.com>
2024-03-04 09:54:08 -0300

    Fix prettier

--- packages/volto/src/components/manage/Controlpanels/Users/UsersControlpanel.jsx
+++ packages/volto/src/components/manage/Controlpanels/Users/UsersControlpanel.jsx
@@ -359,8 +359,8 @@ class UsersControlpanel extends Component {
           entry.id === name && !entry.roles.includes(value)
             ? [...entry.roles, value]
             : entry.id !== name
-            ? entry.roles
-            : pull(entry.roles, value),
+              ? entry.roles
+              : pull(entry.roles, value),
       })),
     });
   }
