bf471c14bcc4e52185b7cf67ab5b926177d47a14
Wesley Barroso Lopes <wesleybl@gmail.com>
2024-03-19 08:48:30 -0300

    Show validation error message as string instead of list (#5842)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -81,6 +81,7 @@ export { default as langmap } from './LanguageMap/LanguageMap';
 export { default as Helmet } from './Helmet/Helmet';
 export { default as FormValidation } from './FormValidation/FormValidation';
 export { validateFileUploadSize } from './FormValidation/FormValidation';
+export { tryParseJSON } from './FormValidation/FormValidation';
 export {
   difference,
   getColor,
