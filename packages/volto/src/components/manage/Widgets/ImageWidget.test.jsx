faf7941f475431c3956f17a280eff99ae8a3c222
dobri1408 <dobriceanionut1408@gmail.com>
2024-04-13 07:17:45 +0300

    fix jest

--- packages/volto/src/components/manage/Widgets/ImageWidget.test.jsx
+++ packages/volto/src/components/manage/Widgets/ImageWidget.test.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-intl-redux';
-import { MemoryRouter } from 'react-router-dom';
-
 import ImageWidget from './ImageWidget';
 
 const mockStore = configureStore();
 
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/route',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa',
+    location: '/route',
+  }),
+}));
+
 test('renders an image sizes widget component', () => {
   const store = mockStore({
-    content: {
-      create: {},
-      data: {},
-      subrequests: {
-        asdasdasd: {},
-      },
-    },
     intl: {
       locale: 'en',
       messages: {},
@@ -25,14 +27,12 @@ test('renders an image sizes widget component', () => {
 
   const component = renderer.create(
     <Provider store={store}>
-      <MemoryRouter>
-        <ImageWidget
-          id="image"
-          title="Image"
-          fieldSet="default"
-          onChange={() => {}}
-        />
-      </MemoryRouter>
+      <ImageWidget
+        id="image"
+        title="Image"
+        fieldSet="default"
+        onChange={() => {}}
+      />
     </Provider>,
   );
   const json = component.toJSON();
