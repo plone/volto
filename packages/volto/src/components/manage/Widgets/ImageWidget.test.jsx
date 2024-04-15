24f4a43fd661b7df3de66696f52e86596a6cad64
dobri1408 <dobriceanionut1408@gmail.com>
2024-04-15 15:43:49 +0300

    add useLocation

--- packages/volto/src/components/manage/Widgets/ImageWidget.test.jsx
+++ packages/volto/src/components/manage/Widgets/ImageWidget.test.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-intl-redux';
+import { MemoryRouter } from 'react-router-dom';
 import ImageWidget from './ImageWidget';
 
-jest.mock('react-router-dom', () => ({
-  useLocation: jest.fn().mockReturnValue({
-    pathname: '/route',
-    search: '',
-    hash: '',
-    state: null,
-    key: '5nvxpbdafa',
-    location: '/route',
-  }),
-}));
+const mockStore = configureStore();
 
-test('renders an image sizes widget component', () => {
+// jest.mock('react-router-dom', () => ({
+//   useLocation: jest.fn().mockReturnValue({
+//     pathname: '/route',
+//     search: '',
+//     hash: '',
+//     state: null,
+//     key: '5nvxpbdafa',
+//     location: '/route',
+//   }),
+// }));
+
+test('renders a textarea widget component', () => {
+  const store = mockStore({
+    intl: {
+      locale: 'en',
+      messages: {},
+    },
+  });
+
   const component = renderer.create(
-    <ImageWidget id="image" title="Image" fieldSet="default" data={''} />,
+    <Provider store={store}>
+      <MemoryRouter>
+        <ImageWidget
+          id="my-field"
+          title="My field"
+          fieldSet="default"
+          onChange={() => {}}
+        />
+      </MemoryRouter>
+    </Provider>,
   );
   const json = component.toJSON();
   expect(json).toMatchSnapshot();
