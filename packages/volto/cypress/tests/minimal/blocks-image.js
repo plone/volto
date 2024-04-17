aee932e5eb8785a2f743d200450d553b00d5b707
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-04-17 11:03:42 +0300

    Update blocks-image.js

--- packages/volto/cypress/tests/minimal/blocks-image.js
+++ packages/volto/cypress/tests/minimal/blocks-image.js
@@ -3,7 +3,6 @@ describe('Blocks Tests', () => {
     cy.intercept('GET', `/**/*?expand*`).as('content');
     cy.intercept('GET', '/**/Document').as('schema');
     cy.intercept('POST', '*').as('saveImage');
-    cy.intercept('GET', '/**/image.png/@@images/image-*').as('getImage');
     // given a logged in editor and a page in edit mode
     cy.autologin();
     cy.createContent({
@@ -111,7 +110,6 @@ describe('Blocks Tests', () => {
     cy.get('#toolbar-save').click();
 
     cy.wait('@saveImage');
-    cy.wait('@getImage');
 
     // then image src must be equal to image name
     cy.get('.block img')
@@ -141,7 +139,6 @@ describe('Blocks Tests', () => {
     });
 
     cy.wait('@saveImage');
-    cy.wait('@getImage');
 
     cy.get('.block img')
       .should('have.attr', 'src')
@@ -166,7 +163,6 @@ describe('Blocks Tests', () => {
       encoding: 'utf8',
     });
     cy.wait('@saveImage');
-    cy.wait('@getImage');
 
     // then in sidebar alt attr should be empty
     cy.get('#sidebar-properties .field-wrapper-alt input#field-alt')
