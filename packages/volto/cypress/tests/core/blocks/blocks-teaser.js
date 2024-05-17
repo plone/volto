0b2814130f2e75866d2dc675fb069667db1cc08d
Philip Bauer <bauer@starzel.de>
2024-05-17 12:22:02 +0200

    add tests the second should fail unless run with the changes in plone.restapi

--- packages/volto/cypress/tests/core/blocks/blocks-teaser.js
+++ packages/volto/cypress/tests/core/blocks/blocks-teaser.js
@@ -74,31 +74,60 @@ context('Blocks Acceptance Tests', () => {
     ).click();
     cy.get('[aria-label="Select Blue Orchidees"]').dblclick();
     cy.wait(500);
-    cy.get('.align-buttons .ui.buttons button[aria-label="Center"]').click();
     cy.get('#toolbar-save').click();
     cy.visit('/document');
-    cy.get('.block.teaser').should('have.class', 'has--align--center');
-    cy.get('.block.teaser .image-wrapper img')
-      .should('have.attr', 'src')
-      .and('include', '/document/blue-orchidees/@@images/preview_image-');
     cy.get('.block.teaser .content h2').contains('Blue Orchidees');
     cy.get('.block.teaser .content p').contains(
       'are growing on the mountain tops',
     );
-    cy.removeContent({ path: 'document' });
-    cy.createContent({
-      contentType: 'Document',
-      contentId: 'red-orchidees',
-      contentTitle: 'Red Orchidees',
-      contentDescription: 'are beautifully growing on the mountain tops',
-      image: true,
-      path: '/document',
-    });
+    cy.visit('/document/blue-orchidees/edit');
+    cy.get('.documentFirstHeading').type(' and Tulips');
+    cy.get('#toolbar-save').click();
+    cy.visit('/document/blue-orchidees');
+    cy.get('.documentFirstHeading').contains('Blue Orchidees and Tulips');
     // THEN I can see the updated content in the teaser
     cy.visit('/document');
-    cy.get('.block.teaser .content h2').contains('Red Orchidees');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
     cy.get('.block.teaser .content p').contains(
       'are beautifully growing on the mountain tops',
     );
+  });
+
+  it('As editor I can create a Teaser block and overwrite the data which is is not updated when the target is changed', () => {
+    // GIVEN a Document with the title document and a Document to reference with the title Blue Orchidees
+    cy.createContent({
+      contentType: 'Document',
+      contentId: 'blue-orchidees',
+      contentTitle: 'Blue Orchidees',
+      contentDescription: 'are growing on the mountain tops',
+      image: true,
+      path: '/document',
+    });
+    cy.visit('/document/edit');
+    // WHEN I create a Teaser block and change the data of the referenced object
+    cy.get('.block .slate-editor [contenteditable=true]').click();
+    cy.get('.button .block-add-button').click({ force: true });
+    cy.get('.blocks-chooser .mostUsed .button.teaser')
+      .contains('Teaser')
+      .click({ force: true });
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href"] button[aria-label="Open object browser"]',
+    ).click();
+    cy.get('[aria-label="Select Blue Orchidees"]').dblclick();
+    cy.wait(500);
+    cy.get('label[for="field-overwrite"]').click();
+    cy.get('#sidebar-properties #field-title').type(' and Tulips');
+    cy.get('#toolbar-save').click();
+    cy.visit('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
+
+    cy.visit('/document/blue-orchidees/edit');
+    cy.get('.documentFirstHeading').type(' but no Tulips');
+    cy.get('#toolbar-save').click();
+    cy.visit('/document/blue-orchidees');
+    cy.get('.documentFirstHeading').contains('Blue Orchidees but no Tulips');
+    // THEN I still see the overwritten content in the teaser
+    cy.visit('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
   });
 });
