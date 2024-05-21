83c395ba50b0e9afc8e20b9184572e2fb4eee13b
David Glick <david@glicksoftware.com>
2024-05-21 00:51:10 -0700

    Server-side redirects for plone.app.redirector aliases (#4854) (#6033)
    Co-authored-by: Jefferson Bledsoe <me@jeffersonbledsoe.com>
    Co-authored-by: Mauro Amico <mauro.amico@gmail.com>
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- src/components/theme/View/View.jsx
+++ src/components/theme/View/View.jsx
@@ -203,8 +203,10 @@ class View extends Component {
    */
   render() {
     const { views } = config;
-    if (this.props.error && this.props.error.code === 301) {
-      const redirect = flattenToAppURL(this.props.error.url).split('?')[0];
+    if ([301, 302].includes(this.props.error?.code)) {
+      const redirect = flattenToAppURL(this.props.error.url)
+        .split('?')[0]
+        .replace('/++api++', '');
       return <Redirect to={`${redirect}${this.props.location.search}`} />;
     } else if (this.props.error && !this.props.connectionRefused) {
       let FoundView;
