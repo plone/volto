5e28370bdd422702622a999abc065887c2fcfcb6
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-05-24 21:20:24 +0300

    Update View.tsx

--- packages/coresandbox/src/components/Blocks/InputBlock/View.tsx
+++ packages/coresandbox/src/components/Blocks/InputBlock/View.tsx
@@ -3,7 +3,7 @@ import type { BlockViewProps } from '@plone/types';
 const InputBlockView = (props: BlockViewProps) => {
   return (
     <div className="test-block">
-      <div>Test Block</div>
+      <div>Input Block</div>
       <p>{JSON.stringify(props.data)}</p>
     </div>
   );
