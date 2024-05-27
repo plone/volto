0916aa036913d028e764b9cc837997d5b59f89d8
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-05-27 10:11:02 +0300

    Move test here metadata.js

--- packages/volto/cypress/tests/core/basic/metadata.js
+++ packages/volto/cypress/tests/core/basic/metadata.js
@@ -39,4 +39,12 @@ describe('Add Content Tests', () => {
       .contains('Required input is missing');
     cy.get('.sidebar-container .tabs-wrapper .active.item').contains('Page');
   });
+
+ it('After removing value of widget the focus should be removed from the field', () => {
+    cy.get('#field-creators').get('.react-select__multi-value__remove').click();
+    cy.getSlateEditorAndType(
+      'Test if all the text will be in this slate block',
+    ).contains('Test if all the text will be in this slate block');
+  });
+
 });
