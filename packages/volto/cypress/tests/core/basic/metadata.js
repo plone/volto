996b6b99264cf176c81d5369ced2bb29816efa47
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-05-27 10:14:31 +0300

    Update metadata.js

--- packages/volto/cypress/tests/core/basic/metadata.js
+++ packages/volto/cypress/tests/core/basic/metadata.js
@@ -40,11 +40,10 @@ describe('Add Content Tests', () => {
     cy.get('.sidebar-container .tabs-wrapper .active.item').contains('Page');
   });
 
- it('After removing value of widget the focus should be removed from the field', () => {
+  it('After removing value of widget the focus should be removed from the field', () => {
     cy.get('#field-creators').get('.react-select__multi-value__remove').click();
     cy.getSlateEditorAndType(
       'Test if all the text will be in this slate block',
     ).contains('Test if all the text will be in this slate block');
   });
-
 });
