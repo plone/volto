18e13a2d1681349d16f28240c3f5a3125bd5009b
Martina Bustacchini <martina.bustacchini@redturtle.it>
2024-06-10 15:22:59 +0200

    feat: allow use of localized vocabularies

--- packages/client/src/restapi/vocabularies/get.ts
+++ packages/client/src/restapi/vocabularies/get.ts
@@ -7,6 +7,7 @@ const getVocabulariesSchema = z.object({
   path: z.string(),
   title: z.string().optional(),
   token: z.string().optional(),
+  locale: z.string().optional(),
   tokens: z.array(z.string()).optional(),
 });
 
@@ -18,12 +19,14 @@ export const getVocabularies = async ({
   path,
   title,
   token,
+  locale,
   tokens,
   config,
 }: VocabulariesArgs): Promise<GetVocabulariesResponse> => {
   const validatedArgs = getVocabulariesSchema.parse({
     path,
     title,
+    locale,
     token,
     tokens,
   });
@@ -36,7 +39,9 @@ export const getVocabularies = async ({
       ...(validatedArgs.tokens && { tokens: validatedArgs.tokens }),
     },
   };
-  const vocabulariesPath = `@vocabularies/${validatedArgs.path}`;
+  const vocabulariesPath = `${locale ? `${locale}/` : ''}@vocabularies/${
+    validatedArgs.path
+  }`;
 
   return apiRequest('get', vocabulariesPath, options);
 };
@@ -45,9 +50,11 @@ export const getVocabulariesQuery = ({
   path,
   title,
   token,
+  locale,
   tokens,
   config,
 }: VocabulariesArgs) => ({
-  queryKey: [path, title, token, tokens, 'get', 'vocabularies'],
-  queryFn: () => getVocabularies({ path, title, token, tokens, config }),
+  queryKey: [locale, path, title, token, tokens, 'get', 'vocabularies'],
+  queryFn: () =>
+    getVocabularies({ path, title, token, tokens, config, locale }),
 });
