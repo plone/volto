3b36216833f1d6c9e6bdd090774557da7654f95e
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-06-15 13:32:14 +0200

    Revert "Fix cypress linkintegrity test"
    This reverts commit 34a4dfbb62e4a2a37c6cec51109952213223253a.

--- packages/volto/cypress/tests/core/basic/linkIntegrityView.js
+++ packages/volto/cypress/tests/core/basic/linkIntegrityView.js
@@ -323,7 +323,6 @@ describe('Test if different forms of Linking content appear in links and referen
     // Manually adding the preview image override
     cy.visit('document-linking/edit');
     cy.get('.block.teaser .content').click();
-    cy.get('.field-wrapper-overwrite label').click();
     cy.get(
       '.field-wrapper-preview_image > .grid > .stretched > .eight > .objectbrowser-field > .button',
     ).click();
