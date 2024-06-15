34a4dfbb62e4a2a37c6cec51109952213223253a
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-06-15 12:03:46 +0200

    Fix cypress linkintegrity test

--- packages/volto/cypress/tests/core/basic/linkIntegrityView.js
+++ packages/volto/cypress/tests/core/basic/linkIntegrityView.js
@@ -323,6 +323,7 @@ describe('Test if different forms of Linking content appear in links and referen
     // Manually adding the preview image override
     cy.visit('document-linking/edit');
     cy.get('.block.teaser .content').click();
+    cy.get('.field-wrapper-overwrite label').click();
     cy.get(
       '.field-wrapper-preview_image > .grid > .stretched > .eight > .objectbrowser-field > .button',
     ).click();
