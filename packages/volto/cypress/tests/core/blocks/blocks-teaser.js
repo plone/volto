3c93951a121d6cf48669add7ca6ba4cc2910820f
Philip Bauer <bauer@starzel.de>
2024-06-19 11:28:10 +0200

    Make dynamic/live teaser without additional requests (#6024)
    Co-authored-by: Victor Fernandez de Alba <sneridagh@gmail.com>
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- packages/volto/cypress/tests/core/blocks/blocks-teaser.js
+++ packages/volto/cypress/tests/core/blocks/blocks-teaser.js
@@ -51,4 +51,89 @@ context('Blocks Acceptance Tests', () => {
       'are growing on the mountain tops',
     );
   });
+
+  it('As editor I can add a (standalone) Teaser block that always fetches the live data', () => {
+    // GIVEN a Document with the title document and a Document to reference with the title Blue Orchidees
+    cy.createContent({
+      contentType: 'Document',
+      contentId: 'blue-orchidees',
+      contentTitle: 'Blue Orchidees',
+      contentDescription: 'are growing on the mountain tops',
+      image: true,
+      path: '/document',
+    });
+
+    cy.navigate('/document/edit');
+    // WHEN I create a Teaser block and change the data of the referenced object
+    cy.get('.block .slate-editor [contenteditable=true]').click();
+    cy.get('.button .block-add-button').click({ force: true });
+    cy.get('.blocks-chooser .mostUsed .button.teaser')
+      .contains('Teaser')
+      .click({ force: true });
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href"] button[aria-label="Open object browser"]',
+    ).click();
+    cy.get('[aria-label="Select Blue Orchidees"]').dblclick();
+    cy.wait(500);
+    cy.get('#toolbar-save').click();
+
+    cy.visit('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees');
+    cy.get('.block.teaser .content p').contains(
+      'are growing on the mountain tops',
+    );
+    cy.navigate('/document/blue-orchidees/edit');
+    cy.wait('@schema');
+    cy.getSlateTitle().type(' and Tulips');
+    cy.get('#field-description')
+      .clear()
+      .type('are beautifully growing on the mountain tops');
+    cy.get('#toolbar-save').click();
+
+    cy.get('.documentFirstHeading').contains('Blue Orchidees and Tulips');
+    // THEN I can see the updated content in the teaser
+    cy.navigate('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
+    cy.get('.block.teaser .content p').contains(
+      'are beautifully growing on the mountain tops',
+    );
+  });
+
+  it('As editor I can create a Teaser block and overwrite the data which is is not updated when the target is changed', () => {
+    // GIVEN a Document with the title document and a Document to reference with the title Blue Orchidees
+    cy.createContent({
+      contentType: 'Document',
+      contentId: 'blue-orchidees',
+      contentTitle: 'Blue Orchidees',
+      contentDescription: 'are growing on the mountain tops',
+      image: true,
+      path: '/document',
+    });
+    cy.visit('/document/edit');
+    // WHEN I create a Teaser block and change the data of the referenced object
+    cy.get('.block .slate-editor [contenteditable=true]').click();
+    cy.get('.button .block-add-button').click({ force: true });
+    cy.get('.blocks-chooser .mostUsed .button.teaser')
+      .contains('Teaser')
+      .click({ force: true });
+    cy.get(
+      '.objectbrowser-field[aria-labelledby="fieldset-default-field-label-href"] button[aria-label="Open object browser"]',
+    ).click();
+    cy.get('[aria-label="Select Blue Orchidees"]').dblclick();
+    cy.wait(500);
+    cy.get('label[for="field-overwrite"]').click();
+    cy.get('#sidebar-properties #field-title').type(' and Tulips');
+    cy.get('#toolbar-save').click();
+    cy.visit('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
+
+    cy.visit('/document/blue-orchidees/edit');
+    cy.get('.documentFirstHeading').type(' but no Tulips');
+    cy.get('#toolbar-save').click();
+    cy.visit('/document/blue-orchidees');
+    cy.get('.documentFirstHeading').contains('Blue Orchidees but no Tulips');
+    // THEN I still see the overwritten content in the teaser
+    cy.visit('/document');
+    cy.get('.block.teaser .content h2').contains('Blue Orchidees and Tulips');
+  });
 });
