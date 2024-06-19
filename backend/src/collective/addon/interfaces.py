c61a5497ba76f13b610ba60d804c9808569093c4
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-06-19 08:35:47 +0200

    Remove backend folder altogether

--- backend/src/collective/addon/interfaces.py
+++
@@ -1,7 +0,0 @@
-"""Module where all interfaces, events and exceptions live."""
-
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-
-
-class IBrowserLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
