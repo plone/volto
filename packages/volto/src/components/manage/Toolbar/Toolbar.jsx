2f9516fc9ee08cabd7725c8b2e2295360d93475a
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2024-06-26 11:52:12 +0200

    fix: state and styles for toolbar handler for hp

--- packages/volto/src/components/manage/Toolbar/Toolbar.jsx
+++ packages/volto/src/components/manage/Toolbar/Toolbar.jsx
@@ -368,6 +368,7 @@ class Toolbar extends Component {
       id: 'folderContents',
     });
     const { expanded } = this.state;
+    const isHomepage = path === '';
 
     return (
       this.props.token && (
@@ -624,6 +625,7 @@ class Toolbar extends Component {
                   messages.shrinkToolbar,
                 )}
                 className={cx({
+                  homepage: isHomepage,
                   [this.props.content?.review_state]:
                     this.props.content?.review_state,
                 })}
