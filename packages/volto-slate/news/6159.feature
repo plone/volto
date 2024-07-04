75ec956ee6ffe1543c4ce574f29a066e046034c5
David Ichim <ichim.david@gmail.com>
2024-07-04 13:17:42 +0300

    Avoid breaking change and bug to the position of AddLink when it's triggered from Slate toolbar

--- packages/volto-slate/news/6159.feature
+++ packages/volto-slate/news/6159.feature
@@ -1,1 +1,7 @@
-Use unused `toggleButton` prop in `PositionedToolbar` to render the toolbar in a different portal target falling back to document.body if `toggleButton` is not provided. @ichim-david
+Use unused `toggleButton` prop in `PositionedToolbar` to render the 
+toolbar in a different portal target falling back to document.body 
+if `toggleButton` is not provided. @ichim-david
+
+When `toggleButton` is provided as portal target, we allow negative
+left positioning but not when target is body to avoid the toolbar 
+going off screen and avoiding breaking changes. @ichim-david
--- packages/volto-slate/src/editor/ui/PositionedToolbar.jsx
+++ packages/volto-slate/src/editor/ui/PositionedToolbar.jsx
@@ -13,7 +13,13 @@ const PositionedToolbar = ({ toggleButton, className, children, position }) => {
     const el = ref.current;
 
     const { style } = position || {};
-    const left = `${style.left - el.offsetWidth / 2}px`;
+    // allow negative left positioning when portal isn't document.body
+    // we need to use Math.max to avoid Slate AddLink popup going off screen
+    // and it adds the toolbar to the body
+    const left =
+      portalTarget === document.body
+        ? `${Math.max(style.left - el.offsetWidth / 2, 0)}px`
+        : `${style.left - el.offsetWidth / 2}px`;
     const top = `${style.top - el.offsetHeight}px`;
 
     el.style.opacity = style.opacity;
