5921acbe02152a590a295bd402570ce1cef3df49
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-07-19 15:20:02 +0200

    fix: name

--- packages/volto/src/config/PublicNonContentRoutes.jsx
+++ packages/volto/src/config/PublicNonContentRoutes.jsx
@@ -3,7 +3,7 @@ import config from '@plone/volto/registry';
 // PublicUi routes that are noContentRoutes, and we won't cms-ui
 // You can include either RegEx or a string representing the ending of the
 // nonContentRoute eg. '/add' will match '/foo/bar/add'
-export const publicUiRoutes = [
+export const publicNonContentRoutes = [
   '/login',
   '/logout',
   '/sitemap',
