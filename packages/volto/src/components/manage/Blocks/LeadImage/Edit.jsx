91d2208d97c0cd49f220dca05dfb591fa2f3a3ce
Bhavishya Sahay <145124737+BhavishyaSahay@users.noreply.github.com>
2024-07-25 12:11:22 +0530

    Update Edit.jsx

--- packages/volto/src/components/manage/Blocks/LeadImage/Edit.jsx
+++ packages/volto/src/components/manage/Blocks/LeadImage/Edit.jsx
@@ -18,91 +18,88 @@ const messages = defineMessages({
   },
 });
 
-const Edit = React.memo((props) => {
-  const {
-    properties,
-    selected,
-    block,
-    data,
-    onChangeBlock,
-  } = props;
+const Edit = React.memo(
+  (props) => {
+    const { properties, selected, block, data, onChangeBlock } = props;
 
-  const intl = useIntl();
-  const node = useRef(null);
+    const intl = useIntl();
+    const node = useRef(null);
 
-  const onAlignBlock = (align) => {
-    onChangeBlock(block, {
-      ...data,
-      align,
-    });
-  };
+    const onAlignBlock = (align) => {
+      onChangeBlock(block, {
+        ...data,
+        align,
+      });
+    };
 
-  const Image = config.getComponent({ name: 'Image' }).component;
-  const placeholder =
-    data.placeholder ||
-    intl.formatMessage(messages.ImageBlockInputPlaceholder);
+    const Image = config.getComponent({ name: 'Image' }).component;
+    const placeholder =
+      data.placeholder ||
+      intl.formatMessage(messages.ImageBlockInputPlaceholder);
 
-  const hasImage = !!properties.image;
-  const hasImageData = hasImage && !!properties.image.data;
-  const className = cx('responsive', { 'full-image': data.align === 'full' });
-  const altText = data.image_caption || properties.image_caption || '';
+    const hasImage = !!properties.image;
+    const hasImageData = hasImage && !!properties.image.data;
+    const className = cx('responsive', { 'full-image': data.align === 'full' });
+    const altText = data.image_caption || properties.image_caption || '';
 
-  return (
-    <div
-      className={cx(
-        'block image align',
-        {
-          center: !Boolean(data.align),
-        },
-        data.align,
-      )}
-    >
-      {!hasImage && (
-        <Message>
-          <center>
-            <img src={imageBlockSVG} alt="" />
-            <div className="message-text">{placeholder}</div>
-          </center>
-        </Message>
-      )}
-      {hasImage && hasImageData && (
-        <img
-          className={className}
-          src={`data:${properties.image['content-type']};base64,${properties.image.data}`}
-          width={properties.image.width}
-          height={properties.image.height}
-          alt={altText}
-          style={{
-            aspectRatio: `${properties.image.width}/${properties.image.height}`,
-          }}
-        />
-      )}
-      {hasImage && !hasImageData && (
-        <Image
-          className={className}
-          item={properties}
-          imageField="image"
-          sizes={(() => {
-            if (data.align === 'full' || data.align === 'center')
-              return '100vw';
-            if (data.align === 'left' || data.align === 'right')
-              return '50vw';
-            return undefined;
-          })()}
-          alt={altText}
-        />
-      )}
-      <SidebarPortal selected={selected}>
-        <LeadImageSidebar {...props} />
-      </SidebarPortal>
-    </div>
-  );
-}, (prevProps, nextProps) => {
-  return (
-    prevProps.selected === nextProps.selected &&
-    isEqual(prevProps.data, nextProps.data)
-  );
-});
+    return (
+      <div
+        className={cx(
+          'block image align',
+          {
+            center: !Boolean(data.align),
+          },
+          data.align,
+        )}
+      >
+        {!hasImage && (
+          <Message>
+            <center>
+              <img src={imageBlockSVG} alt="" />
+              <div className="message-text">{placeholder}</div>
+            </center>
+          </Message>
+        )}
+        {hasImage && hasImageData && (
+          <img
+            className={className}
+            src={`data:${properties.image['content-type']};base64,${properties.image.data}`}
+            width={properties.image.width}
+            height={properties.image.height}
+            alt={altText}
+            style={{
+              aspectRatio: `${properties.image.width}/${properties.image.height}`,
+            }}
+          />
+        )}
+        {hasImage && !hasImageData && (
+          <Image
+            className={className}
+            item={properties}
+            imageField="image"
+            sizes={(() => {
+              if (data.align === 'full' || data.align === 'center')
+                return '100vw';
+              if (data.align === 'left' || data.align === 'right')
+                return '50vw';
+              return undefined;
+            })()}
+            alt={altText}
+          />
+        )}
+        <SidebarPortal selected={selected}>
+          <LeadImageSidebar {...props} />
+        </SidebarPortal>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.selected === nextProps.selected &&
+      isEqual(prevProps.data, nextProps.data)
+    );
+  },
+);
 
 Edit.propTypes = {
   properties: PropTypes.objectOf(PropTypes.any).isRequired,
