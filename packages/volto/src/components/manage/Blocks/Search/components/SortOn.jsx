e91a34c858e791cbbbfba1abf38a809026052f2e
David Glick <david@glicksoftware.com>
2024-07-30 16:34:23 -0700

    Simplify logic for when to show sort options

--- packages/volto/src/components/manage/Blocks/Search/components/SortOn.jsx
+++ packages/volto/src/components/manage/Blocks/Search/components/SortOn.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import { defineMessages, injectIntl } from 'react-intl';
 import cx from 'classnames';
@@ -53,31 +52,20 @@ const SortOn = (props) => {
     querystring = {},
     intl,
   } = props;
-  const [activeSortOnButton, setActiveSortOnButton] = useState(false);
   const { sortable_indexes } = querystring;
   const Select = reactSelect.default;
 
-  const activeSortOn = sortOn || data?.query?.sort_on || '';
+  const defaultSortOn = data?.query?.sort_on || '';
+  const activeSortOn = sortOn || defaultSortOn;
 
   let { sortOnOptions = [] } = data;
-
-  if (data?.query?.sort_on && sortOnOptions.length > 0) {
-    sortOnOptions = [...sortOnOptions, data?.query?.sort_on];
-    sortOnOptions = [...new Set(sortOnOptions)];
-  }
+  sortOnOptions = [defaultSortOn, ...sortOnOptions];
+  sortOnOptions = [...new Set(sortOnOptions)];
 
   const showSelectField = sortOnOptions.length > 1;
-
-  const showButtonSortOn =
-    !data?.query?.sort_on && data.sortOnOptions?.length === 1;
-
-  const buttonText =
-    showButtonSortOn && sortable_indexes
-      ? sortable_indexes[sortOnOptions[0]]?.title
-      : sortOnOptions[0];
-
-  const showSortedLabel = data?.query?.sort_on && sortOnOptions.length === 0;
-
+  if (!showSelectField && !activeSortOn) {
+    return;
+  }
   const value = {
     value: activeSortOn || intl.formatMessage(messages.noSelection),
     label:
@@ -89,95 +77,75 @@ const SortOn = (props) => {
   return (
     <div className="search-sort-wrapper">
       <div className="search-sort-on">
-        {showSortedLabel ? (
-          <span className="sorted-label">
-            {intl.formatMessage(messages.sortedOn)}
-            <span className="sorted-label-value">{value.label}</span>
-          </span>
-        ) : (
+        {showSelectField ? (
           <>
             <span className="sort-label">
               {intl.formatMessage(messages.sortOn)}
             </span>
-            {showButtonSortOn ? (
-              <Button
-                icon
-                basic
-                compact
-                title={intl.formatMessage(messages.sortOnButtonTitle, {
-                  value: value.label,
-                })}
-                className={cx({
-                  active: activeSortOnButton,
-                })}
-                onClick={() => {
-                  if (!activeSortOnButton) {
-                    !isEditMode && setSortOn(sortOnOptions[0]);
-                  } else {
-                    !isEditMode && setSortOn('');
-                  }
-
-                  setActiveSortOnButton(!activeSortOnButton);
-                }}
-              >
-                {buttonText}
-              </Button>
-            ) : (
-              showSelectField && (
-                <Select
-                  id="select-search-sort-on"
-                  name="select-searchblock-sort-on"
-                  className="search-react-select-container"
-                  classNamePrefix="react-select"
-                  placeholder={intl.formatMessage(messages.sortOn)}
-                  styles={sortOnSelectStyles}
-                  theme={selectTheme}
-                  components={{ DropdownIndicator, Option }}
-                  options={[
-                    ...sortOnOptions.map((k) => ({
-                      value: k,
-                      label: sortable_indexes[k]?.title || k,
-                    })),
-                  ]}
-                  isSearchable={false}
-                  value={value}
-                  onChange={(data) => {
-                    !isEditMode && setSortOn(data.value);
-                  }}
-                />
-              )
-            )}
+            <Select
+              id="select-search-sort-on"
+              name="select-searchblock-sort-on"
+              className="search-react-select-container"
+              classNamePrefix="react-select"
+              placeholder={intl.formatMessage(messages.sortOn)}
+              styles={sortOnSelectStyles}
+              theme={selectTheme}
+              components={{ DropdownIndicator, Option }}
+              options={[
+                ...sortOnOptions.map((k) => ({
+                  value: k,
+                  label:
+                    sortable_indexes[k]?.title ||
+                    k ||
+                    intl.formatMessage(messages.noSelection),
+                })),
+              ]}
+              isSearchable={false}
+              value={value}
+              onChange={(data) => {
+                !isEditMode && setSortOn(data.value);
+              }}
+            />
           </>
+        ) : (
+          <span className="sorted-label">
+            {intl.formatMessage(messages.sortedOn)}
+            <span className="sorted-label-value">{value.label}</span>
+          </span>
         )}
       </div>
-      <Button
-        icon
-        basic
-        compact
-        title={intl.formatMessage(messages.ascending)}
-        className={cx({
-          active: sortOrder === 'ascending',
-        })}
-        onClick={() => {
-          !isEditMode && setSortOrder('ascending');
-        }}
-      >
-        <Icon name={upSVG} size="25px" />
-      </Button>
-      <Button
-        icon
-        basic
-        compact
-        title={intl.formatMessage(messages.descending)}
-        className={cx({
-          active: sortOrder === 'descending',
-        })}
-        onClick={() => {
-          !isEditMode && setSortOrder('descending');
-        }}
-      >
-        <Icon name={downSVG} size="25px" />
-      </Button>
+      {activeSortOn ? (
+        <>
+          <Button
+            icon
+            basic
+            compact
+            title={intl.formatMessage(messages.ascending)}
+            className={cx({
+              active: sortOrder === 'ascending',
+            })}
+            onClick={() => {
+              !isEditMode && setSortOrder('ascending');
+            }}
+          >
+            <Icon name={downSVG} size="25px" />
+          </Button>
+          <Button
+            icon
+            basic
+            compact
+            title={intl.formatMessage(messages.descending)}
+            className={cx({
+              active: sortOrder === 'descending',
+            })}
+            onClick={() => {
+              !isEditMode && setSortOrder('descending');
+            }}
+          >
+            <Icon name={upSVG} size="25px" />
+          </Button>
+        </>
+      ) : null}
     </div>
   );
 };
