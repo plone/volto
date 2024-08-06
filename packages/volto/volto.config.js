dd6b6fa41ccfedf85c8e5c713129c275f7817099
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2024-08-06 09:14:06 +0530

    css import fix

--- packages/volto/volto.config.js
+++ packages/volto/volto.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   addons: [],
+  webpack: (config, { env }) => {
+    if (env === 'development') {
+      config.resolve.alias['@adobe/react-spectrum'] = '@adobe/react-spectrum/dist/main.js';
+    }
+    return config;
+  },
+  razzleOptions: {
+    cssModulesExclude: [/@adobe\/react-spectrum/],
+  },
 };
