3924074a4f906cb2ad0a233e0336b44465d91739
David Glick <david@glicksoftware.com>
2024-08-10 11:38:36 -0700

    restore checks for canAssign and canDelete

--- packages/volto/src/components/manage/Controlpanels/Groups/RenderGroups.jsx
+++ packages/volto/src/components/manage/Controlpanels/Groups/RenderGroups.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { Dropdown, Table, Checkbox } from 'semantic-ui-react';
 import trashSVG from '@plone/volto/icons/delete.svg';
 import ploneSVG from '@plone/volto/icons/plone.svg';
 import { Icon } from '@plone/volto/components';
+import { canAssignRole } from '@plone/volto/helpers';
 
 const RenderGroups = (props) => {
   const onChange = (event, { value }) => {
@@ -13,7 +14,10 @@ const RenderGroups = (props) => {
   const isAuthGroup = (roleId) => {
     return props.inheritedRole.includes(roleId);
   };
-
+  const canDeleteGroup = () => {
+    if (props.isUserManager) return true;
+    return !props.group.roles.includes('Manager');
+  };
   return (
     <Table.Row key={props.group.title}>
       <Table.Cell>{props.group.groupname}</Table.Cell>
@@ -37,19 +41,25 @@ const RenderGroups = (props) => {
               }
               onChange={onChange}
               value={`${props.group.id}.${role.id}`}
+              disabled={!canAssignRole(props.isUserManager, role)}
             />
           )}
         </Table.Cell>
       ))}
       <Table.Cell textAlign="right">
-        <Dropdown icon="ellipsis horizontal">
-          <Dropdown.Menu className="left">
-            <Dropdown.Item onClick={props.onDelete} value={props.group['@id']}>
-              <Icon name={trashSVG} size="15px" />
-              <FormattedMessage id="Delete" defaultMessage="Delete" />
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        {canDeleteGroup() && (
+          <Dropdown icon="ellipsis horizontal">
+            <Dropdown.Menu className="left">
+              <Dropdown.Item
+                onClick={props.onDelete}
+                value={props.group['@id']}
+              >
+                <Icon name={trashSVG} size="15px" />
+                <FormattedMessage id="Delete" defaultMessage="Delete" />
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
       </Table.Cell>
     </Table.Row>
   );
@@ -73,4 +83,4 @@ RenderGroups.propTypes = {
   inheritedRole: PropTypes.array,
   onDelete: PropTypes.func.isRequired,
 };
-export default injectIntl(RenderGroups);
+export default RenderGroups;
