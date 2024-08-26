c8506159b0e25633572cf1c8169a7a5dc0494b9e
Wesley Barroso Lopes <wesleybl@gmail.com>
2024-08-26 15:45:49 -0300

    Move Tags component to the slot belowContent (#6269)
    Co-authored-by: David Glick <david@glicksoftware.com>

--- packages/volto/src/config/index.js
+++ packages/volto/src/config/index.js
@@ -18,6 +18,7 @@ import {
 import { components } from './Components';
 import { loadables } from './Loadables';
 import { workflowMapping } from './Workflows';
+import slots from './slots';
 
 import { contentIcons } from './ContentIcons';
 import { styleClassNameConverters, styleClassNameExtenders } from './Style';
@@ -244,6 +245,18 @@ ConfigRegistry.addonReducers = config.addonReducers;
 ConfigRegistry.components = config.components;
 ConfigRegistry.slots = config.slots;
 ConfigRegistry.utilities = config.utilities;
+
+// Register slots
+Object.entries(slots).forEach(([slotName, components]) => {
+  components.forEach(({ name, component, predicates = [] }) => {
+    ConfigRegistry.registerSlotComponent({
+      slot: slotName,
+      name,
+      component,
+      predicates,
+    });
+  });
+});
 
 registerValidators(ConfigRegistry);
 
