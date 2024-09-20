78ea5d7f6babd90f3524b96080d6967fd3ae3fc1
viveak910 <1602-23-737-191@vce.ac.in>
2024-09-20 17:24:37 +0530

    fixes issue #6283

--- packages/volto/src/express-middleware/devproxy.js
+++ packages/volto/src/express-middleware/devproxy.js
@@ -47,6 +47,8 @@ export default function devProxyMiddleware() {
       },
     ),
     onProxyReq: (proxyReq, req, res) => {
+      //Fixes https://github.com/plone/volto/issues/6283
+      proxyReq.setHeader('Host', new URL(config.settings.apiPath).host);
       // Fixes https://github.com/chimurai/http-proxy-middleware/issues/320
       if (!req.body || !Object.keys(req.body).length) {
         return;
