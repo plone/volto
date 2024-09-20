98a75f05330b0c695bb1f36ddc8875332dc32846
Aditya Pandey <askadityapandey@gmail.com>
2024-09-20 20:40:02 +0530

    Update get.ts

--- packages/client/src/restapi/querystring-search/get.ts
+++ packages/client/src/restapi/querystring-search/get.ts
@@ -8,34 +8,38 @@ export type QuerystringSearchArgs = z.infer<
   typeof getQuerystringSearchSchema
 > & {
   config: PloneClientConfig;
+  path?: string;
 };
 
 export const getQuerystringSearch = async ({
   query,
   config,
+  path, // Add path parameter
 }: QuerystringSearchArgs): Promise<GetQuerystringSearchResponse> => {
   const validatedArgs = getQuerystringSearchSchema.parse({
     query,
+    path, // Include path in validation
   });
-
-  const queryObject = { query: validatedArgs.query };
+  const queryObject = { 
+    query: validatedArgs.query,
+    path: validatedArgs.path, // Include path in queryObject if it exists
+  };
   const querystring = JSON.stringify(queryObject);
   const encodedQuery = encodeURIComponent(querystring);
-
   const options: ApiRequestParams = {
     config,
     params: {
       ...(encodedQuery && { query: encodedQuery }),
     },
   };
-
   return apiRequest('get', '/@querystring-search', options);
 };
 
 export const getQuerystringSearchQuery = ({
   query,
   config,
+  path,
 }: QuerystringSearchArgs) => ({
-  queryKey: ['get', 'querystringSearch'],
-  queryFn: () => getQuerystringSearch({ query, config }),
+  queryKey: ['get', 'querystringSearch', path],
+  queryFn: () => getQuerystringSearch({ query, config, path }),
 });
