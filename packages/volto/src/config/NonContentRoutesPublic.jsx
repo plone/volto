d92c303234d1d0025c42214b1f64ae4965000c27
Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>
2024-09-20 12:27:21 +0200

    Update packages/volto/src/config/NonContentRoutesPublic.jsx
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- packages/volto/src/config/NonContentRoutesPublic.jsx
+++ packages/volto/src/config/NonContentRoutesPublic.jsx
@@ -1,6 +1,6 @@
 // PublicUi routes that are noContentRoutes, and we won't cms-ui
 // You can include either RegEx or a string representing the ending of the
-// nonContentRoute eg. '/add' will match '/foo/bar/add'
+// nonContentRoute. For example, '/add' will match '/foo/bar/add'.
 export const nonContentRoutesPublic = [
   '/login',
   '/logout',
