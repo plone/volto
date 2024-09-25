3f13936e55f0f38d6aa380168ff97d8dd62efa04
ana-oprea <anaop3212@gmail.com>
2024-09-25 10:20:14 +0200

    fix: Listing block default summary should use ul tag for its listing

--- packages/volto/src/components/manage/Blocks/Listing/SummaryTemplate.jsx
+++ packages/volto/src/components/manage/Blocks/Listing/SummaryTemplate.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ConditionalLink, Component } from '@plone/volto/components';
 import { flattenToAppURL } from '@plone/volto/helpers';
+import { List } from 'semantic-ui-react';
 
 import { isInternalURL } from '@plone/volto/helpers/Url/Url';
 
@@ -22,17 +23,28 @@ const SummaryTemplate = ({ items, linkTitle, linkHref, isEditMode }) => {
   return (
     <>
       <div className="items">
-        {items.map((item) => (
-          <div className="listing-item" key={item['@id']}>
-            <ConditionalLink item={item} condition={!isEditMode}>
-              <Component componentName="PreviewImage" item={item} alt="" />
-              <div className="listing-body">
-                <h3>{item.title || item.id}</h3>
-                <p>{item.description}</p>
+        <List role="list">
+          {items.map((item) => (
+            <List.Item key={item['@id']}>
+              <div
+                className={`listing-item ${
+                  item.review_state ? `state-${item.review_state}` : ''
+                }`}
+                role="listitem"
+              >
+                <ConditionalLink item={item} condition={!isEditMode}>
+                  <Component componentName="PreviewImage" item={item} alt="" />
+                  <div className="listing-body">
+                    <List.Header className="summary-listing-item-header">
+                      {item.title ? item.title : item.id}
+                    </List.Header>
+                    <p>{item.description}</p>
+                  </div>
+                </ConditionalLink>
               </div>
-            </ConditionalLink>
-          </div>
-        ))}
+            </List.Item>
+          ))}
+        </List>
       </div>
 
       {link && <div className="footer">{link}</div>}
