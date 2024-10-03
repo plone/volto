d1acac9e60300ff8a89a0f50791906ed6be90a44
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-10-03 12:21:09 +0200

    Update to RAC 1.4.0, add new components in there + update storybook (#6364)

---
+++ packages/components/src/components/ColorSlider/ColorSlider.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import {
+  ColorSlider as RACColorSlider,
+  ColorSliderProps as RACColorSliderProps,
+  ColorThumb,
+  Label,
+  SliderOutput,
+  SliderTrack,
+} from 'react-aria-components';
+
+export interface ColorSliderProps extends RACColorSliderProps {
+  label?: string;
+}
+
+export function ColorSlider({ label, ...props }: ColorSliderProps) {
+  return (
+    <RACColorSlider {...props}>
+      <Label>{label}</Label>
+      <SliderOutput />
+      <SliderTrack
+        style={({ defaultStyle }) => ({
+          background: `${defaultStyle.background},
+            repeating-conic-gradient(#CCC 0% 25%, white 0% 50%) 50% / 16px 16px`,
+        })}
+      >
+        <ColorThumb />
+      </SliderTrack>
+    </RACColorSlider>
+  );
+}
