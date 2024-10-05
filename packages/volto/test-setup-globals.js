4e08ce5bed442c1af298fbf60b3db2354a243b7f
aadityaforwork <aadityamalani15@gmail.com>
2024-10-05 19:36:34 +0530

    updates

--- packages/volto/test-setup-globals.js
+++ packages/volto/test-setup-globals.js
@@ -1,19 +1,12 @@
-if (typeof window === 'undefined') {
-  global.__SERVER__ = true;
-  global.__CLIENT__ = false;
-  global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
-} else {
-  global.__CLIENT__ = true;
-  global.__SERVER__ = false;
-  global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
+global.__CLIENT__ = true;
+global.__DEVELOPMENT__ = false;
 
-  window.matchMedia =
-    window.matchMedia ||
-    function () {
-      return {
-        matches: false,
-        addListener: function () {},
-        removeListener: function () {},
-      };
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
     };
-}
+  };
