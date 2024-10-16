b97f9a965ac5c66e49c057f8dae520323c871d67
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-10-16 21:46:17 +0200

    Latest touches

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -1,9 +1,6 @@
 export * from './AppRouter';
 export * from './PloneClient';
 export * from './PloneProvider';
-// FlattenToAppURL provider should live in @plone/components (near to the basic components)
-// but @plone/components can't depend on other packages
-// we are proxying it here for convenience and centralization
-export { FlattenToAppURLProvider, useFlattenToAppURL } from '@plone/components';
-// Also proxying RouterProvider from react-aria-components
+
+// Proxying RouterProvider from react-aria-components
 export { RouterProvider } from 'react-aria-components';
