79faf26fb6abaf528cf5abb85b81cfc8512682f9
David Glick <david@glicksoftware.com>
2024-10-23 10:45:47 -0700

    Backport #6425: Edit aliases in URL Management control panel

--- cypress/tests/core/basic/aliasesControlPanel.js
+++ cypress/tests/core/basic/aliasesControlPanel.js
@@ -1,4 +1,4 @@
-describe('Add a new alias from control panel interface', () => {
+describe('URL Management control panel', () => {
   beforeEach(() => {
     cy.autologin();
     cy.createContent({
@@ -12,13 +12,28 @@ describe('Add a new alias from control panel interface', () => {
     cy.waitForResourceToLoad('@actions');
     cy.waitForResourceToLoad('@types');
     cy.waitForResourceToLoad('my-page');
-  });
-  it('adds a new alias', () => {
+
     cy.visit('/controlpanel/aliases');
     cy.get('#add-alt-url').click();
-    cy.get('#alternative-url-input').type('/alturl');
-    cy.get('#target-url-input').type('/my-page');
-    cy.get('#submit-alias').click();
+    cy.get('#field-altUrlPath').type('/alturl');
+    cy.get('#field-targetUrlPath').type('/my-page');
+    cy.get('button[aria-label="Save"]').click();
+  });
+  it('adds a new alias', () => {
     cy.get('.toast-inner-content').contains('Alias has been added');
+  });
+  it('edits an existing new alias', () => {
+    cy.visit('/controlpanel/aliases');
+    cy.get('button[aria-label="Edit Alternative URL"]').click();
+    cy.get('#field-altUrlPath').clear().type('/alturl2');
+    cy.get('#field-targetUrlPath').clear().type('/my-page');
+    cy.get('button[aria-label="Save"]').click();
+    cy.get('.toast-inner-content').contains('Alias has been added');
+  });
+  it('removes an alias', () => {
+    cy.visit('/controlpanel/aliases');
+    cy.get('input[type="checkbox"][value="/alturl"]').click({ force: true });
+    cy.get('#remove-alt-urls').click();
+    cy.get('.toast-inner-content').contains('Aliases have been removed');
   });
 });
