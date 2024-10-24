1f813ad4bc182320a998fe371f812e57cf618ae2
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-10-24 09:31:07 +0200

    remove only

--- packages/registry/__tests__/addon-registry-app.test.js
+++ packages/registry/__tests__/addon-registry-app.test.js
@@ -3,7 +3,7 @@ import { AddonRegistry } from '../src/addon-registry/addon-registry';
 import { describe, it, expect } from 'vitest';
 
 describe('AddonRegistry - get()', () => {
-  it.only('Basic information', () => {
+  it('Basic information', () => {
     const base = path.join(import.meta.dirname, 'fixtures', 'test-app');
     const { addons, shadowAliases, theme } = AddonRegistry.init(base);
     expect(addons).toStrictEqual(['@plone/slots', 'my-addon']);
