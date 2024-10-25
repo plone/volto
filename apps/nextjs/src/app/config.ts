302ec6f9bedf0173e5b81fd181a0ff49b680d1d5
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-10-25 23:45:37 +0200

    Better Vercel deployment options

--- apps/nextjs/src/app/config.ts
+++ apps/nextjs/src/app/config.ts
@@ -10,6 +10,20 @@ const settings = {
   slate,
 };
 
+if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+  // If we are in Vercel
+  if (process.env.NEXT_PRODUCTION_URL) {
+    // We are in production deployment, we set the apiPath to the production URL
+    settings.apiPath = process.env.NEXT_PRODUCTION_URL;
+  } else {
+    // We are in preview deployment, we set the apiPath to the Vercel URL
+    settings.apiPath = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+} else {
+  // We are in development, we set the apiPath to localhost
+  settings.apiPath = 'http://localhost:3000/';
+}
+
 // @ts-expect-error Improve typings
 config.set('settings', settings);
 
