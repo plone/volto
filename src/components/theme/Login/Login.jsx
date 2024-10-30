5bca368b1eef49e6e66677c943d3ed284291f220
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2024-10-30 11:32:46 +0200

    Update Login.jsx

--- src/components/theme/Login/Login.jsx
+++ src/components/theme/Login/Login.jsx
@@ -75,7 +75,7 @@ const Login = (props) => {
   const loading = useSelector((state) => state.userSession.login.loading);
   const returnUrl =
     qs.parse(props.location?.search ?? location.search).return_url ||
-    location.pathname(/\/[^/]*\/?$/, '') ||
+    location.pathname.replace(/\/[^/]*\/?$/, '') ||
     '/';
 
   useEffect(() => {
