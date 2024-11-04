442666c8342cb853b460218c35e80494b5e75060
vipulgupta28 <121814604+vipulgupta28@users.noreply.github.com>
2024-11-04 13:23:32 +0530

    corrected mailregex

--- packages/volto/src/helpers/FormValidation/validators.ts
+++ packages/volto/src/helpers/FormValidation/validators.ts
@@ -78,7 +78,7 @@ export const emailValidator = ({ value, formatMessage }: Validator): string => {
   // Email Regex taken from from WHATWG living standard:
   // https://html.spec.whatwg.org/multipage/input.html#e-mail-state-(type=email)
   const emailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    /^mailto:([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*(,)?)+(\?[a-zA-Z0-9&=]*)?$/;
   const isValid = emailRegex.test(value);
   return !isValid ? formatMessage(messages.isValidEmail) : null;
 };
