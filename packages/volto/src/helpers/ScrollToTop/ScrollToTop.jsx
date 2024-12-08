e698d560d01ee80c5ac0ce390ec4110c18ebdf24
Faakhir30 <zahidfaakhir@gmail.com>
2024-12-09 02:30:26 +0500

    added news.
    Signed-off-by: Faakhir30 <zahidfaakhir@gmail.com>

--- packages/volto/src/helpers/ScrollToTop/ScrollToTop.jsx
+++ packages/volto/src/helpers/ScrollToTop/ScrollToTop.jsx
@@ -10,6 +10,12 @@ import config from '@plone/volto/registry';
  * @extends {Component}
  */
 class ScrollToTop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isClientSide = false;
+    this.isFirstClientUpdatePending = true;
+  }
+
   /**
    * Property types.
    * @property {Object} propTypes Property types.
@@ -23,11 +29,29 @@ class ScrollToTop extends React.Component {
   };
 
   /**
+   * Used to indicate client-side rendering
+   * @memberof ScrollToTop
+   */
+  componentDidMount() {
+    this.isClientSide = true;
+  }
+
+  /**
    * @param {*} prevProps Previous Props
    * @returns {null} Null
    * @memberof ScrollToTop
    */
   componentDidUpdate(prevProps) {
+    // avoid scrollToTop during SSR
+    if (!this.isClientSide) {
+      return;
+    }
+    // Skip the first client-side update that happens right after hydration
+    if (this.isFirstClientUpdatePending) {
+      this.isFirstClientUpdatePending = false;
+      return;
+    }
+
     const { location } = this.props;
     const noInitialBlocksFocus = // Do not scroll on /edit
       config.blocks?.initialBlocksFocus === null
