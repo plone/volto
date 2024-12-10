5d1fc4fbf11b2019fcf6fe445af901484ac48233
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-12-10 19:07:15 +0100

    Add disable Teaser block Align handlers for teasers inside containers/grids

--- packages/volto/src/components/manage/Blocks/Teaser/schema.js
+++ packages/volto/src/components/manage/Blocks/Teaser/schema.js
@@ -127,3 +127,15 @@ export const gridTeaserDisableStylingSchema = ({ schema, formData, intl }) => {
   schema.fieldsets = schema.fieldsets.filter((item) => item.id !== 'styling');
   return schema;
 };
+
+export const gridTeaserDisableAlignHandlersSchema = ({
+  schema,
+  formData,
+  intl,
+}) => {
+  schema.properties.styles.schema.fieldsets[0].fields =
+    schema.properties.styles.schema.fieldsets[0].fields.filter(
+      (item) => !['align'].includes(item),
+    );
+  return schema;
+};
