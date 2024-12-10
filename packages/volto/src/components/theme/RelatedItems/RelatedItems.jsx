850432677ce0476af04adc708d3ca5b0894f7d64
Ichim David <ichim.david@gmail.com>
2024-12-10 12:45:27 +0200

    Fix imports for non barrel paths in RelatedItems.jsx

--- packages/volto/src/components/theme/RelatedItems/RelatedItems.jsx
+++ packages/volto/src/components/theme/RelatedItems/RelatedItems.jsx
@@ -3,11 +3,11 @@
  * @module components/theme/RelatedItems/RelatedItems
  */
 
-import { UniversalLink } from '@plone/volto/components';
+import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
 import { defineMessages, injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
-import config from '@plone/registry';
+import config from '@plone/volto/registry';;
 
 const messages = defineMessages({
   relatedItems: {
