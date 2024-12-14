317253c7919bd0715eb328a23890d83d98406fad
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-12-14 11:57:41 +0100

    WIP - First components

--- packages/cmsui/index.ts
+++ packages/cmsui/index.ts
@@ -1,5 +1,9 @@
 import type { ConfigType } from '@plone/registry';
+import installWidgets from './config/widgets';
+import '@plone/components/dist/basic.css';
+import '@plone/components/dist/quanta.css';
 
 export default function install(config: ConfigType) {
+  installWidgets(config);
   return config;
 }
--- packages/volto/src/helpers/Html/Html.jsx
+++ packages/volto/src/helpers/Html/Html.jsx
@@ -102,6 +102,12 @@ class Html extends Component {
           {head.meta.toComponent()}
           {head.link.toComponent()}
           {head.script.toComponent()}
+
+          {config.settings.cssLayers && (
+            // Load the CSS layers from config, if any
+            <style>{`@layer ${config.settings.cssLayers.join(', ')};`}</style>
+          )}
+
           {head.style.toComponent()}
 
           <script
