81e2c44cac7cdd23dddcc1c86a11e12055f16b54
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-01-19 00:12:17 +0100

    ESlint import no unresolved

--- packages/voltotng/app/routes.ts
+++ packages/voltotng/app/routes.ts
@@ -2,6 +2,7 @@ import type { RouteConfig } from '@react-router/dev/routes';
 import { index, route } from '@react-router/dev/routes';
 import { getAddonRoutesConfig } from '@plone/react-router';
 import config from '@plone/registry';
+// eslint-disable-next-line import/no-unresolved
 import applyAddonConfiguration, { addonsInfo } from '../.registry.loader';
 
 applyAddonConfiguration(config);
