5c7906ab7ad4e4a676de59063f710e1e9a8e9254
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-01-21 11:27:29 +0530

    Migrated src/helpers to vitest

--- packages/volto/src/helpers/Api/Api.test.js
+++ packages/volto/src/helpers/Api/Api.test.js
@@ -1,21 +1,24 @@
 // import superagent from 'superagent';
 import config from '@plone/volto/registry';
 import Api from './Api';
+import { vi } from 'vitest';
 
 // jest.mock('react-cookie', () => ({
 //   load: jest.fn(() => 'token'),
 // }));
 //
 
-jest.mock('superagent', () => ({
-  get: jest.fn((url) => ({
-    url,
-    query: jest.fn(),
-    set: jest.fn(),
-    type: jest.fn(),
-    send: jest.fn(),
-    end: jest.fn(),
-  })),
+vi.mock('superagent', () => ({
+  default: {
+    get: vi.fn((url) => ({
+      url,
+      query: vi.fn(),
+      set: vi.fn(),
+      type: vi.fn(),
+      send: vi.fn(),
+      end: vi.fn(),
+    })),
+  },
 }));
 
 beforeAll(() => {
