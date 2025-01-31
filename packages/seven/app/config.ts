08b4499e559c607d075484851975f3c67da80dfd
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-01-31 12:48:16 +0100

    Simplify Seven data fetching, add addons styles loader (#6636)

--- packages/seven/app/config.ts
+++ packages/seven/app/config.ts
@@ -2,10 +2,10 @@
  * This is the client side config entry point
  */
 import config from '@plone/registry';
-import applyAddonConfiguration from '@plone/registry/addons-loader';
+// eslint-disable-next-line import/no-unresolved
+import applyAddonConfiguration from '../registry.loader';
 
 export default function install() {
   applyAddonConfiguration(config);
-  config.settings.apiPath = 'http://localhost:3000';
   return config;
 }
