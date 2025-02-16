c70b7cea72b8baa777ae3e361fa314550b8a5957
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-02-16 18:42:44 +0100

    More add-on, add route

--- packages/contents/index.ts
+++ packages/contents/index.ts
@@ -1,8 +1,10 @@
 import type { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
+  config.registerRoute({
+    type: 'route',
+    file: '@plone/contents/routes/contents.tsx',
+    path: 'contents',
+  });
   return config;
 }
-
-export { ContentsTable } from './components/ContentsTable';
-export * from './providers/contents';
