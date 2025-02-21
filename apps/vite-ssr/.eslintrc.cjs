aaa292e0404fcc66b025b1b56c5c0a8a95b03876
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-02-21 15:50:49 +0100

    Remove for now the API PoCs since they have to be updated anyways, we have to make them work in R19 anyways.

--- apps/vite-ssr/.eslintrc.cjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ['../../.eslintrc.cjs', 'plugin:react/jsx-runtime'],
-  ignorePatterns: ['storybook-static', 'dist'],
-};
