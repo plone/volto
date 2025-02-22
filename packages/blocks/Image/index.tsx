5f068e84ab1e25d529758c94cacb699b2f908d2d
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-02-22 23:28:43 +0100

    Fix image block so it goes through the ++api++ routing (#6773)

--- packages/blocks/Image/index.tsx
+++ packages/blocks/Image/index.tsx
@@ -1,13 +1,17 @@
 import type { BlockViewProps } from '@plone/types';
 import { usePloneProvider } from '@plone/providers';
 
+const addApiToUrl = (url: string): string => {
+  const urlObj = new URL(url);
+  urlObj.pathname = `/++api++${urlObj.pathname}`;
+  return urlObj.toString();
+};
+
 const ImageBlockView = (props: BlockViewProps) => {
-  // console.log(props);
-  const flattenToAppURL = usePloneProvider().flattenToAppURL;
   const { data } = props;
   if (!data.url) return null;
   const url = data.image_scales
-    ? `/++api++${flattenToAppURL(data.url)}/${data.image_scales[data.image_field][0].scales.larger.download}`
+    ? `${addApiToUrl(data.url)}/${data.image_scales[data.image_field][0].scales.larger.download}`
     : data.url;
   // data.preview_image?.[0]?.['@id'] ||
   // `/++api++${data.href[0]?.image_scales[data.href[0].image_field][0].base_path}/${data.href[0]?.image_scales[data.href[0].image_field][0].download}`;
