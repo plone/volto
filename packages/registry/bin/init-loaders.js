e1cc6bd5e9a2b068c681ba6693d94c37bf1b4c23
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-02-25 08:21:45 +0100

    Fixed temporarily the upgrade to RR 7.2.0 problem introduced in remix-run/react-router#13078 (#6780)

---
+++ packages/registry/bin/init-loaders.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import { AddonRegistry } from '@plone/registry/addon-registry';
+import { createAddonsLoader } from '@plone/registry/create-addons-loader';
+import { createAddonsStyleLoader } from '@plone/registry/create-addons-styles-loader';
+
+function initPloneRegistryLoaders() {
+  const projectRootPath = path.resolve('.');
+  const { registry, shadowAliases } = AddonRegistry.init(projectRootPath);
+
+  const addonsLoaderPath = createAddonsLoader(
+    registry.getAddonDependencies(),
+    registry.getAddons(),
+    { tempInProject: true },
+  );
+
+  createAddonsStyleLoader(registry.getAddonStyles());
+
+  return { registry, shadowAliases, addonsLoaderPath };
+}
+
+initPloneRegistryLoaders();
---
+++ packages/registry/news/6780.feature
@@ -0,0 +1,2 @@
+New bin `init-loaders` for initialize via command line the @plone/registry loaders.
+This fixes the upgrade to 7.2.0 problem introduced in remix-run/react-router#13078 @sneridagh
