34a03aae13f90b3d8ef068536d51ecb34c109e7c
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-01 11:52:54 +0100

    prettier on missing file

--- packages/components/src/tailwind/utils.ts
+++ packages/components/src/tailwind/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge';
 import { tv } from 'tailwind-variants';
 
 export const focusRing = tv({
-  base: 'outline outline-blue-600 dark:outline-blue-500 forced-colors:outline-[Highlight] outline-offset-2',
+  base: 'outline outline-offset-2 outline-blue-600 dark:outline-blue-500 forced-colors:outline-[Highlight]',
   variants: {
     isFocusVisible: {
       false: 'outline-0',
