fcedabdf136a3f1c7e63c608f1f8844a7a79b623
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-03 13:17:04 +0100

    Fix deployment issue, flattenToAppURL needed

--- packages/volto/src/components/theme/AlternateHrefLangs/AlternateHrefLangs.test.jsx
+++ packages/volto/src/components/theme/AlternateHrefLangs/AlternateHrefLangs.test.jsx
@@ -133,4 +133,57 @@ describe('AlternateHrefLangs', () => {
       hrefLang: 'en',
     });
   });
+
+  it('multilingual site, with all available translations - with server URL', () => {
+    config.settings.isMultilingual = true;
+    config.settings.supportedLanguages = ['en', 'es', 'eu'];
+    const store = mockStore({
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const content = {
+      '@id': 'http://localhost:3000/en',
+      language: { token: 'en', title: 'English' },
+      '@components': {
+        translations: {
+          items: [
+            { '@id': 'http://localhost:3000/eu', language: 'eu' },
+            { '@id': 'http://localhost:3000/es', language: 'es' },
+          ],
+        },
+      },
+    };
+
+    // We need to force the component rendering
+    // to fill the Helmet
+    renderer.create(
+      <Provider store={store}>
+        <AlternateHrefLangs content={content} />
+      </Provider>,
+    );
+
+    const helmetLinks = Helmet.peek().linkTags;
+
+    // We expect having 3 links
+    expect(helmetLinks.length).toBe(3);
+
+    expect(helmetLinks).toContainEqual({
+      rel: 'alternate',
+      href: '/eu',
+      hrefLang: 'eu',
+    });
+    expect(helmetLinks).toContainEqual({
+      rel: 'alternate',
+      href: '/es',
+      hrefLang: 'es',
+    });
+    expect(helmetLinks).toContainEqual({
+      rel: 'alternate',
+      href: '/en',
+      hrefLang: 'en',
+    });
+  });
 });
