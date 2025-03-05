13e98a8409175f1463ce8b2c0fbc274d53256c33
David Glick <david@glicksoftware.com>
2025-03-04 21:44:26 -0800

    Update packages/volto/src/config/index.js

--- packages/volto/src/config/index.js
+++ packages/volto/src/config/index.js
@@ -112,8 +112,8 @@ let config = {
     notSupportedBrowsers: ['ie'],
     defaultPageSize: 25,
     isMultilingual: false,
-    supportedLanguages: ['it'],
-    defaultLanguage: 'it',
+    supportedLanguages: ['en'],
+    defaultLanguage: 'en',
     navDepth: 1,
     expressMiddleware: serverConfig.expressMiddleware, // BBB
     defaultBlockType: 'slate',
