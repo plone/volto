f689611b5b4e312666b4f8a1150f503b08c1d91b
Rob Gietema <rob.gietema@gmail.com>
2025-03-11 12:07:20 +0100

    Fix test label.

--- packages/volto/src/components/theme/Widgets/HiddenWidget.test.jsx
+++ packages/volto/src/components/theme/Widgets/HiddenWidget.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import HiddenWidget from './HiddenWidget';
 
-describe('TextWidget', () => {
+describe('HiddenWidget', () => {
   it('renders an empty hidden view widget component', () => {
     const component = renderer.create(<HiddenWidget />);
     const json = component.toJSON();
