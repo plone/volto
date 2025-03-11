8a4be285e379683115de11f26ccf94b8fad837bc
wesleybl <wesleybl@gmail.com>
2025-03-11 18:05:43 -0300

    Remove unnecessary change in Toolbar.jsx

--- packages/volto/src/components/manage/Toolbar/Toolbar.jsx
+++ packages/volto/src/components/manage/Toolbar/Toolbar.jsx
@@ -540,8 +540,7 @@ class Toolbar extends Component {
                       ((this.props.content.is_folderish &&
                         this.props.types.length > 0) ||
                         (config.settings.isMultilingual &&
-                          this.props.content?.['@components']
-                            ?.translations)) && (
+                          this.props.content['@components']?.translations)) && (
                         <button
                           className="add"
                           aria-label={this.props.intl.formatMessage(
