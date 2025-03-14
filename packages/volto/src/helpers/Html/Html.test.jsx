43391d37d2851f342b2a4ff55fc661791d7984b8
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-03-14 10:13:32 +0530

    fix Datetimewidget and other issue

--- packages/volto/src/helpers/Html/Html.test.jsx
+++ packages/volto/src/helpers/Html/Html.test.jsx
@@ -1,6 +1,5 @@
-import { describe, it, expect, vi } from 'vitest';
 import React from 'react';
-import { create } from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import config from '@plone/volto/registry';
 import Html from './Html';
 
@@ -40,13 +39,12 @@ vi.mock('../BodyClass/BodyClass', () => ({
   },
 }));
 
-config.settings = {
-  initialReducersBlacklist: ['navigation'],
-};
+config.settings = {};
+config.settings.initialReducersBlacklist = ['navigation'];
 
 describe('Html', () => {
   it('renders a html component', () => {
-    const component = create(
+    const component = renderer.create(
       <Html
         extractor={{
           getLinkElements: () => [
@@ -73,101 +71,10 @@ describe('Html', () => {
       />,
     );
     const json = component.toJSON();
-    expect(json).toMatchInlineSnapshot(`
-      <html
-        lang="en"
-      >
-        <head>
-          <meta
-            charSet="utf-8"
-          />
-          <script
-            dangerouslySetInnerHTML={
-              {
-                "__html": "window.env = {};",
-              }
-            }
-          />
-          <link
-            href="/favicon.ico"
-            rel="icon"
-            sizes="any"
-          />
-          <link
-            href="/icon.svg"
-            rel="icon"
-            type="image/svg+xml"
-          />
-          <link
-            href="/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/site.webmanifest"
-            rel="manifest"
-          />
-          <meta
-            content="Plone 6 - https://plone.org"
-            name="generator"
-          />
-          <meta
-            content="width=device-width, initial-scale=1"
-            name="viewport"
-          />
-          <meta
-            content="yes"
-            name="mobile-web-app-capable"
-          />
-          <link
-            as="script"
-            crossOrigin="true"
-            data-chunk="client"
-            href="http://localhost:3001/static/js/runtime~client.js"
-            rel="preload"
-          />
-        </head>
-        <body
-          className="class1 class2"
-        >
-          <div
-            aria-label="Toolbar"
-            id="toolbar"
-            role="navigation"
-          />
-          <div
-            dangerouslySetInnerHTML={
-              {
-                "__html": "<div />",
-              }
-            }
-            id="main"
-          />
-          <div
-            aria-label="Sidebar"
-            id="sidebar"
-            role="complementary"
-          />
-          <script
-            charSet="UTF-8"
-            dangerouslySetInnerHTML={
-              {
-                "__html": "window.__data={"content":{"@id":"http:\\u002F\\u002Fdummy"}};",
-              }
-            }
-          />
-          <script
-            async={true}
-            crossOrigin="true"
-            src="bundle.js"
-          />
-        </body>
-      </html>
-    `);
+    expect(json).toMatchSnapshot();
   });
-
   it('renders a html component with apiPath present', () => {
-    const component = create(
+    const component = renderer.create(
       <Html
         extractor={{
           getLinkElements: () => [
@@ -195,96 +102,6 @@ describe('Html', () => {
       />,
     );
     const json = component.toJSON();
-    expect(json).toMatchInlineSnapshot(`
-      <html
-        lang="en"
-      >
-        <head>
-          <meta
-            charSet="utf-8"
-          />
-          <script
-            dangerouslySetInnerHTML={
-              {
-                "__html": "window.env = {"apiPath":"https:\\u002F\\u002Fplone.org"};",
-              }
-            }
-          />
-          <link
-            href="/favicon.ico"
-            rel="icon"
-            sizes="any"
-          />
-          <link
-            href="/icon.svg"
-            rel="icon"
-            type="image/svg+xml"
-          />
-          <link
-            href="/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/site.webmanifest"
-            rel="manifest"
-          />
-          <meta
-            content="Plone 6 - https://plone.org"
-            name="generator"
-          />
-          <meta
-            content="width=device-width, initial-scale=1"
-            name="viewport"
-          />
-          <meta
-            content="yes"
-            name="mobile-web-app-capable"
-          />
-          <link
-            as="script"
-            crossOrigin="true"
-            data-chunk="client"
-            href="http://localhost:3001/static/js/runtime~client.js"
-            rel="preload"
-          />
-        </head>
-        <body
-          className="class1 class2"
-        >
-          <div
-            aria-label="Toolbar"
-            id="toolbar"
-            role="navigation"
-          />
-          <div
-            dangerouslySetInnerHTML={
-              {
-                "__html": "<div />",
-              }
-            }
-            id="main"
-          />
-          <div
-            aria-label="Sidebar"
-            id="sidebar"
-            role="complementary"
-          />
-          <script
-            charSet="UTF-8"
-            dangerouslySetInnerHTML={
-              {
-                "__html": "window.__data={"content":{"@id":"http:\\u002F\\u002Fdummy"}};",
-              }
-            }
-          />
-          <script
-            async={true}
-            crossOrigin="true"
-            src="bundle.js"
-          />
-        </body>
-      </html>
-    `);
+    expect(json).toMatchSnapshot();
   });
 });
