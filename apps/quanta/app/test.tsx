fffd829b749221826f93822c8f3a01a1edca1b64
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-15 12:58:57 +0100

    More polishments

--- apps/quanta/app/test.tsx
+++ apps/quanta/app/test.tsx
@@ -3,7 +3,7 @@ import { Button as ButtonTW } from '@plone/cmsui/components/Button';
 
 export default function Test() {
   return (
-    <div className="text-center">
+    <div className="flex flex-col text-center items-center justify-center min-h-screen">
       <div className="bootstrap-wrapper">
         <h1>Bootstrap button</h1>
         <Button variant="danger">Primary</Button>
