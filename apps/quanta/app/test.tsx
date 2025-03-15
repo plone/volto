b4a60d451f6b875b777a42813c895f059f00e665
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-15 12:55:46 +0100

    polish

--- apps/quanta/app/test.tsx
+++ apps/quanta/app/test.tsx
@@ -3,15 +3,19 @@ import { Button as ButtonTW } from '@plone/cmsui/components/Button';
 
 export default function Test() {
   return (
-    <div>
+    <div className="text-center">
       <div className="bootstrap-wrapper">
         <h1>Bootstrap button</h1>
         <Button variant="danger">Primary</Button>
       </div>
 
-      <div className="plone-components-tw-wrapper">
-        <h1>@plone/components TW button</h1>
-        <ButtonTW variant="primary">Primary</ButtonTW>
+      <div className="plone-components-tw-wrapper mt-10">
+        <h1>
+          @plone/components TW button (Notice this is an TW H1, not styled!)
+        </h1>
+        <ButtonTW variant="primary" className="mt-4">
+          Primary
+        </ButtonTW>
       </div>
     </div>
   );
