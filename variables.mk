f2f9a474ac7735cb06373cac6d80cb6bd72a8aa9
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-18 17:40:44 +0100

    Update to use Plone 6.1.1rc1

--- variables.mk
+++ variables.mk
@@ -1,6 +1,6 @@
 INSTANCE_PORT=8080
-DOCKER_IMAGE=plone/server-dev:6.1.0
-DOCKER_IMAGE_ACCEPTANCE=plone/server-acceptance:6.1.0
+DOCKER_IMAGE=plone/server-dev:6.1.1rc1
+DOCKER_IMAGE_ACCEPTANCE=plone/server-acceptance:6.1.1rc1
 KGS=
 NODEBIN = ./node_modules/.bin
 SCRIPTSPACKAGE = ./packages/scripts
