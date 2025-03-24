5d541beaf427a1507e07fb1bedf17b34afa16236
Rob Gietema <rob.gietema@gmail.com>
2025-03-24 16:19:55 +0100

    Volto form block fixes (#6370)
    Co-authored-by: Balázs Reé <ree@greenfinity.hu>

---
+++ packages/volto/src/components/theme/Widgets/HiddenWidget.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HiddenWidget from './HiddenWidget';
+
+describe('HiddenWidget', () => {
+  it('renders an empty hidden view widget component', () => {
+    const component = renderer.create(<HiddenWidget />);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+});
