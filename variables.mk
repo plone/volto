edffe43e622cfaa2fd769864e08fef16135d557d
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-03-25 17:46:12 +0200

    Update Plone 6.1.1 final (#6868)

--- variables.mk
+++ variables.mk
@@ -1,6 +1,6 @@
 INSTANCE_PORT=8080
-DOCKER_IMAGE=plone/server-dev:6.1.1rc2
-DOCKER_IMAGE_ACCEPTANCE=plone/server-acceptance:6.1.1rc2
+DOCKER_IMAGE=plone/server-dev:6.1.1
+DOCKER_IMAGE_ACCEPTANCE=plone/server-acceptance:6.1.1
 KGS=
 NODEBIN = ./node_modules/.bin
 SCRIPTSPACKAGE = ./packages/scripts
