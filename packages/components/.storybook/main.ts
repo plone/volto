febf5b623d9befcd9f89d80c63c0fe0deae07726
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-03-27 14:39:54 +0200

    [components] Tailwind support (#6795)
    Co-authored-by: Piero Nicolli <piero.nicolli@redturtle.it>

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   // For some reason the property does not allow negation
@@ -27,6 +28,7 @@ const config: StorybookConfig = {
   },
   async viteFinal(config) {
     return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
       build: {
         minify: false,
       },
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,9 +1,11 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { PloneSVGRVitePlugin } from './vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [PloneSVGRVitePlugin(), react()],
+  plugins: [tsconfigPaths(), tailwindcss(), PloneSVGRVitePlugin(), react()],
   css: {
     transformer: 'lightningcss',
   },
