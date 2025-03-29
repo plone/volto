074a07ff769f69c355ecf5592ef3f605c2f026b6
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-03-29 12:29:48 +0200

    Added setup in `.vscode/settings.json` for configuring the Tailwind settings for Seven (#6912)

--- packages/scripts/vscodesettings.js
+++ packages/scripts/vscodesettings.js
@@ -23,6 +23,25 @@ if (!vscodeSettingsJSON['[markdown]']) {
   vscodeSettingsJSON['[markdown]']['editor.formatOnSave'] = false;
 }
 
+if (!vscodeSettingsJSON['tailwindCSS.experimental.configFile']) {
+  vscodeSettingsJSON['tailwindCSS.experimental.configFile'] = {
+    'apps/seven/addons.styles.css': [
+      'apps/seven/app/**/*',
+      'packages/theming/**/*',
+      'packages/cmsui/**/*',
+      'packages/publicui/**/*',
+      'packages/slots/**/*',
+      'packages/components/**/*',
+    ],
+  };
+}
+
+if (!vscodeSettingsJSON['tailwindCSS.experimental.classRegex']) {
+  vscodeSettingsJSON['tailwindCSS.experimental.classRegex'] = [
+    ['(["\'`][^"\'`]*.*?["\'`])', '["\'`]([^"\'`]*).*?["\'`]'],
+  ];
+}
+
 fs.writeFileSync(
   '.vscode/settings.json',
   `${stringify(vscodeSettingsJSON, null, 2)}`,
