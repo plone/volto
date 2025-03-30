7017d3eec5c2087321e88e96254aefe6dfac1d59
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-03-30 20:37:25 +0200

    Update to vitest 3

--- packages/theming/vitest.config.ts
+++ packages/theming/vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTesting.ts',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
