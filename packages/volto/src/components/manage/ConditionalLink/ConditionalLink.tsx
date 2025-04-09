a4e5d50a875055d42469ce54c2e7aa4f2797eacf
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-09 12:48:30 +0200

    TypeScript version of `ConditionalLink`

---
+++ packages/volto/src/components/manage/ConditionalLink/ConditionalLink.tsx
@@ -0,0 +1,25 @@
+import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
+import type { UniversalLinkProps } from '@plone/volto/components/manage/UniversalLink/UniversalLink';
+
+type ConditionalLinkProps = UniversalLinkProps & {
+  condition: boolean;
+  to: string;
+};
+
+const ConditionalLink = ({ condition, to, ...props }: ConditionalLinkProps) => {
+  if (condition) {
+    if (!props.item && to) {
+      return (
+        <UniversalLink {...props} href={to}>
+          {props.children}
+        </UniversalLink>
+      );
+    } else {
+      <UniversalLink {...props}>{props.children}</UniversalLink>;
+    }
+  } else {
+    return <>{props.children}</>;
+  }
+};
+
+export default ConditionalLink;
