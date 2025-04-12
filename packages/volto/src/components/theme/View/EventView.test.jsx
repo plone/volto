152c1e1662e83a7db17e3c6371d72aa034b54300
Bhanudas Hegaje <abhishekhegaje1702@gmail.com>
2025-04-12 19:39:40 +0530

    Feature/Jest to Vitest migration (#6605)
    Co-authored-by: Victor Fernandez de Alba <sneridagh@gmail.com>
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- packages/volto/src/components/theme/View/EventView.test.jsx
+++ packages/volto/src/components/theme/View/EventView.test.jsx
@@ -14,11 +14,13 @@ const store = mockStore({
   },
 });
 
-jest.mock('@plone/volto/helpers/Loadable/Loadable');
-beforeAll(
-  async () =>
-    await require('@plone/volto/helpers/Loadable/Loadable').__setLoadables(),
-);
+vi.mock('@plone/volto/helpers/Loadable/Loadable');
+beforeAll(async () => {
+  const { __setLoadables } = await import(
+    '@plone/volto/helpers/Loadable/Loadable'
+  );
+  await __setLoadables();
+});
 
 const { settings } = config;
 
