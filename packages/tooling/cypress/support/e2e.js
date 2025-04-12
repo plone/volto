bf7e20d7d936c30e5b78bf050586ea239b14ca3f
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-04-13 00:09:18 +0200

    Login acceptance tests, improve login form (#6944)
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

--- packages/tooling/cypress/support/e2e.js
+++ packages/tooling/cypress/support/e2e.js
@@ -18,8 +18,9 @@ Cypress.on('uncaught:exception', (err) => {
   // Cypress and React Hydrating the document don't get along
   // for some unknown reason. Hopefully, we figure out why eventually
   // so we can remove this.
+
   if (
-    /hydrat/i.test(err.message) ||
+    /ydrat/i.test(err.message) ||
     /Minified React error #418/.test(err.message) ||
     /Minified React error #423/.test(err.message)
   ) {
