2776103b6e8401feaf1f855ac2b23b5fe7649a6d
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-12 19:56:57 +0200

    Add logout

--- packages/cmsui/cypress/tests/login.cy.ts
+++ packages/cmsui/cypress/tests/login.cy.ts
@@ -16,4 +16,17 @@ describe('Login Route Tests', () => {
     cy.findByRole('button', { name: /Sign in/i }).click();
     cy.getCookie('auth_seven').should('exist');
   });
+
+  it('As a registered user I can logout', function () {
+    const user = ploneAuth[0];
+    const password = ploneAuth[1];
+
+    cy.wait(300); // Something is wrong and the field is not there yet :( further investigate this behavior in the future
+    cy.findByLabelText('username').type(user, { force: true });
+    cy.findByLabelText('password').type(password, { force: true });
+    cy.findByRole('button', { name: /Sign in/i }).click();
+    cy.getCookie('auth_seven').should('exist');
+    cy.visit('/logout');
+    cy.getCookie('auth_seven').should('not.exist');
+  });
 });
