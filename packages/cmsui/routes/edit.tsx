3104193d27db83669282a63a4840b8ecf53b1a8b
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-16 19:15:04 +0200

    Initial work

--- packages/cmsui/routes/edit.tsx
+++ packages/cmsui/routes/edit.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import {
+  Form,
   redirect,
   useLoaderData,
   type ActionFunctionArgs,
@@ -9,6 +10,20 @@ import type PloneClient from '@plone/client';
 import config from '@plone/registry';
 import { requireAuthCookie } from './auth/auth';
 import { Button } from '@plone/components/tailwind';
+import { mergeForm, useForm, useTransform } from '@tanstack/react-form';
+import {
+  ServerValidateError,
+  createServerValidate,
+  formOptions,
+  initialFormState,
+} from '@tanstack/react-form/remix';
+import { TextField } from '../components/TextField/TextField';
+import { atom, useAtom, useSetAtom } from 'jotai';
+import { focusAtom } from 'jotai-optics';
+import { useHydrateAtoms } from 'jotai/utils';
+import { useMemo } from 'react';
+import type { WritableAtom } from 'jotai';
+import type { ReactNode } from 'react';
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   const token = await requireAuthCookie(request);
@@ -54,32 +69,114 @@ export async function action({ params, request }: ActionFunctionArgs) {
   return redirect(path);
 }
 
+const formAtom = atom({});
+
+const HydrateAtoms = ({
+  atomValues,
+  children,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  atomValues: Iterable<
+    readonly [WritableAtom<unknown, [any], unknown>, unknown]
+  >;
+  children: ReactNode;
+}) => {
+  // initialising on state with prop on render here
+  useHydrateAtoms(new Map(atomValues));
+  return children;
+};
+
+const ConsoleLog = ({ focusAtom }) => {
+  const [title] = useAtom(focusAtom);
+  const [formData, setFormData] = useAtom(formAtom);
+
+  return (
+    <div className="mt-4">
+      <pre>
+        Global form data main JOTAI atom{' '}
+        {JSON.stringify(formData.title, null, 2)}
+      </pre>
+      <pre>
+        Focused atom GETTER on title key {JSON.stringify(title, null, 2)}
+      </pre>
+    </div>
+  );
+};
+
 export default function Edit() {
   const { content, schema } = useLoaderData<typeof loader>();
   const { t } = useTranslation();
+  console.log(schema);
+  const form = useForm({
+    defaultValues: content,
+  });
+
+  const useTitleAtom = ({ formAtom, field }) => {
+    return useMemo(() => {
+      return focusAtom(formAtom, (optic) => optic.prop(field));
+    }, [formAtom, field]);
+  };
+
+  const titleAtom = useTitleAtom({ formAtom, field: 'title' });
+  const setTitle = useSetAtom(titleAtom);
+
+  // console.log('jotai title', setTitle);
 
   return (
-    <main>
-      <h1>
-        {content.title} - {t('cmsui.edit')}
-      </h1>
-      <form method="post">
-        {Object.entries(schema.properties).map(([key, value]) => {
-          if (key !== 'title') return null;
-          return (
-            <div key={key}>
-              <label htmlFor={key}>{value.title}</label>
-              <input
-                type={value.type}
-                id={key}
-                name={key}
-                defaultValue={content[key]}
-              />
+    <HydrateAtoms atomValues={[[formAtom, content]]}>
+      <main className="mx-4 flex h-screen flex-1 flex-col justify-center">
+        <div className="flex flex-col sm:mx-auto sm:w-full sm:max-w-lg">
+          <h1 className="mb-4 text-2xl font-bold">
+            {content.title} - {t('cmsui.edit')}
+          </h1>
+          <Form method="post" onSubmit={form.handleSubmit}>
+            <form.Field name="title">
+              {(field) => {
+                return (
+                  <div className="mb-8">
+                    <TextField
+                      label={schema.properties[field.name].title}
+                      name={field.name}
+                      defaultValue={field.state.value}
+                      onChange={(value) => {
+                        setTitle(value);
+                        return field.handleChange(value);
+                      }}
+                    />
+                    {field.state.meta.errors.map((error) => (
+                      <p key={error as string}>{error}</p>
+                    ))}
+                  </div>
+                );
+              }}
+            </form.Field>
+            <form.Field name="description">
+              {(field) => {
+                return (
+                  <div>
+                    <TextField
+                      label={schema.properties[field.name].title}
+                      name={field.name}
+                      defaultValue={field.state.value}
+                      onChange={(value) => {
+                        setTitle(value);
+                        return field.handleChange(value);
+                      }}
+                    />
+                    {field.state.meta.errors.map((error) => (
+                      <p key={error as string}>{error}</p>
+                    ))}
+                  </div>
+                );
+              }}
+            </form.Field>
+            <div className="mt-4">
+              <Button type="submit">{t('cmsui.save')}</Button>
             </div>
-          );
-        })}
-        <Button type="submit">{t('cmsui.save')}</Button>
-      </form>
-    </main>
+          </Form>
+        </div>
+        <ConsoleLog focusAtom={titleAtom} />
+      </main>
+    </HydrateAtoms>
   );
 }
