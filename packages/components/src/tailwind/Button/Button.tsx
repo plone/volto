dab8899601a4fbe165cccefc377732cfad976804
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-04-18 19:40:39 +0200

    Improve `Button` Tailwind component + Added `Accordion` Tailwind component (#7003)

--- packages/components/src/tailwind/Button/Button.tsx
+++ packages/components/src/tailwind/Button/Button.tsx
@@ -8,33 +8,61 @@ import { tv } from 'tailwind-variants';
 import { focusRing } from '../utils';
 
 export interface ButtonProps extends RACButtonProps {
-  variant?: 'primary' | 'secondary' | 'destructive';
+  variant?: 'neutral' | 'primary' | 'destructive';
   size?: 'S' | 'L';
+  accent?: boolean;
 }
 
 const button = tv({
   extend: focusRing,
-  base: 'cursor-default rounded-md px-5 py-3 text-center text-sm transition hover:shadow-sm focus:shadow-sm active:shadow-md',
+  base: 'cursor-default rounded-md px-3 py-1.5 text-center text-base font-medium transition hover:shadow-sm focus:shadow-sm active:shadow-md has-[svg]:rounded-full has-[svg]:p-1.5 has-[svg]:text-xs',
   variants: {
     variant: {
+      neutral:
+        'pressed:bg-quanta-cobalt bg-quanta-air hover:bg-quanta-snow active:bg-quanta-silver focus:bg-quanta-snow text-quanta-iron has-[svg]:text-quanta-iron',
       primary:
-        'pressed:bg-quanta-cobalt bg-quanta-sapphire hover:bg-quanta-royal active:bg-quanta-cobalt focus:bg-quanta-royal text-white has-[svg]:rounded-full has-[svg]:p-3 has-[svg]:text-white',
-      secondary:
-        'pressed:bg-gray-300 focus:bg-quanta-smoke bg-quanta-snow hover:bg-quanta-smoke active:bg-quanta-silver text-quanta-iron has-[svg]:text-quanta-iron has-[svg]:rounded-full has-[svg]:p-3',
+        'pressed:bg-quanta-cobalt bg-quanta-air hover:bg-quanta-arctic hover:text-quanta-royal active:bg-quanta-sky active:text-quanta-royal focus:bg-quanta-artic focus:text-quanta-royal text-quanta-sapphire',
       destructive:
-        'pressed:bg-quanta-rose active:bg-quanta-rose bg-quanta-candy hover:bg-quanta-wine focus:bg-quanta-wine text-white',
+        'pressed:bg-quanta-rose active:bg-quanta-flamingo bg-quanta-air hover:bg-quanta-ballet hover:text-quanta-wine active:text-quanta-wine focus:bg-quanta-ballet focus:text-quanta-wine text-quanta-candy',
+    },
+    accent: {
+      true: '',
     },
     size: {
-      S: 'px-4 py-2 text-xs font-medium has-[svg]:p-1.5 [&_svg]:h-5 [&_svg]:w-5',
-      L: 'text-xl font-medium has-[svg]:text-xs',
+      S: 'text-xs has-[svg]:p-1.5 [&_svg]:size-5',
+      L: 'px-4.5 py-3 text-xl/6 has-[svg]:p-3 has-[svg]:text-xs',
     },
     isDisabled: {
-      true: 'bg-quanta-snow hover:bg-quanta-smoke text-quanta-silver cursor-not-allowed',
+      true: 'bg-quanta-air hover:bg-quanta-air hover:text-quanta-silver has-[svg]:hover:text-quanta-silver text-quanta-smoke has-[svg]:text-quanta-smoke cursor-not-allowed',
     },
   },
+  compoundVariants: [
+    {
+      variant: 'neutral',
+      accent: true,
+      class:
+        'pressed:bg-quanta-smoke focus:bg-quanta-smoke bg-quanta-snow hover:bg-quanta-smoke active:bg-quanta-silver text-quanta-iron',
+    },
+    {
+      variant: 'primary',
+      accent: true,
+      class:
+        'text-quanta-air pressed:bg-quanta-cobalt bg-quanta-sapphire hover:bg-quanta-royal hover:text-quanta-air active:text-quanta-air focus:text-quanta-air active:bg-quanta-cobalt focus:bg-quanta-royal',
+    },
+    {
+      variant: 'destructive',
+      accent: true,
+      class:
+        'pressed:bg-quanta-rose active:bg-quanta-rose bg-quanta-candy hover:bg-quanta-wine focus:bg-quanta-wine text-quanta-air hover:text-quanta-air active:text-quanta-air focus:text-quanta-air',
+    },
+    {
+      isDisabled: true,
+      accent: true,
+      class: 'bg-quanta-snow hover:bg-quanta-smoke text-quanta-silver',
+    },
+  ],
   defaultVariants: {
-    variant: 'primary',
-    size: 'L',
+    variant: 'neutral',
   },
 });
 
@@ -47,6 +75,7 @@ export function Button(props: ButtonProps) {
           ...renderProps,
           variant: props.variant,
           size: props.size,
+          accent: props.accent,
           className,
         }),
       )}
