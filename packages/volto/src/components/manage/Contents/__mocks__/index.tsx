06f1da3b77f4c60dc6976f0cb4e5d178b5fdd9a1
Abhishek-17H <abhishekhegaje1702@gmail.com>
2025-04-23 08:18:30 +0530

    minor correction in migration comment

--- packages/volto/src/components/manage/Contents/__mocks__/index.tsx
+++ packages/volto/src/components/manage/Contents/__mocks__/index.tsx
@@ -1,15 +1,15 @@
 /* TODO: When the Volto Team removes Jest configuration support from Volto core, update this file with the Vitest version of the mock.
 Then, in the tests, we need to replace:
 
-vi.mock('@plone/volto/helpers/Loadable/Loadable', async () => {
+vi.mock('@plone/volto/components/manage/Contents', async () => {
   return await import(
-    '@plone/volto/helpers/Loadable/__mocks__/Loadable.vitest.jsx'
+    '@plone/volto/components/manage/Contents/__mocks__/index.vitest.tsx'
   );
 });
 
 with the following:
 
-vi.mock('@plone/volto/helpers/Loadable/Loadable');
+vi.mock('@plone/volto/components/manage/Contents');
 
 Finally, remove this comment.
 */
