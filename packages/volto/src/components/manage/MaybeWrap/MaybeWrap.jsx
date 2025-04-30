3930f6f05776ddb6a4c12c074c9ed4aed7827533
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-04-30 12:47:10 +0200

    Fixed `groupBlocksOrder` typing and move `MaybeWrap` to TS (#7029)

--- packages/volto/src/components/manage/MaybeWrap/MaybeWrap.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function MaybeWrap({
-  condition,
-  as: Component = 'div',
-  ...props
-}) {
-  return condition ? <Component {...props} /> : props.children;
-}
