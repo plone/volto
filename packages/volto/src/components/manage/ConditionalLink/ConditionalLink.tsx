f2486ff8f1fa7f383f2da068db56daab01a8482d
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-05-08 17:14:46 +0200

    Added support for forwardingRef

--- packages/volto/src/components/manage/ConditionalLink/ConditionalLink.tsx
+++ packages/volto/src/components/manage/ConditionalLink/ConditionalLink.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
 import type { UniversalLinkProps } from '@plone/volto/components/manage/UniversalLink/UniversalLink';
 
@@ -6,20 +7,27 @@ type ConditionalLinkProps = UniversalLinkProps & {
   to: string;
 };
 
-const ConditionalLink = ({ condition, to, ...props }: ConditionalLinkProps) => {
+const ConditionalLink = React.forwardRef<
+  HTMLAnchorElement,
+  ConditionalLinkProps
+>(({ condition, to, ...props }, ref) => {
   if (condition) {
     if (!props.item && to) {
       return (
-        <UniversalLink {...props} href={to}>
+        <UniversalLink {...props} href={to} ref={ref}>
           {props.children}
         </UniversalLink>
       );
     } else {
-      return <UniversalLink {...props}>{props.children}</UniversalLink>;
+      return (
+        <UniversalLink {...props} ref={ref}>
+          {props.children}
+        </UniversalLink>
+      );
     }
   } else {
     return <>{props.children}</>;
   }
-};
+});
 
 export default ConditionalLink;
