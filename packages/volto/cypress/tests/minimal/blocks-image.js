8b69fb8444643b6f72d40b7ec7c467565d6b8c2e
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-05-27 12:11:22 +0300

    fix

--- packages/volto/cypress/tests/minimal/blocks-image.js
+++ packages/volto/cypress/tests/minimal/blocks-image.js
@@ -27,9 +27,11 @@ describe('Blocks Tests', () => {
     cy.get('.ui.basic.icon.button.block-add-button').click();
     cy.get('.ui.basic.icon.button.image').contains('Image').click();
     cy.get('.toolbar-inner .buttons').first().next().next().click();
-    cy.get('.ui.input.editor-link.input-anchorlink-theme input').type(
-      `https://github.com/plone/volto/raw/main/logos/volto-colorful.png{enter}`,
-    );
+    cy.get('.link-form-container .react-select-container')
+      .click()
+      .type(
+        `https://github.com/plone/volto/raw/main/logos/volto-colorful.png{enter}`,
+      );
     cy.get('#toolbar-save').click();
     cy.wait('@content');
     cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
