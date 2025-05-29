476c99f8b685c4a67995f8391cc32d1c1d25d75f
Ion Lizarazu <ilizarazu@codesyntax.com>
2025-05-29 09:55:37 +0200

    some Menu stuff

--- packages/components/src/tailwind/Menu/Menu.stories.tsx
+++ packages/components/src/tailwind/Menu/Menu.stories.tsx
@@ -7,10 +7,10 @@ import {
   SubmenuTrigger,
   Keyboard,
   Text,
+  Separator,
   MenuItem,
   MenuSection,
 } from 'react-aria-components';
-import { Button } from '@plone/components';
 import { Menu } from './Menu';
 
 const meta: Meta<typeof Menu> = {
@@ -52,27 +52,146 @@ export const WithTextSlots: Story = {
           description: 'Cut to the clipboard',
           keyboard: '⌘X',
         },
+        {
+          id: 'copy',
+          label: 'Copy',
+          icon: SettingsIcon,
+          description: 'Copy to the clipboard',
+          keyboard: '⌘C',
+        },
+        {
+          id: 'paste',
+          label: 'Paste',
+          icon: SettingsIcon,
+          description: 'Paste from the clipboard',
+          keyboard: '⌘V',
+        },
+      ]}
+    ></Menu>
+  ),
+  args: {},
+};
+
+export const WithIconButton: Story = {
+  render: (args: any) => (
+    <Menu
+      {...args}
+      button={<SettingsIcon />}
+      menuItems={[
+        { id: 'cut', label: 'Cut' },
         { id: 'copy', label: 'Copy' },
         { id: 'paste', label: 'Paste' },
       ]}
+    ></Menu>
+  ),
+  args: {},
+};
+
+export const DisabledItems: Story = {
+  render: (args: any) => (
+    <Menu
+      {...args}
+      button={<SettingsIcon />}
+      disabledKeys={['paste']}
+      menuItems={[
+        { id: 'cut', label: 'Cut' },
+        { id: 'copy', label: 'Copy' },
+        { id: 'paste', label: 'Paste', disabled: true },
+      ]}
+    ></Menu>
+  ),
+  args: {},
+};
+
+export const WithSeparators: Story = {
+  render: (args: any) => (
+    <Menu
+      {...args}
+      button={<SettingsIcon />}
+      menuItems={[
+        { id: 'cut', label: 'Cut' },
+        { id: 'copy', label: 'Copy' },
+        { id: 'paste', label: 'Paste' },
+        { separator: true },
+        { id: 'bold', label: 'Bold' },
+      ]}
+    ></Menu>
+  ),
+  args: {},
+};
+
+export const WithSections: Story = {
+  render: (args: any) => (
+    <Menu
+      {...args}
+      button={<SettingsIcon />}
+      menuItems={[
+        {
+          section: true,
+          header: 'Styles',
+          children: [
+            { id: 'bold', label: 'Bold' },
+            { id: 'underline', label: 'Underline' },
+          ],
+        },
+        {
+          section: true,
+          header: 'Align',
+          children: [
+            { id: 'left', label: 'Left' },
+            { id: 'middle', label: 'Middle' },
+            { id: 'right', label: 'Right' },
+          ],
+        },
+      ]}
+    ></Menu>
+  ),
+  args: {},
+};
+
+export const AsLinks: Story = {
+  render: (args: any) => (
+    <Menu
+      {...args}
+      button={<SettingsIcon />}
+      menuItems={[
+        {
+          id: 'adobe',
+          label: 'Adobe',
+          href: 'https://adobe.com/',
+          target: '_blank',
+        },
+        {
+          id: 'apple',
+          label: 'Apple',
+          href: 'https://apple.com/',
+          target: '_blank',
+        },
+        {
+          id: 'google',
+          label: 'Google',
+          href: 'https://google.com/',
+          target: '_blank',
+        },
+        {
+          id: 'microsoft',
+          label: 'Microsoft',
+          href: 'https://microsoft.com/',
+          target: '_blank',
+        },
+      ]}
     >
-      <MenuItem>
-        <SettingsIcon />
-        <Text slot="label">Cut</Text>
-        <Text slot="description">Cut to the clipboard</Text>
-        <Keyboard>⌘X</Keyboard>
+      <MenuItem href="https://adobe.com/" target="_blank">
+        Adobe
       </MenuItem>
-      <MenuItem>
-        <SettingsIcon />
-        <Text slot="label">Copy</Text>
-        <Text slot="description">Copy to the clipboard</Text>
-        <Keyboard>⌘C</Keyboard>
+      <MenuItem href="https://apple.com/" target="_blank">
+        Apple
       </MenuItem>
-      <MenuItem>
-        <SettingsIcon />
-        <Text slot="label">Paste</Text>
-        <Text slot="description">Paste from the clipboard</Text>
-        <Keyboard>⌘V</Keyboard>
+      <MenuItem href="https://google.com/" target="_blank">
+        Google
+      </MenuItem>
+      <MenuItem href="https://microsoft.com/" target="_blank">
+        Microsoft
       </MenuItem>
     </Menu>
   ),
--- packages/components/src/tailwind/Menu/Menu.tsx
+++ packages/components/src/tailwind/Menu/Menu.tsx
@@ -1,33 +1,84 @@
-import React, { type ReactNode } from 'react';
+import React, { Fragment, type ReactNode } from 'react';
 
 import {
   Menu as RACMenu,
   MenuItem as RACMenuItem,
   MenuSection as RACMenuSection,
-  Separator,
   composeRenderProps,
   MenuTrigger,
   Keyboard,
+  Section,
   Text,
   Header,
-  Collection,
   type MenuSectionProps as RACMenuSectionProps,
-  type MenuItemProps,
+  type MenuItemProps as RACMenuItemProps,
   type SeparatorProps,
   type MenuProps as RACMenuProps,
 } from 'react-aria-components';
 import { Popover, type PopoverProps } from '../Popover/Popover';
 import { CheckboxIcon, ChevronrightIcon } from '../../components/icons';
 import { Button } from '../Button/Button';
+import { Separator } from '../Separator/Separator';
 import { tv } from 'tailwind-variants';
 import { focusRing } from '../utils';
 
 export interface itemProps {
   id: string;
   label: string;
-  description: string;
-  keyboard: string;
-  icon: ReactNode;
+  description?: string;
+  keyboard?: string;
+  icon?: ReactNode;
+  separator?: boolean;
+  disabled?: boolean;
+  section?: boolean;
+  header?: string;
+  href?: string;
+  target?: string;
+  children?: itemProps[];
+}
+
+export const dropdownItemStyles = tv({
+  base: 'group cursor-default items-center gap-x-3 gap-y-0 rounded-lg py-1 pr-1 pl-3 outline outline-0 forced-color-adjust-none select-none',
+  variants: {
+    isDisabled: {
+      false: 'text-gray-900 dark:text-zinc-100',
+      true: 'text-gray-300 dark:text-zinc-600 forced-colors:text-[GrayText]',
+    },
+    isFocused: {
+      true: 'bg-blue-600 text-white forced-colors:bg-[Highlight] forced-colors:text-[HighlightText]',
+    },
+    hasKeyboard: {
+      false: '',
+      true: '',
+    },
+    hasDescription: {
+      false: '',
+      true: '',
+    },
+    hasIcon: {
+      false: '',
+      true: '',
+    },
+  },
+  compoundVariants: [
+    {
+      isFocused: false,
+      isOpen: true,
+      className: 'bg-gray-100 dark:bg-zinc-700/60',
+    },
+    {
+      hasDescription: true,
+      hasIcon: true,
+      hasKeyboard: true,
+      className:
+        'grid grid-flow-col grid-cols-[10%_auto_auto] grid-rows-[auto_auto]',
+    },
+  ],
+});
+export interface MenuItemProps extends RACMenuItemProps {
+  hasDescription: boolean;
+  hasKeyboard: boolean;
+  hasIcon: boolean;
 }
 
 export function MenuItem(props: MenuItemProps) {
@@ -38,30 +89,16 @@ export function MenuItem(props: MenuItemProps) {
     <RACMenuItem
       textValue={textValue}
       {...props}
-      // className={dropdownItemStyles}
-    >
-      {composeRenderProps(
-        props.children,
-        (children, { selectionMode, isSelected, hasSubmenu }) => (
-          <>
-            {selectionMode !== 'none' && (
-              <span className="flex w-4 items-center">
-                {isSelected && <CheckboxIcon aria-hidden className="h-4 w-4" />}
-              </span>
-            )}
-            <span className="group-selected:font-semibold flex flex-1 items-center gap-2 truncate font-normal">
-              {children}
-            </span>
-            {hasSubmenu && (
-              <ChevronrightIcon
-                aria-hidden
-                className="absolute right-2 h-4 w-4"
-              />
-            )}
-          </>
-        ),
+      className={composeRenderProps(props.className, (className, renderProps) =>
+        dropdownItemStyles({
+          ...renderProps,
+          hasDescription: props.hasDescription,
+          hasKeyboard: props.hasKeyboard,
+          hasIcon: props.hasIcon,
+          className,
+        }),
       )}
-    </RACMenuItem>
+    ></RACMenuItem>
   );
 }
 
@@ -71,11 +108,6 @@ const menu = tv({
   variants: {},
 });
 
-const menuItem = tv({
-  extend: focusRing,
-  base: '',
-  variants: {},
-});
 export interface MenuButtonProps<T>
   extends MenuProps<T>,
     Omit<MenuTriggerProps, 'children'> {
@@ -84,6 +116,49 @@ export interface MenuButtonProps<T>
 
   placement?: PopoverProps['placement'];
   menuItems: itemProps[];
+}
+
+interface MenuItemContentProps {
+  selectionMode: string;
+  isSelected: boolean;
+  hasSubmenu: boolean;
+  item: itemProps;
+}
+
+function MenuItemContent({
+  item,
+  selectionMode,
+  isSelected,
+  hasSubmenu,
+}: MenuItemContentProps) {
+  return (
+    <>
+      {selectionMode && (
+        <span className="flex w-4 items-center">
+          {isSelected && <CheckboxIcon aria-hidden className="h-4 w-4" />}
+        </span>
+      )}
+      {item.icon && <item.icon className="col-start-1" />}
+      {item.label && (
+        <Text slot="label" className="col-start-2 text-lg">
+          {item.label}
+        </Text>
+      )}
+      {item.description && (
+        <Text slot="label" className="col-start-2 text-sm">
+          {item.description}
+        </Text>
+      )}
+      {item.keyboard && (
+        <Keyboard className="col-start-3 row-span-2 text-lg">
+          {item.keyboard}
+        </Keyboard>
+      )}
+      {hasSubmenu && (
+        <ChevronrightIcon aria-hidden className="absolute right-2 h-4 w-4" />
+      )}
+    </>
+  );
 }
 
 export function Menu<T extends object>({
@@ -100,14 +175,55 @@ export function Menu<T extends object>({
           {...props}
           className="max-h-[inherit] overflow-auto p-1 outline outline-0 [clip-path:inset(0_0_0_0_round_.75rem)]"
         >
-          {props?.menuItems?.map((item) => (
-            <MenuItem key={item.id}>
-              {item.icon && <item.icon />}
-              <Text slot="label">{item.label}</Text>
-              {item.description && <Text slot="label">{item.description}</Text>}
-              {item.keyboard && <Keyboard>{item.keyboard}</Keyboard>}
-            </MenuItem>
-          ))}
+          {props?.menuItems?.map((item, key) => {
+            return (
+              <Fragment key={item.id || key}>
+                {item.separator && !item.section && <Separator />}
+                {!item.separator && !item.section && (
+                  <MenuItem
+                    key={item.id}
+                    hasDescription={!!item.description}
+                    hasKeyboard={!!item.keyboard}
+                    hasIcon={!!item.icon}
+                    isDisabled={item.disabled}
+                    href={item.href}
+                    target={item.target}
+                  >
+                    <MenuItemContent
+                      item={item}
+                      selectionMode={props.selectionMode}
+                      isSelected={props.isSelected}
+                      hasSubmenu={props.hasSubmenu}
+                    />
+                  </MenuItem>
+                )}
+                {!item.separator && item.section && (
+                  <Section className={key > 0 ? 'mt-4' : ''}>
+                    <Header className="px-2 text-lg font-bold">
+                      {item.header}
+                    </Header>
+                    {item.children.map((child) => (
+                      <MenuItem
+                        key={child.id}
+                        hasDescription={!!child.description}
+                        hasKeyboard={!!child.keyboard}
+                        hasIcon={!!child.icon}
+                        isDisabled={child.disabled}
+                        href={item.href}
+                      >
+                        <MenuItemContent
+                          item={child}
+                          selectionMode={props.selectionMode}
+                          isSelected={props.isSelected}
+                          hasSubmenu={props.hasSubmenu}
+                        />
+                      </MenuItem>
+                    ))}
+                  </Section>
+                )}
+              </Fragment>
+            );
+          })}
         </RACMenu>
       </Popover>
     </MenuTrigger>
