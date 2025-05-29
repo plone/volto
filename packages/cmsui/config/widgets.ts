65e5f42ffdf153d5a8e688c66c38bd1be532d783
Martina Bustacchini <41484878+deodorhunter@users.noreply.github.com>
2025-05-29 19:11:52 +0200

    feat: add getWidget and registerWidget methods in registry (#7141)
    Co-authored-by: Víctor Fernández de Alba <sneridagh@gmail.com>

--- packages/cmsui/config/widgets.ts
+++ packages/cmsui/config/widgets.ts
@@ -4,7 +4,8 @@ import { DateTimePicker } from '@plone/components/tailwind';
 import { DateField } from '@plone/components';
 
 export default function install(config: ConfigType) {
-  config.widgets.default = TextField;
+  config.registerWidget({ key: 'default', definition: TextField });
+
   config.widgets.widget = {
     ...(config.widgets.widget ?? {}),
     datetime: DateTimePicker,
