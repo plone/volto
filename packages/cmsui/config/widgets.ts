cb16ac1a2859ebf10906b861584c4155860f75d5
Roger Boixader Güell <rboixaderg@gmail.com>
2025-05-29 18:50:36 +0200

    Datetime widget (#7131)
    Co-authored-by: Piero Nicolli <pnicolli@users.noreply.github.com>

--- packages/cmsui/config/widgets.ts
+++ packages/cmsui/config/widgets.ts
@@ -1,8 +1,15 @@
 import type { ConfigType } from '@plone/registry';
 import { TextField } from '../components/TextField/TextField';
+import { DateTimePicker } from '@plone/components/tailwind';
+import { DateField } from '@plone/components';
 
 export default function install(config: ConfigType) {
   config.widgets.default = TextField;
+  config.widgets.widget = {
+    ...(config.widgets.widget ?? {}),
+    datetime: DateTimePicker,
+    date: DateField,
+  };
 
   return config;
 }
---
+++ packages/components/news/7131.feature
@@ -0,0 +1,1 @@
+Create DateTimePicker, DatePicker, TimeField and DateInput components. @rboixaderg
