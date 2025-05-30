50e2d0299c43f0e5c3c3160f830bf74bb369cfc0
Katja Süss <k.suess@rohberg.ch>
2025-05-30 13:31:27 +0200

    Fix SitemapProps

--- packages/components/src/components/Sitemap/Sitemap.tsx
+++ packages/components/src/components/Sitemap/Sitemap.tsx
@@ -1,21 +1,18 @@
-/**
- * Sitemap displays a hirarchy of links to all the pages in the site.
- */
-
 import React from 'react';
 import { Link } from '../Link/Link';
 
-export function getSitemapPath(pathname = '', lang) {
-  const prefix = pathname.replace(/\/sitemap$/gm, '').replace(/^\//, '');
-  const path = prefix || lang || '';
-  return path;
-}
+// TODO translations
 
 interface SitemapItem {
   '@id': string;
   title: string;
   description: string;
   items?: SitemapItem[];
+}
+
+interface SitemapProps {
+  items?: SitemapItem[];
+  page_title?: string;
 }
 
 const renderItems = (items: SitemapItem[]): JSX.Element => {
@@ -33,16 +30,6 @@ const renderItems = (items: SitemapItem[]): JSX.Element => {
     </ul>
   );
 };
-
-/**
- * Sitemap displays a hierarchy of links to all the pages in the site.
- * { getNavigation, location, intl, lang, items }
- * TODO translations
- */
-interface SitemapProps {
-  items?: any[];
-  page_title?: string;
-}
 
 export function Sitemap({
   items,
