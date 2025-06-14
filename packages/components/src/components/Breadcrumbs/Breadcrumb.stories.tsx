976f73a0a1fe32464a6ffcf074f23947b5d6f27b
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-06-14 17:34:49 +0200

    Long list of breadcrumbs using Menu component

--- packages/components/src/components/Breadcrumbs/Breadcrumb.stories.tsx
+++ packages/components/src/components/Breadcrumbs/Breadcrumb.stories.tsx
@@ -4,8 +4,10 @@ import {
   ChevronrightIcon,
   FolderIcon,
   HomeIcon,
+  MoreoptionsIcon,
   PageIcon,
 } from '../../components/icons';
+import { Menu, MenuItem } from '../Menu/Menu';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -123,34 +125,57 @@ export const SlotWithRootWithIcons: Story = {
   },
 };
 
+const longItems = [
+  { '@id': '/folder', title: 'Folder' },
+  { '@id': '/folder/folderB', title: 'Folder with long name' },
+  {
+    '@id': '/folder/folderB/folderC',
+    title: 'Folder with long name and a bit more',
+  },
+  {
+    '@id': '/folder/folderB/folderC/folderD',
+    title: 'Folder with long name even more long',
+  },
+  {
+    '@id': '/folder/folderB/folderC/folderD/folderE',
+    title: 'Folder',
+  },
+  { '@id': '/folder/page', title: 'Page' },
+];
+
 export const LotsOfItems: Story = {
-  render: (args) => (
-    <Breadcrumbs {...args}>
-      {(item) => (
-        <Breadcrumb id={item['@id']} href={item['@id']}>
-          {item.title}
+  render: (args) => {
+    const first = longItems ? longItems[0] : null;
+    const last = longItems ? longItems[longItems.length - 1] : null;
+    const inner = longItems ? longItems.slice(1, longItems.length - 1) : [];
+
+    return (
+      <Breadcrumbs {...args}>
+        {args.root && (
+          <Breadcrumb id={args.root['@id']} href={args.root['@id']}>
+            {args.root.icon}
+            {args.root.title}
+          </Breadcrumb>
+        )}
+        <Breadcrumb id={first?.['@id']} href={first?.['@id']}>
+          {first?.title}
         </Breadcrumb>
-      )}
-    </Breadcrumbs>
-  ),
+        <Breadcrumb>
+          <Menu items={inner} button={<MoreoptionsIcon />} placement="bottom">
+            {(item) => (
+              <MenuItem id={item['@id']} href={item['@id']}>
+                {item.title}
+              </MenuItem>
+            )}
+          </Menu>
+        </Breadcrumb>
+        <Breadcrumb id={last?.['@id']} href={last?.['@id']}>
+          {last?.title}
+        </Breadcrumb>
+      </Breadcrumbs>
+    );
+  },
   args: {
     root: { '@id': '/', title: 'Home', icon: <HomeIcon size="sm" /> },
-    items: [
-      { '@id': '/folder', title: 'Folder' },
-      { '@id': '/folder/folderB', title: 'Folder with long name' },
-      {
-        '@id': '/folder/folderB/folderC',
-        title: 'Folder with long name and a bit more',
-      },
-      {
-        '@id': '/folder/folderB/folderC/folderD',
-        title: 'Folder with long name even more long',
-      },
-      {
-        '@id': '/folder/folderB/folderC/folderD/folderE',
-        title: 'Folder',
-      },
-      { '@id': '/folder/page', title: 'Page' },
-    ],
   },
 };
