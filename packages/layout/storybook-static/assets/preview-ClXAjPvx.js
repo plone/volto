e32005977b7f26c33185c48c4d119b5277226f78
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-06-26 16:53:36 -0600

    Remove from storybook-static, added to .gitignore

--- packages/layout/storybook-static/assets/preview-ClXAjPvx.js
+++
@@ -1,6 +0,0 @@
-var _a;
-var PARAM_KEY = "viewport";
-var modern = { [PARAM_KEY]: { value: void 0, isRotated: false } }, legacy = { viewport: "reset", viewportRotated: false }, initialGlobals = ((_a = globalThis.FEATURES) == null ? void 0 : _a.viewportStoryGlobals) ? modern : legacy;
-export {
-  initialGlobals
-};
--- packages/layout/storybook-static/assets/preview-VMy2o69I.js
+++
@@ -1,6 +0,0 @@
-import { i as instrument } from "./index-BX6B_iXa.js";
-var runStep = instrument({ step: (label, play, context) => play(context) }, { intercept: true }).step, parameters = { throwPlayFunctionExceptions: false };
-export {
-  parameters,
-  runStep
-};
