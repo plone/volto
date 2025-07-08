97652fbe207ca9c2d5acf1020721674f9d340493
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-07-08 18:57:29 +0200

    Added layout `Container`

---
+++ packages/layout/components/Container/Container.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import cx from 'clsx';
+
+/**
+ * Props Types for the Container component.
+ * They are able to infer the props of the element type passed to the `as` prop.
+ */
+type ContainerProps<T extends React.ElementType> = {
+  /** Primary content. */
+  children: React.ReactNode;
+  /** An element type to render as (string or function). */
+  as?: T;
+  /** Additional CSS classes. */
+  className?: string;
+  /** Container width */
+  width?: 'layout' | 'default' | 'narrow' | 'full';
+} & React.ComponentPropsWithoutRef<React.ElementType extends T ? 'div' : T>;
+
+const Container = <T extends React.ElementType = 'div'>(
+  props: ContainerProps<T>,
+) => {
+  const { as: Component = 'div', children, className, width, ...rest } = props;
+  const containerClasses = cx('block', className, {
+    [width || '']: true,
+  });
+
+  return (
+    <Component {...rest} className={containerClasses}>
+      <div className="block-inner-container">{children}</div>
+    </Component>
+  );
+};
+
+export default Container;
