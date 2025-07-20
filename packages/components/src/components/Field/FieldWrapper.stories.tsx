31190c565a9c60ffa6b82057e8136e04951f7a07
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-07-20 10:17:50 +0200

    Field and FieldWrapper component for widgets that have no field wrapper by default (#7213)
    Co-authored-by: Steve Piercy <web@stevepiercy.com>

---
+++ packages/components/src/components/Field/FieldWrapper.stories.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { FieldWrapper } from './Field.quanta';
+
+const meta = {
+  title: 'Quanta/FieldWrapper',
+  component: FieldWrapper,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof FieldWrapper>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => <FieldWrapper {...args}>My bare widget here</FieldWrapper>,
+  args: {
+    label: 'Field Label',
+    description: 'This is a description of the field.',
+    children: 'My bare widget here',
+  },
+};
+
+export const Errored: Story = {
+  render: (args) => <FieldWrapper {...args}>My bare widget here</FieldWrapper>,
+  args: {
+    label: 'Field Label',
+    description: 'This is a description of the field.',
+    children: 'My bare widget here',
+    errorMessage: 'This field is required.',
+    isInvalid: true,
+    isRequired: true,
+  },
+};
+
+export const Disabled: Story = {
+  render: (args) => <FieldWrapper {...args}>My bare widget here</FieldWrapper>,
+  args: {
+    label: 'Field Label',
+    description: 'This field is disabled.',
+    children: 'My bare widget here',
+    isDisabled: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  render: (args) => <FieldWrapper {...args}>My bare widget here</FieldWrapper>,
+  args: {
+    label: 'Field Label',
+    description: 'This field is read-only.',
+    children: 'My bare widget here',
+    isReadOnly: true,
+  },
+};
+
+export const Unwrapped: Story = {
+  render: (args) => <FieldWrapper {...args}>My bare widget here</FieldWrapper>,
+  args: {
+    label: 'Field Label',
+    description: 'This field is not wrapped.',
+    children: 'My bare widget here',
+    wrapped: false,
+  },
+};
