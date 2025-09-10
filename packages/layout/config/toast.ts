786d105337d263f0a648fb8d55b1dcfde0889317
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-09-10 13:37:34 +0200

    fix: utilities

--- packages/layout/config/toast.ts
+++ packages/layout/config/toast.ts
@@ -2,11 +2,11 @@ import { type ReactNode } from 'react';
 import { flushSync } from 'react-dom';
 
 import type { ConfigType } from '@plone/registry';
-import { UNSTABLE_ToastQueue as ToastQueue } from 'react-aria-components';
+import { UNSTABLE_ToastQueue as RACToastQueue } from 'react-aria-components';
 export type Toast = {
   error: (content: ReactNode | null) => string | number;
 };
-// Define the type for your toast content.
+// Define the type for our toast content.
 export type ToastItem = {
   title: string;
   description?: string;
@@ -14,7 +14,7 @@ export type ToastItem = {
 };
 
 // Create a global ToastQueue.
-export const toastQueue = new ToastQueue<ToastItem>({
+export const toastQueue = new RACToastQueue<ToastItem>({
   // Wrap state updates in a CSS view transition.
   wrapUpdate(fn) {
     if ('startViewTransition' in document) {
@@ -29,8 +29,15 @@ export const toastQueue = new ToastQueue<ToastItem>({
 
 export default function install(config: ConfigType) {
   config.registerUtility({
-    name: 'toastQueue',
-    type: 'factory',
+    name: 'queue',
+    type: 'toast',
     method: () => toastQueue,
+  });
+  config.registerUtility({
+    name: 'show',
+    type: 'toast',
+    method: (queueElement: ToastItem) => {
+      toastQueue.add(queueElement, { timeout: 5000 });
+    },
   });
 }
