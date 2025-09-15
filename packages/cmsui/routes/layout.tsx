ef955797e35a363fd41bea50273179c7d7a811be
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-09-15 19:25:48 +0200

    Fix load of the default layers ensuring they are on top, using a React 19 managed <link>

--- packages/cmsui/routes/layout.tsx
+++ packages/cmsui/routes/layout.tsx
@@ -86,12 +86,12 @@ export default function Index() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="mobile-web-app-capable" content="yes" />
-        {/* We pre-define here the @layer before tailwind does, adding our own layers */}
-        <style>{`@layer ${cssLayers.join(', ')};`}</style>
         <Meta />
         <Links />
       </head>
       <body className="cmsui">
+        {/* We pre-define here the @layer before tailwind does, adding our own layers in a React 19 managed <link> tag */}
+        <link rel="stylesheet" href="layers.css" precedence="first" />
         <PluggablesProvider>
           <div
             className={clsx(
