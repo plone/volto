742ac833833f5f5e9d5a59c1cfeb1e2e52b7c7fd
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-10-18 00:11:47 +0200

    Added more improvements about i18n support to the `volto-slate` package #7510. @macagua

--- packages/volto-slate/src/editor/plugins/AdvancedLink/schema.js
+++ packages/volto-slate/src/editor/plugins/AdvancedLink/schema.js
@@ -3,91 +3,168 @@ import internalSVG from '@plone/volto/icons/nav.svg';
 import emailSVG from '@plone/volto/icons/email.svg';
 // import pageLinkSVG from '@plone/volto/icons/show-blocks.svg';
 
-export const EmailLinkSchema = {
-  title: 'Email address',
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  emailAddress: {
+    id: 'Email address',
+    defaultMessage: 'Email address',
+  },
+  email: {
+    id: 'Email',
+    defaultMessage: 'Email',
+  },
+  emailSubject: {
+    id: 'Email subject',
+    defaultMessage: 'Email subject',
+  },
+  optional: {
+    id: 'Optional',
+    defaultMessage: 'Optional',
+  },
+  internalLink: {
+    id: 'Internal link',
+    defaultMessage: 'Internal link',
+  },
+  internal: {
+    id: 'Internal',
+    defaultMessage: 'Internal',
+  },
+  externalLink: {
+    id: 'External link',
+    defaultMessage: 'External link',
+  },
+  external: {
+    id: 'External',
+    defaultMessage: 'External',
+  },
+  externalURL: {
+    id: 'External URL',
+    defaultMessage: 'External URL',
+  },
+  descriptionExternalURL: {
+    id: 'URL can be relative within this site or absolute if it starts with http:// or https://',
+    defaultMessage:
+      'URL can be relative within this site or absolute if it starts with http:// or https://',
+  },
+  target: {
+    id: 'Target',
+    defaultMessage: 'Target',
+  },
+  targetSelf: {
+    id: 'Open in this window / frame',
+    defaultMessage: 'Open in this window / frame',
+  },
+  targetBlank: {
+    id: 'Open in new window',
+    defaultMessage: 'Open in new window',
+  },
+  targetParent: {
+    id: 'Open in parent window / frame',
+    defaultMessage: 'Open in parent window / frame',
+  },
+  targetTop: {
+    id: 'Open in top frame (replaces all frames)',
+    defaultMessage: 'Open in top frame (replaces all frames)',
+  },
+  insertLink: {
+    id: 'Insert link',
+    defaultMessage: 'Insert link',
+  },
+  linkTitle: {
+    id: 'Link Title',
+    defaultMessage: 'Link Title',
+  },
+  link: {
+    id: 'Link',
+    defaultMessage: 'Link',
+  },
+});
+
+export const EmailLinkSchema = ({ intl }) => ({
+  title: intl.formatMessage(messages.emailAddress),
   fieldsets: [
     {
       id: 'email',
-      title: 'Email',
+      title: intl.formatMessage(messages.email),
       fields: ['email_address', 'email_subject'],
     },
   ],
   properties: {
     email_address: {
-      title: 'Email address',
+      title: intl.formatMessage(messages.emailAddress),
     },
     email_subject: {
-      title: 'Email subject',
-      description: 'Optional',
+      title: intl.formatMessage(messages.emailSubject),
+      description: intl.formatMessage(messages.optional),
     },
   },
   required: [],
-};
+});
 
-export const InternalLinkSchema = {
-  title: 'Internal link',
+export const InternalLinkSchema = ({ intl }) => ({
+  title: intl.formatMessage(messages.internalLink),
   fieldsets: [
     {
       id: 'internal',
-      title: 'Internal',
+      title: intl.formatMessage(messages.internal),
       fields: ['internal_link'],
     },
   ],
   properties: {
     internal_link: {
       widget: 'object_browser',
-      title: 'Internal link',
+      title: intl.formatMessage(messages.internalLink),
       multiple: false,
       mode: 'link',
       selectedItemAttrs: [],
     },
   },
   required: [],
-};
+});
 
-export const ExternalLinkSchema = {
-  title: 'External link',
+export const ExternalLinkSchema = ({ intl }) => ({
+  title: intl.formatMessage(messages.externalLink),
   fieldsets: [
     {
       id: 'external',
-      title: 'External',
+      title: intl.formatMessage(messages.external),
       fields: ['external_link', 'target'],
     },
   ],
   properties: {
     external_link: {
-      title: 'External URL',
-      description:
-        'URL can be relative within this site or absolute if it starts with http:// or https://',
+      title: intl.formatMessage(messages.externalURL),
+      description: intl.formatMessage(messages.descriptionExternalURL),
     },
     target: {
-      title: 'Target',
+      title: intl.formatMessage(messages.target),
       choices: [
-        ['_self', 'Open in this window / frame'],
-        ['_blank', 'Open in new window'],
-        ['_parent', 'Open in parent window / frame'],
-        ['_top', 'Open in top frame (replaces all frames)'],
+        ['_self', intl.formatMessage(messages.targetSelf)],
+        ['_blank', intl.formatMessage(messages.targetBlank)],
+        ['_parent', intl.formatMessage(messages.targetParent)],
+        ['_top', intl.formatMessage(messages.targetTop)],
       ],
     },
   },
   required: [],
-};
+});
 
-const LinkEditSchema = {
-  title: 'Insert link',
+const LinkEditSchema = ({ intl }) => ({
+  title: intl.formatMessage(messages.insertLink),
   fieldsets: [
     {
       id: 'default',
-      title: 'Internal link',
+      title: intl.formatMessage(messages.internalLink),
       fields: ['link', 'title'],
     },
   ],
   properties: {
     title: {
-      title: 'Link Title',
+      title: intl.formatMessage(messages.linkTitle),
     },
     link: {
-      title: 'Link',
+      title: intl.formatMessage(messages.link),
       widget: 'object_by_type',
       schemas: [
         {
@@ -109,6 +186,6 @@ const LinkEditSchema = {
     },
   },
   required: [],
-};
+});
 
 export default LinkEditSchema;
--- packages/volto/news/7510.feature
+++ packages/volto/news/7510.feature
@@ -1,1 +1,2 @@
 Added more improvements about i18n support to the `volto` package. @macagua
+Added more improvements about i18n support to the `volto-slate` package. @macagua
