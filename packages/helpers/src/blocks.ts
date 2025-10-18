339593a6b95e913c950294fe768aeb61c6a25972
katrinzaks <zaks@interaktiv.de>
2025-10-18 12:45:37 +0300

    added getBlocksFieldName to blocks helpers

--- packages/helpers/src/blocks.ts
+++ packages/helpers/src/blocks.ts
@@ -7,3 +7,14 @@ export function hasBlocksData(content: Content) {
     ) !== undefined
   );
 }
+
+export function getBlocksFieldName(
+  props: Content,
+  suffix: string = 'blocks',
+): string | null {
+  return (
+    Object.keys(props).find(
+      (key) => key !== 'volto.blocks' && key.endsWith(suffix),
+    ) || null
+  );
+}
