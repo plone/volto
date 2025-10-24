8e736172ebd09d6610305fad10e4b75e767cdae9
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-10-24 18:46:53 +0200

    Compat with Volto and React 18

--- packages/plate/components/ui/button.tsx
+++ packages/plate/components/ui/button.tsx
@@ -34,25 +34,24 @@ const buttonVariants = cva(
   },
 );
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
+// Make it compatible with React 18 forwardRef
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<'button'> &
+    VariantProps<typeof buttonVariants> & {
+      asChild?: boolean;
+    }
+>(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
       {...props}
     />
   );
-}
+});
 
 export { Button, buttonVariants };
--- packages/plate/index.ts
+++ packages/plate/index.ts
@@ -1,6 +1,7 @@
 import type { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
+  // TODO: do not add things to the registry
   if (!config.settings.plate) config.settings.plate = {};
 
   return config;
