e7f2ee1255b474b04f1173339649204a3f2a02c4
Tishasoumya-02 <tishasoumya@gmail.com>
2025-10-30 11:49:23 +0530

    add wait to test

--- packages/volto/cypress/tests/core/blocks/blocks-search.js
+++ packages/volto/cypress/tests/core/blocks/blocks-search.js
@@ -613,10 +613,12 @@ describe('Search Block Tests', () => {
     cy.get('#toolbar-save').click();
     // then we are able to see label and sort option
     cy.get('.sort-label').should('have.text', 'Sort on');
-
     cy.get('#select-search-sort-on').click();
+    cy.wait(300);
     cy.findByText('Effective date').click({ force: true });
-
+    cy.get(
+      'div#select-search-sort-on.search-react-select-container.css-2b097c-container',
+    ).contains('Effective date');
     // Verify the presence of Ascending button
     cy.get('button[title="Ascending"]').should('be.visible');
     // Verify the presence of Descending button
@@ -655,6 +657,7 @@ describe('Search Block Tests', () => {
     // then we are able to see label and sort option
     cy.get('.sort-label').should('have.text', 'Sort on');
     cy.get('#select-search-sort-on').click();
+    cy.wait(300);
     cy.findByText('Effective date').click({ force: true });
     cy.get(
       'div#select-search-sort-on.search-react-select-container.css-2b097c-container',
