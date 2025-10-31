8db92f2db3366b7acea24a3fe89159a2fa2b53c4
Víctor Fernández de Alba <sneridagh@gmail.com>
2025-10-31 09:27:58 +0100

    Update Cypress to 15 (#7538)
    Co-authored-by: Tishasoumya-02 <tishasoumya@gmail.com>

--- packages/volto/cypress/tests/core/blocks/blocks-teaser.js
+++ packages/volto/cypress/tests/core/blocks/blocks-teaser.js
@@ -74,6 +74,7 @@ context('Blocks Acceptance Tests', () => {
     cy.get('#toolbar-save').click();
 
     cy.visit('/document');
+
     cy.get('.block.teaser .content h2').contains('Blue Orchids');
     cy.get('.block.teaser .content p').contains(
       'are growing on the mountain tops',
@@ -88,7 +89,8 @@ context('Blocks Acceptance Tests', () => {
 
     cy.get('.documentFirstHeading').contains('Blue Orchids and Tulips');
     // THEN I can see the updated content in the teaser
-    cy.navigate('/document');
+    cy.visit('/document');
+    cy.wait('@content');
     cy.get('.block.teaser .content h2').contains('Blue Orchids and Tulips');
     cy.get('.block.teaser .content p').contains(
       'are beautifully growing on the mountain tops',
