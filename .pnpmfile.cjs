b5c8ed04c82a1a9742435edf6510ab7b2dccd008
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-11-10 11:29:21 +0100

    test

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -14,7 +14,9 @@ if (fs.existsSync(catalogPath)) {
 module.exports = {
   hooks: {
     updateConfig(config) {
-      config.catalogs.default ??= catalog;
+      if (config.catalogs) {
+        config.catalogs.default ??= catalog;
+      }
       return config;
     },
   },
