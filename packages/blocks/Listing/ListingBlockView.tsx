404fdae92abc0a67e54f755d918f304f1eab5816
Eric BREHAULT <ebrehault@gmail.com>
2025-11-23 12:28:00 +0100

    i18n

--- packages/blocks/Listing/ListingBlockView.tsx
+++ packages/blocks/Listing/ListingBlockView.tsx
@@ -1,4 +1,5 @@
 import type { BlockViewProps, ListingBlockFormData } from '@plone/types';
+import { useTranslation } from 'react-i18next';
 
 /**
  * View listing block component.
@@ -9,12 +10,13 @@ const ListingBlockView = (props: BlockViewProps) => {
   const data = props.data as ListingBlockFormData;
   const HeadlineTag = data.headlineTag || 'h2';
   const ItemTitleTag = data.headlineTag === 'h2' ? 'h3' : 'h4';
+  const { t } = useTranslation();
 
   return (
     <>
       {data.headline ? <HeadlineTag>{data.headline}</HeadlineTag> : ''}
       {!data.items || data.items?.length === 0 ? (
-        <div>No results.</div>
+        <div>{t('blocks.listing.no-results')}</div>
       ) : (
         data.items.map((item) => (
           <div key={item['@id']}>
