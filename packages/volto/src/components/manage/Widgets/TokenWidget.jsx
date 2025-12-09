37183b9cb479818527f3103efb493a49881652ad
Manik Khajuria <manik.khajuria491@gmail.com>
2025-12-09 16:26:50 +0530

    Changed Class based Token Component to functional Component

--- packages/volto/src/components/manage/Widgets/TokenWidget.jsx
+++ packages/volto/src/components/manage/Widgets/TokenWidget.jsx
@@ -3,11 +3,11 @@
  * @module components/manage/Widgets/TokenWidget
  */
 
-import React, { Component } from 'react';
+import React, { useEffect, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 import {
   getVocabFromHint,
   getVocabFromField,
@@ -39,174 +39,161 @@ const messages = defineMessages({
 });
 
 /**
- * TokenWidget component class.
+ * TokenWidget component.
  *
  * Because new terms are created through the web by using the widget, the token
  * widget conflates the meaning of token, label and value and assumes they can
  * be used interchangeably.
  *
- * @class TokenWidget
- * @extends Component
+ * @param {Object} props Component properties
+ * @returns {JSX.Element} Markup for the component.
  */
-class TokenWidget extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    required: PropTypes.bool,
-    error: PropTypes.arrayOf(PropTypes.string),
-    getVocabulary: PropTypes.func.isRequired,
-    choices: PropTypes.arrayOf(PropTypes.object),
-    vocabLoading: PropTypes.bool,
-    vocabLoaded: PropTypes.bool,
-    items: PropTypes.shape({
-      vocabulary: PropTypes.object,
-    }),
-    widgetOptions: PropTypes.shape({
-      vocabulary: PropTypes.object,
-    }),
-    value: PropTypes.arrayOf(PropTypes.string),
-    onChange: PropTypes.func.isRequired,
-    wrapped: PropTypes.bool,
-    placeholder: PropTypes.string,
-  };
+const TokenWidget = (props) => {
+  const {
+    id,
+    value,
+    onChange,
+    placeholder,
+    reactSelectCreateable,
+    isDisabled,
+    fieldSet,
+    choices: propChoices,
+    vocabLoading,
+    vocabLoaded,
+    vocabBaseUrl,
+    lang,
+    getVocabulary,
+  } = props;
 
-  /**
-   * Default properties
-   * @property {Object} defaultProps Default properties.
-   * @static
-   */
-  static defaultProps = {
-    description: null,
-    required: false,
-    items: {
-      vocabulary: null,
-    },
-    widgetOptions: {
-      vocabulary: null,
-    },
-    error: [],
-    choices: [],
-    value: null,
-  };
+  const intl = useIntl();
 
-  /**
-   * Constructor
-   * @method constructor
-   * @param {Object} props Component properties
-   * @constructs Actions
-   */
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+  const choices = useMemo(() => propChoices || [], [propChoices]);
 
-  /**
-   * Component did mount
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
-    if (!this.props.choices?.length) {
-      this.props.getVocabulary({
-        vocabNameOrURL: this.props.vocabBaseUrl,
-        size: -1,
-        subrequest: this.props.lang,
-      });
-    }
-  }
-
-  componentDidUpdate() {
+  useEffect(() => {
     if (
-      !this.props.choices?.length &&
-      this.props.vocabLoading === undefined &&
-      !this.props.vocabLoaded
+      !choices?.length &&
+      vocabLoading === undefined &&
+      !vocabLoaded &&
+      vocabBaseUrl &&
+      getVocabulary
     ) {
-      this.props.getVocabulary({
-        vocabNameOrURL: this.props.vocabBaseUrl,
+      getVocabulary({
+        vocabNameOrURL: vocabBaseUrl,
         size: -1,
-        subrequest: this.props.lang,
+        subrequest: lang,
       });
     }
-  }
+  }, [
+    choices?.length,
+    vocabLoading,
+    vocabLoaded,
+    vocabBaseUrl,
+    lang,
+    getVocabulary,
+  ]);
 
-  /**
-   * Handle the field change, store it in the local state and back to simple
-   * array of tokens for correct serialization
-   * @method handleChange
-   * @param {array} selectedOption The selected options (already aggregated).
-   * @returns {undefined}
-   */
-  handleChange(selectedOption) {
-    this.props.onChange(
-      this.props.id,
-      selectedOption ? selectedOption.map((item) => item.label) : null,
-    );
-  }
+  const handleChange = useCallback(
+    (selectedOption) => {
+      onChange(
+        id,
+        selectedOption ? selectedOption.map((item) => item.label) : null,
+      );
+    },
+    [onChange, id],
+  );
 
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    const selectedOption = this.props.value
-      ? this.props.value.map((item) => ({ label: item, value: item }))
-      : [];
+  const selectedOption = useMemo(
+    () => (value ? value.map((item) => ({ label: item, value: item })) : []),
+    [value],
+  );
 
-    const defaultOptions = (this.props.choices || [])
-      .filter(
-        (item) => !selectedOption.find(({ label }) => label === item.label),
-      )
-      .map((item) => ({
-        label: item.label || item.value,
-        value: item.value,
-      }));
-    const CreatableSelect = this.props.reactSelectCreateable.default;
+  const defaultOptions = useMemo(
+    () =>
+      (choices || [])
+        .filter(
+          (item) => !selectedOption.find(({ label }) => label === item.label),
+        )
+        .map((item) => ({
+          label: item.label || item.value,
+          value: item.value,
+        })),
+    [choices, selectedOption],
+  );
 
-    return (
-      <FormFieldWrapper {...this.props}>
-        <CreatableSelect
-          id={`field-${this.props.id}`}
-          aria-labelledby={`fieldset-${this.props.fieldSet}-field-label-${this.props.id}`}
-          key={this.props.id}
-          menuShouldScrollIntoView={false}
-          isDisabled={this.props.isDisabled}
-          className="react-select-container"
-          classNamePrefix="react-select"
-          defaultOptions={defaultOptions}
-          options={defaultOptions}
-          styles={customSelectStyles}
-          theme={selectTheme}
-          components={{
-            MultiValueContainer,
-            ClearIndicator,
-            DropdownIndicator,
-            Option,
-          }}
-          isMulti
-          value={selectedOption || []}
-          onChange={this.handleChange}
-          placeholder={
-            this.props.placeholder ??
-            this.props.intl.formatMessage(messages.select)
-          }
-          noOptionsMessage={() =>
-            this.props.intl.formatMessage(messages.no_options)
-          }
-        />
-      </FormFieldWrapper>
-    );
-  }
-}
+  const CreatableSelect = reactSelectCreateable.default;
+
+  return (
+    <FormFieldWrapper {...props}>
+      <CreatableSelect
+        id={`field-${id}`}
+        aria-labelledby={`fieldset-${fieldSet}-field-label-${id}`}
+        key={id}
+        menuShouldScrollIntoView={false}
+        isDisabled={isDisabled}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        defaultOptions={defaultOptions}
+        options={defaultOptions}
+        styles={customSelectStyles}
+        theme={selectTheme}
+        components={{
+          MultiValueContainer,
+          ClearIndicator,
+          DropdownIndicator,
+          Option,
+        }}
+        isMulti
+        value={selectedOption || []}
+        onChange={handleChange}
+        placeholder={placeholder ?? intl.formatMessage(messages.select)}
+        noOptionsMessage={() => intl.formatMessage(messages.no_options)}
+      />
+    </FormFieldWrapper>
+  );
+};
+
+TokenWidget.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  required: PropTypes.bool,
+  error: PropTypes.arrayOf(PropTypes.string),
+  getVocabulary: PropTypes.func.isRequired,
+  choices: PropTypes.arrayOf(PropTypes.object),
+  vocabLoading: PropTypes.bool,
+  vocabLoaded: PropTypes.bool,
+  vocabBaseUrl: PropTypes.string,
+  lang: PropTypes.string,
+  items: PropTypes.shape({
+    vocabulary: PropTypes.object,
+  }),
+  widgetOptions: PropTypes.shape({
+    vocabulary: PropTypes.object,
+  }),
+  value: PropTypes.arrayOf(PropTypes.string),
+  onChange: PropTypes.func.isRequired,
+  wrapped: PropTypes.bool,
+  placeholder: PropTypes.string,
+  reactSelectCreateable: PropTypes.object.isRequired,
+  isDisabled: PropTypes.bool,
+  fieldSet: PropTypes.string,
+};
+
+TokenWidget.defaultProps = {
+  description: null,
+  required: false,
+  items: {
+    vocabulary: null,
+  },
+  widgetOptions: {
+    vocabulary: null,
+  },
+  error: [],
+  choices: [],
+  value: null,
+};
 
 export default compose(
-  injectIntl,
   injectLazyLibs(['reactSelectCreateable']),
   connect(
     (state, props) => {
