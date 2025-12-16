80dab628507d4a7637e82b04999970313b55f726
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-12-16 11:07:09 +0100

    WIP

--- packages/types/src/config/Utilities.d.ts
+++ packages/types/src/config/Utilities.d.ts
@@ -1,4 +1,23 @@
-// export type Utility = Record<string, { method: (args: any) => any }>;
-export type Utility = Record<string, { method: (...args: any[]) => any }>;
+/**
+ * Map utility "type" to its method signature.
+ * Extend via module augmentation:
+ * declare module '@plone/types' { interface UtilityTypeMap { foo: (id: string) => string } }
+ */
+export interface UtilityTypeMap {
+  // Add known utility types here via declaration merging.
+}
 
-export type UtilitiesConfig = Record<string, Utility>;
+type UtilityMethodFor<Type extends string> = Type extends keyof UtilityTypeMap
+  ? UtilityTypeMap[Type]
+  : (...args: any[]) => any;
+
+export type Utility<Type extends string = string> = Record<
+  string,
+  { method: UtilityMethodFor<Type> }
+>;
+
+type UtilitiesByMap = {
+  [Type in keyof UtilityTypeMap]: Utility<Type>;
+};
+
+export type UtilitiesConfig = UtilitiesByMap & Record<string, Utility>;
