602915700fddbdec1d4c67c4a88f5010675ed971
Manik Khajuria <manik.khajuria491@gmail.com>
2025-12-17 16:59:04 +0530

    Changed History class based component to functional component

--- packages/volto/src/components/manage/History/History.jsx
+++ packages/volto/src/components/manage/History/History.jsx
@@ -3,7 +3,7 @@
  * @module components/manage/History/History
  */
 
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from '@plone/volto/helpers/Helmet/Helmet';
 import { Link } from 'react-router-dom';
@@ -21,7 +21,7 @@ import map from 'lodash/map';
 import reverse from 'lodash/reverse';
 import find from 'lodash/find';
 import { createPortal } from 'react-dom';
-import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+import { FormattedMessage, defineMessages, useIntl } from 'react-intl';
 import { asyncConnect } from '@plone/volto/helpers/AsyncConnect';
 
 import FormattedDate from '@plone/volto/components/theme/FormattedDate/FormattedDate';
@@ -51,87 +51,39 @@ const messages = defineMessages({
 });
 
 /**
- * History class.
- * @class History
- * @extends Component
+
+ * @function History
+ * @param {Object} props
+ * @returns {JSX.Element} 
  */
-class History extends Component {
-  /**
-   * Property types.
-   * @property {Object} propTypes Property types.
-   * @static
-   */
-  static propTypes = {
-    getHistory: PropTypes.func.isRequired,
-    revertHistory: PropTypes.func.isRequired,
-    revertRequest: PropTypes.shape({
-      loaded: PropTypes.bool,
-      loading: PropTypes.bool,
-    }).isRequired,
-    pathname: PropTypes.string.isRequired,
-    entries: PropTypes.arrayOf(
-      PropTypes.shape({
-        transition_title: PropTypes.string,
-        type: PropTypes.string,
-        action: PropTypes.string,
-        state_title: PropTypes.string,
-        time: PropTypes.string,
-        comments: PropTypes.string,
-        actor: PropTypes.shape({ fullname: PropTypes.string }),
-      }),
-    ).isRequired,
-    title: PropTypes.string.isRequired,
-  };
+const History = (props) => {
+  const [isClient, setIsClient] = useState(false);
 
-  /**
-   * Constructor
-   * @method constructor
-   * @param {Object} props Component properties
-   * @constructs Workflow
-   */
-  constructor(props) {
-    super(props);
-    this.onRevert = this.onRevert.bind(this);
-    this.state = { isClient: false };
-  }
+  const {
+    getHistory: getHistoryAction,
+    revertHistory: revertHistoryAction,
+    revertRequest,
+    pathname,
+    entries: entriesProp,
+    title,
+    objectActions,
+    token,
+    staticContext,
+  } = props;
 
-  /**
-   * Component did mount
-   * @method componentDidMount
-   * @returns {undefined}
-   */
-  componentDidMount() {
-    this.props.getHistory(getBaseUrl(this.props.pathname));
-    this.setState({ isClient: true });
-  }
+  const intl = useIntl();
 
-  /**
-   * Component will receive props
-   * @method componentWillReceiveProps
-   * @param {Object} nextProps Next properties
-   * @returns {undefined}
-   */
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (this.props.revertRequest.loading && nextProps.revertRequest.loaded) {
-      this.props.getHistory(getBaseUrl(this.props.pathname));
-    }
-  }
+  const onRevert = useCallback(
+    (event, { value }) => {
+      revertHistoryAction(getBaseUrl(pathname), value);
+    },
+    [revertHistoryAction, pathname],
+  );
 
-  /**
-   * On revert
-   * @method onRevert
-   * @param {object} event Event object
-   * @param {number} value Value
-   * @returns {undefined}
-   */
-  onRevert(event, { value }) {
-    this.props.revertHistory(getBaseUrl(this.props.pathname), value);
-  }
-
-  processHistoryEntries = () => {
+  const processHistoryEntries = useCallback(() => {
     // Getting the history entries from the props
     // No clue why the reverse(concat()) is necessary
-    const entries = reverse(concat(this.props.entries));
+    const entries = reverse(concat(entriesProp));
     let title = entries.length > 0 ? entries[0].state_title : '';
     for (let x = 1; x < entries.length; x += 1) {
       entries[x].prev_state_title = title;
@@ -146,207 +98,233 @@ class History extends Component {
       current_version.is_current = true;
     }
     return entries;
-  };
+  }, [entriesProp]);
 
-  /**
-   * Render method.
-   * @method render
-   * @returns {string} Markup for the component.
-   */
-  render() {
-    const historyAction = find(this.props.objectActions, {
-      id: 'history',
-    });
-    const entries = this.processHistoryEntries();
+  useEffect(() => {
+    getHistoryAction(getBaseUrl(pathname));
+    setIsClient(true);
+  }, [getHistoryAction, pathname]);
 
-    const Container =
-      config.getComponent({ name: 'Container' }).component || SemanticContainer;
+  useEffect(() => {
+    if (revertRequest.loading && revertRequest.loaded) {
+      getHistoryAction(getBaseUrl(pathname));
+    }
+  }, [revertRequest.loading, revertRequest.loaded, getHistoryAction, pathname]);
 
-    return !historyAction ? (
-      <>
-        {this.props.token ? (
-          <Forbidden
-            pathname={this.props.pathname}
-            staticContext={this.props.staticContext}
+  const historyAction = find(objectActions, {
+    id: 'history',
+  });
+  const entries = processHistoryEntries();
+
+  const Container =
+    config.getComponent({ name: 'Container' }).component || SemanticContainer;
+
+  return !historyAction ? (
+    <>
+      {token ? (
+        <Forbidden pathname={pathname} staticContext={staticContext} />
+      ) : (
+        <Unauthorized pathname={pathname} staticContext={staticContext} />
+      )}
+    </>
+  ) : (
+    <Container id="page-history">
+      <Helmet title={intl.formatMessage(messages.history)} />
+      <Segment.Group raised>
+        <Segment className="primary">
+          <FormattedMessage
+            id="History of {title}"
+            defaultMessage="History of {title}"
+            values={{
+              title: <q>{title}</q>,
+            }}
           />
-        ) : (
-          <Unauthorized
-            pathname={this.props.pathname}
-            staticContext={this.props.staticContext}
+        </Segment>
+        <Segment secondary>
+          <FormattedMessage
+            id="You can view the history of your item below."
+            defaultMessage="You can view the history of your item below."
           />
-        )}
-      </>
-    ) : (
-      <Container id="page-history">
-        <Helmet title={this.props.intl.formatMessage(messages.history)} />
-        <Segment.Group raised>
-          <Segment className="primary">
-            <FormattedMessage
-              id="History of {title}"
-              defaultMessage="History of {title}"
-              values={{
-                title: <q>{this.props.title}</q>,
-              }}
-            />
-          </Segment>
-          <Segment secondary>
-            <FormattedMessage
-              id="You can view the history of your item below."
-              defaultMessage="You can view the history of your item below."
-            />
-          </Segment>
-          <Table selectable compact singleLine attached>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell width={1}>
-                  <FormattedMessage
-                    id="History Version Number"
-                    defaultMessage="#"
-                  />
-                </Table.HeaderCell>
-                <Table.HeaderCell width={4}>
-                  <FormattedMessage id="What" defaultMessage="What" />
-                </Table.HeaderCell>
-                <Table.HeaderCell width={4}>
-                  <FormattedMessage id="Who" defaultMessage="Who" />
-                </Table.HeaderCell>
-                <Table.HeaderCell width={4}>
-                  <FormattedMessage id="When" defaultMessage="When" />
-                </Table.HeaderCell>
-                <Table.HeaderCell width={4}>
-                  <FormattedMessage
-                    id="Change Note"
-                    defaultMessage="Change Note"
-                  />
-                </Table.HeaderCell>
-                <Table.HeaderCell />
+        </Segment>
+        <Table selectable compact singleLine attached>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell width={1}>
+                <FormattedMessage
+                  id="History Version Number"
+                  defaultMessage="#"
+                />
+              </Table.HeaderCell>
+              <Table.HeaderCell width={4}>
+                <FormattedMessage id="What" defaultMessage="What" />
+              </Table.HeaderCell>
+              <Table.HeaderCell width={4}>
+                <FormattedMessage id="Who" defaultMessage="Who" />
+              </Table.HeaderCell>
+              <Table.HeaderCell width={4}>
+                <FormattedMessage id="When" defaultMessage="When" />
+              </Table.HeaderCell>
+              <Table.HeaderCell width={4}>
+                <FormattedMessage
+                  id="Change Note"
+                  defaultMessage="Change Note"
+                />
+              </Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {map(entries, (entry) => (
+              <Table.Row key={entry.time}>
+                <Table.Cell>
+                  {('version' in entry && entry.version > 0 && (
+                    <Link
+                      className="item"
+                      to={`${getBaseUrl(pathname)}/diff?one=${
+                        entry.version - 1
+                      }&two=${entry.version}`}
+                    >
+                      {entry.version}
+                    </Link>
+                  )) || <span>{entry.version}</span>}
+                </Table.Cell>
+                <Table.Cell>
+                  {('version' in entry && entry.version > 0 && (
+                    <Link
+                      className="item"
+                      to={`${getBaseUrl(pathname)}/diff?one=${
+                        entry.version - 1
+                      }&two=${entry.version}`}
+                    >
+                      {entry.transition_title}
+                    </Link>
+                  )) || (
+                    <span>
+                      {entry.transition_title}
+                      {entry.type === 'workflow' &&
+                        ` (${
+                          entry.action ? `${entry.prev_state_title} → ` : ''
+                        }${entry.state_title})`}
+                    </span>
+                  )}
+                </Table.Cell>
+                <Table.Cell>{entry.actor.fullname}</Table.Cell>
+                <Table.Cell>
+                  <FormattedDate date={entry.time} />
+                </Table.Cell>
+                <Table.Cell>{entry.comments}</Table.Cell>
+                <Table.Cell>
+                  {entry.type === 'versioning' && (
+                    <Dropdown icon="ellipsis horizontal">
+                      <Dropdown.Menu className="left">
+                        {'version' in entry && entry.version > 0 && (
+                          <Link
+                            className="item"
+                            to={`${getBaseUrl(
+                              pathname,
+                            )}/diff?one=${entry.version - 1}&two=${
+                              entry.version
+                            }`}
+                          >
+                            <Icon name="copy" />{' '}
+                            <FormattedMessage
+                              id="View changes"
+                              defaultMessage="View changes"
+                            />
+                          </Link>
+                        )}
+                        {'version' in entry && (
+                          <Link
+                            className="item"
+                            to={`${getBaseUrl(pathname)}?version=${
+                              entry.version
+                            }`}
+                          >
+                            <Icon name="eye" />{' '}
+                            <FormattedMessage
+                              id="View this revision"
+                              defaultMessage="View this revision"
+                            />
+                          </Link>
+                        )}
+                        {'version' in entry &&
+                          entry.may_revert &&
+                          !entry.is_current && (
+                            <Dropdown.Item
+                              value={entry.version}
+                              onClick={onRevert}
+                            >
+                              <Icon name="undo" />{' '}
+                              <FormattedMessage
+                                id="Revert to this revision"
+                                defaultMessage="Revert to this revision"
+                              />
+                            </Dropdown.Item>
+                          )}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  )}
+                </Table.Cell>
               </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {map(entries, (entry) => (
-                <Table.Row key={entry.time}>
-                  <Table.Cell>
-                    {('version' in entry && entry.version > 0 && (
-                      <Link
-                        className="item"
-                        to={`${getBaseUrl(this.props.pathname)}/diff?one=${
-                          entry.version - 1
-                        }&two=${entry.version}`}
-                      >
-                        {entry.version}
-                      </Link>
-                    )) || <span>{entry.version}</span>}
-                  </Table.Cell>
-                  <Table.Cell>
-                    {('version' in entry && entry.version > 0 && (
-                      <Link
-                        className="item"
-                        to={`${getBaseUrl(this.props.pathname)}/diff?one=${
-                          entry.version - 1
-                        }&two=${entry.version}`}
-                      >
-                        {entry.transition_title}
-                      </Link>
-                    )) || (
-                      <span>
-                        {entry.transition_title}
-                        {entry.type === 'workflow' &&
-                          ` (${
-                            entry.action ? `${entry.prev_state_title} → ` : ''
-                          }${entry.state_title})`}
-                      </span>
-                    )}
-                  </Table.Cell>
-                  <Table.Cell>{entry.actor.fullname}</Table.Cell>
-                  <Table.Cell>
-                    <FormattedDate date={entry.time} />
-                  </Table.Cell>
-                  <Table.Cell>{entry.comments}</Table.Cell>
-                  <Table.Cell>
-                    {entry.type === 'versioning' && (
-                      <Dropdown icon="ellipsis horizontal">
-                        <Dropdown.Menu className="left">
-                          {'version' in entry && entry.version > 0 && (
-                            <Link
-                              className="item"
-                              to={`${getBaseUrl(
-                                this.props.pathname,
-                              )}/diff?one=${entry.version - 1}&two=${
-                                entry.version
-                              }`}
-                            >
-                              <Icon name="copy" />{' '}
-                              <FormattedMessage
-                                id="View changes"
-                                defaultMessage="View changes"
-                              />
-                            </Link>
-                          )}
-                          {'version' in entry && (
-                            <Link
-                              className="item"
-                              to={`${getBaseUrl(this.props.pathname)}?version=${
-                                entry.version
-                              }`}
-                            >
-                              <Icon name="eye" />{' '}
-                              <FormattedMessage
-                                id="View this revision"
-                                defaultMessage="View this revision"
-                              />
-                            </Link>
-                          )}
-                          {'version' in entry &&
-                            entry.may_revert &&
-                            !entry.is_current && (
-                              <Dropdown.Item
-                                value={entry.version}
-                                onClick={this.onRevert}
-                              >
-                                <Icon name="undo" />{' '}
-                                <FormattedMessage
-                                  id="Revert to this revision"
-                                  defaultMessage="Revert to this revision"
-                                />
-                              </Dropdown.Item>
-                            )}
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    )}
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </Segment.Group>
-        {this.state.isClient &&
-          createPortal(
-            <Toolbar
-              pathname={this.props.pathname}
-              hideDefaultViewButtons
-              inner={
-                <Link
-                  to={`${getBaseUrl(this.props.pathname)}`}
-                  className="item"
-                >
-                  <IconNext
-                    name={backSVG}
-                    className="contents circled"
-                    size="30px"
-                    title={this.props.intl.formatMessage(messages.back)}
-                  />
-                </Link>
-              }
-            />,
-            document.getElementById('toolbar'),
-          )}
-      </Container>
-    );
-  }
-}
+            ))}
+          </Table.Body>
+        </Table>
+      </Segment.Group>
+      {isClient &&
+        createPortal(
+          <Toolbar
+            pathname={pathname}
+            hideDefaultViewButtons
+            inner={
+              <Link to={`${getBaseUrl(pathname)}`} className="item">
+                <IconNext
+                  name={backSVG}
+                  className="contents circled"
+                  size="30px"
+                  title={intl.formatMessage(messages.back)}
+                />
+              </Link>
+            }
+          />,
+          document.getElementById('toolbar'),
+        )}
+    </Container>
+  );
+};
+
+History.propTypes = {
+  getHistory: PropTypes.func.isRequired,
+  revertHistory: PropTypes.func.isRequired,
+  revertRequest: PropTypes.shape({
+    loaded: PropTypes.bool,
+    loading: PropTypes.bool,
+  }).isRequired,
+  pathname: PropTypes.string.isRequired,
+  entries: PropTypes.arrayOf(
+    PropTypes.shape({
+      transition_title: PropTypes.string,
+      type: PropTypes.string,
+      action: PropTypes.string,
+      state_title: PropTypes.string,
+      time: PropTypes.string,
+      comments: PropTypes.string,
+      actor: PropTypes.shape({ fullname: PropTypes.string }),
+    }),
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+  objectActions: PropTypes.array,
+  token: PropTypes.string,
+  intl: PropTypes.object.isRequired,
+  staticContext: PropTypes.object,
+};
+
+History.defaultProps = {
+  objectActions: [],
+  token: null,
+  staticContext: null,
+};
 
 export default compose(
-  injectIntl,
   asyncConnect([
     {
       key: 'actions',
