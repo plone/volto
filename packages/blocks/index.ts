a304e2741db03200d2186cf6a423d309d345d79e
Eric BREHAULT <ebrehault@gmail.com>
2026-01-28 14:53:03 +0100

    Reapply "Ebrehault listing block (#7603)" (#7820)
    This reverts commit c19111ddc716b6c17c3ea87add93cde85cc829d1.

--- packages/blocks/index.ts
+++ packages/blocks/index.ts
@@ -1,9 +1,11 @@
 import type { ConfigType } from '@plone/registry';
+import type { ListingBlockFormData, LoaderUtilityArgs } from '@plone/types';
 
 import TitleBlockInfo from './Title';
 import TextBlockInfo from './Text';
 import ImageBlockInfo from './Image';
 import TeaserBlockInfo from './Teaser';
+import ListingBlockInfo from './Listing';
 
 export default function install(config: ConfigType) {
   // @ts-expect-error this is a quick hack for now
@@ -16,6 +18,25 @@ export default function install(config: ConfigType) {
   config.blocks.blocksConfig.slate = TextBlockInfo;
   config.blocks.blocksConfig.image = ImageBlockInfo;
   config.blocks.blocksConfig.teaser = TeaserBlockInfo;
+  config.blocks.blocksConfig.listing = ListingBlockInfo;
+
+  config.registerUtility({
+    name: 'ListingBlocksContentLoading',
+    type: 'rootContentSubRequest',
+    method: async (args: LoaderUtilityArgs) => {
+      const listingBlocks: { id: string; block: ListingBlockFormData }[] =
+        Object.entries(args.content.blocks)
+          .filter(([, block]) => block['@type'] === 'listing')
+          .map(([id, block]) => ({ id, block: block as ListingBlockFormData }));
+      for (let i = 0; i < listingBlocks.length; i++) {
+        const { id, block } = listingBlocks[i];
+        if (block.querystring) {
+          const results = await args.cli.querystringSearch(block.querystring);
+          args.content.blocks[id].items = results.data.items;
+        }
+      }
+    },
+  });
 
   return config;
 }
