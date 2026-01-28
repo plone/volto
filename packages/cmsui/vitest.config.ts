01d2d253195c6e10cb13740229169c6e1c553954
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-01-28 16:58:00 +0100

    Fix unit tests

--- packages/cmsui/vitest.config.ts
+++ packages/cmsui/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
-    exclude: ['**/node_modules/**', '**/lib/**'],
+    exclude: ['**/node_modules/**', '**/lib/**', '**/acceptance/**'],
     coverage: {
       exclude: [
         ...coverageConfigDefaults.exclude,
