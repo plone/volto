33a1f96a4b5f72ff05c5189dd2581edea322b660
Wagner Trezub <wagner.trezub@redturtle.it>
2026-02-04 10:25:32 +0100

    fix: removed cypress test for outline none

--- packages/volto/cypress/tests/core/blocks/blocks-slate.js
+++ packages/volto/cypress/tests/core/blocks/blocks-slate.js
@@ -16,10 +16,4 @@ describe('Slate Block Tests', () => {
     cy.navigate('/my-page/edit');
     cy.wait('@schema');
   });
-
-  it('No border in input', () => {
-    cy.get('.block-editor-slate [role=textbox]')
-      .click()
-      .should('have.css', 'outline', 'rgba(0, 0, 0, 0.87) none 0px');
-  });
 });
