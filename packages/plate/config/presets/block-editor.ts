b041cd23cf0d5a4eef91a322acf221853d0ff6d0
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-02-24 18:37:02 +0100

    More better splitting

--- packages/plate/config/presets/block-editor.ts
+++ packages/plate/config/presets/block-editor.ts
@@ -1,13 +1,8 @@
-import type { PlateConfig } from '@plone/types';
 import { BlockEditorKit } from '../../components/editor/block-editor-kit';
-import { BlockBaseEditorKit } from '../../components/editor/block-editor-base-kit';
 import { BlockFloatingToolbarButtons } from '../../components/ui/preset-block-floating-toolbar-buttons';
 import { setFloatingToolbarButtons } from '../../components/editor/plugins/floating-toolbar-kit';
 import { PlaywrightPlugin } from '@platejs/playwright';
 import type { PlateConfig } from '../../types';
-
-import { BlockEditorKit } from '../../components/editor/block-editor-kit';
-import { BlockFloatingToolbarButtons } from '../../components/ui/preset-block-floating-toolbar-buttons';
 
 setFloatingToolbarButtons(BlockFloatingToolbarButtons);
 
