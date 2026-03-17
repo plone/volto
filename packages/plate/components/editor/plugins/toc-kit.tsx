704c68ddd04d4b2b648990065a418b4ae070be77
Alexander Rybakov <rybakov@interaktiv.de>
2026-03-17 12:59:51 +0200

    added scrolling and highlighting when clicking headings

--- packages/plate/components/editor/plugins/toc-kit.tsx
+++ packages/plate/components/editor/plugins/toc-kit.tsx
@@ -7,7 +7,9 @@ import { queryHeadingWithTitle } from './toc-query-heading';
 export const TocKit = [
   TocPlugin.configure({
     options: {
-      isScroll: true,
+      // isScroll requires the EditorContainer to have overflow-y: auto/scroll
+      // Therefore, we have custom scrolling behaviour outside the EditorContainer
+      isScroll: false,
       topOffset: 80,
       queryHeading: queryHeadingWithTitle,
       blockWidth: {
