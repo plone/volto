553bf9902ef90670dc130a48b66cd61e2d133656
Cihan Andac <cihanandac@gmail.com>
2026-03-17 17:24:57 +0200

    [Seven] Add Maps block

--- packages/blocks/index.ts
+++ packages/blocks/index.ts
@@ -3,6 +3,7 @@ import type { ConfigType } from '@plone/registry';
 import ImageBlockInfo from './Image';
 import TeaserBlockInfo from './Teaser';
 import ListingBlockInfo from './Listing';
+import MapsBlockInfo from './Maps';
 
 export default function install(config: ConfigType) {
   // @ts-expect-error this is a quick hack for now
@@ -14,6 +15,7 @@ export default function install(config: ConfigType) {
   config.blocks.blocksConfig.image = ImageBlockInfo;
   config.blocks.blocksConfig.teaser = TeaserBlockInfo;
   config.blocks.blocksConfig.listing = ListingBlockInfo;
+  config.blocks.blocksConfig.maps = MapsBlockInfo;
 
   return config;
 }
