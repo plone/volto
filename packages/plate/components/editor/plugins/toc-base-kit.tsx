becef66b86b9c2f5b2d8343a9bf17e18f9c65003
Alexander Rybakov <rybakov@interaktiv.de>
2026-03-17 12:43:13 +0200

    include title block in table of contents

--- packages/plate/components/editor/plugins/toc-base-kit.tsx
+++ packages/plate/components/editor/plugins/toc-base-kit.tsx
@@ -2,10 +2,12 @@ import { BaseTocPlugin } from '@platejs/toc';
 
 import { TocElementStatic } from '../../ui/toc-node-static';
 import { BLOCK_WIDTH_VALUES } from './block-width-plugin';
+import { queryHeadingWithTitle } from './toc-query-heading';
 
 export const BaseTocKit = [
   BaseTocPlugin.configure({
     options: {
+      queryHeading: queryHeadingWithTitle,
       blockWidth: {
         defaultWidth: BLOCK_WIDTH_VALUES.default,
       },
--- packages/plate/components/editor/plugins/toc-kit.tsx
+++ packages/plate/components/editor/plugins/toc-kit.tsx
@@ -2,12 +2,14 @@ import { TocPlugin } from '@platejs/toc/react';
 
 import { TocElement } from '../../ui/toc-node';
 import { BLOCK_WIDTH_VALUES } from './block-width-plugin';
+import { queryHeadingWithTitle } from './toc-query-heading';
 
 export const TocKit = [
   TocPlugin.configure({
     options: {
-      // isScroll: true,
+      isScroll: true,
       topOffset: 80,
+      queryHeading: queryHeadingWithTitle,
       blockWidth: {
         defaultWidth: BLOCK_WIDTH_VALUES.default,
       },
