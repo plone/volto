963eb63260782390db80580ab0745f613a6aea15
Teodor <teodorvoicu87@gmail.com>
2026-03-19 14:52:04 +0200

    remove publicURL and internalApiPath

--- packages/helpers/src/isInternalURL.ts
+++ packages/helpers/src/isInternalURL.ts
@@ -7,16 +7,7 @@ export function isInternalURL(url?: string): boolean {
   if (!url) return false;
 
   const settings = config.settings ?? ({} as Record<string, string>);
-  const prefixes = [
-    settings.publicURL,
-    settings.internalApiPath,
-    settings.apiPath,
-  ];
+  const prefixes = [settings.apiPath, '/', '.', '#'];
 
-  return (
-    prefixes.some((prefix) => prefix && url.startsWith(prefix as string)) ||
-    url.startsWith('/') ||
-    url.startsWith('.') ||
-    url.startsWith('#')
-  );
+  return prefixes.some((prefix) => prefix && url.startsWith(prefix as string));
 }
