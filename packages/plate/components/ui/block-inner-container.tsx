b0ab847e08beb6d59a205da90d2fde380b7c373a
Victor Fernandez de Alba <sneridagh@gmail.com>
2026-03-27 15:19:24 +0100

    Fixed blockinnercontainer component, no longer getting parent classes, not needed

--- packages/plate/components/ui/block-inner-container.tsx
+++ packages/plate/components/ui/block-inner-container.tsx
@@ -1,23 +1,7 @@
 import type { PropsWithChildren } from 'react';
 
-import { cn } from '../../lib/utils';
+type BlockInnerContainerProps = PropsWithChildren;
 
-type BlockInnerContainerProps = PropsWithChildren<{
-  className?: string;
-}>;
-
-export function BlockInnerContainer({
-  children,
-  className,
-}: BlockInnerContainerProps) {
-  return (
-    <div
-      className={cn(
-        'block-inner-container mx-auto grid w-full max-w-(--block-width) justify-items-start',
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
+export function BlockInnerContainer({ children }: BlockInnerContainerProps) {
+  return <div className="block-inner-container">{children}</div>;
 }
