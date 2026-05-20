2481490368e8356c5fe2f8f608610032306b393d
Alexander Rybakov <rybakov@interaktiv.de>
2026-05-20 10:33:28 +0200

    use native form behavior for search widget

--- packages/layout/slots/HeaderTools/SearchWidget.tsx
+++ packages/layout/slots/HeaderTools/SearchWidget.tsx
@@ -1,7 +1,5 @@
-import { type FormEvent, useState } from 'react';
 import { Form, Input, Label, TextField } from 'react-aria-components';
 import type { SlotComponentProps } from '../SlotRenderer';
-import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@plone/components';
 import Search from '@plone/components/icons/search.svg?react';
@@ -10,38 +8,26 @@ import clsx from 'clsx';
 
 const SearchWidget = (props: SlotComponentProps) => {
   const { location } = props;
-  const [searchQuery, setSearchQuery] = useState('');
 
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const pathname = location.pathname;
-
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    const path =
-      pathname?.length > 0 ? `&path=${encodeURIComponent(pathname)}` : '';
-
-    navigate(
-      `/search?SearchableText=${encodeURIComponent(searchQuery)}${path}`,
-    );
-
-    setSearchQuery('');
-    e.preventDefault();
-  };
+  const hasPath = pathname?.length > 0 && pathname !== '/';
 
   return (
     <Form
       className={clsx(styles['searchwidget'], 'searchwidget')}
-      onSubmit={onSubmit}
+      action="/search"
+      method="get"
     >
       <TextField name="search" type="text">
         <Label className="sr-only">
           {t('layout.slots.tools.searchWidget.searchSite')}
         </Label>
         <Input
+          name="SearchableText"
           placeholder={t('layout.slots.tools.searchWidget.searchSite')}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          value={searchQuery}
         />
+        {hasPath && <input type="hidden" name="path" value={pathname} />}
       </TextField>
       <Button type="submit">
         <Search />
