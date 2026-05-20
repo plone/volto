e76a8dbf79aab5cefbade4ad7c268458de7d75a8
nileshgulia1 <nileshgulia@gmail.com>
2026-05-20 16:46:43 +0200

    fix(Subpath): dispatch async actions for controlpanels on /controlpanels

--- packages/volto/src/helpers/Site/index.js
+++ packages/volto/src/helpers/Site/index.js
@@ -1,5 +1,12 @@
 import { GET_SITE } from '@plone/volto/constants/ActionTypes';
 import { getSite } from '@plone/volto/actions/site/site';
+import {
+  listControlpanels,
+  getSystemInformation,
+} from '@plone/volto/actions/controlpanels/controlpanels';
+import config from '@plone/volto/registry';
+
+const prefixPath = config.settings.subpathPrefix || '';
 
 const getSiteAsyncPropExtender = {
   path: '/',
@@ -18,4 +25,44 @@ const getSiteAsyncPropExtender = {
   },
 };
 
-export { getSiteAsyncPropExtender };
+const getControlpanelAsyncPropExtender = {
+  path: `${prefixPath}/controlpanel`,
+  extend: (dispatchActions) => {
+    if (
+      dispatchActions.filter(
+        (asyncAction) => asyncAction.key === 'controlpanels',
+      ).length === 0
+    ) {
+      dispatchActions.push({
+        key: 'controlpanels',
+        promise: ({ location, store: { dispatch } }) =>
+          __SERVER__ && dispatch(listControlpanels()),
+      });
+    }
+    return dispatchActions;
+  },
+};
+
+const SystemInfoAsyncPropExtender = {
+  path: `${prefixPath}/controlpanel`,
+  extend: (dispatchActions) => {
+    if (
+      dispatchActions.filter(
+        (asyncAction) => asyncAction.key === 'systemInformation',
+      ).length === 0
+    ) {
+      dispatchActions.push({
+        key: 'systemInformation',
+        promise: ({ location, store: { dispatch } }) =>
+          __SERVER__ && dispatch(getSystemInformation()),
+      });
+    }
+    return dispatchActions;
+  },
+};
+
+export {
+  getSiteAsyncPropExtender,
+  SystemInfoAsyncPropExtender,
+  getControlpanelAsyncPropExtender,
+};
