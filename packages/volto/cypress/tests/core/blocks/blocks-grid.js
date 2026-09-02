a6cdcbce59b10d2d2edad7ecd88dbcbdeaaae5b8
nileshgulia1 <nileshgulia@gmail.com>
2026-09-02 19:32:09 +0530

    chore: remove blocks-grid.js cypress test that requires container multi-selection feature

--- packages/volto/cypress/tests/core/blocks/blocks-grid.js
+++ packages/volto/cypress/tests/core/blocks/blocks-grid.js
@@ -114,53 +114,5 @@ context('Blocks Acceptance Tests', () => {
         .should('have.attr', 'href')
         .and('include', 'https://google.com');
     });
-
-    it('As editor I can copy/paste a block inside a Grid', function () {
-      cy.intercept('PATCH', '/**/document').as('edit');
-      // GIVEN: a grid with a slate text block in column 0 and a slate in column 1
-      cy.getSlate().click();
-      cy.addNewBlock('grid');
-
-      cy.findByText('2 columns').click();
-
-      // Add a slate in the first column (position 0)
-      cy.get('button[aria-label="Add block in position 0"]').click();
-      cy.get('.blocks-chooser [aria-label="Unfold Text blocks"]').click();
-      cy.wait(200);
-      cy.get('.blocks-chooser .text .button.slate').click();
-      cy.getSlateEditorSelectorAndType(
-        '.block.gridBlock.selected .slate-editor [contenteditable=true]',
-        'Copy me from column 0',
-      );
-
-      // Add a slate in the second column (position 1)
-      cy.get('button[aria-label="Add block in position 1"]').click();
-      cy.get('.blocks-chooser [aria-label="Unfold Text blocks"]').click();
-      cy.wait(200);
-      cy.get('.blocks-chooser .text .button.slate').click();
-
-      // WHEN: I multi-select the inner blocks with Shift and copy them
-      cy.scrollTo('top');
-      cy.get('.block.gridBlock .block-editor-slate').first().click();
-      cy.get('.block.gridBlock .block-editor-slate')
-        .last()
-        .click({ shiftKey: true });
-      cy.get('#toolbar-copy-blocks').should('be.visible');
-      cy.get('#toolbar-copy-blocks').click();
-
-      // AND: I select the inner slate of column 1 and paste after it
-      cy.get('.block.gridBlock .block-editor-slate').last().click();
-      cy.get('#toolbar-paste-blocks').should('be.visible');
-      cy.get('#toolbar-paste-blocks').click();
-
-      cy.get('#toolbar-save').click();
-      cy.wait('@edit');
-      cy.wait('@content');
-
-      // THEN: the grid contains a pasted copy of the column 0 slate
-      cy.get('.block.gridBlock')
-        .findAllByText('Copy me from column 0')
-        .should('have.length', 2);
-    });
   });
 });
